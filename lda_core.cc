a04d824f0de808cbcce9607dabaf0c3c560dee13
John Langford <jl@hunch.net>
2011-06-26 18:20:48 -0400

    added Paul Mineiros LDA speedups

--- lda_core.cc
+++ lda_core.cc
@@ -9,8 +9,6 @@ embodied in the content of this file are licensed under the BSD
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-#include <boost/math/special_functions/digamma.hpp>
-#include <boost/math/special_functions/gamma.hpp>
 #include "parse_example.h"
 #include "constant.h"
 #include "sparse_dense.h"
@@ -21,14 +19,271 @@ embodied in the content of this file are licensed under the BSD
 #include "simple_label.h"
 #include "delay_ring.h"
 
+#define MINEIRO_SPECIAL
+#ifdef MINEIRO_SPECIAL
+
+namespace {
+
+inline float 
+fastlog2 (float x)
+{
+  union { float f; uint32_t i; } vx = { x };
+  union { uint32_t i; float f; } mx = { (vx.i & 0x007FFFFF) | (0x7e << 23) };
+  float y = vx.i;
+  y *= 1.0f / (1 << 23);
+
+  return 
+    y - 124.22544637f - 1.498030302f * mx.f - 1.72587999f / (0.3520887068f + mx.f);
+}
+
+inline float
+fastlog (float x)
+{
+  return 0.69314718f * fastlog2 (x);
+}
+
+inline float
+fastpow2 (float p)
+{
+  union { float f; uint32_t i; } vp = { p };
+  int sign = (vp.i >> 31);
+  int w = p;
+  float z = p - w + sign;
+  union { uint32_t i; float f; } v = { (1 << 23) * (p + 121.2740838f + 27.7280233f / (4.84252568f - z) - 1.49012907f * z) };
+
+  return v.f;
+}
+
+inline float
+fastexp (float p)
+{
+  return fastpow2 (1.442695040f * p);
+}
+
+inline float
+fastpow (float x,
+         float p)
+{
+  return fastpow2 (p * fastlog2 (x));
+}
+
+inline float
+fastlgamma (float x)
+{
+  float logterm = fastlog (x * (1.0f + x) * (2.0f + x));
+  float xp3 = 3.0f + x;
+
+  return 
+    -2.081061466f - x + 0.0833333f / xp3 - logterm + (2.5f + x) * fastlog (xp3);
+}
+
+inline float
+fastdigamma (float x)
+{
+  float twopx = 2.0f + x;
+  float logterm = fastlog (twopx);
+
+  return - (1.0f + 2.0f * x) / (x * (1.0f + x)) 
+         - (13.0f + 6.0f * x) / (12.0f * twopx * twopx) 
+         + logterm;
+}
+
+#define log fastlog
+#define exp fastexp
+#define powf fastpow
+#define mydigamma fastdigamma
+#define mylgamma fastlgamma
+
+#ifdef __SSE__
+typedef float v4sf __attribute__ ((__vector_size__ (16)));
+typedef int v4si __attribute__ ((__vector_size__ (16)));
+typedef float v4sf_aligned __attribute__ ((__vector_size__ (16))) __attribute__
+((aligned (16)));
+#define v4sf_dup_literal(x) ((v4sf) { x, x, x, x })
+#define v4si_dup_literal(x) ((v4si) { x, x, x, x })
+#define v4sf_index(x, i) (__builtin_ia32_vec_ext_v4sf (x, i))
+#define v4sf_from_v4si __builtin_ia32_cvtdq2ps
+#define v4si_from_v4sf __builtin_ia32_cvttps2dq
+
+inline v4sf
+vfastpow2 (const v4sf p)
+{
+  union { v4sf f; v4si i; } vp = { p };
+  v4si sign = __builtin_ia32_psrldi128 (vp.i, 31);
+  v4si w = v4si_from_v4sf (p);
+  v4sf z = p - v4sf_from_v4si (w) + v4sf_from_v4si (sign);
+  const v4sf c_121_2740838 = v4sf_dup_literal (121.2740838f);
+  const v4sf c_27_7280233 = v4sf_dup_literal (27.7280233f);
+  const v4sf c_4_84252568 = v4sf_dup_literal (4.84252568f);
+  const v4sf c_1_49012907 = v4sf_dup_literal (1.49012907f);
+  union { v4si i; v4sf f; } v = {
+    v4si_from_v4sf (
+      v4sf_dup_literal ((1 << 23)) *
+      (p + c_121_2740838 + c_27_7280233 / (c_4_84252568 - z) - c_1_49012907 * z)
+    )
+  };
+
+  return v.f;
+}
+
+inline v4sf
+vfastexp (const v4sf p)
+{
+  const v4sf c_invlog_2 = v4sf_dup_literal (1.442695040f);
+  return vfastpow2 (c_invlog_2 * p);
+}
+
+inline v4sf
+vfastlog2 (v4sf x)
+{
+  union { v4sf f; v4si i; } vx = { x };
+  union { v4si i; v4sf f; } mx = {
+    (vx.i & v4si_dup_literal (0x007FFFFF)) | v4si_dup_literal ((0x7e << 23))
+  };
+  v4sf y = v4sf_from_v4si (vx.i);
+  y *= v4sf_dup_literal ((1.0f / (1 << 23)));
+
+  const v4sf c_124_22544637 = v4sf_dup_literal (124.22544637f);
+  const v4sf c_1_498030302 = v4sf_dup_literal (1.498030302f);
+  const v4sf c_1_725877999 = v4sf_dup_literal (1.72587999f);
+  const v4sf c_0_3520087068 = v4sf_dup_literal (0.3520887068f);
+
+  return y - c_124_22544637 - c_1_498030302 * mx.f - c_1_725877999 / (c_0_3520087068 + mx.f);
+}
+
+inline v4sf
+vfastlog (v4sf x)
+{ 
+  const v4sf c_0_69314718 = v4sf_dup_literal (0.69314718f);
+
+  return c_0_69314718 * vfastlog2 (x);
+}
+
+inline v4sf
+vfastdigamma (v4sf x)
+{
+  const v4sf c_1_0 = v4sf_dup_literal (1.0f);
+  const v4sf c_2_0 = v4sf_dup_literal (2.0f);
+  const v4sf c_6_0 = v4sf_dup_literal (6.0f);
+  const v4sf c_12_0 = v4sf_dup_literal (12.0f);
+  const v4sf c_13_0 = v4sf_dup_literal (13.0f);
+  v4sf twopx = c_2_0 + x;
+  v4sf logterm = vfastlog (twopx);
+
+  return - (c_1_0 + c_2_0 * x) / (x * (c_1_0 + x))
+         - (c_13_0 + c_6_0 * x) / (c_12_0 * twopx * twopx)
+         + logterm;
+}
+
+void
+vexpdigammify (float* gamma)
+{
+  unsigned int n = global.lda;
+  float extra_sum = 0.0f;
+  v4sf sum = v4sf_dup_literal (0.0f);
+  size_t i;
+
+  for (i = 0; i < n && ((uintptr_t) (gamma + i)) % 16 > 0; ++i)
+    { 
+      extra_sum += gamma[i];
+      gamma[i] = fastdigamma (gamma[i]);
+    }
+
+  for (; i + 4 < n; i += 4)
+    { 
+      v4sf arg = * (v4sf_aligned*) (gamma + i);
+      sum += arg;
+      arg = vfastdigamma (arg);
+      * (v4sf_aligned*) (gamma + i) = arg;
+    }
+
+  for (; i < n; ++i)
+    { 
+      extra_sum += gamma[i];
+      gamma[i] = fastdigamma (gamma[i]);
+    } 
+
+  extra_sum += v4sf_index (sum, 0) + v4sf_index (sum, 1) +
+               v4sf_index (sum, 2) + v4sf_index (sum, 3);
+  extra_sum = fastdigamma (extra_sum);
+  sum = v4sf_dup_literal (extra_sum);
+
+  for (i = 0; i < n && ((uintptr_t) (gamma + i)) % 16 > 0; ++i)
+    { 
+      gamma[i] = fmaxf (1e-10f, fastexp (gamma[i] - v4sf_index (sum, 0)));
+    }
+
+  for (; i + 4 < n; i += 4)
+    { 
+      v4sf arg = * (v4sf_aligned*) (gamma + i);
+      arg -= sum;
+      arg = vfastexp (arg);
+      arg = __builtin_ia32_maxps (v4sf_dup_literal (1e-10f), arg);
+      * (v4sf_aligned*) (gamma + i) = arg;
+    }
+
+  for (; i < n; ++i)
+    {
+      gamma[i] = fmaxf (1e-10f, fastexp (gamma[i] - v4sf_index (sum, 0)));
+    } 
+}
+
+void 
+vexpdigammify_2(float*       gamma, 
+                const float* norm)
+{
+  size_t n = global.lda;
+  size_t i;
+
+  for (i = 0; i < n && ((uintptr_t) (gamma + i)) % 16 > 0; ++i)
+    { 
+      gamma[i] = fmaxf (1e-10f, fastexp (fastdigamma (gamma[i]) - norm[i]));
+    }
+
+  for (; i + 4 < n; i += 4)
+    {
+      v4sf arg = * (v4sf_aligned*) (gamma + i);
+      arg = vfastdigamma (arg);
+      v4sf vnorm = (v4sf) { norm[i], norm[i+1], norm[i+2], norm[i+3] };
+      arg -= vnorm;
+      arg = vfastexp (arg);
+      arg = __builtin_ia32_maxps (v4sf_dup_literal (1e-10f), arg);
+      * (v4sf_aligned*) (gamma + i) = arg;
+    }
+
+  for (; i < n; ++i)
+    {
+      gamma[i] = fmaxf (1e-10f, fastexp (fastdigamma (gamma[i]) - norm[i]));
+    }
+}
+
+#define myexpdigammify vexpdigammify
+#define myexpdigammify_2 vexpdigammify_2
+
+#else
+#warning "lda IS NOT using sse instructions"
+#define myexpdigammify expdigammify
+#define myexpdigammify_2 expdigammify_2
+
+#endif // __SSE__
+
+} // end anonymous namespace
+
+#else 
+
+#include <boost/math/special_functions/digamma.hpp>
+#include <boost/math/special_functions/gamma.hpp>
+
 using namespace boost::math::policies;
 
-size_t max_w = 0;
+#define mydigamma boost::math::digamma
+#define mylgamma boost::math::lgamma
+#define myexpdigammify expdigammify
+#define myexpdigammify_2 expdigammify_2
 
-float mydigamma(float x) 
-{
-  return boost::math::digamma(x);
-}
+#endif // MINEIRO_SPECIAL
+
+size_t max_w = 0;
 
 float decayfunc(float t, float old_t, float power_t) {
   float result = 1;
@@ -101,13 +356,13 @@ float theta_kl(float* gamma)
     gammasum += gamma[k];
   }
   float digammasum = mydigamma(gammasum);
-  gammasum = boost::math::lgamma(gammasum);
-  float kl = -global.lda*boost::math::lgamma(global.lda_alpha);
-  kl += boost::math::lgamma(global.lda_alpha*global.lda) - gammasum;
+  gammasum = mylgamma(gammasum);
+  float kl = -global.lda*mylgamma(global.lda_alpha);
+  kl += mylgamma(global.lda_alpha*global.lda) - gammasum;
   for (size_t k = 0; k < global.lda; k++) {
     Elogtheta[k] -= digammasum;
     kl += (global.lda_alpha - gamma[k]) * Elogtheta[k];
-    kl += boost::math::lgamma(gamma[k]);
+    kl += mylgamma(gamma[k]);
   }
 
   return kl;
@@ -147,11 +402,10 @@ float lda_loop(float* v,weight* weights,example* ec, float power_t)
   do
     {
       memcpy(v,new_gamma,sizeof(float)*global.lda);
-      expdigammify(v);
+      myexpdigammify(v);
 
       memcpy(old_gamma,new_gamma,sizeof(float)*global.lda);
-      for (size_t k = 0; k<global.lda; k++)
-	new_gamma[k] = 0;
+      memset(new_gamma,0,sizeof(float)*global.lda);
 
       score = 0;
       size_t word_count = 0;
@@ -265,6 +519,7 @@ void start_lda(gd_thread_params t)
   example* ec = NULL;
 
   double total_lambda[global.lda];
+
   for (size_t k = 0; k < global.lda; k++)
     total_lambda[k] = 0;
   size_t stride = global.stride;
@@ -313,7 +568,7 @@ void start_lda(gd_thread_params t)
       minuseta = 1.0 - eta;
       eta *= global.lda_D / batch_size;
       push(decay_levels, decay_levels.last() + log(minuseta));
-      
+
       float digammas[global.lda];
       float additional = (float)(global.length()) * global.lda_rho;
       for (size_t i = 0; i<global.lda; i++) {
@@ -336,7 +591,7 @@ void start_lda(gd_thread_params t)
 	      weights_for_w[k] *= decay;
 	      u_for_w[k] = weights_for_w[k] + global.lda_rho;
 	    }
-	  expdigammify_2(u_for_w, digammas);
+	  myexpdigammify_2(u_for_w, digammas);
 	}
 
       float v[batch_size*global.lda];
