64a616f74bd93301cdb56f65a0bd88884c208602
John Langford <jl@hunch.net>
2011-08-13 19:34:19 -0400

    MAP_ANON hack II for macs

--- parser.cc
+++ parser.cc
@@ -253,6 +253,12 @@ bool member(v_array<size_t> ids, size_t id)
   return false;
 }
 
+//For macs
+#ifndef MAP_ANONYMOUS
+# define MAP_ANONYMOUS MAP_ANON
+#endif
+
+
 void parse_source_args(po::variables_map& vm, parser* par, bool quiet, size_t passes)
 {
   par->input->current = 0;
@@ -320,10 +326,6 @@ void parse_source_args(po::variables_map& vm, parser* par, bool quiet, size_t pa
 	{
 	  // weights will be shared across processes, accessible to children
 
-	  //For macs
-#ifndef MAP_ANONYMOUS
-# define MAP_ANONYMOUS MAP_ANON
-#endif
 	  float* shared_weights = 
 	    (float*)mmap(0,global.stride * global.length() * sizeof(float), 
 			 PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
