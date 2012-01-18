e729c8264ac477231a6ffc0a0783e8e37871116d
Hal Daume III <me@hal3.name>
2012-01-18 14:00:37 -0500

    merge

--- vowpalwabbit/sequence.h
+++ vowpalwabbit/sequence.h
@@ -3,7 +3,6 @@
 
 #include "oaa.h"
 
-<<<<<<< HEAD
 typedef uint32_t* history;  // histories have the most recent prediction at the END
 
 struct history_item {
@@ -13,11 +12,7 @@ struct history_item {
   bool     same;
 };
 
-void parse_sequence_args(po::variables_map& vm, example* (**gf)(), void (**rf)(example*));
-void drive_sequence();
-
-=======
 void parse_sequence_args(po::variables_map& vm);
->>>>>>> 8e9e53ef8f1f544d9c1078af5483a705b030bfdb
+void drive_sequence();
 
 #endif
