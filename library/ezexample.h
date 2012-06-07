ae36b228975e0a34faadc8fd75e3cde5c8f54253
Hal Daume III <me@hal3.name>
2012-06-07 09:37:48 -0400

    added ezexample

---
+++ library/ezexample.h
@@ -0,0 +1,97 @@
+#ifndef EZEXAMPLE_H
+#define EZEXAMPLE_H
+
+#include <stdio.h>
+#include "/home/hal/projects/vowpal_wabbit/vowpalwabbit/vw.h"
+
+using namespace std;
+typedef uint32_t fid;
+
+class ezexample {
+private:
+  vw*vw_ref;
+  vector<VW::feature_space> dat;
+  vector<fid> past_seeds;
+  fid current_seed;
+  vector<feature>*current_ns;
+  char str[2];
+  bool pass_empty;
+
+public:
+  ezexample(vw*this_vw, bool pe) { 
+    vw_ref = this_vw;
+    current_seed = 0;
+    current_ns = NULL;
+    str[0] = '|'; str[1] = 0;
+    pass_empty = pe;
+  }
+
+  ezexample(vw*this_vw) { ezexample(this_vw, false); }
+
+  void addns(char c) {
+    current_ns = new vector<feature>();
+    str[0] = c;
+    dat.push_back( VW::feature_space(c, *current_ns) );
+    past_seeds.push_back(current_seed);
+    current_seed = VW::hash_space(*vw_ref, str);
+    //feature f = { 1.0, 13 };
+    //current_ns->push_back(f);
+  }
+
+  void remns() { 
+    current_seed = past_seeds.back();
+    past_seeds.pop_back();
+    dat.pop_back();
+    current_ns = &(dat.back().second);
+  }
+
+  inline fid hash(string fstr) { 
+    return VW::hash_feature(*vw_ref, fstr, current_seed); 
+  }
+  inline fid hash(const char* fstr) { return hash(string(fstr)); }
+
+  inline fid hash(char c, string fstr) { 
+    str[0] = c;
+    return VW::hash_feature(*vw_ref, fstr, VW::hash_space(*vw_ref, str)); 
+  }
+
+  inline fid addf(fid fint, float val) {
+    if (!current_ns) return 0;
+    feature f = { val, fint };
+    current_ns->push_back(f);
+    return fint;
+  }
+  inline fid addf(fid    fint           ) { return addf(fint      , 1.0); }
+  inline fid addf(string fstr, float val) { return addf(hash(fstr), val); }
+  inline fid addf(string fstr           ) { return addf(hash(fstr), 1.0); }
+
+  float predict() {
+    static example* empty_example = VW::read_example(*vw_ref, str);
+    example *ec = VW::import_example(*vw_ref, dat);
+    vw_ref->learn(vw_ref, ec);
+    if (pass_empty)
+      vw_ref->learn(vw_ref, empty_example);
+    float pred = ec->final_prediction;
+    VW::finish_example(*vw_ref, ec);
+    return pred;
+  }
+
+
+  inline float     operator()() { return predict(); }
+
+  inline ezexample operator()(fid         fint           ) { addf(fint, 1.0); return *this; }
+  inline ezexample operator()(string      fstr           ) { addf(fstr, 1.0); return *this; }
+  inline ezexample operator()(const char* fstr           ) { addf(fstr, 1.0); return *this; }
+  inline ezexample operator()(fid         fint, float val) { addf(fint, val); return *this; }
+  inline ezexample operator()(string      fstr, float val) { addf(fstr, val); return *this; }
+  inline ezexample operator()(const char* fstr, float val) { addf(fstr, val); return *this; }
+
+  inline ezexample operator()(const char c               ) { addns(c); return *this; }
+
+  inline ezexample operator--() { remns(); return *this; }
+};
+
+
+
+
+#endif
---
+++ library/temp2.cc
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "../vowpalwabbit/vw.h"
+//#include "ezexample.h"
+
+using namespace std;
+
+inline feature vw_feature_from_string(vw& v, string fstr, unsigned long seed, float val)
+{
+  uint32_t foo = VW::hash_feature(v, fstr, seed);
+  feature f = { val, foo};
+  return f;
+}
+
+int main(int argc, char *argv[])
+{
+  // INITIALIZE WITH WHATEVER YOU WOULD PUT ON THE VW COMMAND LINE -- THIS READS IN A MODEL FROM train.w
+  vw vw = VW::initialize("--hash all -q st --noconstant -i train.w");
+
+  // JOHN'S CLUNKY INTERFACE USING STRINGS
+  example *vec1 = VW::read_example(vw, (char*)"|s p^the_man w^the w^man |t p^un_homme w^un w^homme");
+  vw.learn(&vw, vec1);
+  cerr << "p1 = " << vec1->final_prediction << endl;
+  VW::finish_example(vw, vec1);
+
+  example *vec2 = VW::read_example(vw, (char*)"|s p^the_man w^the w^man |t p^le_homme w^le w^homme");
+  vw.learn(&vw, vec2);
+  cerr << "p2 = " << vec2->final_prediction << endl;
+  VW::finish_example(vw, vec2);
+
+  // JOHN'S CLUNKY INTERFACE USING VECTORS
+  vector< VW::feature_space > ec_info;
+  vector<feature> s_features, t_features;
+  uint32_t s_hash = VW::hash_space(vw, "s");
+  uint32_t t_hash = VW::hash_space(vw, "t");
+  s_features.push_back( vw_feature_from_string(vw, "p^the_man", s_hash, 1.0) );
+  s_features.push_back( vw_feature_from_string(vw, "w^the", s_hash, 1.0) );
+  s_features.push_back( vw_feature_from_string(vw, "w^man", s_hash, 1.0) );
+  t_features.push_back( vw_feature_from_string(vw, "p^le_homme", t_hash, 1.0) );
+  t_features.push_back( vw_feature_from_string(vw, "w^le", t_hash, 1.0) );
+  t_features.push_back( vw_feature_from_string(vw, "w^homme", t_hash, 1.0) );
+  ec_info.push_back( VW::feature_space('s', s_features) );
+  ec_info.push_back( VW::feature_space('t', t_features) );
+  example* vec3 = VW::import_example(vw, ec_info);
+  vw.learn(&vw, vec3);
+  cerr << "p3 = " << vec3->final_prediction << endl;
+  VW::finish_example(vw, vec3);
+
+  /*
+  // HAL'S SPIFFY INTERFACE USING C++ CRAZINESS
+  ezexample et(&vw, false);
+  et('s')
+    ("p^the_man")
+    ("w^the")
+    ("w^man", 1.0)
+    ('t')
+    ("p^le_homme")
+    (et.hash("w^le"))
+    (et.hash("w^homme"), 1.0);
+  cerr << "p4 = " << et() << endl;
+  --et;  // remove the most recent namespace
+  cerr << "p5 = " << et('t')("p^un_homme")("w^un")("w^homme")() << endl;
+  */
+
+  // AND FINISH UP
+  vw.finish(&vw);
+}
