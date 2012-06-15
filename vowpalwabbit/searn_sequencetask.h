7faa89de360c3129569ccfba62fbf59688107bd5
Hal Daume III <me@hal3.name>
2012-06-15 15:03:44 -0400

    added ability to distinguish train from test

--- vowpalwabbit/searn_sequencetask.h
+++ vowpalwabbit/searn_sequencetask.h
@@ -13,13 +13,14 @@ namespace SequenceTask {
   state  copy(state);
   void   finish(state);
   void   start_state_multiline(example**, size_t, state*);
+  bool   is_test_example(example**, size_t);
   void   cs_example(vw&, state, example*&, bool);
   bool   initialize(vw&,std::vector<std::string>&, po::variables_map&);
   size_t hash(state);
   bool   equivalent(state, state);
   std::string to_string(state, bool, std::vector<action>);
-  void cs_ldf_example(vw&, state, action, example*&, bool);
-  bool allowed(state, action);
+  void   cs_ldf_example(vw&, state, action, example*&, bool);
+  bool   allowed(state, action);
 }
 
 #endif
