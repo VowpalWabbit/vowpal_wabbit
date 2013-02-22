932f2e48060ccb483a1793c944c887d2fce10ee5
John Langford <jl@hunch.net>
2013-02-22 16:35:13 -0500

    example of multiple vw models

--- library/library_example.cc
+++ library/library_example.cc
@@ -13,31 +13,39 @@ inline feature vw_feature_from_string(vw& v, string fstr, unsigned long seed, fl
 
 int main(int argc, char *argv[])
 {
-  vw vw = VW::initialize("--hash all -q st --noconstant -i train.w");
+  vw model = VW::initialize("--hash all -q st --noconstant -i train.w -f train2.vw");
 
-  example *vec2 = VW::read_example(vw, (char*)"|s p^the_man w^the w^man |t p^un_homme w^un w^homme");
-  vw.learn(&vw, vec2);
+  example *vec2 = VW::read_example(model, (char*)"|s p^the_man w^the w^man |t p^un_homme w^un w^homme");
+  model.learn(&model, vec2);
   cerr << "p2 = " << vec2->final_prediction << endl;
+  VW::finish_example(model, vec2);
 
   vector< VW::feature_space > ec_info;
   vector<feature> s_features, t_features;
-  uint32_t s_hash = VW::hash_space(vw, "s");
-  uint32_t t_hash = VW::hash_space(vw, "t");
-  s_features.push_back( vw_feature_from_string(vw, "p^the_man", s_hash, 1.0) );
-  s_features.push_back( vw_feature_from_string(vw, "w^the", s_hash, 1.0) );
-  s_features.push_back( vw_feature_from_string(vw, "w^man", s_hash, 1.0) );
-  t_features.push_back( vw_feature_from_string(vw, "p^le_homme", t_hash, 1.0) );
-  t_features.push_back( vw_feature_from_string(vw, "w^le", t_hash, 1.0) );
-  t_features.push_back( vw_feature_from_string(vw, "w^homme", t_hash, 1.0) );
+  uint32_t s_hash = VW::hash_space(model, "s");
+  uint32_t t_hash = VW::hash_space(model, "t");
+  s_features.push_back( vw_feature_from_string(model, "p^the_man", s_hash, 1.0) );
+  s_features.push_back( vw_feature_from_string(model, "w^the", s_hash, 1.0) );
+  s_features.push_back( vw_feature_from_string(model, "w^man", s_hash, 1.0) );
+  t_features.push_back( vw_feature_from_string(model, "p^le_homme", t_hash, 1.0) );
+  t_features.push_back( vw_feature_from_string(model, "w^le", t_hash, 1.0) );
+  t_features.push_back( vw_feature_from_string(model, "w^homme", t_hash, 1.0) );
   ec_info.push_back( VW::feature_space('s', s_features) );
   ec_info.push_back( VW::feature_space('t', t_features) );
-  example* vec3 = VW::import_example(vw, ec_info);
+  example* vec3 = VW::import_example(model, ec_info);
     
-  vw.learn(&vw, vec3);
+  model.learn(&model, vec3);
   cerr << "p3 = " << vec3->final_prediction << endl;
-  VW::finish_example(vw, vec2);
-  VW::finish_example(vw, vec3);
+  VW::finish_example(model, vec3);
 
-  VW::finish(vw);
+  VW::finish(model);
+
+
+  vw model2 = VW::initialize("--hash all -q st --noconstant -i train2.vw");
+  vec2 = VW::read_example(model2, (char*)"|s p^the_man w^the w^man |t p^un_homme w^un w^homme");
+  model2.learn(&model2, vec2);
+  cerr << "p4 = " << vec2->final_prediction << endl;
+  VW::finish_example(model2, vec2);
+  VW::finish(model2);
 }
 
