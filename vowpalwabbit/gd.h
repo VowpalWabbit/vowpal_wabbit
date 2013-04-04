30da4ec6fcb3a8e379a25ef941b70c98526b2661
John Langford <jl@hunch.net>
2013-04-04 10:18:33 -0400

    generic feature loop

--- vowpalwabbit/gd.h
+++ vowpalwabbit/gd.h
@@ -34,6 +34,48 @@ void train_one_example_single_thread(regressor& r, example* ex);
  void save_load_regressor(vw& all, io_buf& model_file, bool read, bool text);
 void output_and_account_example(example* ec);
 
+ template <void (*T)(void*, float, uint32_t)>
+   void foreach_feature(void* dat, feature* begin, feature* end, uint32_t offset=0, float mult=1.)
+   {
+     for (feature* f = begin; f!= end; f++)
+       T(dat, mult*f->x, f->weight_index + offset);
+   }
+ 
+ template <void (*T)(void*, float, uint32_t)>
+   void foreach_feature(vw& all, void* dat, example* ec)
+   {
+     uint32_t offset = ec->ft_offset;
+
+     for (unsigned char* i = ec->indices.begin; i != ec->indices.end; i++) 
+       foreach_feature<T>(dat, ec->atomics[*i].begin, ec->atomics[*i].end, offset);
+     
+     for (vector<string>::iterator i = all.pairs.begin(); i != all.pairs.end();i++) {
+       if (ec->atomics[(int)(*i)[0]].size() > 0) {
+	 v_array<feature> temp = ec->atomics[(int)(*i)[0]];
+	 for (; temp.begin != temp.end; temp.begin++)
+	   {
+	     uint32_t halfhash = quadratic_constant * (temp.begin->weight_index + offset);
+	     foreach_feature<T>(dat, ec->atomics[(int)(*i)[1]].begin, ec->atomics[(int)(*i)[1]].end, 
+				halfhash + offset, temp.begin->x);
+	   }
+       }
+     }
+     
+     for (vector<string>::iterator i = all.triples.begin(); i != all.triples.end();i++) {
+       if ((ec->atomics[(int)(*i)[0]].size() == 0) || (ec->atomics[(int)(*i)[1]].size() == 0) || (ec->atomics[(int)(*i)[2]].size() == 0)) { continue; }
+       v_array<feature> temp1 = ec->atomics[(int)(*i)[0]];
+       for (; temp1.begin != temp1.end; temp1.begin++) {
+	 v_array<feature> temp2 = ec->atomics[(int)(*i)[1]];
+	 for (; temp2.begin != temp2.end; temp2.begin++) {
+	   
+	   uint32_t halfhash = cubic_constant2 * (cubic_constant * (temp1.begin->weight_index + offset) + temp2.begin->weight_index + offset);
+	   float mult = temp1.begin->x * temp2.begin->x;
+	   return foreach_feature<T>(dat, ec->atomics[(int)(*i)[2]].begin, ec->atomics[(int)(*i)[2]].end, halfhash + offset, mult);
+	 }
+       }
+     }
+   }
+ 
 template <float (*T)(vw&,float,uint32_t)>
 float inline_predict(vw& all, example* &ec)
 {
