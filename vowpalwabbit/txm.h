64b5094eab5c589698b3ec03d69b826f25e86b38
U-AnnaChoromanska\Anna Choromanska <Anna Choromanska@AnnaChoromanska.(none)>
2013-09-29 11:31:26 -0400

    ania

--- vowpalwabbit/txm.h
+++ vowpalwabbit/txm.h
@@ -16,7 +16,7 @@ license as described in the file LICENSE.
 #define TXM_PRED_LIM	1.f
 #define TXM_PRED_ALFA   1.f
 #define TXM_LEAF_TH		0.95
-#define TXM_LEVEL_LIM	5
+#define TXM_LEVEL_LIM	1
 #define TXM_MULTIPLICATIVE_FACTOR	10
 
 #define TXM_DEBUG_FILE1
@@ -25,6 +25,9 @@ license as described in the file LICENSE.
 //#define TXM_DEBUG_PASS_STOP
 //#define TXM_DEBUG_EX_STOP
 //#define TXM_DEBUG_PRED
+#define TXM_ARBITRARY_ROOT
+
+	
 
 namespace TXM
 {
