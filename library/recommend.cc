18282de095b98dfa0ba53bec4b819a99749af1ff
Vaclav Petricek <vpetricek@eharmony.com>
2013-11-02 23:05:32 -0700

    murmurhash used + testcases

--- library/recommend.cc
+++ library/recommend.cc
@@ -38,19 +38,6 @@ void progress()
         fprintf(stderr, "%12d %8d %8d %8d %12d %s %s\n", pairs, users, items, recs, skipped, userfilename.c_str(), itemfilename.c_str());
 }
 
-//      return uniform_hash((unsigned char *)s.begin, s.end - s.begin, h);
-
-
-//unsigned hash_ber(char *in, size_t len) {
-//        unsigned hashv = 0;
-//        while (len--)  hashv = ((hashv) * 33) + *in++;
-//        return hashv;
-//}
-//unsigned hash_fnv(char *in, size_t len) {
-//        unsigned hashv = 2166136261UL;
-//        while(len--) hashv = (hashv * 16777619) ^ *in++;
-//        return hashv;
-//}
 #define MASK(u,b) ( u & ((1UL << b) - 1))
 #define NUM_HASHES 2
 void get_hashv(char *in, size_t len, unsigned *out) {
