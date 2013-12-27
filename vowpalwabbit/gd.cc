0ece0fdf8afc0b233035d067d2f35250660d3998
ariel faigon <github.2009@yendor.com>
2013-12-26 18:49:15 -0800

    Slightly more informative error

--- vowpalwabbit/gd.cc
+++ vowpalwabbit/gd.cc
@@ -229,7 +229,7 @@ float finalize_prediction(vw& all, float ret)
 {
   if ( nanpattern(ret))
     {
-      cerr << "You have a NAN in example " << all.sd->example_number + 1 << " !!!" << endl;
+      cerr << "NAN prediction in example " << all.sd->example_number + 1 << ", forcing 0.0" << endl;
       return 0.;
     }
   if ( ret > all.sd->max_label )
