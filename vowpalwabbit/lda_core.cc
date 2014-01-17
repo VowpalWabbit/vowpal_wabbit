38c027b8665a64e0b5353d41b71885b1789719e7
Juha Reunanen <juha.reunanen@gmail.com>
2014-01-17 10:53:25 +0200

    When constructing the lda object, better avoid placement new and rather use just plain old new (pun intended). Rationale: placement new should be used only when really necessary. (http://www.parashift.com/c++-faq/placement-new.html) Drawback: if new POD members are added, they need to be manually zeroed here (or, preferably, in the lda struct's constructor).

--- vowpalwabbit/lda_core.cc
+++ vowpalwabbit/lda_core.cc
@@ -729,8 +729,8 @@ void end_examples(lda* l)
 
 learner* setup(vw&all, std::vector<std::string>&opts, po::variables_map& vm)
 {
-  lda* ld = (lda*)calloc(1,sizeof(lda));
-  ld = new(ld) lda; // properly initialize the non-POD members
+  lda* ld = new lda;
+  ld->total_lambda_init = 0;
   ld->all = &all;
   ld->example_t = all.initial_t;
 
