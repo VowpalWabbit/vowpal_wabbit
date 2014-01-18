8892c99a534ad456cab7ee9d513336d7a2e772f5
U-NORTHAMERICA\jcl <jcl@JCL.northamerica.corp.microsoft.com>
2014-01-18 10:56:31 -0500

    fixed online print in cygwin bug

--- vowpalwabbit/simple_label.cc
+++ vowpalwabbit/simple_label.cc
@@ -179,6 +179,7 @@ void print_update(vw& all, example *ec)
       all.sd->sum_loss_since_last_dump = 0.0;
       all.sd->old_weighted_examples = all.sd->weighted_examples;
       VW::update_dump_interval(all);
+	  fflush(stderr);
     }
 }
 
