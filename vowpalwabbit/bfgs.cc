ca6c8006f41cb92001d1c1a1102927f68ab30f34
John Langford <jl@hunch.net>
2014-05-17 09:51:20 -0400

    early terminate the parser when bfgs converges, originally from spaghettification

--- vowpalwabbit/bfgs.cc
+++ vowpalwabbit/bfgs.cc
@@ -802,8 +802,10 @@ void end_pass(bfgs& b)
                set_done(*all);
                cerr<<"Early termination reached w.r.t. holdout set error";
              }
-
-	   } 
+	   } if (b.final_pass == b.current_pass) {
+	     finalize_regressor(*all, all->final_regressor_name); 
+	     set_done(*all);
+	   }
            
        }else{//reaching convergence in the previous pass
         if(b.output_regularizer) 
