65defd76751fb8cf4cdd6b7267bdf951b2b3acb4
U-NORTHAMERICA\jcl <jcl@JCL.northamerica.corp.microsoft.com>
2014-07-14 17:04:57 -0400

    added topic predictions to library interface

--- vowpalwabbit/vw.h
+++ vowpalwabbit/vw.h
@@ -52,6 +52,7 @@ namespace VW {
   void parse_example_label(vw&all, example&ec, string label);
   example* new_unused_example(vw& all);
   example* get_example(parser* pf);
+  float get_topic_prediction(example*ec, size_t i);//i=0 to max topic -1
   float get_label(example*ec);
   float get_importance(example*ec);
   float get_initial(example*ec);
