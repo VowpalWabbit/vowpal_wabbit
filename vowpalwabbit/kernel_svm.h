fdca398aab7e05b4989fe6aded763d5dc7ba58b5
Alekh Agarwal <alekh@alekha-z420.corp.microsoft.com>
2014-08-20 17:18:01 -0400

    adding the code files

---
+++ vowpalwabbit/kernel_svm.h
@@ -0,0 +1,9 @@
+/*
+Copyright (c) by respective owners including Yahoo!, Microsoft, and
+individual contributors. All rights reserved.  Released under a BSD (revised)
+license as described in the file LICENSE.
+ */
+namespace KSVM
+{
+LEARNER::learner* setup(vw &all, po::variables_map& vm);
+}
