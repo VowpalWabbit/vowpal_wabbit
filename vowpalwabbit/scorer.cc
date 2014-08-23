6fbcc37474a3a2bcc5c47701c95c6e8302793139
ariel faigon <github.2009@yendor.com>
2014-08-23 13:54:12 -0700

    Rename function to glf1 (generalized logistic func with [-1, 1] range)

--- vowpalwabbit/scorer.cc
+++ vowpalwabbit/scorer.cc
@@ -8,7 +8,7 @@ namespace Scorer {
   struct scorer{
     vw* all;
   };
-  
+
   template <bool is_learn, float (*link)(float in)>
   void predict_or_learn(scorer& s, learner& base, example& ec)
   {
@@ -19,7 +19,7 @@ namespace Scorer {
       base.learn(ec);
     else
       base.predict(ec);
-    
+
     if(ld->weight > 0 && ld->label != FLT_MAX)
       ec.loss = s.all->loss->getLoss(s.all->sd, ld->prediction, ld->label) * ld->weight;
 
@@ -32,8 +32,10 @@ namespace Scorer {
     return 1.f / (1.f + exp(- in));
   }
 
+  // http://en.wikipedia.org/wiki/Generalized_logistic_curve
+  // where the lower & upper asymptotes are -1 & 1 respectively
   // y = f(x) -> [-1, 1]
-  float logistic_1(float in)
+  float glc1(float in)
   {
     return 2.f / (1.f + exp(- in)) - 1.f;
   }
@@ -49,9 +51,9 @@ namespace Scorer {
     s->all = &all;
 
     po::options_description link_opts("Link options");
-    
+
     link_opts.add_options()
-      ("link", po::value<string>()->default_value("identity"), "Specify the link function: identity, logistic or logistic1");
+      ("link", po::value<string>()->default_value("identity"), "Specify the link function: identity, logistic or glc1");
 
     vm = add_options(all, link_opts);
 
@@ -59,7 +61,7 @@ namespace Scorer {
 
     string link = vm["link"].as<string>();
     if (!vm.count("link") || link.compare("identity") == 0)
-      {      
+      {
 	l->set_learn<scorer, predict_or_learn<true, noop> >();
 	l->set_predict<scorer, predict_or_learn<false, noop> >();
       }
@@ -69,13 +71,13 @@ namespace Scorer {
 	l->set_learn<scorer, predict_or_learn<true, logistic> >();
 	l->set_predict<scorer, predict_or_learn<false, logistic> >();
       }
-    else if (link.compare("logistic1") == 0)
+    else if (link.compare("glc1") == 0)
       {
-	all.file_options.append(" --link=logistic1 ");
-	l->set_learn<scorer, predict_or_learn<true, logistic_1> >();
-	l->set_predict<scorer, predict_or_learn<false, logistic_1> >();
+	all.file_options.append(" --link=glc1 ");
+	l->set_learn<scorer, predict_or_learn<true, glc1> >();
+	l->set_predict<scorer, predict_or_learn<false, glc1> >();
       }
-    else 
+    else
       {
 	cerr << "Unknown link function: " << link << endl;
 	throw exception();
