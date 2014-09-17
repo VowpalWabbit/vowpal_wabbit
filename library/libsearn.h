dbcf40d7f3acbd29ccebe9f903e2beaec4f9b854
Hal Daume III <me@hal3.name>
2014-09-17 14:28:49 -0400

    bug fixes

--- library/libsearn.h
+++ library/libsearn.h
@@ -15,10 +15,10 @@ using namespace std;
 
 template<class INPUT, class OUTPUT> class SearchTask {
   public:
-  SearchTask(vw* all) : all(*all), srn(*(Searn::searn*)all->searnstr) {
-    bogus_example = VW::read_example(*all, (char*)"1 | x");
-    blank_line    = VW::read_example(*all, (char*)"");
-    VW::finish_example(*all, blank_line);
+  SearchTask(vw& vw_obj) : vw_obj(vw_obj), srn(*(Searn::searn*)vw_obj.searnstr) {
+    bogus_example = VW::read_example(vw_obj, (char*)"1 | x");
+    blank_line    = VW::read_example(vw_obj, (char*)"");
+    VW::finish_example(vw_obj, blank_line);
     HookTask::task_data* d = srn.get_task_data<HookTask::task_data>();
     d->run_f = _searn_run_fn;
     d->run_object = this;
@@ -27,31 +27,31 @@ template<class INPUT, class OUTPUT> class SearchTask {
     d->extra_data  = NULL;
     d->extra_data2 = NULL;
   }
-  ~SearchTask() { VW::finish_example(all, bogus_example); }
+  ~SearchTask() { VW::finish_example(vw_obj, bogus_example); }
 
   virtual void _run(Searn::searn&srn, INPUT& input_example, OUTPUT& output) {}  // YOU MUST DEFINE THIS FUNCTION!
 
   void learn(INPUT& input_example, OUTPUT& output) {
-    HookTask::task_data* d = srn.template get_task_data<HookTask::task_data> (); // ugly calling convention :(
+    HookTask::task_data* d = srn.template get_task_data<HookTask::task_data> (); // ugly cvw_objing convention :(
     bogus_example->test_only = false;
     d->extra_data  = (void*)&input_example;
     d->extra_data2 = (void*)&output;
-    all.learn(bogus_example);
-    all.learn(blank_line);   // this will cause our searn_run_fn hook to get called
+    vw_obj.learn(bogus_example);
+    vw_obj.learn(blank_line);   // this will cause our searn_run_fn hook to get cvw_objed
   }
 
   void predict(INPUT& input_example, OUTPUT& output) {
-    HookTask::task_data* d = srn.template get_task_data<HookTask::task_data> (); // ugly calling convention :(
+    HookTask::task_data* d = srn.template get_task_data<HookTask::task_data> (); // ugly cvw_objing convention :(
     bogus_example->test_only = true;
     d->extra_data  = (void*)&input_example;
     d->extra_data2 = (void*)&output;
-    all.learn(bogus_example);
-    all.learn(blank_line);   // this will cause our searn_run_fn hook to get called
+    vw_obj.learn(bogus_example);
+    vw_obj.learn(blank_line);   // this will cause our searn_run_fn hook to get cvw_objed
   }
   
 
   protected:
-  vw& all;
+  vw& vw_obj;
   Searn::searn& srn;
   
   private:
@@ -60,7 +60,7 @@ template<class INPUT, class OUTPUT> class SearchTask {
   static void _searn_run_fn(Searn::searn&srn) {
     HookTask::task_data* d = srn.get_task_data<HookTask::task_data>();
     if ((d->run_object == NULL) || (d->extra_data == NULL) || (d->extra_data2 == NULL)) {
-      cerr << "error: calling _searn_run_fn without setting run object" << endl;
+      cerr << "error: cvw_objing _searn_run_fn without setting run object" << endl;
       throw exception();
     }
     ((SearchTask*)d->run_object)->_run(srn, *(INPUT*)d->extra_data, *(OUTPUT*)d->extra_data2);
--- library/test_search.cc
+++ library/test_search.cc
@@ -1,43 +1,57 @@
 #include <stdio.h>
 #include "../vowpalwabbit/vw.h"
+#include "../vowpalwabbit/ezexample.h"
 #include "libsearn.h"
 
 struct wt {
   string word;
-  int tag;
-  wt(string w, int t) : word(w), tag(t) {}
+  uint32_t tag;
+  wt(string w, uint32_t t) : word(w), tag(t) {}
 };
 
-class SequenceLabelerTask : public SearchTask< vector<wt>, vector<int> > {
+class SequenceLabelerTask : public SearchTask< vector<wt>, vector<uint32_t> > {
   public:
-  SequenceLabelerTask(vw*all) : SearchTask< vector<wt>, vector<int> >(all) {  // must run parent constructor!
-    srn.set_options( Searn::AUTO_HAMMING_LOSS | Searn::AUTO_HISTORY | Searn::EXAMPLES_DONT_CHANGE );
+  SequenceLabelerTask(vw& vw_obj)
+      : SearchTask< vector<wt>, vector<uint32_t> >(vw_obj) {  // must run parent constructor!
+    srn.set_options( Searn::AUTO_HAMMING_LOSS | Searn::AUTO_HISTORY );
   }
 
-  void _run(Searn::searn& srn, vector<wt> & input_example, vector<int> & output) {
+  // using vanilla vw interface
+  void _run(Searn::searn& srn, vector<wt> & input_example, vector<uint32_t> & output) {
     output.clear();
     for (size_t i=0; i<input_example.size(); i++) {
-      string   s  = "1 | " + input_example[i].word;
-      example* ex = VW::read_example(all, (char*)s.c_str());
+      example* ex = VW::read_example(vw_obj, "1 |w " + input_example[i].word);
       uint32_t p  = srn.predict(ex, input_example[i].tag);
-      VW::finish_example(all, ex);
+      VW::finish_example(vw_obj, ex);
       output.push_back(p);
     }
   }
+
+  // using ezexample
+  void _run2(Searn::searn& srn, vector<wt> & input_example, vector<uint32_t> & output) {
+    output.clear();
+    for (size_t i=0; i<input_example.size(); i++) {
+      ezexample ex(&vw_obj);
+      ex(vw_namespace('w'))(input_example[i].word);  // add the feature
+      uint32_t p  = srn.predict(ex.get(), input_example[i].tag);
+      output.push_back(p);
+    }
+  }
+
 };
 
 int main(int argc, char *argv[]) {
   // initialize VW as usual, but use 'hook' as the search_task
-  vw* all = VW::initialize("--search 4 --quiet --search_task hook --search_no_snapshot --ring_size 1024");
+  vw& vw_obj = *VW::initialize("--search 4 --quiet --search_task hook --search_no_snapshot --ring_size 1024");
 
   {
     // we put this in its own scope so that its destructor gets called
     // *before* VW::finish gets called; otherwise we'll get a
     // segfault :(. not sure what to do about this :(.
-    SequenceLabelerTask task(all);
+    SequenceLabelerTask task(vw_obj);
     vector<wt> data;
-    vector<int> output;
-    int DET = 1, NOUN = 2, VERB = 3, ADJ = 4;
+    vector<uint32_t> output;
+    uint32_t DET = 1, NOUN = 2, VERB = 3, ADJ = 4;
     data.push_back( wt("the", DET) );
     data.push_back( wt("monster", NOUN) );
     data.push_back( wt("ate", VERB) );
@@ -53,7 +67,7 @@ int main(int argc, char *argv[]) {
     cerr << " ]" << endl;
   }
   
-  VW::finish(*all);
+  VW::finish(vw_obj);
 }
 
 
