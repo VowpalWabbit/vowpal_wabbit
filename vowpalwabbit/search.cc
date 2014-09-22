6981ebe73973eeb3614cbe07f816d6ac25f3a553
Hal Daume III <me@hal3.name>
2014-09-21 22:12:09 -0400

    added missing include

--- vowpalwabbit/search.cc
+++ vowpalwabbit/search.cc
@@ -3,7 +3,7 @@ Copyright (c) by respective owners including Yahoo!, Microsoft, and
 individual contributors. All rights reserved.  Released under a BSD (revised)
 license as described in the file LICENSE.
  */
-#include <limits>
+#include <float.h>
 #include "search.h"
 #include "v_hashmap.h"
 #include "hash.h"
