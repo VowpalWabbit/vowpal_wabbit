6a6c2276813f379489734d9dd4bb0ca25ba2e672
Luong Hoang <lhoang@live.com>
2014-09-29 13:13:50 -0400

    Revert "fixes #14"
    This reverts commit 93dbf65af561b0074a3e75b421e539886110a717.

--- vw_explore_static/PolicyFunc.h
+++ vw_explore_static/PolicyFunc.h
@@ -11,19 +11,19 @@ public:
 
 class MWT_Empty { };
 
-typedef u32 Stateful_Policy_Func(void* state_Context, void* application_context);
-typedef u32 Stateless_Policy_Func(void* application_context);
+typedef u32 Stateful_Policy_Func(void* state_Context, void* application_Context);
+typedef u32 Stateless_Policy_Func(void* application_Context);
 //TODO: I think vectors will cause problem with C# interop
-typedef void Stateful_Scorer_Func(void* state_context, void* application_context, float*& scores, size_t& num_scores);
-typedef void Stateless_Scorer_Func(void* application_context, float*& scores, size_t& num_scores);
+typedef std::vector<float> Stateful_Scorer_Func(void* state_Context, void* application_Context);
+typedef std::vector<float> Stateless_Scorer_Func(void* application_Context);
 
 
 template <class T>
 class StatefulFunctionWrapper : public BaseFunctionWrapper
 {
 public:
-	typedef u32 Policy_Func(T* state_context, Context* application_context);
-	typedef void Scorer_Func(T* state_context, Context* application_context, float*& scores, size_t& num_scores);
+	typedef u32 Policy_Func(T* state_Context, Context* application_Context);
+	typedef std::vector<float> Scorer_Func(T* state_Context, Context* application_Context);
 
 	Stateful_Policy_Func* m_policy_function;
 	Stateful_Scorer_Func* m_scorer_function;
@@ -32,8 +32,8 @@ public:
 class StatelessFunctionWrapper : public BaseFunctionWrapper
 {
 public:
-	typedef u32 Policy_Func(Context* application_context);
-	typedef void Scorer_Func(Context* application_context, float*& scores, size_t& num_scores);
+	typedef u32 Policy_Func(Context* application_Context);
+	typedef std::vector<float> Scorer_Func(Context* application_Context);
 
 	Stateless_Policy_Func* m_policy_function;
 	Stateless_Scorer_Func* m_scorer_function;
