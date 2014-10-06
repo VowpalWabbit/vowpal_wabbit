db43b53f577a02ea8cc7b3ddbb42d571f9bafa5d
Luong Hoang <lhoang@live.com>
2014-10-06 14:57:57 -0400

    Added more C# tests to cover mixed mode interactions

--- explore/tests/MWTExploreTests.cs
+++ explore/tests/MWTExploreTests.cs
@@ -8,11 +8,14 @@ namespace ExploreTests
     [TestClass]
     public class MWTExploreTests
     {
+        /* 
+        ** C# Tests do not need to be as extensive as those for C++. These tests should ensure
+        ** the interactions between managed and native code are as expected.
+        */
         [TestMethod]
         public void EpsilonGreedyStateful()
         {
-            float epsilon = 0;
-            mwt.InitializeEpsilonGreedy(epsilon, 
+            mwt.InitializeEpsilonGreedy(Epsilon, 
                 new StatefulPolicyDelegate(TestStatefulPolicyFunc),
                 new IntPtr(PolicyParams),
                 NumActions);
@@ -32,6 +35,148 @@ namespace ExploreTests
 
             Assert.AreEqual(2, interactions[0].ApplicationContext.Features.Length);
             Assert.AreEqual(0.9f, interactions[0].ApplicationContext.Features[1].X);
+        }
+
+        [TestMethod]
+        public void EpsilonGreedyStateless()
+        {
+            mwt.InitializeEpsilonGreedy(Epsilon,
+                new StatelessPolicyDelegate(TestStatelessPolicyFunc),
+                NumActions);
+
+            uint expectedAction = MWTExploreTests.TestStatelessPolicyFunc((IntPtr)contextHandle);
+
+            uint chosenAction = mwt.ChooseAction(context, UniqueKey);
+            Assert.AreEqual(expectedAction, chosenAction);
+
+            Tuple<uint, ulong> chosenActionAndKey = mwt.ChooseActionAndKey(context);
+            Assert.AreEqual(expectedAction, chosenActionAndKey.Item1);
+
+            INTERACTION[] interactions = mwt.GetAllInteractions();
+            Assert.AreEqual(2, interactions.Length);
+
+            Assert.AreEqual(2, interactions[0].ApplicationContext.Features.Length);
+            Assert.AreEqual(0.9f, interactions[0].ApplicationContext.Features[1].X);
+        }
+
+        [TestMethod]
+        public void TauFirstStateful()
+        {
+            mwt.InitializeTauFirst(Tau,
+                new StatefulPolicyDelegate(TestStatefulPolicyFunc),
+                new IntPtr(PolicyParams),
+                NumActions);
+
+            uint expectedAction = MWTExploreTests.TestStatefulPolicyFunc(
+                new IntPtr(PolicyParams),
+                (IntPtr)contextHandle);
+
+            uint chosenAction = mwt.ChooseAction(context, UniqueKey);
+            Assert.AreEqual(expectedAction, chosenAction);
+
+            Tuple<uint, ulong> chosenActionAndKey = mwt.ChooseActionAndKey(context);
+            Assert.AreEqual(expectedAction, chosenActionAndKey.Item1);
+
+            INTERACTION[] interactions = mwt.GetAllInteractions();
+            Assert.AreEqual(0, interactions.Length);
+        }
+
+        [TestMethod]
+        public void TauFirstStateless()
+        {
+            mwt.InitializeTauFirst(Tau,
+                new StatelessPolicyDelegate(TestStatelessPolicyFunc),
+                NumActions);
+
+            uint expectedAction = MWTExploreTests.TestStatelessPolicyFunc((IntPtr)contextHandle);
+
+            uint chosenAction = mwt.ChooseAction(context, UniqueKey);
+            Assert.AreEqual(expectedAction, chosenAction);
+
+            Tuple<uint, ulong> chosenActionAndKey = mwt.ChooseActionAndKey(context);
+            Assert.AreEqual(expectedAction, chosenActionAndKey.Item1);
+
+            INTERACTION[] interactions = mwt.GetAllInteractions();
+            Assert.AreEqual(0, interactions.Length);
+        }
+
+        [TestMethod]
+        public void BaggingStateful()
+        {
+            StatefulPolicyDelegate[] funcs = new StatefulPolicyDelegate[Bags];
+            IntPtr[] funcParams = new IntPtr[Bags];
+            for (int i = 0; i < Bags; i++)
+			{
+                funcs[i] = new StatefulPolicyDelegate(TestStatefulPolicyFunc);
+                funcParams[i] = new IntPtr(PolicyParams);
+			}
+
+            mwt.InitializeBagging(Bags, funcs, funcParams, NumActions);
+
+            uint expectedAction = MWTExploreTests.TestStatefulPolicyFunc(
+                new IntPtr(PolicyParams),
+                (IntPtr)contextHandle);
+
+            uint chosenAction = mwt.ChooseAction(context, UniqueKey);
+            Assert.AreEqual(expectedAction, chosenAction);
+
+            Tuple<uint, ulong> chosenActionAndKey = mwt.ChooseActionAndKey(context);
+            Assert.AreEqual(expectedAction, chosenActionAndKey.Item1);
+
+            INTERACTION[] interactions = mwt.GetAllInteractions();
+            Assert.AreEqual(2, interactions.Length);
+
+            Assert.AreEqual(2, interactions[0].ApplicationContext.Features.Length);
+            Assert.AreEqual(0.9f, interactions[0].ApplicationContext.Features[1].X);
+        }
+
+        [TestMethod]
+        public void BaggingStateless()
+        {
+            StatelessPolicyDelegate[] funcs = new StatelessPolicyDelegate[Bags];
+            for (int i = 0; i < Bags; i++)
+            {
+                funcs[i] = new StatelessPolicyDelegate(TestStatelessPolicyFunc);
+            }
+
+            mwt.InitializeBagging(Bags, funcs, NumActions);
+
+            uint expectedAction = MWTExploreTests.TestStatelessPolicyFunc(
+                (IntPtr)contextHandle);
+
+            uint chosenAction = mwt.ChooseAction(context, UniqueKey);
+            Assert.AreEqual(expectedAction, chosenAction);
+
+            Tuple<uint, ulong> chosenActionAndKey = mwt.ChooseActionAndKey(context);
+            Assert.AreEqual(expectedAction, chosenActionAndKey.Item1);
+
+            INTERACTION[] interactions = mwt.GetAllInteractions();
+            Assert.AreEqual(2, interactions.Length);
+
+            Assert.AreEqual(2, interactions[0].ApplicationContext.Features.Length);
+            Assert.AreEqual(0.9f, interactions[0].ApplicationContext.Features[1].X);
+        }
+
+        [TestMethod] // Currently failing
+        public void SoftmaxStateful()
+        {
+            mwt.InitializeSoftmax(Lambda,
+                new StatefulScorerDelegate(TestStatefulScorerFunc), 
+                new IntPtr(PolicyParams), NumActions);
+
+            uint numDecisions = (uint)(NumActions * Math.Log(NumActions * 1.0) + Math.Log(NumActionsCover * 1.0 / NumActions) * C * NumActions);
+            uint[] actions = new uint[NumActions];
+            
+            for (uint i = 0; i < numDecisions; i++)
+            {
+                Tuple<uint, ulong> actionAndKey = mwt.ChooseActionAndKey(context);
+                actions[ActionID.Make_ZeroBased(actionAndKey.Item1)]++;
+            }
+            
+            for (uint i = 0; i < numDecisions; i++)
+            {
+                Assert.IsTrue(actions[i] > 0);
+            }
         }
 
         [TestInitialize]
@@ -86,8 +231,12 @@ namespace ExploreTests
             }
         }
 
+        private static readonly uint NumActionsCover = 100;
+        private static readonly float C = 5;
+
         private static readonly uint NumActions = 10;
-        private static readonly uint Tau = 5;
+        private static readonly float Epsilon = 0;
+        private static readonly uint Tau = 0;
         private static readonly uint Bags = 2;
         private static readonly float Lambda = 0.5f;
         private static readonly int PolicyParams = 1003;
