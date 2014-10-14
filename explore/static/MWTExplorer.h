0862c22ea3ea311eb8dd605279c2d77097d9ca64
Luong Hoang <lhoang@live.com>
2014-10-14 18:37:49 -0400

    fixes #53

--- explore/static/MWTExplorer.h
+++ explore/static/MWTExplorer.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include "stdafx.h"
+#include "Common.h"
 #include "Explorer.h"
 #include <tuple>
 
--- explore/static/MWTOptimizer.h
+++ explore/static/MWTOptimizer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "stdafx.h"
+#include "Common.h"
 #include "vwdll.h"
 
 //
