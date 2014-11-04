113d7bd9ad5e4b186bc9e5ee5348bcda7369b00b
Luong Hoang <lhoang@live.com>
2014-11-04 12:53:57 -0500

    moved float_print to numberutils

--- explore/static/Interaction.h
+++ explore/static/Interaction.h
@@ -95,69 +95,11 @@ private:
 	u32 m_count;
 };
 
-const size_t max_int = 100000;
-const float max_float = max_int;
-const float min_float = 0.00001;
-const size_t max_digits = roundf(- log(min_float) / log (10.));
-	
 class BaseContext : Serializable
 {
 private:
 	virtual void Get_Features(size_t& num_features, Feature*& features) = 0;
 
-	template<bool trailing_zeros>
-	  void print_mantissa(char*& begin, float f)
-	  { // helper for print_float
-	    char values[10];
-	    size_t v = (size_t) f;
-	    size_t digit = 0;
-	    size_t first_nonzero = 0;
-	    for (size_t max = 1; max <= v; ++digit)
-	      {
-		size_t max_next = max*10;
-		char v_mod = v % max_next / max;
-		if (!trailing_zeros && v_mod != '\0' && first_nonzero == 0)
-		  first_nonzero = digit;
-		values[digit] = '0'+v_mod;
-		max = max_next;
-	      }
-	    if (!trailing_zeros)
-	      for (size_t i = max_digits; i > digit; i--)
-		*begin++ = '0';
-	    while (digit > first_nonzero)
-	      *begin++ = values[--digit];
-	  }
-	
-	void print_float(char* begin, float f)
-	{
-	  bool sign = false;
-	  if (f < 0.f)
-	    sign = true;
-	  float unsigned_f = fabsf(f);
-	  if (unsigned_f < max_float && unsigned_f > min_float)
-	    {
-	      if (sign)
-		*begin++ = '-';
-	      print_mantissa<true>(begin, unsigned_f);
-	      unsigned_f -= (size_t) unsigned_f;
-	      unsigned_f *= max_int;
-	      if (unsigned_f >= 1.f)
-		{
-		  *begin++ = '.';
-		  print_mantissa<false>(begin, unsigned_f);
-		}
-	    }
-	  else if (unsigned_f == 0.)
-	    *begin++ = '0';
-	  else
-	    {
-	      sprintf(begin, "%g", f);
-	      return;
-	    }
-	  *begin = '\0';
-	  return;
-	}
-	
 	void Serialize(std::string& stream)// final // does not compile in g++
 	{
 		Feature* features = nullptr;
@@ -169,12 +111,16 @@ private:
 
 			for (size_t i = 0; i < num_features; i++)
 			{
-			  int chars;
-			  if (i == 0)
+				int chars;
+				if (i == 0)
+				{
 					chars = sprintf(feature_str, "%d:", features[i].Id);
+				}
 				else
+				{
 					chars = sprintf(feature_str, " %d:", features[i].Id);
-			  print_float(feature_str+chars, features[i].Value);
+				}
+				NumberUtils::print_float(feature_str + chars, features[i].Value);
 
 				stream.append(feature_str);
 			}
