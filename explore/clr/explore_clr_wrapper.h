b6c6831f30a3ca643d20d5f65c11e8281e30b08b
Luong Hoang <lhoang@live.com>
2014-11-11 13:51:20 -0500

    Added more comments to c# interface

--- explore/clr/explore_clr_wrapper.h
+++ explore/clr/explore_clr_wrapper.h
@@ -167,16 +167,33 @@ namespace MultiWorldTesting {
 		NativeMultiWorldTesting::BootstrapExplorer<NativeContext>* m_explorer;
 	};
 
+	/// <summary>
+	/// The top level MwtExplorer class.  Using this makes sure that the
+	/// right bits are recorded and good random actions are chosen.
+	/// </summary>
+	/// <typeparam name="Ctx">The Context type.</typeparam>
 	generic <class Ctx>
 	public ref class MwtExplorer : public RecorderCallback<Ctx>
 	{
 	public:
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="appId">This should be unique to each experiment to avoid correlation bugs.</param>
+		/// <param name="recorder">A user-specified class for recording the appropriate bits for use in evaluation and learning.</param>
 		MwtExplorer(String^ appId, IRecorder<Ctx>^ recorder)
 		{
 			this->appId = appId;
 			this->recorder = recorder;
 		}
 
+		/// <summary>
+		/// Choose_Action should be drop-in replacement for any existing policy function.
+		/// </summary>
+		/// <param name="explorer">An existing exploration algorithm (one of the above) which uses the default policy as a callback.</param>
+		/// <param name="unique_key">A unique identifier for the experimental unit. This could be a user id, a session id, etc...</param>
+		/// <param name="context">The context upon which a decision is made. See SimpleContext above for an example.</param>
+		/// <returns>An unsigned 32-bit integer representing the 1-based chosen action.</returns>
 		UInt32 ChooseAction(IExplorer<Ctx>^ explorer, String^ unique_key, Ctx context)
 		{
 			String^ salt = this->appId;
@@ -244,6 +261,10 @@ namespace MultiWorldTesting {
 		UInt32 Id;
 	};
 
+	/// <summary>
+	/// A sample recorder class that converts the exploration tuple into string format.
+	/// </summary>
+	/// <typeparam name="Ctx">The Context type.</typeparam>
 	generic <class Ctx> where Ctx : IStringContext
 	public ref class StringRecorder : public IRecorder<Ctx>, public ToStringCallback<Ctx>
 	{
--- explore/clr/explore_interface.h
+++ explore/clr/explore_interface.h
@@ -18,20 +18,46 @@ generic <class Ctx>
 public interface class IRecorder
 {
 public:
+	/// <summary>
+	/// Records the exploration data associated with a given decision.
+	/// </summary>
+	/// <param name="context">A user-defined context for the decision.</param>
+	/// <param name="action">Chosen by an exploration algorithm given context.</param>
+	/// <param name="probability">The probability of the chosen action given context.</param>
+	/// <param name="uniqueKey">A user-defined identifer for the decision.</param>
 	virtual void Record(Ctx context, UInt32 action, float probability, String^ uniqueKey) = 0;
 };
 
+/// <summary>
+/// Exposes a method for choosing an action given a generic context. IPolicy objects are 
+/// passed to (and invoked by) exploration algorithms to specify the default policy behavior.
+/// </summary>
+/// <typeparam name="Ctx">The Context type.</typeparam>
 generic <class Ctx>
 public interface class IPolicy
 {
 public:
+	/// <summary>
+	/// Determines the action to take for a given context.
+	/// </summary>
+	/// <param name="context">A user-defined context for the decision.</param>
+	/// <returns>Index of the action to take (1-based)</returns>
 	virtual UInt32 ChooseAction(Ctx context) = 0;
 };
 
+/// <summary>
+/// Exposes a method for specifying a score (weight) for each action given a generic context. 
+/// </summary>
+/// <typeparam name="Ctx">The Context type.</typeparam>
 generic <class Ctx>
 public interface class IScorer
 {
 public:
+	/// <summary>
+	/// Determines the score of each action for a given context.
+	/// </summary>
+	/// <param name="context">A user-defined context for the decision.</param>
+	/// <returns>Vector of scores indexed by action (1-based).</returns>
 	virtual List<float>^ ScoreActions(Ctx context) = 0;
 };
 
