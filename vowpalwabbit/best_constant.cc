b74a7193ebd99c54d878fdcf1c0272902b726745
trufanov-nok <trufanovan@gmail.com>
2014-11-22 14:29:58 +0400

    multilabel squared loss constant + bugfix

--- vowpalwabbit/best_constant.cc
+++ vowpalwabbit/best_constant.cc
@@ -6,12 +6,12 @@ float second_observed_label = FLT_MAX;
 
 bool get_best_constant(vw& all, float& best_constant, float& best_constant_loss)
 {
-    if (is_more_than_two_labels_observed  ||   // more than 2 different labels were observed
-            first_observed_label == FLT_MAX || // no non-test labels observed or function was never called
+    if (    first_observed_label == FLT_MAX || // no non-test labels observed or function was never called
             (all.loss == NULL) || (all.sd == NULL)) return false;
 
-    float label1 = all.sd->min_label;
-    float label2 = all.sd->max_label;
+    float label1 = first_observed_label; // observed labels might be inside [sd->Min_label, sd->Max_label], so can't use Min/Max
+    float label2 = (second_observed_label == FLT_MAX)?0:second_observed_label; // if only one label observed, second might be 0
+    if (label1 > label2) {float tmp = label1; label1 = label2; label2 = tmp;} // as don't use min/max - make sure label1 < label2
 
     float label1_cnt;
     float label2_cnt;
@@ -43,7 +43,11 @@ bool get_best_constant(vw& all, float& best_constant, float& best_constant_loss)
 
     if(funcName.compare("squared") == 0 || funcName.compare("Huber") == 0 || funcName.compare("classic") == 0)
     {
-        best_constant = (label1*label1_cnt + label2*label2_cnt) / (label1_cnt + label2_cnt);
+        best_constant = (float) all.sd->weighted_labels / (float) (all.sd->weighted_examples - all.sd->weighted_unlabeled_examples + all.initial_t); //GENERIC. WAS: (label1*label1_cnt + label2*label2_cnt) / (label1_cnt + label2_cnt);
+
+    } else if (is_more_than_two_labels_observed) {
+        //loss functions below don't have generic formuas for constant yet.
+        return false;
 
     } else if(funcName.compare("hinge") == 0) {
 
@@ -73,9 +77,11 @@ bool get_best_constant(vw& all, float& best_constant, float& best_constant_loss)
         return false;
 
 
+    if (!is_more_than_two_labels_observed)
     best_constant_loss = ( all.loss->getLoss(all.sd, best_constant, label1) * label1_cnt +
                            all.loss->getLoss(all.sd, best_constant, label2) * label2_cnt )
             / (label1_cnt + label2_cnt);
+    else best_constant_loss = FLT_MIN;
 
 
     return true;
