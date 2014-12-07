890f528d46293a88f79d8ee3ff363db7925fbbcd
John Langford <jl@hunch.net>
2014-12-06 19:52:03 -0500

    fix java interface to use predict

--- java/src/main/c++/vw_VWScorer.cc
+++ java/src/main/c++/vw_VWScorer.cc
@@ -12,14 +12,13 @@ JNIEXPORT void JNICALL Java_vw_VWScorer_initialize
 JNIEXPORT jfloat JNICALL Java_vw_VWScorer_getPrediction
   (JNIEnv *env, jobject obj, jstring example_string) {
     example *vec2 = VW::read_example(*vw, env->GetStringUTFChars(example_string, NULL));
-    vw->learn(vec2);
+    vw->l->predict(*vec2);
     float prediction;
     if (vw->p->lp.parse_label == simple_label.parse_label)
         prediction = vec2->pred.scalar;
     else
         prediction = vec2->pred.multiclass;
-    fflush(stdout);
-    VW::finish_example(*vw, vec2);
+     VW::finish_example(*vw, vec2);
     return prediction;
 }
 
