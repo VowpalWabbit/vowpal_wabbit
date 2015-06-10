38af1265f31db7f367a97634aeead17c19f06400
Jon Morra <jmorra@eharmony.com>
2015-06-10 16:01:00 -0700

    Making JNI build script easier to read

--- java/src/main/bin/build.sh
+++ java/src/main/bin/build.sh
@@ -6,6 +6,47 @@
 __not_darwin=1
 __brew_not_installed=2
 
+make_base="cd /vowpal_wabbit;
+make clean;
+make;"
+
+ubuntu_base="apt-get update;
+apt-get install -qq software-properties-common g++ make libboost-all-dev default-jdk;"
+
+ubuntu_12="$ubuntu_base
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64;
+$make_base
+mv java/target/vw_jni.lib java/target/vw_jni.Ubuntu.12.amd64.lib"
+
+ubuntu_14_32="$ubuntu_base
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386;
+$make_base
+mv java/target/vw_jni.lib java/target/vw_jni.Ubuntu.14.i386.lib"
+
+ubuntu_14="$ubuntu_base
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64;
+$make_base
+mv java/target/vw_jni.lib java/target/vw_jni.Ubuntu.14.amd64.lib"
+
+red_hat_6="yum update -q -y;
+yum install -q -y wget which boost-devel zlib-devel java-1.7.0-openjdk-devel;
+cd /etc/yum.repos.d;
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo;
+yum clean all;
+yum install -q -y devtoolset-2-gcc devtoolset-2-gcc-c++ devtoolset-2-binutils;
+ln -s /opt/rh/devtoolset-2/root/usr/bin/* /usr/local/bin/;
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64;
+cd /vowpal_wabbit;
+make clean;
+cat Makefile | sed 's/-fPIC/-fpermissive -fPIC/g' > Makefile.permissive
+make -f Makefile.permissive;
+rm -f Makefile.permissive
+mv java/target/vw_jni.lib java/target/vw_jni.Red_Hat.6.amd64.lib"
+
+red_hat_7="yum install -q -y gcc-c++ make boost-devel zlib-devel java-1.7.0-openjdk-devel;
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk;
+$make_base
+mv java/target/vw_jni.lib java/target/vw_jni.Red_Hat.7.amd64.lib"
 
 # =============================================================================
 #  Function Definitions:
@@ -75,6 +116,11 @@ install_cask_app() {
   fi
 }
 
+run_docker() {
+  local machine=$1
+  local script=$2
+  docker run -v $(pwd):/vowpal_wabbit $machine /bin/bash -c "$script"
+}
 
 # =============================================================================
 #  Main
@@ -89,76 +135,16 @@ boot2docker delete
 boot2docker init
 boot2docker up
 
-
 # After running boot2docker up this is printed out and it should be the same for everyone
 export DOCKER_HOST=tcp://192.168.59.103:2376
 export DOCKER_CERT_PATH=~/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
 
-docker run -v $(pwd):/vowpal_wabbit 32bit/ubuntu:12.04 /bin/bash -c "\
-apt-get install -qq software-properties-common; \
-apt-get update; \
-apt-get install -qq g++ make libboost-all-dev default-jdk; \
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-i386; \
-cd /vowpal_wabbit; \
-make clean; \
-make; \
-mv java/target/vw_jni.lib java/target/vw_jni.Ubuntu.12.i386.lib"
-
-docker run -v $(pwd):/vowpal_wabbit ubuntu:12.04 /bin/bash -c "\
-apt-get install -qq software-properties-common; \
-apt-get update; \
-apt-get install -qq g++ make libboost-all-dev default-jdk; \
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64; \
-cd /vowpal_wabbit; \
-make clean; \
-make; \
-mv java/target/vw_jni.lib java/target/vw_jni.Ubuntu.12.amd64.lib"
-
-docker run -v $(pwd):/vowpal_wabbit 32bit/ubuntu:14.04 /bin/bash -c "\
-apt-get install -qq software-properties-common; \
-apt-get update; \
-apt-get install -qq g++ make libboost-all-dev default-jdk; \
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386; \
-cd /vowpal_wabbit; \
-make clean; \
-make; \
-mv java/target/vw_jni.lib java/target/vw_jni.Ubuntu.14.i386.lib"
-
-docker run -v $(pwd):/vowpal_wabbit ubuntu:14.04 /bin/bash -c "\
-apt-get install -qq software-properties-common; \
-apt-get update; \
-apt-get install -qq g++ make libboost-all-dev default-jdk; \
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64; \
-cd /vowpal_wabbit; \
-make clean; \
-make; \
-mv java/target/vw_jni.lib java/target/vw_jni.Ubuntu.14.amd64.lib"
-
-docker run -v $(pwd):/vowpal_wabbit centos:7 /bin/bash -c "\
-yum install -q -y gcc-c++ make boost-devel zlib-devel java-1.7.0-openjdk-devel; \
-export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk; \
-cd /vowpal_wabbit; \
-make clean; \
-make; \
-mv java/target/vw_jni.lib java/target/vw_jni.Red_Hat.7.amd64.lib"
-
-cat Makefile | perl -pe 's/-fPIC/-fpermissive -fPIC/g' > Makefile.permissive
-
-docker run -v $(pwd):/vowpal_wabbit centos:6 /bin/bash -c "\
-yum update -q -y; \
-yum install -q -y wget which boost-devel zlib-devel java-1.7.0-openjdk-devel; \
-cd /etc/yum.repos.d; \
-wget http://people.centos.org/tru/devtools-2/devtools-2.repo; \
-yum install -q -y devtoolset-2-gcc devtoolset-2-gcc-c++ devtoolset-2-binutils; \
-ln -s /opt/rh/devtoolset-2/root/usr/bin/* /usr/local/bin/; \
-export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64; \
-cd /vowpal_wabbit; \
-make clean; \
-make -f Makefile.permissive; \
-mv java/target/vw_jni.lib java/target/vw_jni.Red_Hat.6.amd64.lib"
-
-rm Makefile.permissive
+run_docker "ubuntu:12.04" "$ubuntu_12"
+run_docker "32bit/ubuntu:14.04" "$ubuntu_14_32"
+run_docker "ubuntu:14.04" "$ubuntu_14"
+run_docker "centos:6" "$red_hat_6"
+run_docker "centos:7" "$red_hat_7"
 
 make clean
 make
