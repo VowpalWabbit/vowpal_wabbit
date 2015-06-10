e18c9bffcec37fedc0850df882c86db863ef4e0b
deaktator <r.m.deak@gmail.com>
2015-06-10 12:28:33 -0700

    Update build.sh
    Updated build.sh to check for OS via uname.   If not Darwin, die.  Check if brew is not and installed.  If not, prompt user whether to attempt install.  Then only install brew packages that aren't present.

--- java/src/main/bin/build.sh
+++ java/src/main/bin/build.sh
@@ -1,13 +1,94 @@
 #!/usr/bin/env bash
 
-brew tap phinze/homebrew-cask
-brew install brew-cask
-brew cask install virtualbox
-brew install boot2docker
-brew install docker
+# =============================================================================
+#  Constants:
+# =============================================================================
+__not_darwin=1
+__brew_not_installed=2
+
+
+# =============================================================================
+#  Function Definitions:
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+#  Print red text to stderr.
+# -----------------------------------------------------------------------------
+red() {
+  # https://linuxtidbits.wordpress.com/2008/08/11/output-color-on-bash-scripts/
+  echo >&2 "$(tput setaf 1)${1}$(tput sgr0)"
+}
+
+# -----------------------------------------------------------------------------
+#  Print yellow text to stderr.
+# -----------------------------------------------------------------------------
+yellow() {
+  echo >&2 "$(tput setaf 3)${1}$(tput sgr0)"
+}
+
+die() { red $2; exit $1; }
+
+# -----------------------------------------------------------------------------
+#  Check that the OS is OS X.  If not, die.  If so, check that brew is 
+#  installed.  If brew is not installed, ask the user if they want to install.  
+#  If so, attempt to install.  After attempting install, check for existence.  
+#  If it still doesn't exist, fail.
+# -----------------------------------------------------------------------------
+check_brew_installed() {
+  local os=$(uname)
+  if [[ "$os" != "Darwin" ]]; then
+    die $__not_darwin "Build script only supported on OS X.  OS=${os}.  Aborting ..."
+  else
+    if ! brew help 1>/dev/null 2>/dev/null; then 
+      red "brew not installed.  To install: Y or N?"
+      read should_install
+      if [[ "Y" == "${should_install^^}" ]]; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      fi
+      if ! brew help 1>/dev/null 2>/dev/null; then 
+        die $__brew_not_installed "brew not installed.  Aborting ..."
+      fi
+    fi
+  fi
+}
+
+install_brew_cask() {
+  if ! brew cask 1>/dev/null 2>/dev/null; then 
+    yellow "Installing brew-cask..."
+    brew install caskroom/cask/brew-cask
+  fi
+}
+
+install_brew_app() {
+  local app=$1
+  if ! brew list | grep $app 1>/dev/null; then
+    yellow "installing brew app: $app"
+    brew install $app
+  fi
+}
+
+install_cask_app() {
+  local app=$1
+  if ! brew cask list | grep $app 1>/dev/null; then
+    yellow "installing brew cask app: $app"
+    brew cask install $app
+  fi
+}
+
+
+# =============================================================================
+#  Main
+# =============================================================================
+
+check_brew_installed
+install_brew_cask
+install_cask_app "virtualbox"
+install_brew_app "boot2docker"
+install_brew_app "docker"
 boot2docker delete
 boot2docker init
 boot2docker up
+
 
 # After running boot2docker up this is printed out and it should be the same for everyone
 export DOCKER_HOST=tcp://192.168.59.103:2376
