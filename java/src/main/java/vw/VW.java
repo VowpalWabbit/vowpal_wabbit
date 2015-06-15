082e99f070005d781744f0def823288eb37f6771
Jon Morra <jmorra@eharmony.com>
2015-06-15 12:25:11 -0700

    Refactoring some things and making JNI boost exception handling more robust

--- java/src/main/java/vw/VW.java
+++ java/src/main/java/vw/VW.java
@@ -1,14 +1,24 @@
 package vw;
 
+import vw.jni.NativeUtils;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * A JNI layer for submitting examples to VW and getting predictions back.  It should be noted
- * that at this time VW is NOT thread safe, and therefore neither is the JNI layer.
+ * that at this time VW is NOT thread safe, and therefore neither is the JNI layer.  It should be noted
+ * that this was originally written with a bulk interface that was later removed because of benchmarking
+ * data found <a href="https://microbenchmarks.appspot.com/runs/817d246a-5f90-478a-bc27-d5912d2ff874#r:scenario.benchmarkSpec.methodName,scenario.benchmarkSpec.parameters.loss,scenario.benchmarkSpec.parameters.mutabilityPolicy,scenario.benchmarkSpec.parameters.nExamples">here</a>.
  */
 public class VW implements Closeable {
+
+    public static void main(String[] args) {
+        new VW("").close();
+        new VW("--quiet").close();
+    }
+
     static {
         try {
             NativeUtils.loadOSDependentLibrary("/vw_jni", ".lib");
