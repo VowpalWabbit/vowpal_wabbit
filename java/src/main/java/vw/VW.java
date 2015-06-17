5f50935ed4c4f73b29473fb585b6df2e13b6a195
Jon Morra <jmorra@eharmony.com>
2015-06-17 10:23:18 -0700

    Adding docs

--- java/src/main/java/vw/VW.java
+++ java/src/main/java/vw/VW.java
@@ -12,6 +12,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * that at this time VW is NOT thread safe, and therefore neither is the JNI layer.  It should be noted
  * that this was originally written with a bulk interface that was later removed because of benchmarking
  * data found <a href="https://microbenchmarks.appspot.com/runs/817d246a-5f90-478a-bc27-d5912d2ff874#r:scenario.benchmarkSpec.methodName,scenario.benchmarkSpec.parameters.loss,scenario.benchmarkSpec.parameters.mutabilityPolicy,scenario.benchmarkSpec.parameters.nExamples">here</a>.
+ * Please note that close MUST be called in order to free up the memory on the C side.
  */
 public class VW implements Closeable {
 
