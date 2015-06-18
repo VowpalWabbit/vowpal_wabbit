6a7b383a1a205b0d31d419c8d81c51cf6463d036
Luong Hoang <lhoang@live.com>
2015-06-18 02:06:08 -0400

    bootstrap & code-complete

--- Explore/BootstrapExplorer.cs
+++ Explore/BootstrapExplorer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Linq;
 namespace MultiWorldTesting
 {
     /// <summary>
@@ -12,6 +14,11 @@ namespace MultiWorldTesting
 	/// <typeparam name="TContext">The Context type.</typeparam>
 	public class BootstrapExplorer<TContext> : IExplorer<TContext>, IConsumePolicies<TContext>
 	{
+        private IPolicy<TContext>[] defaultPolicyFunctions;
+        private bool explore;
+        private readonly uint bags;
+	    private readonly uint numActions;
+
 		/// <summary>
 		/// The constructor is the only public member, because this should be used with the MwtExplorer.
 		/// </summary>
@@ -19,32 +26,90 @@ namespace MultiWorldTesting
 		/// <param name="numActions">The number of actions to randomize over.</param>
         public BootstrapExplorer(IPolicy<TContext>[] defaultPolicies, uint numActions)
 		{
-            // TODO: implement
+            VariableActionHelper.ValidateNumberOfActions(numActions);
+
+            if (defaultPolicies == null || defaultPolicies.Length < 1)
+		    {
+			    throw new ArgumentException("Number of bags must be at least 1.");
+		    }
+
+            this.defaultPolicyFunctions = defaultPolicies;
+            this.bags = (uint)this.defaultPolicyFunctions.Length;
+            this.numActions = numActions;
+            this.explore = true;
         }
 
         /// <summary>
         /// Initializes a bootstrap explorer with variable number of actions.
         /// </summary>
         /// <param name="defaultPolicies">A set of default policies to be uniform random over.</param>
-        public BootstrapExplorer(IPolicy<TContext>[] defaultPolicies)
+        public BootstrapExplorer(IPolicy<TContext>[] defaultPolicies) :
+            this(defaultPolicies, uint.MaxValue)
         {
-            // TODO: implement
+            VariableActionHelper.ValidateContextType<TContext>();
         }
 
         public void UpdatePolicy(IPolicy<TContext>[] newPolicies)
         {
-            // TODO: implement
+            this.defaultPolicyFunctions = newPolicies;
         }
 
         public void EnableExplore(bool explore)
         {
-            // TODO: implement
+            this.explore = explore;
         }
 
         public ExploreDecision Choose_Action(long saltedSeed, TContext context)
         {
-            // TODO: implement
-            return null;
+            uint numActions = VariableActionHelper.GetNumberOfActions(context, this.numActions);
+
+            var random = new Random((int)saltedSeed);
+
+            // Select bag
+            uint chosenBag = (uint)random.Next(0, (int)this.bags);
+
+            // Invoke the default policy function to get the action
+            uint chosenAction = 0;
+            float actionProbability = 0f;
+
+            if (this.explore)
+            {
+                uint actionFromBag = 0;
+                uint[] actionsSelected = Enumerable.Repeat<uint>(0, (int)numActions).ToArray();
+
+                // Invoke the default policy function to get the action
+                for (int currentBag = 0; currentBag < this.bags; currentBag++)
+                {
+                    // TODO: can VW predict for all bags on one call? (returning all actions at once)
+                    // if we trigger into VW passing an index to invoke bootstrap scoring, and if VW model changes while we are doing so, 
+                    // we could end up calling the wrong bag
+                    actionFromBag = this.defaultPolicyFunctions[currentBag].ChooseAction(context);
+
+                    if (actionFromBag == 0 || actionFromBag > numActions)
+                    {
+                        throw new ArgumentException("Action chosen by default policy is not within valid range.");
+                    }
+
+                    if (currentBag == chosenBag)
+                    {
+                        chosenAction = actionFromBag;
+                    }
+                    //this won't work if actions aren't 0 to Count
+                    actionsSelected[actionFromBag - 1]++; // action id is one-based
+                }
+                actionProbability = (float)actionsSelected[chosenAction - 1] / this.bags; // action id is one-based
+            }
+            else
+            {
+                chosenAction = this.defaultPolicyFunctions[0].ChooseAction(context);
+                actionProbability = 1f;
+            }
+            return new ExploreDecision
+            {
+                Action = chosenAction,
+                Probability = actionProbability,
+                ShouldRecord = true
+            };
         }
     };
 }
