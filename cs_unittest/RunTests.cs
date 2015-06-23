13d402b6f0f03730fc7e441b8c582fe8bfc37fcb
Markus Cozowicz <marcozo@microsoft.com>
2015-06-22 20:33:26 -0400

    trying to fix build

--- cs_unittest/RunTests.cs
+++ cs_unittest/RunTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cs_unittest
@@ -19,8 +20,20 @@ namespace cs_unittest
         [DeploymentItem(@"pred-sets\ref", @"pred-sets\ref")]
         public void RunAllTest()
         {
-            var runTests = File.ReadAllLines("RunTests")
-                .SkipWhile(l => l != "__DATA__");
+//            var runTests = File.ReadAllText("RunTests");
+
+//            var match = Regex.Match(runTests, "__DATA__(.*)$", RegexOptions.Multiline);
+
+//            Assert.IsTrue(match.Success,"RunTests format changed");
+
+//            runTests = match.Groups[1].Value;
+
+//            var matches = Regex.Matches(runTests, @"
+//                    # Test (?<nr>\d+) : (?<comment>.*\\n)?
+//                    {VW}
+//
+//                    ", RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace)
+
         }
     }
 }
