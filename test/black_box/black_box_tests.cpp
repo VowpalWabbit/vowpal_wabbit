d7f0a0b550dd8c2bd55acee18f9555533d564a13
Luong Hoang <lhoang@live.com>
2015-06-23 19:33:09 -0400

    added bb interface for testing & prg test

---
+++ test/black_box/black_box_tests.cpp
@@ -0,0 +1,88 @@
+#include "black_box_tests.h"
+
+void prg_test(Value& v)
+{
+    u64 seed = v["Seed"].GetUint64();
+    int iterations = v["Iterations"].GetInt();
+
+    int uniformIntervalStart = -1;
+    int uniformIntervalEnd = -1;
+    if (!v["UniformInterval"].IsNull())
+    {
+        uniformIntervalStart = v["UniformInterval"]["Item1"].GetInt();
+        uniformIntervalEnd = v["UniformInterval"]["Item2"].GetInt();
+    }
+
+    const char* outputFile = v["OutputFile"].GetString();
+
+    PRG::prg rg(seed);
+
+    ofstream out_file(outputFile);
+
+    for (int i = 0; i < iterations; i++)
+    {
+        if (uniformIntervalStart != -1 && uniformIntervalEnd != -1)
+        {
+            u32 random = rg.Uniform_Int(uniformIntervalStart, uniformIntervalEnd);
+            out_file << random << endl;
+        }
+        else
+        {
+            float random = rg.Uniform_Unit_Interval();
+            out_file << random << endl;
+        }
+    }
+}
+
+void hash_test(Value& v)
+{
+
+}
+
+void explore_test(Value& v)
+{
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    if (argc != 2)
+    {
+        return 0;
+    }
+
+    ifstream json_file(argv[1]);
+    string json_file_content;
+    while (json_file >> json_file_content) { }
+
+    Document d;
+    d.Parse(json_file_content.c_str());
+
+    for (SizeType i = 0; i < d.Size(); i++)
+    {
+        switch (d[i]["Type"].GetInt())
+        {
+        case 0:
+            prg_test(d[i]);
+            break;
+        case 1:
+            hash_test(d[i]);
+            break;
+        case 2:
+            explore_test(d[i]);
+            break;
+        }
+    }
+    //Value& s = d["stars"];
+    //s.SetInt(s.GetInt() + 1);
+
+    //// 3. Stringify the DOM
+    //StringBuffer buffer;
+    //Writer<StringBuffer> writer(buffer);
+    //d.Accept(writer);
+
+    //// Output {"project":"rapidjson","stars":11}
+    //std::cout << buffer.GetString() << std::endl;
+
+	return 0;
+}
---
+++ test/black_box/black_box_tests.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "tchar.h"
+#include "MWTExplorer.h"
+#include "utility.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using namespace rapidjson;
+using namespace MultiWorldTesting;
