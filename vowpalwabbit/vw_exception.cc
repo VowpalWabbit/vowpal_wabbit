c6278113bf5de63e790c279d8356a17d9056aaec
Martin Popel <popel@ufal.mff.cuni.cz>
2015-06-26 22:49:27 +0200

    fix order of lineNumber and message initialization
    This should solve the following warning:
    
    In file included from vw_exception.cc:1:0:
    vw_exception.h: In constructor ‘VW::vw_exception::vw_exception(const char*, int, std::string)’:
    vw_exception.h:28:7: warning: ‘VW::vw_exception::lineNumber’ will be initialized after [-Wreorder]
       int lineNumber;
           ^
    vw_exception.h:22:15: warning:   ‘std::string VW::vw_exception::message’ [-Wreorder]
       std::string message;
                   ^
    vw_exception.cc:5:2: warning:   when initialized here [-Wreorder]
      vw_exception::vw_exception(const char* pfile, int plineNumber, std::string pmessage)

--- vowpalwabbit/vw_exception.cc
+++ vowpalwabbit/vw_exception.cc
@@ -2,32 +2,26 @@
 
 namespace VW {
 
-	vw_exception::vw_exception(const char* pfile, int plineNumber, std::string pmessage)
-	  : file(pfile), message(pmessage), lineNumber(plineNumber)
-	{
-	}
+  vw_exception::vw_exception(const char* pfile, int plineNumber, std::string pmessage)
+    : file(pfile), lineNumber(plineNumber), message(pmessage) {
+  }
 
-	vw_exception::vw_exception(const vw_exception& ex)
-	  : file(ex.file), message(ex.message), lineNumber(ex.lineNumber)
-	{
-	}
+  vw_exception::vw_exception(const vw_exception& ex)
+    : file(ex.file), lineNumber(ex.lineNumber), message(ex.message) {
+  }
 
-	vw_exception::~vw_exception() _NOEXCEPT
-	{
-	}
+  vw_exception::~vw_exception() _NOEXCEPT {
+  }
 
-	const char* vw_exception::what() const _NOEXCEPT
-	{
-		return message.c_str();
-	}
-	
-	const char* vw_exception::Filename() const
-	{
-		return file;
-	}
+  const char* vw_exception::what() const _NOEXCEPT {
+    return message.c_str();
+  }
 
-	int vw_exception::LineNumber() const
-	{
-		return lineNumber;
-	}
+  const char* vw_exception::Filename() const {
+    return file;
+  }
+
+  int vw_exception::LineNumber() const {
+    return lineNumber;
+  }
 }
