c9b0d20b77a56ba14a9bf27ab370ae84702c9d33
Markus Cozowicz <marcozo@microsoft.com>
2015-06-30 15:36:47 -0400

    Trigger Build

--- cs/CreateNuGet.cmd
+++ cs/CreateNuGet.cmd
@@ -1,3 +1,5 @@
 @echo off
 del bin\x64\Release\*.nupkg
 ..\vowpalwabbit\.nuget\nuget pack cs.nuspec -Prop "Configuration=Release;Platform=x64" -Prop SolutionDir=..\vowpalwabbit\ -OutputDirectory bin\x64\Release
+
+
