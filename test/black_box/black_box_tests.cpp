cc486b3209afc94d8b36bb7b601793565613accf
Luong Hoang <lhoang@live.com>
2015-07-01 19:06:23 -0400

    softmax c++ bb interface

--- test/black_box/black_box_tests.cpp
+++ test/black_box/black_box_tests.cpp
@@ -76,6 +76,18 @@ TauFirstExplorer<Ctx>* CreateTauFirstExplorer(IPolicy<Ctx>& policy, u32 tau, u32
 }
 
 template <typename Ctx>
+SoftmaxExplorer<Ctx>* CreateSoftmaxExplorer(IScorer<Ctx>& scorer, float lambda, u32 num_actions, std::true_type)
+{
+    return new SoftmaxExplorer<Ctx>(scorer, lambda);
+}
+
+template <typename Ctx>
+SoftmaxExplorer<Ctx>* CreateSoftmaxExplorer(IScorer<Ctx>& scorer, float lambda, u32 num_actions, std::false_type)
+{
+    return new SoftmaxExplorer<Ctx>(scorer, lambda, num_actions);
+}
+
+template <typename Ctx>
 void explore_epsilon_greedy(
     const char* app_id, 
     int policy_type, 
@@ -134,26 +146,85 @@ void explore_tau_first(
 
     switch (policy_type)
     {
-    case 0: // fixed policy
-    {
-        u32 policy_action = config_policy["Action"].GetUint();
-
-        TestPolicy<Ctx> policy;
-        policy.Set_Action_To_Choose(policy_action);
-
-        unique_ptr<TauFirstExplorer<Ctx>> explorer;
-
-        explorer.reset(CreateTauFirstExplorer(policy, tau, num_actions, std::is_base_of<IVariableActionContext, Ctx>()));
-
-        for (SizeType i = 0; i < experimental_unit_ids.Size(); i++)
+        case 0: // fixed policy
         {
-            mwt.Choose_Action(*explorer.get(), string(experimental_unit_ids[i].GetString()), contexts.at(i));
+            u32 policy_action = config_policy["Action"].GetUint();
+
+            TestPolicy<Ctx> policy;
+            policy.Set_Action_To_Choose(policy_action);
+
+            unique_ptr<TauFirstExplorer<Ctx>> explorer;
+
+            explorer.reset(CreateTauFirstExplorer(policy, tau, num_actions, std::is_base_of<IVariableActionContext, Ctx>()));
+
+            for (SizeType i = 0; i < experimental_unit_ids.Size(); i++)
+            {
+                mwt.Choose_Action(*explorer.get(), string(experimental_unit_ids[i].GetString()), contexts.at(i));
+            }
+
+            out_file << recorder.Get_Recording() << endl;
+
+            break;
         }
-
-        out_file << recorder.Get_Recording() << endl;
-
-        break;
     }
+}
+
+template <typename Ctx>
+void explore_softmax(
+    const char* app_id,
+    int scorer_type,
+    Value& config_scorer,
+    float lambda,
+    u32 num_actions,
+    Value& experimental_unit_ids,
+    vector<Ctx> contexts,
+    const char* output_file)
+{
+    ofstream out_file(output_file);
+
+    StringRecorder<Ctx> recorder;
+    MwtExplorer<Ctx> mwt(std::string(app_id), recorder);
+
+    switch (scorer_type)
+    {
+        case 0: // all-equal fixed scorer
+        {
+            int scorer_score = config_scorer["Score"].GetInt();
+
+            TestScorer<Ctx> scorer(scorer_score, num_actions);
+
+            unique_ptr<SoftmaxExplorer<Ctx>> explorer;
+
+            explorer.reset(CreateSoftmaxExplorer(scorer, lambda, num_actions, std::is_base_of<IVariableActionContext, Ctx>()));
+
+            for (SizeType i = 0; i < experimental_unit_ids.Size(); i++)
+            {
+                mwt.Choose_Action(*explorer.get(), string(experimental_unit_ids[i].GetString()), contexts.at(i));
+            }
+
+            out_file << recorder.Get_Recording() << endl;
+
+            break;
+        }
+        case 1: // integer progression scorer
+        {
+            int scorer_start_score = config_scorer["Start"].GetInt();
+
+            TestScorer<Ctx> scorer(scorer_start_score, num_actions, false);
+
+            unique_ptr<SoftmaxExplorer<Ctx>> explorer;
+
+            explorer.reset(CreateSoftmaxExplorer(scorer, lambda, num_actions, std::is_base_of<IVariableActionContext, Ctx>()));
+
+            for (SizeType i = 0; i < experimental_unit_ids.Size(); i++)
+            {
+                mwt.Choose_Action(*explorer.get(), string(experimental_unit_ids[i].GetString()), contexts.at(i));
+            }
+
+            out_file << recorder.Get_Recording() << endl;
+
+            break;
+        }
     }
 }
 
@@ -217,34 +288,80 @@ void test_tau_first(Value& v)
 
     switch (v["ContextType"].GetInt())
     {
-    case 0: // fixed action context
-    {
-        vector<TestContext> contexts;
-        for (SizeType i = 0; i < experimental_unit_id_list.Size(); i++)
+        case 0: // fixed action context
         {
-            TestContext tc;
-            tc.Id = i;
-            contexts.push_back(tc);
+            vector<TestContext> contexts;
+            for (SizeType i = 0; i < experimental_unit_id_list.Size(); i++)
+            {
+                TestContext tc;
+                tc.Id = i;
+                contexts.push_back(tc);
+            }
+
+            explore_tau_first(app_id, policy_type, config_policy, tau, num_actions, experimental_unit_id_list, contexts, output_file);
+
+            break;
         }
-
-        explore_tau_first(app_id, policy_type, config_policy, tau, num_actions, experimental_unit_id_list, contexts, output_file);
-
-        break;
-    }
-    case 1: // variable action context
-    {
-        vector<TestVarContext> contexts;
-        for (SizeType i = 0; i < experimental_unit_id_list.Size(); i++)
+        case 1: // variable action context
         {
-            TestVarContext tc(num_actions);
-            tc.Id = i;
-            contexts.push_back(tc);
+            vector<TestVarContext> contexts;
+            for (SizeType i = 0; i < experimental_unit_id_list.Size(); i++)
+            {
+                TestVarContext tc(num_actions);
+                tc.Id = i;
+                contexts.push_back(tc);
+            }
+
+            explore_tau_first(app_id, policy_type, config_policy, tau, num_actions, experimental_unit_id_list, contexts, output_file);
+
+            break;
         }
-
-        explore_tau_first(app_id, policy_type, config_policy, tau, num_actions, experimental_unit_id_list, contexts, output_file);
-
-        break;
     }
+}
+
+// TODO: refactor
+void test_softmax(Value& v)
+{
+    const char* output_file = v["OutputFile"].GetString();
+    const char* app_id = v["AppId"].GetString();
+    u32 num_actions = v["NumberOfActions"].GetUint();
+
+    Value& experimental_unit_id_list = v["ExperimentalUnitIdList"];
+
+    float lambda = v["Lambda"].GetDouble();
+    Value& config_scorer = v["ScorerConfiguration"];
+    int scorer_type = config_scorer["ScorerType"].GetInt();
+
+    switch (v["ContextType"].GetInt())
+    {
+        case 0: // fixed action context
+        {
+            vector<TestContext> contexts;
+            for (SizeType i = 0; i < experimental_unit_id_list.Size(); i++)
+            {
+                TestContext tc;
+                tc.Id = i;
+                contexts.push_back(tc);
+            }
+
+            explore_softmax(app_id, scorer_type, config_scorer, lambda, num_actions, experimental_unit_id_list, contexts, output_file);
+
+            break;
+        }
+        case 1: // variable action context
+        {
+            vector<TestVarContext> contexts;
+            for (SizeType i = 0; i < experimental_unit_id_list.Size(); i++)
+            {
+                TestVarContext tc(num_actions);
+                tc.Id = i;
+                contexts.push_back(tc);
+            }
+
+            explore_softmax(app_id, scorer_type, config_scorer, lambda, num_actions, experimental_unit_id_list, contexts, output_file);
+
+            break;
+        }
     }
 }
 
@@ -277,6 +394,9 @@ int _tmain(int argc, _TCHAR* argv[])
             break;
         case 3:
             test_tau_first(d[i]);
+            break;
+        case 4:
+            test_softmax(d[i]);
             break;
         }
     }
