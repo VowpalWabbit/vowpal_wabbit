cbb0634d2f034104c88b540d1845291b92b15a00
Luong Hoang <lhoang@live.com>
2015-07-09 21:05:37 -0400

    some hash benchmark codes

--- Explore/MurMurHash3.cs
+++ Explore/MurMurHash3.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace MultiWorldTesting
 {
@@ -10,6 +12,8 @@ namespace MultiWorldTesting
     {
         private const uint c1 = 0xcc9e2d51;
         private const uint c2 = 0x1b873593;
+        private static byte[] ring = new byte[8];
+        private static ByteToUIntStruct btus = new ByteToUIntStruct();
 
         /// <summary>
         /// Computes hash code for the specified string or converts it to integer if applicable.
@@ -29,14 +33,14 @@ namespace MultiWorldTesting
 
             uint h1 = seed;
 
-            byte[] ring = new byte[8];
             var pos1 = 0;
             var pos2 = 0;
 
             for (int i = 0; i < str.Length;)
             {
                 ulong codePoint = str[i++];
-                
+
+                // TODO: add test for all possible unicode characters
                 // Convert to Unicode code point
                 // More information can be found at: https://en.wikipedia.org/wiki/UTF-16 
                 if ((codePoint & 0xFC00) == 0xD800)
@@ -141,10 +145,7 @@ namespace MultiWorldTesting
         /// </summary>
         /// <param name="s">The string to hash.</param>
         /// <returns>The computed hash code or converted integer for the string.</returns>
-        /// <remarks>
-        /// If strings are known to be ASCII only, this overload can be used for faster performance.
-        /// </remarks>
-        public static ulong ComputeIdHashAscii(string s)
+        public static ulong ComputeIdHashGetBytes(string s)
         {
             int sInt = 0;
             if (int.TryParse(s, out sInt))
@@ -154,11 +155,7 @@ namespace MultiWorldTesting
             else
             {
                 // get raw bytes from string
-                byte[] keys = new byte[s.Length];
-                for (int j = 0; j < s.Length; j++)
-                {
-                    keys[j] = (byte)s[j];
-                }
+                byte[] keys = System.Text.Encoding.UTF8.GetBytes(s);
 
                 uint h1 = 0;
                 uint k1 = 0;
@@ -207,6 +204,178 @@ namespace MultiWorldTesting
             }
         }
 
+        public static ulong ComputeIdHashManualGetBytes(string s)
+        {
+            int sInt = 0;
+            if (int.TryParse(s, out sInt))
+            {
+                return (ulong)sInt;
+            }
+            else
+            {
+                // get raw bytes from string
+                byte[] keys = ManualGetUTF8Bytes(s);
+
+                uint h1 = 0;
+                uint k1 = 0;
+
+                int length = keys.Length;
+                int i = 0;
+                while (i <= length - 4)
+                {
+                    // convert byte array to integer
+                    k1 = (uint)(keys[i] | keys[i + 1] << 8 | keys[i + 2] << 16 | keys[i + 3] << 24);
+
+                    k1 *= c1;
+                    k1 = rotl32(k1, 15);
+                    k1 *= c2;
+
+                    h1 ^= k1;
+                    h1 = rotl32(h1, 13);
+                    h1 = h1 * 5 + 0xe6546b64;
+
+                    i += 4;
+                }
+
+                k1 = 0;
+                int tail = length - length % 4;
+                switch (length & 3)
+                {
+                    case 3:
+                        k1 ^= (uint)(keys[tail + 2] << 16);
+                        goto case 2;
+                    case 2:
+                        k1 ^= (uint)(keys[tail + 1] << 8);
+                        goto case 1;
+                    case 1:
+                        k1 ^= (uint)(keys[tail]);
+                        k1 *= c1;
+                        k1 = rotl32(k1, 15);
+                        k1 *= c2;
+                        h1 ^= k1;
+                        break;
+                }
+
+                // finalization
+                h1 ^= (uint)length;
+
+                return fmix(h1);
+            }
+        }
+
+        public static ulong ComputeIdHashGetBytesStruct(string s)
+        {
+            int sInt = 0;
+            if (int.TryParse(s, out sInt))
+            {
+                return (ulong)sInt;
+            }
+            else
+            {
+                // get raw bytes from string
+                
+                btus.Bytes = System.Text.Encoding.UTF8.GetBytes(s);
+
+                uint h1 = 0;
+                uint k1 = 0;
+
+                int length = btus.Bytes.Length;
+                int i = 0;
+                int j = 0;
+                while (i <= length - 4)
+                {
+                    // convert byte array to integer
+                    k1 = btus.UnsignedIntegers[j];
+
+                    k1 *= c1;
+                    k1 = rotl32(k1, 15);
+                    k1 *= c2;
+
+                    h1 ^= k1;
+                    h1 = rotl32(h1, 13);
+                    h1 = h1 * 5 + 0xe6546b64;
+
+                    i += 4;
+                    j++;
+                }
+
+                k1 = 0;
+                int tail = length - length % 4;
+                switch (length & 3)
+                {
+                    case 3:
+                        k1 ^= (uint)(btus.Bytes[tail + 2] << 16);
+                        goto case 2;
+                    case 2:
+                        k1 ^= (uint)(btus.Bytes[tail + 1] << 8);
+                        goto case 1;
+                    case 1:
+                        k1 ^= (uint)(btus.Bytes[tail]);
+                        k1 *= c1;
+                        k1 = rotl32(k1, 15);
+                        k1 *= c2;
+                        h1 ^= k1;
+                        break;
+                }
+
+                // finalization
+                h1 ^= (uint)length;
+
+                return fmix(h1);
+            }
+        }
+
+        private static byte[] ManualGetUTF8Bytes(string str)
+        {
+            List<byte> bytes = new List<byte>(str.Length * 2);
+            for (int i = 0; i < str.Length; )
+            {
+                ulong codePoint = str[i++];
+
+                // Convert to Unicode code point
+                // More information can be found at: https://en.wikipedia.org/wiki/UTF-16 
+                if ((codePoint & 0xFC00) == 0xD800)
+                {
+                    // Equiv. range check between 0xD800 to 0xDBFF inclusive
+                    var code_unit_2 = str[i];
+                    if ((code_unit_2 & 0xFC00) == 0xDC00) // Equiv. range check between 0xDC00 to 0xDFFF inclusive
+                    {
+                        codePoint = (codePoint << 10) + code_unit_2 - 0x35FDC00;
+                        i++;
+                    }
+                }
+
+                // Convert to UTF-8 bytes 
+                if (codePoint < 0x80)
+                {
+                    bytes.Add((byte)codePoint);
+                }
+                else if (codePoint <= 0x7FF)
+                {
+                    bytes.Add((byte)((codePoint >> 6) + 0xC0));
+                    bytes.Add((byte)((codePoint & 0x3F) + 0x80));
+                }
+                else if (codePoint <= 0xFFFF)
+                {
+                    bytes.Add((byte)((codePoint >> 12) + 0xE0));
+                    bytes.Add((byte)(((codePoint >> 6) & 0x3F) + 0x80));
+                    bytes.Add((byte)((codePoint & 0x3F) + 0x80));
+                }
+                else if (codePoint <= 0x10FFFF)
+                {
+                    bytes.Add((byte)((codePoint >> 18) + 0xF0));
+                    bytes.Add((byte)(((codePoint >> 12) & 0x3F) + 0x80));
+                    bytes.Add((byte)(((codePoint >> 6) & 0x3F) + 0x80));
+                    bytes.Add((byte)((codePoint & 0x3F) + 0x80));
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid codepoint: " + codePoint);
+                }
+            }
+            return bytes.ToArray();
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static uint rotl32(uint x, byte r)
         {
@@ -251,5 +420,15 @@ namespace MultiWorldTesting
             h1 = rotl32(h1, 13);
             h1 = h1 * 5 + 0xe6546b64;
         }
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct ByteToUIntStruct
+    {
+        [FieldOffset(0)]
+        public byte[] Bytes;
+
+        [FieldOffset(0)]
+        public uint[] UnsignedIntegers;
     }
 }
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -13,4 +13,158 @@ namespace ExploreSample
             cs_test.ExploreOnlySample.Run();
         }
     }
+
+    public class HashBenchmarks
+    {
+        static string[] data = 
+        {
+            "wLceNLHVtpuZYtPwPaQ",
+            "949007             ",
+            "Kvq7Hj6RSImhZUhcZuL",
+            "KDqmcZO2h6CIM1j    ",
+            "656656             ",
+            "X8bRcLpb8yrIkA2A   ",
+            "k5USpack9N         ",
+            "Okv90y9lOamog3qXRIk",
+            "860628             ",
+            "903251             ",
+            "289149             ",
+            "727293             ",
+            "660473             ",
+            "ciajGNL930GlKi5b   ",
+            "744202             ",
+            "458807             ",
+            "466125             ",
+            "101824             ",
+            "0RVP8HWyKGFjkJG8RA ",
+            "mm3XQ0ZKJQ4rPmtNN  ",
+            "ZL7Z6bzFVsL0VQGe5ss",
+            "690592             ",
+            "Apbr4WNUDHmL7OWxm  ",
+            "342052             ",
+            "286245             ",
+            "JbIIXVVbS3Y79uj4iI ",
+            "S9E90IvFAVt        ",
+            "z2QWOpzi63         ",
+            "gnCClcujq79        ",
+            "hOZPaw9s4922I3S    ",
+            "mFWZjVtCOiymM2     ",
+            "m6a93w7IRLNaadJbL  ",
+            "758870             ",
+            "164290             ",
+            "971935             ",
+            "MVObGSH9iWxiyvp    ",
+            "135400             ",
+            "T2b9WalhX9c        ",
+            "CQaS6KtGArRLtM5v   ",
+            "B0lNkkeP57ZLJjZAwfP",
+            "695049             ",
+            "BSbUX2YPm1daHvo6   ",
+            "ReVgoh7mtQpghPDl   ",
+            "I0RnHRdk5IRFHJZaZST",
+            "489901             ",
+            "a9IZGkY6WLtX0X37D  ",
+            "061731             ",
+            "402102             ",
+            "IgRGpl2Z0OdgNzr6AH ",
+            "tNlzNvlPQ0hXFlzjpe8",
+            "m2JmhQ8L6DEnauuvSst",
+            "141010             ",
+            "534087             ",
+            "599686             ",
+            "000093             ",
+            "707313             ",
+            "563622             ",
+            "HlcM6fNDjW         ",
+            "4qEn6lfmhd2b6Fo    ",
+            "ph5x9nJTFV         ",
+            "783062             ",
+            "403127             ",
+            "fGbvUKatET3SAf0rfA ",
+            "IduDv41Z1z7Opirz   ",
+            "625285             ",
+            "HbsPUqTZvWHI4ylB   ",
+            "554240             ",
+            "849636             ",
+            "1ElP3So1fCS        ",
+            "539836             ",
+            "jELB4FrYkqwpmecr   ",
+            "Ko4EWBb3gFqN0PR7pvf",
+            "VMX4dVyfAZ0V9VwK   ",
+            "K0BYm86Zg8PogMNSo  ",
+            "ajfcoff0sqt        ",
+            "373791             ",
+            "220160             ",
+            "dxZoyNeZZMiO       ",
+            "286375             ",
+            "DEy4nNiHHd9nN      ",
+            "3gverMSb6ANY3wLj   ",
+            "ATGPA40OShUer      ",
+            "548754             ",
+            "7NdgIl223apO       ",
+            "aaas hu as 撒 asfasd	阿萨",
+            "oof Ồ hử hị ộ ở ỗ õ ẽ uyễn \r \t \n \\    ",
+            "934625             "
+        };
+
+        public static void Run()
+        {
+            var sw = new System.Diagnostics.Stopwatch();
+            foreach (string item in data)
+            {
+                ulong ring = MurMurHash3.ComputeIdHash(item);
+                ulong gbs = MurMurHash3.ComputeIdHashGetBytesStruct(item);
+                ulong gb = MurMurHash3.ComputeIdHashGetBytes(item);
+                ulong mgb = MurMurHash3.ComputeIdHashManualGetBytes(item);
+
+                if (ring != gbs || ring != gb || ring != mgb)
+                {
+                    throw new Exception();
+                }
+            }
+            Console.WriteLine("Correct.");
+
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                foreach (string item in data)
+                {
+                    MurMurHash3.ComputeIdHash(item);
+                }
+            }
+
+            Console.WriteLine("Ring: " + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                foreach (string item in data)
+                {
+                    MurMurHash3.ComputeIdHashGetBytesStruct(item);
+                }
+            }
+
+            Console.WriteLine("Struct Get Bytes: " + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                foreach (string item in data)
+                {
+                    MurMurHash3.ComputeIdHashGetBytes(item);
+                }
+            }
+            Console.WriteLine("Get Bytes: " + sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                foreach (string item in data)
+                {
+                    MurMurHash3.ComputeIdHashManualGetBytes(item);
+                }
+            }
+            Console.WriteLine("Manual Get Bytes: " + sw.ElapsedMilliseconds);
+        }
+    }
 }
