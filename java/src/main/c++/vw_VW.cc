15860fbf49f7a4eca1f6990584467b4268d32a1e
Ryan Deak <r.m.deak@gmail.com>
2015-09-28 13:40:02 -0700

    Working build #799

--- java/src/main/c++/vw_VW.cc
+++ java/src/main/c++/vw_VW.cc
@@ -3,8 +3,7 @@
 #include "vw_errors.h"
 #include "vw_VW.h"
 
-
-JNIEXPORT jstring JNICALL Java_vw_VW_version(JNIEnv *env, jobject obj) {
+JNIEXPORT jstring JNICALL Java_vw_VW_version(JNIEnv *env, jclass obj) {
     return env->NewStringUTF(PACKAGE_VERSION);
 }
 
