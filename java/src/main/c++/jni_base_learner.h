ff7c034bce211848212b6999731b0715d293f255
Jon Morra <jmorra@eharmony.com>
2015-10-03 13:46:42 -0700

    Updating the base learner to use a template parameter to increase performance

--- java/src/main/c++/jni_base_learner.h
+++ java/src/main/c++/jni_base_learner.h
@@ -6,14 +6,20 @@
 void throw_java_exception(JNIEnv *env, const char* name, const char* msg);
 void rethrow_cpp_exception_as_java_exception(JNIEnv *env);
 
-template<class T>
+// It would appear that after reading posts like
+// http://stackoverflow.com/questions/6458612/c0x-proper-way-to-receive-a-lambda-as-parameter-by-reference
+// and
+// http://stackoverflow.com/questions/3203305/write-a-function-that-accepts-a-lambda-expression-as-argument
+// it is more efficient to use another type parameter instead of std::function<T(example*)>
+// but more difficult to read.
+template<typename T, typename F>
 T base_predict(
         JNIEnv *env,
         jobject obj,
         jstring example_string,
         jboolean learn,
         jlong vwPtr,
-        const std::function<T(example*)> &predictor) {
+        const F &predictor) {
     T result;
     try {
         vw* vwInstance = (vw*)vwPtr;
