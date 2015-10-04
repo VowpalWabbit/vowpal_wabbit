75fe0db6c3e3afa6640be4ea75e1f051de4dd53f
Jon Morra <jmorra@eharmony.com>
2015-10-04 08:04:21 -0700

    Cleaning up some LDA JNI tests

--- java/src/test/java/vw/learner/VWFloatArrayLearnerTest.java
+++ java/src/test/java/vw/learner/VWFloatArrayLearnerTest.java
@@ -1,36 +1,39 @@
 package vw.learner;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import vw.VWTestHelper;
 
-import java.io.File;
 import java.io.IOException;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jmorra on 10/2/15.
  */
 public class VWFloatArrayLearnerTest {
-    private static final int REPS = 100;
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @BeforeClass
     public static void globalSetup() throws IOException {
         VWTestHelper.loadLibrary();
     }
 
-    private static class ModelInfo {
-        String model;
-        String readableModel;
-        public ModelInfo(String model, String readableModel) {
-            this.model = model;
-            this.readableModel = readableModel;
-        }
-    };
+    private String model;
+    private String readableModel;
 
-    final static String[] data = new String[] {
+    @Before
+    public void setupFiles() throws IOException {
+        model = temporaryFolder.newFile().getAbsolutePath();
+        readableModel = temporaryFolder.newFile().getAbsolutePath();
+    }
+
+    private final String[] data = new String[] {
             "| 0 2",
             "| 0 3",
             "| 0 4",
@@ -46,68 +49,25 @@ public class VWFloatArrayLearnerTest {
     };
 
     @Test
-    public void testLdaTrainModel() throws Exception {
-        trainNewModel();
-    }
-
-    @Test
-    public void testLdaTrainAndLoadModel() throws Exception {
-        assertTrue(rehydratedModelOk(trainNewModel()));
-    }
-
-    @Test
-    public void testLdaPredict() throws Exception {
-        VWFloatArrayLearner v = rehydrateModel(trainNewModel());
+    public void testLdaPredict() {
+        writeVwModelToDisk();
+        VWFloatArrayLearner v = rehydrateModel();
         float[] vector = v.predict("| 1:1 2:2 3:3");
         assertNotNull(vector);
     }
 
-    private static ModelInfo trainNewModel() throws Exception {
-        ModelInfo mi = new ModelInfo(getModelPath(false), getModelPath(true));
-        writeVwModelToDisk(mi);
-        return mi;
-    }
-
-    private static void writeVwModelToDisk(ModelInfo mi) throws Exception {
+    private void writeVwModelToDisk() {
         final VWFloatArrayLearner vwModel = new VWFloatArrayLearner(String.format("--quiet -b 3 --lda 2 -f %s --readable_model %s",
-                mi.model, mi.readableModel));
+                model, readableModel));
 
-        for (int i = 0; i < REPS; ++i) {
-            for (String d : data) {
-                vwModel.learn(d);
-            }
+        for (String d : data) {
+            vwModel.learn(d);
         }
 
         vwModel.close();
     }
 
-    /**
-     * Instantiate model to see if it's possible or whether we get an exception.
-     * @param mi path to binary VW model.
-     */
-    private static boolean rehydratedModelOk(ModelInfo mi) {
-        VWFloatArrayLearner v = null;
-        try {
-            v = rehydrateModel(mi);
-            return true;
-        } catch (Throwable e) {
-            return false;
-        } finally {
-            try {
-                v.close();
-            } catch (Throwable ignored) {}
-        }
-    }
-
-    private static VWFloatArrayLearner rehydrateModel(ModelInfo mi) {
-        return new VWFloatArrayLearner("-i " + mi.model + " -t --quiet");
-    }
-
-    private static String getModelPath(final boolean readable) throws Exception {
-        File f = File.createTempFile("vwtest", (readable ? ".readable" : "") + ".lda.model");
-        String p = f.getAbsolutePath();
-        f.deleteOnExit();
-        f.delete();
-        return p;
+    private VWFloatArrayLearner rehydrateModel() {
+        return new VWFloatArrayLearner("-i " + model + " -t --quiet");
     }
 }
