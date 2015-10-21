33dda2b89ec9c44b07a57faa8d4e1663bc88e49e
John Langford <jl@hunch.net>
2015-10-21 12:25:30 -0400

    refactoring importance weight

--- vowpalwabbit/ect.cc
+++ vowpalwabbit/ect.cc
@@ -225,8 +225,10 @@ void ect_train(ect& e, base_learner& base, example& ec)
 
     ec.l.simple = simple_temp;
     base.learn(ec, id-e.k);
+    float old_weight = ec.weight;
     ec.weight = 0.;
     base.learn(ec, id-e.k);//inefficient, we should extract final prediction exactly.
+    ec.weight = old_weight;
 
     bool won = ec.pred.scalar * simple_temp.label > 0;
 
