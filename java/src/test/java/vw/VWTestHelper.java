7ea3f6ab21eb9231ca4c30696d2e54eb4104b3a0
Jon Morra <jmorra@eharmony.com>
2015-10-29 10:54:42 -0700

    Making tests more robust by using locks

--- java/src/test/java/vw/VWTestHelper.java
+++ java/src/test/java/vw/VWTestHelper.java
@@ -4,24 +4,24 @@ import org.junit.BeforeClass;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by jmorra on 10/2/15.
  */
 public class VWTestHelper {
 
-    private static boolean loaded = false;
+    private static AtomicBoolean loaded = new AtomicBoolean(false);
 
     @BeforeClass
     public static void loadLibrary() throws IOException {
-        if (!loaded) {
+        if (!loaded.getAndSet(true)) {
             try {
                 System.load(new File(".").getCanonicalPath() + "/target/vw_jni.lib");
             }
             catch (UnsatisfiedLinkError ignored) {
                 // Do nothing as this means that the library should be loaded as part of the jar
             }
-            loaded = true;
         }
     }
 }
