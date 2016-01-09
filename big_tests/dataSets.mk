f5b3ec37136e537652df7d7e7ccfcd5fe4533dc9
Dan M <melamed@microsoft.com>
2016-01-09 05:30:21 -0500

    moved entity relation data set from demo dir to big_tests

--- big_tests/dataSets.mk
+++ big_tests/dataSets.mk
@@ -1,10 +1,11 @@
 # Makefile for obtaining and preparing data sets
 
-allData := mnist covtype URLRep
+allData := mnist covtype URLRep ER
 
 ################ begin generic stuff #########
 
 VPATH+=$(testCodeDir)
+WGET ?= wget -N --no-use-server-timestamps
 
 .PHONY:	prepData eraseData %.prep
 
@@ -21,19 +22,40 @@ eraseData:
 
 ################ end generic stuff #########
 
+#ER
+ER.prep:	$(dataDir)/ER.dir/train.prep $(dataDir)/ER.dir/test.prep ;
+
+$(dataDir)/ER.dir/train.prep:	ER.raw
+	cd $(dataDir)/ER.dir/ ;\
+	ln -sf ER_train.vw train.prep
+
+$(dataDir)/ER.dir/test.prep:	ER.raw
+	cd $(dataDir)/ER.dir/ ;\
+	ln -sf ER_test.vw test.prep
+
+ER.raw:	$(dataDir)/ER.dir/er.zip ;
+
+$(dataDir)/ER.dir/er.zip:
+	dir=`dirname $@` ;\
+	mkdir -p $$dir ;\
+	cd $$dir ;\
+	sleep 1 ;\
+	$(WGET) http://web.engr.illinois.edu/~kchang10/data/er.zip ;\
+	unzip -o er.zip
+
 # URLRep
 $(dataDir)/URLRep.dir/prep:	URLRep.raw URLRep.munge.sh
 	export testCodeDir=$(testCodeDir) ;\
 	cd $(dataDir)/URLRep.dir/ ;\
 	$(testCodeDir)/URLRep.munge.sh url_svmlight.tar.gz > prep
 
-URLRep.raw: $(dataDir)/URLRep.dir $(dataDir)/URLRep.dir/url_svmlight.tar.gz
+URLRep.raw:	$(dataDir)/URLRep.dir/url_svmlight.tar.gz ;
 
 $(dataDir)/URLRep.dir/url_svmlight.tar.gz:
 	dir=`dirname $@` ;\
 	mkdir -p $$dir ;\
 	cd $$dir ;\
-	wget https://archive.ics.uci.edu/ml/machine-learning-databases/url/url_svmlight.tar.gz
+	$(WGET) https://archive.ics.uci.edu/ml/machine-learning-databases/url/url_svmlight.tar.gz
 
 
 # COVERTYPE
@@ -42,18 +64,18 @@ $(dataDir)/covtype.dir/prep:	covtype.raw covtype.munge.sh
 	cd $(dataDir)/covtype.dir/ ;\
 	$(testCodeDir)/covtype.munge.sh covtype.data.gz > prep
 
-covtype.raw: $(dataDir)/covtype.dir $(dataDir)/covtype.dir/covtype.data.gz ;
+covtype.raw:	$(dataDir)/covtype.dir/covtype.data.gz ;
 
 $(dataDir)/covtype.dir/covtype.data.gz:
 	dir=`dirname $@` ;\
 	mkdir -p $$dir ;\
 	cd $$dir ;\
-	wget https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz
+	$(WGET) https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz
 
 
 ## MNIST
 # override implicit %.prep rule
-mnist.prep:	$(dataDir)/mnist.dir $(dataDir)/mnist.dir/train.prep $(dataDir)/mnist.dir/test.prep ;
+mnist.prep:	$(dataDir)/mnist.dir/train.prep $(dataDir)/mnist.dir/test.prep ;
 
 $(dataDir)/mnist.dir/train.prep:	 mnist-train.raw mnist.munge.sh mnist.extractfeatures mnist.extract-labels.pl shuffle.pl
 	export testCodeDir=$(testCodeDir) ;\
@@ -65,18 +87,18 @@ mnist.extractfeatures:	mnist.extractfeatures.cpp
 	cd $(testCodeDir)/ ;\
 	g++ -O3 -Wall $^ -o $@
 
-mnist-train.raw: $(dataDir)/mnist.dir $(dataDir)/mnist.dir/train-labels-idx1-ubyte.gz $(dataDir)/mnist.dir/train-images-idx3-ubyte.gz ;
+mnist-train.raw:	$(dataDir)/mnist.dir/train-labels-idx1-ubyte.gz $(dataDir)/mnist.dir/train-images-idx3-ubyte.gz ;
 
 $(dataDir)/mnist.dir/%.gz:
 	dir=`dirname $@` ;\
 	mkdir -p $$dir ;\
 	cd $$dir ;\
 	fileName=`basename $@` ;\
-	wget http://yann.lecun.com/exdb/mnist/$$fileName
+	$(WGET) http://yann.lecun.com/exdb/mnist/$$fileName
 
 $(dataDir)/mnist.dir/test.prep:	mnist-test.raw mnist.munge.sh mnist.extractfeatures mnist.extract-labels.pl
 	export testCodeDir=$(testCodeDir) ;\
 	cd $(dataDir)/mnist.dir/ ;\
 	$(testCodeDir)/mnist.munge.sh t10k-labels-idx1-ubyte.gz t10k-images-idx3-ubyte.gz > test.prep
 
-mnist-test.raw:	$(dataDir)/mnist.dir $(dataDir)/mnist.dir/t10k-labels-idx1-ubyte.gz $(dataDir)/mnist.dir/t10k-images-idx3-ubyte.gz ;
+mnist-test.raw:	$(dataDir)/mnist.dir/t10k-labels-idx1-ubyte.gz $(dataDir)/mnist.dir/t10k-images-idx3-ubyte.gz ;
--- big_tests/testDetails.mk
+++ big_tests/testDetails.mk
@@ -24,3 +24,11 @@ regression_group:	1a.valid 1b.valid 3.valid ;
 # URL Reputation
 3.inData := $(dataDir)/URLRep.dir/prep
 3.params := -d $(3.inData)
+
+# Entity Relation training
+4a.inData := $(dataDir)/ER.dir/train.prep
+4a.params := -b 24 -d $(4a.inData) --search 10 --passes 10 --search_task entity_relation --constraints --search_alpha 1e-8 -f $(stageDir)/4a.dir/er.model --cache_file $(stageDir)/4a.dir/er.cache
+
+# Entity Relation prediction
+4b.inData := $(dataDir)/ER.dir/test.prep
+4b.params := -t -d $(4b.inData) -i $(stageDir)/4a.dir/er.model
