819fa135eac9811c112dcba86fcf24cb5eae0c88
Vladimir Vladimirov <vladimir.vladimirov@magnetic.com>
2016-02-12 11:17:33 -0700

    ksm support for latest VW version

--- vowpalwabbit/memory.h
+++ vowpalwabbit/memory.h
@@ -21,4 +21,44 @@ T* calloc_or_throw(size_t nmemb)
 template<class T> T& calloc_or_throw()
 { return *calloc_or_throw<T>(1); }
 
+template<class T>
+T* calloc_mergable_or_throw(size_t nmemb)
+{ if (nmemb == 0)
+    return nullptr;
+
+  size_t length = nmemb * sizeof(T);
+  void* data;
+  if (0 != posix_memalign(&data, sysconf(_SC_PAGE_SIZE), length))
+  {
+    const char* msg = "internal error: memory allocation failed!\n";
+    fputs(msg, stderr);
+    THROW(msg);
+  }
+  if (data == nullptr)
+  { const char* msg = "internal error: memory allocation failed!\n";
+    fputs(msg, stderr);
+    THROW(msg);
+  }
+  memset(data, 0, lengh);
+#ifdef MADV_MERGEABLE
+// mark weight vector as KSM sharable
+// it allows to save memory if you run multiple instances of the same model
+// see more https://www.kernel.org/doc/Documentation/vm/ksm.txt
+// you need to have Linux kernel >= 2.6.32 and KSM enabled
+// to check is KSM enabled: `grep KSM /boot/config-`uname -r` CONFIG_KSM=y`
+// you can enable ksmd with sudo "echo 1 > /sys/kernel/mm/ksm/run"
+// mark address space as a candidate for merging
+
+  if (0 != madvise(data, length, MADV_MERGEABLE)) {
+    const char* msg = "internal warning: marking memory as ksm mergeable failed!\n";
+    fputs(msg, stderr);
+  }
+#endif
+  return (T*)data;
+}
+
+template<class T> T& calloc_mergable_or_throw()
+{ return *calloc_mergable_or_throw<T>(1); }
+
+
 inline void free_it(void* ptr) { if (ptr != nullptr) free(ptr); ptr = nullptr; }
