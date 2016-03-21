35e8325c64b0cf17c970ab35adb7e7ef4f5fa3bf
John Langford <jl@hunch.net>
2016-03-21 14:07:03 -0400

    fix mwt learn code

--- vowpalwabbit/cb.cc
+++ vowpalwabbit/cb.cc
@@ -90,7 +90,6 @@ bool substring_eq(substring ss, const char* str)
 
 void parse_label(parser* p, shared_data*, void* v, v_array<substring>& words)
 { CB::label* ld = (CB::label*)v;
-
   for (size_t i = 0; i < words.size(); i++)
   { cb_class f;
     tokenize(':', words[i], p->parse_name);
