a4d5be80a53c81a90fed56acaa3378d0fcd4a45a
Markus Cozowicz <eisber@eisber.net>
2016-04-28 22:26:48 -0400

    added GitLink to allow debugging from nuget

--- cs/cs/CreateNuGet.cmd
+++ cs/cs/CreateNuGet.cmd
@@ -1,3 +1,4 @@
+..\..\vowpalwabbit\packages\gitlink.2.2.0\lib\net45\GitLink.exe ..\.. -f vowpalwabbit\vw.sln -p x64 -c Release -d ..\..\vowpalwabbit\x64\Release
 ..\..\vowpalwabbit\.nuget\nuget pack cs.nuspec -Prop "Configuration=Release;Platform=x64" -Prop SolutionDir=..\..\vowpalwabbit\ -OutputDirectory ..\..\vowpalwabbit\x64\Release
 
 ..\..\vowpalwabbit\.nuget\nuget push ..\..\vowpalwabbit\x64\Release\VowpalWabbit.*.nupkg %NUGETAPIKEY%
