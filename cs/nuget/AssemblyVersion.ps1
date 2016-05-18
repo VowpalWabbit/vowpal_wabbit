4c86fe2261f2d3a45b5b69132d15a470587350f9
Markus Cozowicz <eisber@eisber.net>
2016-05-18 15:55:36 -0400

    added missing powershell scripts

---
+++ cs/nuget/AssemblyVersion.ps1
@@ -0,0 +1,13 @@
+﻿$version = Get-Content version.txt
+
+function PatchAssemblyInfo ($file)
+{
+    $text = (Get-Content $file) -replace "Version\(""[0-9\.]+""\)", "Version(""$version"")"
+    echo $text
+    Set-Content -Path $file -Value $text
+}
+
+PatchAssemblyInfo "..\cs\Properties\AssemblyInfo.cs"
+PatchAssemblyInfo "..\cs_json\Properties\AssemblyInfo.cs"
+PatchAssemblyInfo "..\cs_parallel\Properties\AssemblyInfo.cs"
+
---
+++ cs/nuget/CreateNuGet.ps1
@@ -0,0 +1,15 @@
+param([string]$apiKey="")
+param([string]$outputDirectory="..\..\vowpalwabbit\x64\Release")
+
+$version = Get-Content version.txt
+
+..\..\vowpalwabbit\packages\gitlink.2.2.0\lib\net45\GitLink.exe ..\.. -f vowpalwabbit\vw.sln -p x64 -c Release -d ..\..\vowpalwabbit\x64\Release
+..\..\vowpalwabbit\.nuget\nuget pack cs.nuspec -Version $version -Prop "Configuration=Release;Platform=x64" -Prop SolutionDir=..\..\vowpalwabbit\ -OutputDirectory $outputDirectory
+..\..\vowpalwabbit\.nuget\nuget pack cs-json.nuspec -Version $version -Prop "Configuration=Release;Platform=x64" -Prop SolutionDir=..\..\vowpalwabbit\ -OutputDirectory $outputDirectory
+..\..\vowpalwabbit\.nuget\nuget pack cs-parallel.nuspec -Version $version -Prop "Configuration=Release;Platform=x64" -Prop SolutionDir=..\..\vowpalwabbit\ -OutputDirectory $outputDirectory
+
+
+if (not [string]::IsNullOrEmpty($apiKey))
+{ 
+    ..\..\vowpalwabbit\.nuget\nuget push ..\..\vowpalwabbit\x64\Release\VowpalWabbit.*.nupkg $apiKey
+}
