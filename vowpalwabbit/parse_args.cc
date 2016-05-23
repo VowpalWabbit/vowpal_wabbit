baa70f7e20059c15f65a2447367f406018913934
Paul Mineiro <paul-github@mineiro.com>
2016-05-23 09:56:07 -0700

    scaffolding

--- vowpalwabbit/parse_args.cc
+++ vowpalwabbit/parse_args.cc
@@ -53,6 +53,7 @@ license as described in the file LICENSE.
 #include "lrqfa.h"
 #include "autolink.h"
 #include "log_multi.h"
+#include "recall_tree.h"
 #include "stagewise_poly.h"
 #include "active.h"
 #include "active_cover.h"
@@ -1073,6 +1074,7 @@ void parse_reductions(vw& all)
   all.reduction_stack.push_back(boosting_setup);
   all.reduction_stack.push_back(ect_setup);
   all.reduction_stack.push_back(log_multi_setup);
+  all.reduction_stack.push_back(recall_tree_setup);
   all.reduction_stack.push_back(multilabel_oaa_setup);
 
   all.reduction_stack.push_back(csoaa_setup);
