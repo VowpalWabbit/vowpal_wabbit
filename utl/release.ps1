ddea28e782130855ac8d0054b421323dee18b0e0
Markus Cozowicz <eisber@eisber.net>
2016-06-13 18:00:24 -0400

    add github release

--- utl/release.ps1
+++ utl/release.ps1
@@ -1,11 +1,11 @@
-$token = "b07595278db1d21048347b802901bb0fafb8ae04"
-$repo = "eisber"
-$version = "8.0.0.65" 
+param([string]$githubToken, [string]$repo = "eisber")
+
 $headers = @{ Authorization = "token $token" }
+
+$version = ([xml](Get-Content -Path cs\version.props)).Project.PropertyGroup.VowpalWabbitAssemblyVersion
 
 $body = @{ tag_name = "v$version"; name = "v$version"; body = "v$version"; draft = $false; prerelease = $false }
 $release = Invoke-RestMethod -Headers $headers  https://api.github.com/repos/$repo/vowpal_wabbit/releases -Method Post -Body (ConvertTo-Json $body)
 
 $url = ($release.upload_url.Replace("{?name,label}", "?name=VowpalWabbit-$version.msi"))
-$asset = Invoke-RestMethod -Headers $headers -Method Post -ContentType "application/zip" -InFile ..\vowpalwabbit\x64\Release\VowpalWabbit.msi $url
-
+$asset = Invoke-RestMethod -Headers $headers -Method Post -ContentType "application/zip" -InFile vowpalwabbit\x64\Release\VowpalWabbit.msi $url
