e10b1ba90bf96fc0a6ddc98216a28da20ed1af6e
Markus Cozowicz <eisber@eisber.net>
2016-06-14 10:50:17 -0400

    vso #11

--- utl/release.ps1
+++ utl/release.ps1
@@ -7,8 +7,17 @@ $version = ([xml](Get-Content -Path cs\version.props)).Project.PropertyGroup.Vow
 $body = @{ tag_name = "v$version"; name = "v$version"; body = "v$version"; draft = $false; prerelease = $false }
 $release = Invoke-RestMethod -Headers @{ Authorization = "token $githubToken" }  https://api.github.com/repos/$repo/vowpal_wabbit/releases -Method Post -Body (ConvertTo-Json $body)
 
-$url = ($release.upload_url.Replace("{?name,label}", "?name=VowpalWabbit-$version.msi"))
-$asset = Invoke-RestMethod -Headers @{ Authorization = "token $githubToken" } -Method Post -ContentType "application/zip" -InFile vowpalwabbit\x64\Release\VowpalWabbit.msi $url
-
+try {
 $url = ($release.upload_url.Replace("{?name,label}", "?name=VowpalWabbit-AzureCloudService-$version.cspkg"))
 $asset = Invoke-RestMethod -Headers @{ Authorization = "token $githubToken" } -Method Post -ContentType "application/zip" -InFile cs\azure_service\bin\Release\app.publish\azure_service.cspkg $url
+}
+catch [Exception] {
+   [Exception]$e = $_;
+   for ($i=1;($i -le 10) -and ($e -ne $null);$i++) {
+        Write-Host $e.Message $e.StackTrace
+        $e = $e.InnerException
+   }
+}
+
+$url = ($release.upload_url.Replace("{?name,label}", "?name=VowpalWabbit-$version.msi"))
+$asset = Invoke-RestMethod -Headers @{ Authorization = "token $githubToken" } -Method Post -ContentType "application/zip" -InFile vowpalwabbit\x64\Release\VowpalWabbit.msi $url
