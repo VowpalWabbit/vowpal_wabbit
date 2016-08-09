72f1d13b61c57880718e29b49b0e61c7380515b8
Jon Morra <jmorra@eharmony.com>
2016-08-09 15:35:06 -0700

    Rolling back some minor changes

--- vowpalwabbit/gen_cs_example.cc
+++ vowpalwabbit/gen_cs_example.cc
@@ -74,7 +74,7 @@ cb_class* get_observed_cost(CB::label& ld)
 
 //Multiline version
 void gen_cs_example_ips(v_array<example*> examples, COST_SENSITIVE::label& cs_labels)
-{
+{ 
   cs_labels.costs.erase();
   bool shared = CB::ec_is_example_header(*examples[0]);
   for (uint32_t i = 0; i < examples.size()-1; i++)
