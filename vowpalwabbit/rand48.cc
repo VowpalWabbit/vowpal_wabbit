01238332346db99f69711ddb45229f6044ba299f
Jon Morra <jmorra@eharmony.com>
2016-08-22 12:29:00 -0700

    Reverting back some changes that were fixed

--- vowpalwabbit/rand48.cc
+++ vowpalwabbit/rand48.cc
@@ -20,14 +20,6 @@ float merand48(uint64_t& initial) {
   return bits_to_float(temp) - 1;
 }
 
-#ifdef __APPLE__
-float merand48(unsigned long& initial) {
-    initial = a * initial + c;
-    int32_t temp = ((initial >> 25) & 0x7FFFFF) | bias;
-    return bits_to_float(temp) - 1;
-}
-#endif
-
 uint64_t v = c;
 
 void msrand48(uint64_t initial) { v = initial; }
--- vowpalwabbit/rand48.h
+++ vowpalwabbit/rand48.h
@@ -1,12 +1,6 @@
 #pragma once
 void msrand48(uint64_t initial);
-
-#ifdef __APPLE__
-float merand48(unsigned long& initial);
-#endif
-
 float merand48(uint64_t& initial);
-
 float frand48();
 float frand48_noadvance();
 
