55b4a38788087670f232107d0220f572d708854f
Markus Cozowicz <eisber@eisber.net>
2017-07-14 14:49:35 -0400

    fixed gzip support in VS2015 switched back to zlib c-interface as boost added 30% overhead

--- vowpalwabbit/comp_io.cc
+++ vowpalwabbit/comp_io.cc
@@ -1,157 +1,76 @@
+#include "zlib.h"
 #include "comp_io.h"
-// #include <boost/iostreams/filter/gzip.hpp>
-#include <boost/iostreams/filter/zlib.hpp>
-#include <fstream>
-#include <memory>
-
-using namespace std;
-using namespace boost::iostreams;
-
-// comp_io_buf::comp_io_input::comp_io_input(std::istream& input) : in(&buf)
-comp_io_buf::comp_io_input::comp_io_input(std::istream* pinput, bool own) : input(nullptr), in(&buf)
-{
-	zlib_params params;
-	params.noheader = true;
-	buf.push(zlib_decompressor(params));
-	// buf.push(gzip_decompressor());
-	buf.push(*pinput);
-
-	if (own)
-		input = pinput;
-}
-
-comp_io_buf::comp_io_input::~comp_io_input()
-{
-	if (input)
-		delete input;
-}
-
-comp_io_buf::comp_io_output::comp_io_output(std::ostream* poutput) : output(poutput), out(&buf)
-{
-	buf.push(zlib_compressor());
-	// buf.push(gzip_compressor());
-	buf.push(*poutput);
-}
 
 int comp_io_buf::open_file(const char* name, bool stdin_off, int flag)
-{ int ret = -1;
-	switch (flag)
-	{
-	case READ:
-	{
-		unique_ptr<comp_io_input> f;
-		if (*name != '\0')
-			f = unique_ptr<comp_io_input>(new comp_io_input(new ifstream(name, ios_base::in | ios_base::binary)));
-		else if (!stdin_off)
-			f = unique_ptr<comp_io_input>(new comp_io_input(&cin, /* own */ false));
+{ gzFile fil = nullptr;
+  int ret = -1;
+  switch (flag)
+  { case READ:
+      if (*name != '\0')
+        fil = gzopen(name, "rb");
+      else if (!stdin_off)
+#ifdef _WIN32
+        fil = gzdopen(_fileno(stdin), "rb");
+#else
+        fil = gzdopen(fileno(stdin), "rb");
+#endif
+      if (fil != nullptr)
+      { gz_files.push_back(fil);
+        ret = (int)gz_files.size() - 1;
+        files.push_back(ret);
+      }
+      break;
 
-		if (f)
-		{
-			if (f->in.bad())
-				return -1;
+    case WRITE:
+      fil = gzopen(name, "wb");
+      if (fil != nullptr)
+      { gz_files.push_back(fil);
+        ret = (int)gz_files.size() - 1;
+        files.push_back(ret);
+      }
+      break;
 
-			gz_input.push_back(std::move(f));
-			gz_spec.push_back({ name, stdin_off });
-			ret = (int)gz_input.size() - 1;
-			files.push_back(ret);
-		}
-	}
-
-	break;
-
-	case WRITE:
-	{
-		unique_ptr<comp_io_output> f(new comp_io_output(new ofstream(name, ios_base::out | ios_base::binary)));
-		if (f->out.bad())
-			return -1;
-
-		gz_output.push_back(std::move(f));
-		gz_spec.push_back({ name, stdin_off });
-		ret = (int)gz_output.size() - 1;
-		files.push_back(ret);
-
-	}
-	break;
-
-	default:
-		std::cerr << "Unknown file operation. Something other than READ/WRITE specified" << std::endl;
-	}
-	return ret;
+    default:
+      std::cerr << "Unknown file operation. Something other than READ/WRITE specified" << std::endl;
+  }
+  return ret;
 }
 
 void comp_io_buf::reset_file(int f)
-{
-	comp_spec& spec = gz_spec[f];
-	if (f < gz_input.size())
-	{
-		if (spec.name.length() > 0)
-			gz_input[f] = unique_ptr<comp_io_input>(new comp_io_input(new ifstream(spec.name, ios_base::in | ios_base::binary)));
-		else if (!spec.stdin_off)
-			gz_input[f] = unique_ptr<comp_io_input>(new comp_io_input(&cin, /* own */ false));
-	}
-	else
-		gz_output[f] = unique_ptr<comp_io_output>(new comp_io_output(new ofstream(spec.name, ios_base::out | ios_base::binary)));
-
-	space.end() = space.begin();
-	head = space.begin();
+{ gzFile fil = gz_files[f];
+  gzseek(fil, 0, SEEK_SET);
+  space.end() = space.begin();
+  head = space.begin();
 }
 
 ssize_t comp_io_buf::read_file(int f, void* buf, size_t nbytes)
-{
-	auto& in = gz_input[f]->in;
-
-	if (!in.good())
-		return -1;
-
-	in.read((char*)buf, nbytes);
-	
-	return in.gcount();
+{ gzFile fil = gz_files[f];
+  int num_read = gzread(fil, buf, (unsigned int)nbytes);
+  return (num_read > 0) ? num_read : 0;
 }
 
-size_t comp_io_buf::num_files() { return gz_input.size() + gz_output.size(); }
+size_t comp_io_buf::num_files() { return gz_files.size(); }
 
-ssize_t comp_io_buf::write_file(int f, const void* buf, size_t nbytes)
-{
-	auto& out = gz_output[f]->out;
-
-	out.write((const char*)buf, nbytes);
-	if (out.fail())
-		return -1;
-
-	return nbytes;
+ssize_t comp_io_buf::write_file(int file, const void* buf, size_t nbytes)
+{ int num_written = gzwrite(gz_files[file], buf, (unsigned int)nbytes);
+  return (num_written > 0) ? num_written : 0;
 }
 
 bool comp_io_buf::compressed() { return true; }
 
 void comp_io_buf::flush()
-{
-	if (write_file(0, space.begin(), head - space.end()) != (int)((head - space.end())))
-		std::cerr << "error, failed to write to cache\n";
-	head = space.begin();
+{ if (write_file(0, space.begin(), head - space.end()) != (int)((head - space.end())))
+    std::cerr << "error, failed to write to cache\n";
+  head = space.begin();
 }
 
 bool comp_io_buf::close_file()
-{
-	if (gz_input.size() == 0 && gz_output.size() == 0)
-		return false;
-
-	while (gz_input.size() > 0)
-	{
-		gz_input.back().release();
-		gz_input.pop_back();
-
-		if (files.size() > 0)
-			files.pop();
-	}
-
-	while (gz_output.size() > 0)
-	{
-		gz_output.back().release();
-		gz_output.pop_back();
-
-		if (files.size() > 0)
-			files.pop();
-	}
-
-	return true;
+{ if (gz_files.size()>0)
+  { gzclose(gz_files.back());
+    gz_files.pop_back();
+    if (files.size() > 0)
+      files.pop();
+    return true;
+  }
+  return false;
 }
