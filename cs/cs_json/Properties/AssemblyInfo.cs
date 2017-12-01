ecee3869caa0f24bfdc8da04bcc1307c7ac9030f
U-NORTHAMERICA\jcl <jcl@JCL-X250.northamerica.corp.microsoft.com>
2017-12-01 17:52:17 +0000

    fix windows warnings

--- cs/cs_json/Properties/AssemblyInfo.cs
+++ cs/cs_json/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(false)]
 [assembly: System.Runtime.InteropServices.Guid("8a34db14-bac2-474b-8102-be25ca5f2c55")]
-[assembly: System.Reflection.AssemblyVersion("8.3.0.9")]
-[assembly: System.Reflection.AssemblyFileVersion("8.3.0.9")]
+[assembly: System.Reflection.AssemblyVersion("8.4.0.1")]
+[assembly: System.Reflection.AssemblyFileVersion("8.4.0.1")]
 
 
