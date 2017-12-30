c7c4de0d6c68eaa139d498a35a4013a0f13e00b7
Denis Bilenko <denis.bilenko@booking.com>
2017-12-30 22:46:34 +0100

    Add --onethread option that disables parse thread

--- vowpalwabbit/learner.cc
+++ vowpalwabbit/learner.cc
@@ -60,6 +60,71 @@ void process_example(vw& all, example* ec)
     dispatch_example(all, *ec);
 }
 
+template <class T, void(*f)(T, example*)> void generic_driver_onethread(vw& x_all, T context)
+{
+  vw* all = &x_all;
+
+  v_array<example*> examples = v_init<example*>();
+  size_t example_number = 0;  // for variable-size batch learning algorithms
+
+  // XXX get rid of ring buffer there?
+
+  try
+  {
+    size_t examples_available;
+    while(!all->p->done)
+    {
+      examples.push_back(&VW::get_unused_example(all)); // need at least 1 example
+      if (!all->do_reset_source && example_number != all->pass_length && all->max_examples > example_number && all->p->reader(all, examples) > 0)
+      {
+        VW::setup_examples(*all, examples);
+        example_number+=examples.size();
+        examples_available=examples.size();
+      }
+      else
+      {
+        reset_source(*all, all->num_bits);
+        all->do_reset_source = false;
+        all->passes_complete++;
+
+        examples[0]->end_pass = true;
+        all->p->in_pass_counter = 0;
+
+        if (all->passes_complete == all->numpasses && example_number == all->pass_length)
+        {
+          all->passes_complete = 0;
+          all->pass_length = all->pass_length*2+1;
+        }
+        if (all->passes_complete >= all->numpasses && all->max_examples >= example_number)
+        {
+          all->p->done = true;
+        }
+        example_number = 0;
+        examples_available=1;
+      }
+
+      all->p->end_parsed_examples+=examples_available;
+
+      for (int i = 0; i < examples_available; ++i)
+        f(context, examples[i]);
+
+      examples.erase();
+    }
+    all->l->end_examples();
+  }
+  catch (VW::vw_exception& e)
+  {
+    cerr << "vw example #" << example_number << "(" << e.Filename() << ":" << e.LineNumber() << "): " << e.what() << endl;
+  }
+  catch (exception& e)
+  {
+    cerr << "vw: example #" << example_number << e.what() << endl;
+  }
+
+  all->p->done = true;
+  examples.delete_v();
+}
+
 template <class T, void(*f)(T, example*)> void generic_driver(vw& all, T context)
 {
   example* ec = nullptr;
@@ -92,6 +157,19 @@ void generic_driver(vector<vw*> alls)
     (*it)->l->end_examples();
 }
 
+void generic_driver_onethread(vector<vw*> alls)
+{
+  generic_driver_onethread<vector<vw*>, process_multiple>(**alls.begin(), alls);
+
+  // skip first as it already called end_examples()
+  auto it = alls.begin();
+  for (it++; it != alls.end(); it++)
+    (*it)->l->end_examples();
+}
+
 void generic_driver(vw& all)
 { generic_driver<vw&, process_example>(all, all); }
+
+void generic_driver_onethread(vw& all)
+{ generic_driver_onethread<vw&, process_example>(all, all); }
 }
