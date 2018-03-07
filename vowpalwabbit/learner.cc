4bedbeef8b1fee7e365a61e77e011df5283ab0ca
John Langford <jl@hunch.net>
2018-03-07 17:13:20 -0500

    refactored parse loop for one thread or threaded use

--- vowpalwabbit/learner.cc
+++ vowpalwabbit/learner.cc
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include "vw.h"
 #include "parse_regressor.h"
+#include "parse_dispatch_loop.h"
 using namespace std;
 
 void dispatch_example(vw& all, example& ec)
@@ -60,70 +61,6 @@ void process_example(vw& all, example* ec)
     dispatch_example(all, *ec);
 }
 
-  template <class T, void(*f)(T, example*)> void generic_driver_onethread(vw& x_all, T context)
-{
-  vw* all = &x_all;
-
-  v_array<example*> examples = v_init<example*>();
-  size_t example_number = 0;  // for variable-size batch learning algorithms
-
-  // XXX get rid of ring buffer there?
-
-  try
-  {
-    size_t examples_available;
-    while(!all->p->done)
-    {
-      examples.push_back(&VW::get_unused_example(all)); // need at least 1 example
-      if (!all->do_reset_source && example_number != all->pass_length && all->max_examples > example_number && all->p->reader(all, examples) > 0)
-      {
-        VW::setup_examples(*all, examples);
-        example_number+=examples.size();
-        examples_available=examples.size();
-      }
-      else
-      {
-        reset_source(*all, all->num_bits);
-        all->do_reset_source = false;
-        all->passes_complete++;
-
-        examples[0]->end_pass = true;
-        all->p->in_pass_counter = 0;
-
-        if (all->passes_complete == all->numpasses && example_number == all->pass_length)
-        {
-          all->passes_complete = 0;
-          all->pass_length = all->pass_length*2+1;
-        }
-        if (all->passes_complete >= all->numpasses && all->max_examples >= example_number)
-        {
-          all->p->done = true;
-        }
-        example_number = 0;
-        examples_available=1;
-      }
-
-      all->p->end_parsed_examples+=examples_available;
-
-      for (size_t i = 0; i < examples_available; ++i)
-        f(context, examples[i]);
-
-      examples.erase();
-    }
-  }
-  catch (VW::vw_exception& e)
-  {
-    cerr << "vw example #" << example_number << "(" << e.Filename() << ":" << e.LineNumber() << "): " << e.what() << endl;
-  }
-  catch (exception& e)
-  {
-    cerr << "vw: example #" << example_number << e.what() << endl;
-  }
-
-  all->p->done = true;
-  examples.delete_v();
-}
-
 template <class T, void(*f)(T, example*)> void generic_driver(vw& all, T context)
 {
   example* ec = nullptr;
@@ -159,11 +96,18 @@ void generic_driver(vector<vw*> alls)
 void generic_driver(vw& all)
 { generic_driver<vw&, process_example>(all, all); }
 
-  void set_done(parser& p){p.done=true;}
+void set_done(parser& p){p.done=true;}
+
+  void dispatch(vw& all, v_array<example*> examples)
+  {
+    all.p->end_parsed_examples+=examples.size();//divergence: lock & signal
+    for (size_t i = 0; i < examples.size(); ++i)
+      process_example(all, examples[i]);
+  }
 
 void generic_driver_onethread(vw& all)
 {
-  generic_driver_onethread<vw&, process_example>(all, all);
+  parse_dispatch<dispatch, set_done>(all);
   all.l->end_examples();
 }
 }
