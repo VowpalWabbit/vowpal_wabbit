630fd5fa49bf1bfa11e39e896fd7cc21cafa7922
Chicheng Zhang <zcc1307@gmail.com>
2018-03-09 17:10:45 -0500

    tweak the python script

--- scripts/plot_warm_start.py
+++ scripts/plot_warm_start.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import subprocess
 import pylab
 from itertools import product
+import os.path
 
 class model:
 	def __init__(self):
@@ -14,9 +15,6 @@ def collect_stats(mod):
 	filename = mod.filename
 	# using progress parameter
 	# num_rows = mod.bandit / mod.progress
-
-
-
 	avg_loss = []
 	last_loss = []
 	wt = []
@@ -51,8 +49,7 @@ def execute_vw(mod):
 		alg_option += ' --cb_explore_adf '
 
 	cmd_catfile = '( head -n ' + str(mod.warm_start) + ' ' + mod.dataset_supervised + ';' + ' head -n ' + str(mod.bandit) + ' ' + mod.dataset_bandit + '; )'
-	cmd_vw = mod.vw_path + ' --cbify ' + str(mod.num_classes) + ' --cb_type ' + str(mod.cb_type) + ' --warm_start ' + str(mod.warm_start) + ' --bandit ' + str(mod.bandit) + ' --choices_lambda ' + str(mod.choices_lambda) + alg_option
-	#+ ' --progress ' + str(mod.progress)
+	cmd_vw = mod.vw_path + ' --cbify ' + str(mod.num_classes) + ' --cb_type ' + str(mod.cb_type) + ' --warm_start ' + str(mod.warm_start) + ' --bandit ' + str(mod.bandit) + ' --choices_lambda ' + str(mod.choices_lambda) + alg_option + ' --progress ' + str(mod.progress)
 
 	cmd = cmd_catfile + ' | ' + cmd_vw
 
@@ -74,7 +71,7 @@ def gen_comparison_graph(mod):
 		mod.no_bandit = False
 		mod.no_supervised = False
 		mod.no_exploration = False
-		mod.filename = config_name
+		mod.filename = mod.output_dir+'/'+config_name
 		execute_vw(mod)
 		avg_loss_comb, last_loss_comb, wt_comb = collect_stats(mod)
 		line = plt.plot(wt_comb, avg_loss_comb, 'r', label=('Combined approach, #lambdas=' + str(mod.choices_lambda) ))
@@ -83,7 +80,7 @@ def gen_comparison_graph(mod):
 		mod.no_bandit = False
 		mod.no_supervised = True
 		mod.no_exploration = False
-		mod.filename = config_name+'_no_supervised'
+		mod.filename = mod.output_dir+'/'+config_name+'_no_supervised'
 		execute_vw(mod)
 		avg_loss_band_only, last_loss_band_only, wt_band_only = collect_stats(mod)
 		line = plt.plot(wt_band_only, avg_loss_band_only, 'b', label='Bandit only')
@@ -92,7 +89,7 @@ def gen_comparison_graph(mod):
 		mod.no_bandit = True
 		mod.no_supervised = False
 		mod.no_exploration = False
-		mod.filename = config_name+'_no_bandit'
+		mod.filename = mod.output_dir+'/'+config_name+'_no_bandit'
 		execute_vw(mod)
 		avg_loss_sup_only, last_loss_sup_only, wt_sup_only = collect_stats(mod)
 		line = plt.plot(wt_sup_only, avg_loss_sup_only, 'g', label='Supervised only')
@@ -101,7 +98,7 @@ def gen_comparison_graph(mod):
 		pylab.xlabel('#bandit examples')
 		pylab.ylabel('Progressive validation error')
 		pylab.title('Source 1 feature flipping prob = ' + str(mod.fprob1) + '; source 2 feature flipping prob = ' + str(mod.fprob2) + 'cb_type = '+ mod.cb_type )
-		pylab.savefig('figs/'+config_name +'.png')
+		pylab.savefig(mod.output_dir+'/'+config_name +'.png')
 		plt.gcf().clear()
 		print('')
 		#plt.show()
@@ -113,13 +110,15 @@ if __name__ == '__main__':
 
 	mod = model()
 
-	mod.vw_path = './vowpalwabbit/vw'
+	mod.vw_path = '../vowpalwabbit/vw'
+	mod.output_dir = '../figs'
+	mod.data_dir = '../data'
 	#mod.warm_start = 50
-	mod.bandit = 4096
+	mod.bandit = 4000
 	mod.num_classes = 10
 	#mod.cb_type = 'mtr'  #'ips'
     #mod.choices_lambda = 10
-	#mod.progress = 25
+	mod.progress = 25
 	mod.adf_on = True
 
 	mod.choices_warm_start = [pow(2,i) for i in range(11)] #put it here in order to plot 2d mesh
@@ -127,6 +126,7 @@ if __name__ == '__main__':
 	#choices_fprob1 = [0.1, 0.2, 0.3]
 	#choices_fprob2 = [0.1, 0.2, 0.3]
 	#choices_cb_types = ['mtr', 'ips']
+	choices_cb_types = ['mtr']
 	choices_choices_lambda = [pow(2,i) for i in range(5)]
 
 	#for correctness test
@@ -134,8 +134,13 @@ if __name__ == '__main__':
 	choices_fprob1 = [0.1]
 	choices_fprob2 = [0.1]
 
-
 	for mod.fprob1, mod.fprob2, mod.cb_type, mod.choices_lambda in product(choices_fprob1, choices_fprob2, choices_cb_types, choices_choices_lambda):
-		mod.dataset_supervised = './source1_' + str(mod.fprob1) + '_m.vw'
-		mod.dataset_bandit = './source2_' + str(mod.fprob2) + '_m.vw'
+		mod.dataset_supervised = mod.data_dir + '/source1_' + str(mod.fprob1) + '_m.vw'
+		mod.dataset_bandit = mod.data_dir + '/source2_' + str(mod.fprob2) + '_m.vw'
+		if not os.path.isfile(mod.dataset_supervised):
+			print 'The supervised dataset does not exist!'
+			break
+		if not os.path.isfile(mod.dataset_bandit):
+			print 'The bandit dataset does not exist!'
+			break
 		gen_comparison_graph(mod)
