f2f9bb6e56df2cafe1f7c3d34317e0ac530cf00c
Chicheng Zhang <zcc1307@gmail.com>
2018-03-15 13:11:37 -0400

    retracted the matplotlib inclusion

--- scripts/plot_warm_start.py
+++ scripts/plot_warm_start.py
@@ -1,6 +1,6 @@
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+#import matplotlib
+#matplotlib.use('Agg')
+#import matplotlib.pyplot as plt
 import subprocess
 import pylab
 from itertools import product
