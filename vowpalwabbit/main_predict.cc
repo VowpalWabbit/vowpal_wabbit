d148a84c34cee2e334691bed09e03551e9f8165e
marcozo <marcozo@microsoft.com>
2018-03-23 13:50:02 -0400

    added slim example parser

--- vowpalwabbit/main_predict.cc
+++ vowpalwabbit/main_predict.cc
@@ -4,6 +4,12 @@
 #include <iostream>
 #include <fstream>
 
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/bind.hpp>
+
 enum Features
 {
   // Shared
@@ -22,19 +28,72 @@ enum Features
 enum Namespaces
 {
   SharedA = 0,
-  SharedB = 1,
-  SharedC = 2,
-  ActionDependentX = 3,
-  ActionDependentY = 4,
+  SharedB = 10,
+  SharedC = 20,
+  ActionDependentX = 30,
+  ActionDependentY = 40,
 };
 
-using namespace std;
+struct example_features_push_back_impl
+{
+  struct result
+  {
+    typedef void type;
+  };
+
+  void operator()(safe_example_predict& c, namespace_index const& ns, feature_index const& idx, feature_value const& x) const
+  {
+    c.feature_space[ns].push_back(x, idx);
+  }
+};
+
+boost::phoenix::function<example_features_push_back_impl> const example_features_push_back = example_features_push_back_impl();
+
+template <typename Iterator>
+bool parse_complex(Iterator first, Iterator last)
+{
+  using boost::spirit::ascii::space;
+
+  namespace phx = boost::phoenix;
+  namespace qi = boost::spirit::qi;
+
+  double label = 0.0;
+  namespace_index current_ns;
+  safe_example_predict ex;
+
+  bool r = qi::parse(first, last,
+    (
+      qi::double_[phx::ref(label) = qi::_1]
+      // namespaces
+      >> +( +space
+        >> '|' >> qi::int_[phx::push_back(phx::ref(ex.indices), qi::_1), phx::ref(current_ns) = qi::_1]
+        // features
+        >> +( +space
+          >> (qi::ulong_long >> ':' >> qi::double_)[example_features_push_back(phx::ref(ex), phx::ref(current_ns), qi::_1, qi::_2)]
+          )
+        )
+      )
+    );
+
+  std::cout << "label: " << label << std::endl;
+  for (auto& ns : ex.indices)
+  {
+    std::cout << "ns: " << (uint64_t)ns << std::endl;
+    for (auto& fi : ex.feature_space[ns])
+      std::cout << "\tidx: " << fi.index() << " " << fi.value() << std::endl;
+  }
+
+  if (!r || first != last) // fail if we did not get a full match
+    return false;
+  //c = std::complex<double>(rN, iN);
+  return r;
+}
 
 int main(int argc, char *argv[])
 {
   if (argc != 4)
   {
-    cout << "usage: " << argv[0] << " <model> <dataset> <prediction-output>" << endl;
+    std::cout << "usage: " << argv[0] << " <model> <dataset> <prediction-output>" << std::endl;
     return -1;
   }
 
@@ -61,17 +120,20 @@ int main(int argc, char *argv[])
   action1.feature_space[Namespaces::ActionDependentX].push_back(1, Features::Action3);
 
   // read from file
-  ifstream ifs(argv[1], ios::binary | ios::ate);
-  ifstream::pos_type model_size = ifs.tellg();
+  std::ifstream ifs(argv[1], std::ios::binary | std::ios::ate);
+  std::ifstream::pos_type model_size = ifs.tellg();
 
   std::vector<char> model_file(model_size);
 
-  ifs.seekg(0, ios::beg);
+  ifs.seekg(0, std::ios::beg);
   ifs.read(&model_file[0], model_size);
 
   vw_predict predictor(&model_file[0], model_size);
 
-  // TODO: load model
+  std::string str = "1 |0 25:1 28:4.2 |5 26:1 27:3.4";
+  parse_complex(str.begin(), str.end());
+
+   // TODO: load model
   // TODO: slim parser
   // TODO: score
   /*
