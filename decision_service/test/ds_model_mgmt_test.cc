7fd1e3f1c863663f13d15191f511d0fd3af3a46d
Rajan Chari <ranaras@microsoft.com>
2018-04-24 16:59:49 -0400

    Fixed test to work with vcpkg install of boost test

---
+++ decision_service/test/ds_model_mgmt_test.cc
@@ -0,0 +1,13 @@
+﻿#define BOOST_TEST_MODULE ds_model_mgmt
+
+#include <boost/test/unit_test.hpp>
+
+int add(int i, int j)
+{
+  return i + j;
+}
+
+BOOST_AUTO_TEST_CASE(simpleUsage)
+{
+  BOOST_CHECK(add(2, 2) == 4);
+}
