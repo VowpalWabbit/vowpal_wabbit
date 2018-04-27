54f1bb92a0bbd0fb31f6fffdafc68e5acc431303
Rajan Chari <ranaras@microsoft.com>
2018-04-27 08:29:48 -0400

    fixed fork/merge issues from Yann's repo

--- decision_service/src/ds_configuration.h
+++ decision_service/src/ds_configuration.h
@@ -4,43 +4,40 @@
 
 namespace decision_service {
 
-	class configuration {
-	public:
-		//app name
-		const std::string& app_id() const;
+  class configuration {
 
-		//model settings
-		const std::string& model_url() const;
-		const int model_refresh_period_ms() const;
+  public:
+    const std::string & app_id() const;
 
-		//logging settings
-		const std::string& eventhub_interaction_url() const;
-		const std::string& eventhub_interaction_name() const;
-		const std::string& eventhub_observation_url() const;
-		const std::string& eventhub_observation_name() const;
-		const std::string& eventhub_host() const;
-		const std::string& eventhub_key_name() const;
-		const std::string& eventhub_key_value() const;
-		const size_t batch_max_size() const;
-		const size_t queue_max_size() const;
-		const int batch_timeout_ms() const;
+    //model config
+    const std::string & model_url() const;
+    const int model_refresh_period_ms() const;
 
-		configuration();
-		configuration(const std::string&);//deserialize the json string
+    //eventhub config
+    const size_t batch_max_size() const;
+    const int batch_timeout_ms() const;
+    const size_t queue_max_size() const;
+    const std::string & eventhub_host() const;
+    const std::string & shared_access_key_name() const;
+    const std::string & shared_access_key() const;
+    const std::string & eventhub_interaction_name() const;
+    const std::string & eventhub_observation_name() const;
 
-	private:
-		std::string _app_id;
-		std::string _model_url;
-		int _model_refresh_period_ms;
-		std::string _eventhub_interaction_url;
-		std::string _eventhub_interaction_name;
-		std::string _eventhub_observation_url;
-		std::string _eventhub_observation_name;
-		std::string _eventhub_host;
-		std::string _eventhub_key_name;
-		std::string _eventhub_key_value;
-		size_t _batch_max_size;
-		size_t _queue_max_size;
-		int _batch_timeout_ms;
-	};
+    configuration();//default values
+    configuration(const std::string& json);//deserialize the json string
+
+
+  private:
+    std::string _app_id;
+    std::string _model_url;
+    int _model_refresh_period_ms;
+    std::string _eventhub_interaction_name;
+    std::string _eventhub_observation_name;
+    std::string _eventhub_host;
+    std::string _shared_access_key_name;
+    std::string _shared_access_key;
+    size_t _batch_max_size;
+    size_t _queue_max_size;
+    int _batch_timeout_ms;
+  };
 }
