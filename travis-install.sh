4dda3cb9c61a79b1917fddf26f05a26bdcb5473a
marcozo <marcozo@microsoft.com>
2018-05-16 15:25:23 -0400

    switch to docker based ubuntu (xenial)

---
+++ travis-install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+sudo apt-get update -qq
+sudo apt-get install -qq libboost-all-dev
+sudo apt-get install maven
+sudo apt-get install libgtest-dev google-mock
+sudo pip install cpp-coveralls wheel
+# use miniconda for python package testing
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda create -q -n test-python27 python=2.7 nomkl numpy scipy scikit-learn
+
---
+++ travis-script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+make all
+if [[ "$LEGACY" == "false" ]]; then make vwslim ; fi
+make python
+mvn clean test -f java/pom.xml
+make test
+if [[ "$LEGACY" == "false" ]]; then make vwslimtests ; fi
+cd test
+./test_race_condition.sh
+cd ..
+make test_gcov --always-make
+cd python
+source activate test-python27
+pip install pytest readme_renderer
+python setup.py check -mrs
+python setup.py install
+py.test tests
+source deactivate
+cd ..
