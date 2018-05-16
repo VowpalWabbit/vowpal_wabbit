7402559924677e22aea7613219bd3d9d2a7d3ca9
marcozo <marcozo@microsoft.com>
2018-05-16 16:50:14 -0400

    fix docker travis shell script paths

--- travis-install.sh
+++ travis-install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-apt-get update -qq
-apt-get install -y -qq libboost-all-dev
-apt-get install -y maven
-apt-get install -y libgtest-dev google-mock
+if [[ "$LEGACY" == "false" ]]; then SUDO=sudo ; fi
+$SUDO apt-get update -qq
+$SUDO apt-get install -y -qq libboost-all-dev
+$SUDO apt-get install -y maven
+if [[ "$LEGACY" == "false" ]]; then 
+sudo apt-get install -y libgtest-dev google-mock wget pip conda
+fi
 pip install cpp-coveralls wheel
 # use miniconda for python package testing
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
