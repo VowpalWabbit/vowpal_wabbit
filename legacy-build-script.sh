2cca824c413df96e1353d69162884bd5ed70a4f0
marcozo <marcozo@microsoft.com>
2018-05-31 12:29:09 -0400

    fix path in shell script

--- legacy-build-script.sh
+++ legacy-build-script.sh
@@ -6,6 +6,10 @@ cd test
 ./test_race_condition.sh
 cd ..
 make test_gcov --always-make
+
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+
 cd python
 source activate test-python27
 pip install pytest readme_renderer
