0b5f60c6703d79f53e63f662f198b44d243b355b
Rajan Chari <ranaras@microsoft.com>
2018-06-18 11:43:19 -0400

    For api usage example, load config from local json file

--- decision_service/examples/cmdline_cpp/local_file_override.cc
+++ decision_service/examples/cmdline_cpp/local_file_override.cc
@@ -1,30 +1,32 @@
+#include <memory>
+#include <cassert>
+#include <boost/program_options.hpp>
 #include "example.h"
 #include "factory_resolver.h"
 #include "live_model.h"
-#include <memory>
 #include "config_utility.h"
-#include <cassert>
 
 // namespace manipulation for brevity
 using namespace reinforcement_learning;
 namespace cfg_util = reinforcement_learning::utility::config;
 namespace m = model_management;
 namespace u = utility;
+namespace po = boost::program_options;
 
 char const * const LOCAL_FILE_TRANSPORT = "LOCAL_FILE";
 
 // Forward declaration
 class local_model_file;
 int file_tranport_create(m::i_data_transport** , const u::config_collection& , api_status*);
-std::unique_ptr<live_model> init_override();
+std::unique_ptr<live_model> init_override(const po::variables_map& vm);
 void use_this_action(size_t choosen_action);
 
 // Create and run RL interface
-void override_transport_usage() {
+void override_transport_usage(const po::variables_map& vm) {
 
   // Create an interface to reinforcement learning loop 
   // and initialize it
-  auto rl = init_override();
+  auto rl = init_override(vm);
   //  MODEL_SRC
 
   // Response class
@@ -45,12 +47,16 @@ void override_transport_usage() {
   assert(scode == error_code::success);
 }
 
-std::unique_ptr<live_model> init_override() {
+std::string load_config_file(const std::string& file_name);
+
+std::unique_ptr<live_model> init_override(const po::variables_map& vm) {
   // Register local file transport in data_transport factory
   data_transport_factory.register_type(LOCAL_FILE_TRANSPORT, file_tranport_create);
 
   //create a configuration object from json data
-  const auto config = cfg_util::init_from_json(JSON_CFG_DATA);
+  const auto json_config = vm["json_config"].as<std::string>();
+  auto const config_str = load_config_file(json_config);
+  const auto config = cfg_util::create_from_json(config_str);
 
   //create the rl live_model, and initialize it with the config
   auto rl = std::make_unique<live_model>(config);
@@ -64,19 +70,18 @@ int file_tranport_create(m::i_data_transport** retval, const u::config_collectio
 }
 
 class local_model_file : public m::i_data_transport {
+  public:
+    int get_data(model_management::model_data& data, api_status* status) override {
+      data = read_new_data_file();
+      return error_code::success;
+    }
 
-public:
-  int get_data(model_management::model_data& data, api_status* status) override {
-    data = read_new_data_file();
-    return error_code::success;
-  }
-
-private:
-  m::model_data read_new_data_file() {
-    m::model_data md;
-    // read this data from a local file if the data did not change
-    md.data = new char[1000];
-    md.data_sz = 1000;
-    return md;
-  }
+  private:
+    m::model_data read_new_data_file() {
+      m::model_data md;
+      // read this data from a local file if the data did not change
+      md.data = new char[1000];
+      md.data_sz = 1000;
+      return md;
+    }
 };
--- decision_service/examples/cmdline_cpp/main.cc
+++ decision_service/examples/cmdline_cpp/main.cc
@@ -3,34 +3,124 @@
 
 #include "example.h"
 
-#include "config_utility.h"
-#include "live_model.h"
 #include <memory>
 #include <cassert>
 #include <iostream>
-#include "factory_resolver.h"
-#include "local_file_override.h"
-#include "constants.h"
 #include <cpprest/http_listener.h>
+#include <boost/program_options.hpp>
+#include "factory_resolver.h"
+#include "constants.h"
+#include "config_utility.h"
+#include "live_model.h"
+#include "../../rlclientlib/str_util.h"
 
-// namespace manipulation for brevity
+// Namespace manipulation for brevity
+using namespace web::http::experimental::listener;
 namespace r = reinforcement_learning;
 namespace cfg_util = reinforcement_learning::utility::config;
-namespace util = reinforcement_learning::utility;
+namespace po = boost::program_options;
+namespace u = reinforcement_learning::utility;
 
-// forward declartion of functions
-std::unique_ptr<r::live_model> init();
-void use_this_action(size_t choosen_action);
-
+// Globals & Constants
+std::unique_ptr<http_listener> g_http;
 const auto HTTP_URL = U("http://localhost:8951");
 
+// Forward declartion of functions
+po::variables_map process_cmd_line(int argc, char** argv);
+bool start_mock_server(const po::variables_map& vm);
+void basic_usage(const po::variables_map& vm);
+void override_transport_usage(const po::variables_map& vm);
+
+// App entry point
+int main(int argc, char** argv) {
+  try {
+    po::variables_map vm = process_cmd_line(argc, argv);
+    start_mock_server(vm);
+    basic_usage(vm);
+    override_transport_usage(vm);
+  }
+  catch ( const std::exception& e) {
+    std::cout << "Error: " << e.what() << std::endl;
+    return -1;
+  }  
+  return 0;
+}
+
+// Implementation
+po::variables_map process_cmd_line(const int argc, char** argv) {
+  po::options_description desc("Options");
+  desc.add_options()
+    ( "local_server,l", po::value<bool>()->default_value(false)   , "Start local server to test against" )
+
+    ( "json_config,j",  po::value<std::string>()->
+                        default_value("client.json")              , "JSON file with config information for hosted RL loop" );
+
+  po::variables_map vm;
+  store(parse_command_line(argc, argv, desc), vm);
+  return vm;
+}
+
+std::string load_config_file(const std::string& file_name) {
+  std::ifstream fs;
+  fs.open(file_name);
+  if ( !fs.good() )
+    throw std::runtime_error(u::concat("Unable to open file. ", file_name));
+  std::stringstream buffer;
+  buffer << fs.rdbuf();
+  return buffer.str();
+}
+
+std::unique_ptr<r::live_model> init(const po::variables_map& vm) {
+  //create a configuration object from json data
+  const auto json_config = vm["json_config"].as<std::string>();
+  auto const config_str = load_config_file(json_config);
+  auto config = cfg_util::create_from_json(config_str);
+  config.set(r::name::MODEL_BLOB_URI,"http://localhost:8951");
+  //create the rl live_model, and initialize it with the config
+  auto rl = std::make_unique<r::live_model>(config);
+  r::api_status status;
+  const auto scode = rl->init(&status);
+  if(scode != r::error_code::success) 
+    throw std::runtime_error(status.get_error_msg());
+  return rl;
+}
+
+void use_this_action(size_t choosen_action) {
+  std::cout << "The choosen action is: " << choosen_action << std::endl;
+}
+
+
+void handle_get(web::http::http_request request) {
+  web::http::http_response resp;
+  auto& headers = resp.headers();
+  headers.set_date(utility::datetime::utc_now());
+  headers.add(U("Last-Modified"), headers.date());
+  resp.set_status_code(web::http::status_codes::OK);
+  resp.set_body("Test content");
+  request.reply(resp);
+}
+
+bool start_mock_server(const po::variables_map& vm) {
+  if ( !vm["local_server"].as<bool>() )
+    return false;
+
+  const ::utility::string_t surl(HTTP_URL);
+  web::uri_builder addr(surl);
+  g_http = std::make_unique<http_listener>(addr.to_uri());
+  g_http->support(web::http::methods::GET, handle_get);
+  g_http->support(web::http::methods::HEAD, handle_get);
+  g_http->open().wait();
+  return true;
+}
+
+
 /**
- * \brief Basic usage of reinforcement learning API
- */
-void basic_usage() {
+* \brief Basic usage of reinforcement learning API
+*/
+void basic_usage(const po::variables_map& vm) {
   // Create an interface to reinforcement learning loop 
   // and initialize it
-  auto rl = init();
+  auto rl = init(vm);
 
   // Response class
   r::ranking_response response;
@@ -48,53 +138,4 @@ void basic_usage() {
   // Report reward recieved
   scode = rl->report_outcome(uuid, reward);
   assert(scode == r::error_code::success);
-}
-
-std::unique_ptr<r::live_model> init() {
-
-  //create a configuration object from json data
-  auto config = cfg_util::init_from_json(JSON_CFG_DATA);
-  config.set(r::name::MODEL_BLOB_URI,"http://localhost:8951");
-  //create the rl live_model, and initialize it with the config
-  auto rl = std::make_unique<r::live_model>(config);
-  rl->init(nullptr);
-
-  return rl;
-}
-
-void use_this_action(size_t choosen_action) {
-  std::cout << "The choosen action is: " << choosen_action << std::endl;
-}
-
-bool start_mock_server();
-
-int main() {
-  start_mock_server();
-  basic_usage();
-  override_transport_usage();
-
-  return 0;
-}
-
-using namespace web::http::experimental::listener;
-std::unique_ptr<http_listener> g_http;
-
-void handle_get(web::http::http_request request) {
-  web::http::http_response resp;
-  auto& headers = resp.headers();
-  headers.set_date(utility::datetime::utc_now());
-  headers.add(U("Last-Modified"), headers.date());
-  resp.set_status_code(web::http::status_codes::OK);
-  resp.set_body("Test content");
-  request.reply(resp);
-}
-
-bool start_mock_server() {
-  const ::utility::string_t surl(HTTP_URL);
-  web::uri_builder addr(surl);
-  g_http = std::make_unique<http_listener>(addr.to_uri());
-  g_http->support(web::http::methods::GET, handle_get);
-  g_http->support(web::http::methods::HEAD, handle_get);
-  g_http->open().wait();
-  return true;
 }
