ae6e6165fe4884b95e0d939b9df8a8a2f0916256
Rajan Chari <ranaras@microsoft.com>
2018-07-05 02:51:40 -0400

    Removed cmdline_cpp since its becoming a kitchen_sink

--- decision_service/examples/cmdline_cpp/example.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-char const * const  uuid = "uuid";
-char const * const  context = R"({"_multi":[{"n1":"v1"},{"n2":"v2"}]})";
-char const * const  reward = "reward";
--- decision_service/examples/cmdline_cpp/local_file_override.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void override_transport_usage();
