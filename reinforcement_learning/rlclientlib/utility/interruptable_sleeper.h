eff581ad61516974e812281c9594d7e990aa7326
Rajan <rajan.chari@yahoo.com>
2018-07-23 17:26:58 -0400

    Bug fixes and more diagnostics in rl_sim (#1545)
    * Renamed decision_service to reinforcement_learning
    
    * Missed bug fix checkin
    
    * Initial commit doxygen
    
    * Added doxygen comments.
    
    * Fix docs
    
    * PR Suggestions
    
    * File descriptions and simple documentation fixes
    
    * 1) Added namespace to action features 2) fix action order
    
    * PR suggestions
    
    * When reporting outcome the value field must be numeric for floats.
    
    * std::condition_variable::wait_for can return even if not triggered.  Also guard against exceptions.
    
    * 1) handle bg error in rl_sim 2) add stats to rl_sim
    
    * Fix linux compile issues
    
    * Remove unused exception
    
    * Handle error upstream

--- reinforcement_learning/rlclientlib/utility/interruptable_sleeper.h
+++ reinforcement_learning/rlclientlib/utility/interruptable_sleeper.h
@@ -8,6 +8,7 @@ namespace reinforcement_learning { namespace utility {
   class interruptable_sleeper {
     std::condition_variable _cv;
     std::mutex _mutex;
+    bool _interrupt = false;
   public:
     // waits until wake is called or the specified time passes
     template< class Rep, class Period >
@@ -18,13 +19,19 @@ namespace reinforcement_learning { namespace utility {
   };
 
   inline void interruptable_sleeper::interrupt() {
+    {
+      std::unique_lock <std::mutex> lock(_mutex);
+      _interrupt = true;
+    }
     _cv.notify_one();
   }
 
+  /*
+   * Sleep returns true if timeout expires and returns false if sleep was interrupted.
+   */
   template <class Rep, class Period>
   bool interruptable_sleeper::sleep(const std::chrono::duration<Rep, Period>& timeout_duration) {
     std::unique_lock <std::mutex> lock(_mutex);
-    auto sret = _cv.wait_for(lock, timeout_duration);
-    return sret == std::cv_status::timeout;
+    return ! ( _cv.wait_for(lock, timeout_duration, [this]() { return _interrupt; }) );
   }
 }}
