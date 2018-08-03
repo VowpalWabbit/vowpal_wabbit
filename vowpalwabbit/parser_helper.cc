1a8835847567f88def9fb68e9149e053f3ab2458
Jack Gerrits <jackgerrits95@gmail.com>
2018-08-03 15:53:16 -0400

    Move stable_unique to own header

--- vowpalwabbit/parser_helper.cc
+++ vowpalwabbit/parser_helper.cc
@@ -1,7 +1,7 @@
 #include <boost/foreach.hpp>
 #include "parser_helper.h"
 #include <iostream>
-#include <set>
+#include "stable_unique.h"
 
 using namespace std;
 
@@ -32,30 +32,6 @@ std::vector<std::string> opts_to_args(const std::vector<boost::program_options::
   }
 
   return args;
-}
-
-// Performs unique operation over collection without requiring the collection to be sorted first.
-// Returns pointer to first element after unique elements to erase from until end of collection.
-// Original ordering of elements is preserved.
-template <typename ForwardIterator>
-ForwardIterator stable_unique(ForwardIterator begin, ForwardIterator end)
-{
-  using value_t = typename std::iterator_traits<ForwardIterator>::value_type;
-
-  set<value_t> unique_set;
-
-  auto current_head = begin;
-  for (auto current_check = begin; current_check != end; current_check++)
-  {
-    if (unique_set.find(*current_check) == unique_set.end())
-    {
-      unique_set.insert(*current_check);
-      *current_head = *current_check;
-      current_head++;
-    }
-  }
-
-  return current_head;
 }
 
 // blackbox wrapping of boost program options to ignore duplicate specification of options allowed only ones, but specified multiple times
