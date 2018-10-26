c555f9fd309aab7513ab04f0b4b029c79e653968
Chenxi Zhao <homezcx616@gmail.com>
2018-10-26 10:39:09 -0700

    fix flatbuffer linux build order

--- build-linux.sh
+++ build-linux.sh
@@ -3,23 +3,24 @@ set -e
 
 export PATH="$HOME/miniconda/bin:$PATH"
 
-make all
-make python
-mvn clean test -f java/pom.xml
-make test
-make rl_clientlib_test
-
 # add flat buffers
 cd ..
 git clone git@github.com:google/flatbuffers.git
 cd ./flatbuffers
 git checkout 925c1d77fcc72636924c3c13428a34180c30f96f  # release commit
+cmake 
 make
 sudo make install
 sudo cp ./flatc /usr/local/bin/
 cd ..  # to ../
 cd ./vovowpal_wabbit
 #
+
+make all
+make python
+mvn clean test -f java/pom.xml
+make test
+make rl_clientlib_test
 
 cd test
 ./test_race_condition.sh
