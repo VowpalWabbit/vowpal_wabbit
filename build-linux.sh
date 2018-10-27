b96642539245c48676c290df2b22a89a4963a991
Jack Gerrits <jackgerrits95@gmail.com>
2018-10-27 13:58:25 -0400

    Cd to correct directory

--- build-linux.sh
+++ build-linux.sh
@@ -16,6 +16,7 @@ sudo mkdir /opt/cmake
 sudo sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
 sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 
+cd /vw
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWARNINGS=OFF
