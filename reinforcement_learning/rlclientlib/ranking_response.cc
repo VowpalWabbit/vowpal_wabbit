12f426e08bcd593279a5e08a4a7e5759bba94627
Rajan <rajan.chari@yahoo.com>
2018-10-30 12:47:38 -0400

    Fix explore library and change rl_clientlib to use explore lib to swap actions (#1658)
    * 1) sample_after_normalizing will always normalize pdf
    2) remove sample_after_normalizing that takes scores
    3) add swap_chosen() to swap chosen action with first action
    4) added unit tests
    5) refactored ranking_response.  added a const iterator.
    
    * fixed merge conflict
    
    * Remove byte order mark.
    
    * Address PR comments
    
    * 1) fix rl_clientlib merge bug. 2) add more tests
    
    * improved rl_sim_cpp.  more topics.

--- reinforcement_learning/rlclientlib/ranking_response.cc
+++ reinforcement_learning/rlclientlib/ranking_response.cc
@@ -1,98 +1,154 @@
 #include "ranking_response.h"
-#include "ranking_response_impl.h"
 #include "api_status.h"
 #include "err_constants.h"
 
 namespace reinforcement_learning {
 
-  ranking_response::ranking_response()
-    : _pimpl {new ranking_response_impl()} {}
+  action_prob::action_prob(const size_t action_id, const float probability) : action_id { action_id }, probability { probability } {}
 
   ranking_response::ranking_response(char const* event_id)
-    : _pimpl{new ranking_response_impl(event_id)} {}
-
-  ranking_response::~ranking_response() {
-    delete _pimpl;
-  }
+    : _event_id { event_id }, _chosen_action_id { 0 } {}
 
   char const* ranking_response::get_event_id() const {
-    return _pimpl->_event_id.c_str();
+    return _event_id.c_str();
   }
 
   int ranking_response::get_chosen_action_id(size_t& action_id, api_status* status) const {
-    if (!_pimpl->get_chosen_action_id(action_id)) {
+    if ( _ranking.empty() ) {
       RETURN_ERROR_LS(nullptr, status, action_not_found);
     }
+    action_id = _chosen_action_id;
     return error_code::success;
   }
 
   int ranking_response::set_chosen_action_id(size_t action_id, api_status* status) {
-    return _pimpl->set_chosen_action_id(action_id, status);
+    if ( action_id >= _ranking.size() ) {
+      RETURN_ERROR_LS(nullptr, status, action_out_of_bounds) << " id:" << action_id << ", size:" << _ranking.size();
+    }
+
+    _chosen_action_id = action_id;
+    return error_code::success;
   }
 
   void ranking_response::set_event_id(char const* event_id) {
-    _pimpl->_event_id = event_id;
+    _event_id = event_id;
   }
 
   void ranking_response::push_back(const size_t action_id, const float prob) {
-    _pimpl->push_back(action_id, prob);
+    _ranking.emplace_back(action_id, prob);
   }
 
   size_t ranking_response::size() const {
-    return _pimpl->size();
+    return _ranking.size();
   }
 
   void ranking_response::set_model_id(const char* model_id) {
-    _pimpl->set_model_id(model_id);
+    _model_id = model_id;
   }
 
   const char* ranking_response::get_model_id() const {
-    return _pimpl->get_model_id();
+    return _model_id.c_str();
   }
 
   void ranking_response::clear() {
-    _pimpl->reset();
+    _event_id.clear();
+    _chosen_action_id = 0;
+    _model_id.clear();
+    _ranking.clear();
   }
 
-  ranking_response::ranking_response(ranking_response&& tmp) noexcept {
-    _pimpl = tmp._pimpl;
-    tmp._pimpl = nullptr;
-  }
+  ranking_response::ranking_response(ranking_response&& tmp) noexcept :
+  _event_id(std::move(tmp._event_id)),
+    _chosen_action_id(tmp._chosen_action_id),
+    _model_id(std::move(tmp._model_id)),
+    _ranking(std::move(tmp._ranking)) {}
 
   ranking_response& ranking_response::operator=(ranking_response&& tmp) noexcept {
-    const auto swap = _pimpl;
-    _pimpl = tmp._pimpl;
-    tmp._pimpl = swap;
+    std::swap(_event_id, tmp._event_id);
+    std::swap(_chosen_action_id, tmp._chosen_action_id);
+    std::swap(_model_id, tmp._model_id);
+    std::swap(_ranking, tmp._ranking);
     return *this;
   }
 
-  ranking_response::ranking_iterator::ranking_iterator(ranking_response_impl* p_resp_impl)
-    : _p_resp_impl(p_resp_impl), _idx(0) { }
+  using iterator = ranking_response::iterator;
+  using const_iterator = ranking_response::const_iterator;
 
-  ranking_response::ranking_iterator::ranking_iterator(ranking_response_impl* p_resp_impl, size_t idx)
-    : _p_resp_impl(p_resp_impl), _idx(idx) { }
+  iterator::iterator(ranking_response* p_resp)
+    : _p_resp(p_resp), _idx(0) {}
 
-  ranking_response::ranking_iterator& ranking_response::ranking_iterator::operator++() {
+  iterator::iterator(ranking_response* p_resp, const size_t idx)
+    : _p_resp(p_resp), _idx(idx) {}
+
+  iterator& iterator::operator++() {
     ++_idx;
     return *this;
   }
 
-  bool ranking_response::ranking_iterator::operator!=(const ranking_iterator& other) const {
+  bool iterator::operator!=(const iterator& other) const {
     return _idx != other._idx;
   }
 
-  action_prob ranking_response::ranking_iterator::operator*() const {
-    size_t action_id;
-    float prob;
-    _p_resp_impl->get_action(_idx, &action_id, &prob);
-    return action_prob{action_id, prob};
+  action_prob& iterator::operator*() {
+    return _p_resp->_ranking[_idx];
   }
 
-  ranking_response::ranking_iterator ranking_response::begin() const {
-    return {_pimpl};
+  bool iterator::operator<(const iterator& rhs) const {
+    return _idx < rhs._idx;
   }
 
-  ranking_response::ranking_iterator ranking_response::end() const {
-    return {_pimpl, _pimpl->size()};
+  int64_t iterator::operator-(const iterator& rhs) const {
+    return _idx - rhs._idx;
+  }
+
+  iterator iterator::operator+(const uint32_t idx) const {
+    return { _p_resp,_idx + idx };
+  }
+
+  const_iterator::const_iterator(const ranking_response* p_resp)
+    : _p_resp { p_resp }, _idx { 0 } {}
+
+  const_iterator::const_iterator(const ranking_response* p_resp, const size_t idx)
+    : _p_resp { p_resp }, _idx { idx } {}
+
+  const_iterator& ranking_response::const_iterator::operator++() {
+    ++_idx;
+    return *this;
+  }
+
+  bool const_iterator::operator!=(const const_iterator& other) const {
+    return _idx != other._idx;
+  }
+
+  const action_prob& const_iterator::operator*() const {
+    return _p_resp->_ranking[_idx];
+  }
+
+  bool const_iterator::operator<(const const_iterator& rhs) const {
+    return _idx < rhs._idx;
+  }
+
+  int64_t const_iterator::operator-(const const_iterator& rhs) const {
+    return _idx - rhs._idx;
+  }
+
+  const_iterator const_iterator::operator+(const uint32_t idx) const {
+    return { _p_resp,_idx + idx };
+  }
+
+  const_iterator ranking_response::begin() const {
+    return { this };
+  }
+
+  iterator ranking_response::begin() {
+    return { this };
+  }
+
+  const_iterator ranking_response::end() const {
+    return { this, _ranking.size() };
+  }
+
+  iterator ranking_response::end() {
+    return { this, _ranking.size() };
   }
 }
