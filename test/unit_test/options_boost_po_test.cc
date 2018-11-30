047337bfd0dcd265797dc6fe4b5e71f5d74d0e5e
Jack Gerrits <jackgerrits95@gmail.com>
2018-11-30 16:32:56 -0500

    Initial framework commit of options refactor

---
+++ test/unit_test/options_boost_po_test.cc
@@ -0,0 +1,21 @@
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+
+/*
+name
+short
+default
+keep
+implicit bool switch
+vector multi token
+compose
+duplicate options clash
+duplicate options muliple locations
+matching values
+mismatching values
+several groups combined
+was_supplied
+
+*/
---
+++ test/unit_test/options_test.cc
@@ -0,0 +1,4 @@
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
