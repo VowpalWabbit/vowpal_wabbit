65a2b3bcacf393cc3cf8d4588dad3cd0c34fe21a
Jack Gerrits <jackgerrits95@gmail.com>
2019-02-11 21:10:29 -0500

    Use std::find

--- vowpalwabbit/parser.cc
+++ vowpalwabbit/parser.cc
@@ -142,14 +142,6 @@ uint32_t cache_numbits(io_buf* buf, int filepointer)
   return cache_numbits;
 }
 
-bool member(v_array<int> ids, int id)
-{
-  for (size_t i = 0; i < ids.size(); i++)
-    if (ids[i] == id)
-      return true;
-  return false;
-}
-
 void reset_source(vw& all, size_t numbits)
 {
   io_buf* input = all.p->input;
@@ -171,7 +163,10 @@ void reset_source(vw& all, size_t numbits)
       else
       {
         int fd = input->files.pop();
-        if (!member(all.final_prediction_sink, (size_t)fd))
+        auto& fps = all.final_prediction_sink;
+
+        // If the current popped file is not in the list of final predictions sinks, close it.
+        if(std::find(fps.begin(), fps.end(), fd) == fps.end())
           io_buf::close_file_or_socket(fd);
       }
     input->open_file(all.p->output->finalname.begin(), all.stdin_off, io_buf::READ);  // pushing is merged into
