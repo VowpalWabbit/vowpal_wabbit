c5a031636803cdb20977685f793665ef9de8f0ed
Rajan Chari <ranaras@microsoft.com>
2019-02-21 14:50:24 -0500

    Copy results from offset_tree predict() into example.pred.a_s data structure

--- vowpalwabbit/offset_tree.cc
+++ vowpalwabbit/offset_tree.cc
@@ -1,22 +1,29 @@
 #include "offset_tree.h"
 #include "offset_tree_internal.h"
-#include "parse_args.h" // setup_base()
-#include "learner.h"    // init_learner()
+#include "parse_args.h"  // setup_base()
+#include "learner.h"     // init_learner()
 
 using namespace VW::config;
 using namespace LEARNER;
 namespace ot = offset_tree;
 
-void learn(ot::offset_tree& tree, single_learner& base, example& ec) {
+void learn(ot::offset_tree& tree, single_learner& base, example& ec)
+{
   THROW("Offset tree learn() - not yet impemented.");
 }
 
-void predict(ot::offset_tree& tree, single_learner& base, example& ec) {
+void predict(ot::offset_tree& ot, single_learner& base, example& ec)
+{
   // get predictions for all internal nodes in binary tree.
-  auto scores = tree.predict(base, ec);
+  ec.pred.a_s.clear();
+  auto scores = ot.predict(base, ec);
+  for (size_t idx = 0; idx < scores.size(); ++idx){
+    ec.pred.a_s.push_back({idx,scores[idx]});
+  }
 }
 
-base_learner* offset_tree_setup(VW::config::options_i& options, vw& all) {
+base_learner* offset_tree_setup(VW::config::options_i& options, vw& all)
+{
   option_group_definition new_options("Offset tree Options");
   uint32_t num_actions;
   new_options.add(make_option("ot", num_actions).keep().help("Offset tree with <k> labels"));
@@ -26,7 +33,8 @@ base_learner* offset_tree_setup(VW::config::options_i& options, vw& all) {
     return nullptr;
 
   // Ensure that cb_explore will be the base reduction
-  if (!options.was_supplied("cb_explore")) {
+  if (!options.was_supplied("cb_explore"))
+  {
     options.insert("cb_explore", "2");
   }
 
@@ -36,9 +44,9 @@ base_learner* offset_tree_setup(VW::config::options_i& options, vw& all) {
   base_learner* base = setup_base(options, all);
 
   all.delete_prediction = ACTION_SCORE::delete_action_scores;
-  
+
   learner<ot::offset_tree, example>& l = init_learner(
-    offset_tree, as_singleline(base), learn, predict, offset_tree->learner_count(), prediction_type::action_probs);
+      offset_tree, as_singleline(base), learn, predict, offset_tree->learner_count(), prediction_type::action_probs);
 
   return make_base(l);
 }
