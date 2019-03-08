cd7f9d10d9f2b0425c155b235ee4abe91dac73a4
Wen Sun <wensun@andrew.cmu.edu>
2019-03-08 15:54:50 -0500

    update readme

--- vowpalwabbit/memory_tree_scripts/xml_rcv1x.script.py
+++ vowpalwabbit/memory_tree_scripts/xml_rcv1x.script.py
@@ -11,7 +11,7 @@ alpha = 0.1
 passes = 6 #4
 learn_at_leaf = 1
 use_oas = 1
-dream_at_update = 0# 1
+dream_at_update =0 # 1
 #num_queries = 1  #does not really use
 #hal_version = 1 #does not really use
 loss = "squared"
@@ -33,7 +33,7 @@ saved_model = "{}.vw".format(train_data)
 
 print("## Training...")
 start = time.time()
-#train_data = 'tmp_rcv1x.vw.txt'
+#train_data = 'tmp_rcv1.vw.txt'
 os.system("../../build/vowpalwabbit/vw {} --memory_tree {} --learn_at_leaf {} --dream_at_update {}\
                 --max_number_of_labels {} --dream_repeats {} --oas {} \
                 --leaf_example_multiplier {} --Alpha {} -l {} -b {} -c --passes {} --loss_function {} -f {}".format(
