06e9f19c8b456a7d85384f8a106932918741c36a
Jack Gerrits <jackgerrits95@gmail.com>
2019-03-18 14:39:50 -0400

    Fix quoting of strings

--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -1,12 +1,12 @@
 REM Integration points for toolchain customization
 IF NOT DEFINED nugetPath (
-    SET nugetPath=nuget
+    SET "nugetPath=nuget"
 )
-@ECHO ON
+
 IF NOT DEFINED msbuildPath (
     REM Try to find VS Install
     for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-        set InstallDir=%%i
+        set "InstallDir=%%i"
     )
 
     if not exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
