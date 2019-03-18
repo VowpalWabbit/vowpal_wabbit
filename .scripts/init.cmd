55fb7e3623d7f9e9e7c310b5363571d8010f251c
Jack Gerrits <jackgerrits95@gmail.com>
2019-03-18 14:07:10 -0400

    Update msbuild detection

--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -4,7 +4,17 @@ IF NOT DEFINED nugetPath (
 )
 
 IF NOT DEFINED msbuildPath (
-    SET msbuildPath=msbuild
+    REM Try to find VS Install
+    for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+        set InstallDir=%%i
+    )
+
+    if not exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+        echo ERROR: MsBuild couldn't be found
+        exit /b 1
+    ) else (
+        SET "msBuildPath=%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+    )
 )
 
 REM Repo-specific paths
