ee2b078e47bdaf4a1fee16bcbf97034d331c1e08
Jack Gerrits <jackgerrits95@gmail.com>
2019-03-20 10:39:59 -0400

    move back to hard coded path

--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -2,19 +2,9 @@ REM Integration points for toolchain customization
 IF NOT DEFINED nugetPath (
     SET "nugetPath=nuget"
 )
-@ECHO ON
-IF NOT DEFINED msbuildPath (
-    REM Try to find VS Install
-    for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-        set InstallDir=%%i
-    )
 
-    if not exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
-        echo ERROR: MsBuild couldn't be found
-        exit /b 1
-    ) else (
-        SET "msBuildPath=%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
-    )
+IF NOT DEFINED msbuildPath (
+    SET msbuildPath=msbuild
 )
 
 REM Repo-specific paths
