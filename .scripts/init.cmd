da4e284a841d2c99c2498f070f85a80a530d9a11
Jacob Alber <jaalber@microsoft.com>
2019-04-24 13:08:56 -0400

    Fix init.cmd to also find vstest.console when it is not provided

--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -7,12 +7,25 @@ IF NOT DEFINED msbuildPath (
     CALL %~dp0find-vs2017.cmd
 )
 
+IF NOT DEFINED vstestPath (
+    CALL %~dp0find-vs2017.cmd
+)
+
 IF NOT DEFINED msbuildPath (
     IF NOT EXIST "%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
         ECHO ERROR: MsBuild couldn't be found
         EXIT /b 1
     ) ELSE (
         SET "msBuildPath=%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+    )
+)
+
+IF NOT DEFINED vstestPath (
+    IF NOT EXIST "%VsInstallDir%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" (
+        ECHO ERROR: vstest.console couldn't be found
+        EXIT /b 1
+    ) ELSE (
+        SET "vstestPath=%VsInstallDir%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
     )
 )
 
--- .scripts/test.cmd
+++ .scripts/test.cmd
@@ -16,7 +16,7 @@ ECHO Running VW Unit Tests in C++
 "%vwRoot%\vowpalwabbit\out\target\Release\x64\vw_unit_test.exe"
 
 ECHO Running VW Unit Tests in C#
-%vstestPath% /Platform:x64 /inIsolation "%vwRoot%\vowpalwabbit\x64\Release\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO"
+"%vstestPath%" /Platform:x64 /inIsolation "%vwRoot%\vowpalwabbit\x64\Release\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO"
 REM this is the same as above, just xcopied to the out folder. This is broken due to baked-in paths. The fix is to place the output straight into out.
 REM %vstestPath% /Platform:x64 /inIsolation "%vwRoot%\vowpalwabbit\x64\Release\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO"
 
