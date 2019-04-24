12643304b24748fef5e30ff952a92b76ac39f254
Jacob Alber <jaalber@microsoft.com>
2019-04-24 13:02:45 -0400

    Enable finding VS2017 MSBuild automatically

--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -4,7 +4,16 @@ IF NOT DEFINED nugetPath (
 )
 
 IF NOT DEFINED msbuildPath (
-    SET msbuildPath=msbuild
+    CALL %~dp0find-vs2017.cmd
+)
+
+IF NOT DEFINED msbuildPath (
+    IF NOT EXIST "%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+        ECHO ERROR: MsBuild couldn't be found
+        EXIT /b 1
+    ) ELSE (
+        SET "msBuildPath=%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+    )
 )
 
 REM Repo-specific paths
