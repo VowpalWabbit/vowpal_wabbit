ecd2720e843eaddc1c895ae76a86188d69241e35
Jacob Alber <jaalber@microsoft.com>
2019-05-02 10:17:35 -0400

    Bring set closer to check for envvars

--- .scripts/init.cmd
+++ .scripts/init.cmd
@@ -12,20 +12,20 @@ IF NOT DEFINED vstestPath (
 )
 
 IF NOT DEFINED msbuildPath (
-    IF NOT EXIST "%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+    IF EXIST "%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+        SET "msBuildPath=%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+    ) ELSE (
         ECHO ERROR: MsBuild couldn't be found
         EXIT /b 1
-    ) ELSE (
-        SET "msBuildPath=%VsInstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
     )
 )
 
 IF NOT DEFINED vstestPath (
-    IF NOT EXIST "%VsInstallDir%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" (
+    IF EXIST "%VsInstallDir%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" (
+        SET "vstestPath=%VsInstallDir%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+    ) ELSE (
         ECHO ERROR: vstest.console couldn't be found
         EXIT /b 1
-    ) ELSE (
-        SET "vstestPath=%VsInstallDir%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
     )
 )
 
