d533941739b4c6a56d17df3ff99468e99d74a723
Alexey Taymanov <41013086+ataymano@users.noreply.github.com>
2019-05-26 19:23:06 -0400

    do not output progressive validation loss for oaa with subsampling (#1880)
    * do not output progressive validation loss for oaa with subsampling
    
    * oaa_subsample test
    
    * oaa_subsample unit test fix
    
    * tests reordering
    
    * tests fix

--- vowpalwabbit/multiclass.cc
+++ vowpalwabbit/multiclass.cc
@@ -157,13 +157,13 @@ void print_update_with_probability(vw& all, example& ec, uint32_t pred)
 }
 void print_update_with_score(vw& all, example& ec, uint32_t pred) { print_update<print_score>(all, ec, pred); }
 
-void finish_example(vw& all, example& ec)
+void finish_example(vw& all, example& ec, bool update_loss)
 {
   float loss = 0;
   if (ec.l.multi.label != (uint32_t)ec.pred.multiclass && ec.l.multi.label != (uint32_t)-1)
     loss = ec.weight;
 
-  all.sd->update(ec.test_only, ec.l.multi.label != (uint32_t)-1, loss, ec.weight, ec.num_features);
+  all.sd->update(ec.test_only, update_loss && (ec.l.multi.label != (uint32_t)-1), loss, ec.weight, ec.num_features);
 
   for (int sink : all.final_prediction_sink)
     if (!all.sd->ldict)
