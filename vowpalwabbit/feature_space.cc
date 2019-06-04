8eb9868acf507937673159acab9122cb18cf3439
ataymano@microsoft.com <Alexey Taymanov>
2019-06-04 14:14:41 -0400

    Revert "get rid of initializers"
    This reverts commit 36bf448ddfbb1f536f16257a4e64a300045a604d.

--- vowpalwabbit/feature_space.cc
+++ vowpalwabbit/feature_space.cc
@@ -11,27 +11,42 @@ namespace VW {
   {
   }
 
-  primitive_feature_space& primitive_feature_space::reset(size_t _len)
+  primitive_feature_space::primitive_feature_space(
+    ::vw& _all, const std::string& _name, std::initializer_list<feature> features)
+  : primitive_feature_space(_all, _name, features.size())
   {
-    fs.resize(_len);
-    return *this;
+    size_t i = 0;
+    for (const auto& f : features) {
+      fs[i] = f;
+    }
   }
+
+  primitive_feature_space::primitive_feature_space(
+    ::vw& _all, const std::string& _name, std::initializer_list<std::tuple<const char*, float>> features)
+  : primitive_feature_space(_all, _name, features.size())
+  {
+    size_t i = 0;
+    for (const auto& f : features)
+    {
+      set(i, std::get<0>(f), std::get<1>(f));
+    }
+  }
+
+  void primitive_feature_space::reset(size_t _len) { fs.resize(_len); }
 
   const feature& primitive_feature_space::operator[](size_t index) const { return fs[index]; }
   feature& primitive_feature_space::operator[](size_t index) { return fs[index]; }
 
-  primitive_feature_space& primitive_feature_space::set(size_t index, const std::string& feature_name, float value)
+  void primitive_feature_space::set(size_t index, const std::string& feature_name, float value)
   {
     fs[index].weight_index = hash_feature(all, feature_name, hash);
     fs[index].x = value;
-    return *this;
   }
 
-  primitive_feature_space& primitive_feature_space::set_name(const std::string& _name)
+  void primitive_feature_space::set_name(const std::string& _name)
   {
     name = _name[0];
     hash = hash_space(all, _name);
-    return *this;
   }
   unsigned char primitive_feature_space::get_name() const { return name; }
   size_t primitive_feature_space::size() const { return fs.size(); }
