911f901af286b88bde14e1a0be05e62147f5e826
Jacob Alber <jalber@fernir.com>
2019-06-04 15:15:06 -0400

    Add more resilience to find-vs2017.cmd

--- .scripts/find-vs2017.cmd
+++ .scripts/find-vs2017.cmd
@@ -1,4 +1,9 @@
 IF NOT DEFINED VsInstallDir (
+    IF NOT EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
+        ECHO Failed to find VsWhere. Unable to discover VS2017 Installation Path.
+        EXIT /B 1
+    )
+    
     REM Try to find VS Install
     FOR /f "usebackq tokens=*" %%i IN (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) DO (
         SET "VsInstallDir=%%i"
