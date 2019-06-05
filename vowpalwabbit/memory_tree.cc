cf0ca60e78b3653d4e1b2fdcc4a970983a1dc1fe
Wen Sun <wensun@andrew.cmu.edu>
2019-06-04 21:35:59 -0400

    .

--- vowpalwabbit/memory_tree.cc
+++ vowpalwabbit/memory_tree.cc
@@ -257,8 +257,8 @@ namespace memory_tree_ns
         flat_example* fec1 = flatten_sort_example(*b.all, ec1);
         flat_example* fec2 = flatten_sort_example(*b.all, ec2);
         float linear_prod = linear_kernel(fec1, fec2);
-        fec1->fs.delete_v(); 
-        fec2->fs.delete_v();
+        //fec1->fs.delete_v(); 
+        //fec2->fs.delete_v();
         free_flatten_example(fec1);
         free_flatten_example(fec2);
         return linear_prod/pow(fec1->total_sum_feat_sq*fec2->total_sum_feat_sq, 0.5f);
