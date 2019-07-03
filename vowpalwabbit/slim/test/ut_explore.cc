0677c46b92ae092eba3454dc60b54afa1a0ded4e
Rajan Chari <ranaras@microsoft.com>
2019-07-03 04:26:13 -0400

    removed sample_after_normalization that has scores and ranking

--- vowpalwabbit/slim/test/ut_explore.cc
+++ vowpalwabbit/slim/test/ut_explore.cc
@@ -158,51 +158,64 @@ TEST(ExploreTestSuite, sampling)
 
 TEST(ExploreTestSuite, sampling_rank)
 {
-	std::vector<float> scores = { 0.f, 3.f, 1.f };  // 0,2,1 <-- ranking based on min scores.
-	std::vector<float> histogram(scores.size() * scores.size());
-	std::vector<int> ranking(3);
+  std::vector<float> scores = {0.f, 3.f, 1.f};  // 1,2,0 <-- top ranking
+  std::vector<float> histogram(scores.size() * scores.size());
+  std::vector<int> ranking(3);
 
-	//std::fstream log("c:\\temp\\skype.log", std::fstream::app);
+  // std::fstream log("c:\\temp\\skype.log", std::fstream::app);
 
-	size_t rep = 50000;
-	for (size_t i = 0; i < rep; i++)
-	{
-		std::vector<float> pdf = { 0.8f, 0.1f, 0.1f };
+  size_t rep = 50000;
+  for (size_t i = 0; i < rep; i++)
+  {
+    std::vector<float> pdf = {0.8f, 0.1f, 0.1f};
 
-		std::stringstream s;
-		s << "abcde" << i;
-		ASSERT_EQ(S_EXPLORATION_OK, exploration::sample_after_normalizing(s.str().c_str(), std::begin(pdf), std::end(pdf), std::begin(scores), std::end(scores), std::begin(ranking), std::end(ranking)));
+    std::stringstream s;
+    s << "abcde" << i;
 
-		for (size_t i = 0; i < ranking.size(); i++)
-			histogram[i*ranking.size() + ranking[i]]++;
-	}
+    ASSERT_EQ(S_EXPLORATION_OK,
+        vw_slim::vw_predict<float>::sort_by_scores(std::begin(pdf), std::end(pdf), std::begin(scores), std::end(scores),
+            std::begin(ranking), std::end(ranking)));
 
-	for (auto& d : histogram)
-		d /= rep;
-	
-	// best order is 0, 2, 1
-	// rows: slots
-	// cols: actions
-	std::vector<float> ranking_pdf = {
-		// see top action 1 w / 0.8
-		0.8f, 0.1f, 0.1f,	// slot 0 
-		// most of the time we should see action 0
-		// in 10% we should see the top action 1 swapped from top-slot to here
-		0.1f, 0.0f, 0.9f,	// slot 1 
-		// most of the time we should see action 2
-		// in 10% we should see the top action swapped from top-slot to here
-		0.1f, 0.9f, 0.0f,	// slot 2
-	};
+    // Sample from the pdf
+    uint32_t chosen_action_idx;
+    ASSERT_EQ(S_EXPLORATION_OK,
+        exploration::sample_after_normalizing(s.str().c_str(), std::begin(pdf), std::end(pdf), chosen_action_idx));
 
-	//for (size_t i = 0; i < 3; i++)
-	//{
-	//	log << "slot " << i << " ";
-	//	for (size_t j = 0; j < 3; j++)
-	//		log << histogram[i * 3 + j] << " ";
-	//	log << std::endl;
-	//}
+    // Swap top element with chosen one (unless chosen is the top)
+    if (chosen_action_idx != 0)
+    {
+      std::iter_swap(std::begin(ranking), std::begin(ranking) + chosen_action_idx);
+      std::iter_swap(std::begin(pdf), std::begin(pdf) + chosen_action_idx);
+    }
 
-	EXPECT_THAT(histogram, Pointwise(FloatNearPointwise(1e-2f), ranking_pdf));
+    for (size_t i = 0; i < ranking.size(); i++) histogram[i * ranking.size() + ranking[i]]++;
+  }
+
+  for (auto& d : histogram) d /= rep;
+
+  // best order is 0, 2, 1
+  // rows: slots
+  // cols: actions
+  std::vector<float> ranking_pdf = {
+      // see top action 1 w / 0.8
+      0.8f, 0.1f, 0.1f,  // slot 0
+      // most of the time we should see action 0
+      // in 10% we should see the top action 1 swapped from top-slot to here
+      0.1f, 0.0f, 0.9f,  // slot 1
+      // most of the time we should see action 2
+      // in 10% we should see the top action swapped from top-slot to here
+      0.1f, 0.9f, 0.0f,  // slot 2
+  };
+
+  // for (size_t i = 0; i < 3; i++)
+  //{
+  //	log << "slot " << i << " ";
+  //	for (size_t j = 0; j < 3; j++)
+  //		log << histogram[i * 3 + j] << " ";
+  //	log << std::endl;
+  //}
+
+  EXPECT_THAT(histogram, Pointwise(FloatNearPointwise(1e-2f), ranking_pdf));
 }
 
 TEST(ExploreTestSuite, sampling_rank_bad_range)
@@ -215,33 +228,18 @@ TEST(ExploreTestSuite, sampling_rank_bad_range)
 
   EXPECT_THAT(E_EXPLORATION_BAD_RANGE, exploration::sample_after_normalizing("abc", std::begin(pdf), std::end(pdf), chosen_index));
   EXPECT_THAT(E_EXPLORATION_BAD_RANGE, exploration::sample_after_normalizing("abc", &x, &x - 3, chosen_index));
-	EXPECT_THAT(E_EXPLORATION_BAD_RANGE, exploration::sample_after_normalizing("abc", std::begin(pdf), std::end(pdf), std::begin(scores), std::end(scores), std::begin(ranking), std::end(ranking)));
-	EXPECT_THAT(E_EXPLORATION_BAD_RANGE, exploration::sample_after_normalizing("abc", &x, &x - 3, std::begin(scores), std::end(scores), std::begin(ranking), std::end(ranking)));
 }
 
 TEST(ExploreTestSuite, sampling_rank_zero_pdf)
 {
   std::vector<float> pdf = { 0.f, 0.f, 0.f };
   std::vector<float> expected_pdf = { 1.f, 0.f, 0.f };
-	std::vector<float> scores(3);
-	std::vector<int> ranking(3);
 
-	EXPECT_THAT(S_EXPLORATION_OK, exploration::sample_after_normalizing("abc", std::begin(pdf), std::end(pdf), std::begin(scores), std::end(scores), std::begin(ranking), std::end(ranking)));
-	
-	EXPECT_THAT(expected_pdf, Pointwise(FloatNearPointwise(1e-2f), pdf));
+  uint32_t chosen_index;
 
-  ASSERT_THAT(3, (int)ranking.size());
-  ASSERT_THAT(ranking, ElementsAre(0, 1, 2));
-}
-
-TEST(ExploreTestSuite, sampling_rank_pdf_rank_mismatch)
-{
-	std::vector<float> pdf = { 0.f, 0.f, 0.f };
-	std::vector<float> expected_pdf = { 1.f, 0.f, 0.f };
-	std::vector<float> scores(3);
-	std::vector<int> ranking(2);
-
-	EXPECT_THAT(E_EXPLORATION_PDF_RANKING_SIZE_MISMATCH, exploration::sample_after_normalizing("abc", std::begin(pdf), std::end(pdf), std::begin(scores), std::end(scores), std::begin(ranking), std::end(ranking)));
+  EXPECT_THAT( S_EXPLORATION_OK, exploration::sample_after_normalizing("abc", std::begin(pdf), std::end(pdf), chosen_index));
+  
+  EXPECT_THAT(expected_pdf, Pointwise(FloatNearPointwise(1e-2f), pdf));
 }
 
 TEST(ExploreTestSuite, sampling_rank_negative_pdf)
