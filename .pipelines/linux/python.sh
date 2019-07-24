99fd484ffadaabbca67de6552b232f3e8a1b55ab
Jack Gerrits <jackgerrits95@gmail.com>
2019-07-24 14:28:05 -0400

    Use pip as module

--- .pipelines/linux/python.sh
+++ .pipelines/linux/python.sh
@@ -8,8 +8,8 @@ export PATH="/opt/miniconda/bin:$PATH"
 # Run python build and tests
 cd $1
 source activate test-python27
-sudo pip install pytest readme_renderer pandas
+sudo python -m pip install pytest readme_renderer pandas
 python setup.py check -mrs
-sudo python setup.py install
+sudo python -m python setup.py install
 py.test tests
 source deactivate
