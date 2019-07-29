1fc57d41dab48645e30fb3438efaf8810dde1e4a
Jack Gerrits <jackgerrits95@gmail.com>
2019-07-29 18:40:45 -0400

    test build with coverage

--- .scripts/linux/build-with-coverage.sh
+++ .scripts/linux/build-with-coverage.sh
@@ -7,5 +7,6 @@ cd $1
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DGCOV=ON -DWARNINGS=OFF -DBUILD_JAVA=Off -DBUILD_PYTHON=Off -DBUILD_TESTS=On
+cmake .. -DCMAKE_BUILD_TYPE=Release -DGCOV=ON -DWARNINGS=OFF -DBUILD_JAVA=Off -DBUILD_PYTHON=Off -DBUILD_TESTS=Off
+NUM_PROCESSORS=$(cat nprocs.txt)
 make vw-bin -j ${NUM_PROCESSORS}
