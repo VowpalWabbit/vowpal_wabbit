0f91efbe8ae6b02f787bbafe044554fd52b3d98c
Jack Gerrits <jackgerrits95@gmail.com>
2019-07-29 13:27:02 -0400

    Add Python 3.6 build

---
+++ .scripts/linux/python27.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -x
+
+# Miniconda is installed to root in docker file
+export PATH="/opt/miniconda/bin:$PATH"
+
+# Clear build dir as it uses the conda deps.
+cd $1
+rm -fr build
+mkdir build
+cd build
+
+# Run python build and tests
+cd $1
+source activate test-python27
+python setup.py check -mrs
+python setup.py install
+py.test ./python/tests/
+source deactivate
--- .scripts/linux/python36.sh
+++ .scripts/linux/python36.sh
@@ -5,9 +5,14 @@ set -x
 # Miniconda is installed to root in docker file
 export PATH="/opt/miniconda/bin:$PATH"
 
-# Run python build and tests
+# Clear build dir as it uses the conda deps.
 cd $1
-source activate test-python27
+rm -fr build
+mkdir build
+cd build
+
+# Run python build and tests
+source activate test-python36
 python setup.py check -mrs
 python setup.py install
 py.test ./python/tests/
