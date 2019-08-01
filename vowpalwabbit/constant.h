de2048cf9868dfc28080d708d697976670599ffc
Jack Gerrits <jackgerrits@users.noreply.github.com>
2019-08-01 14:50:22 -0400

    Add constexpr and noexcept to some functions, cleanup unused functions (#1985)
    * Add constexpr and noexcept to some functions, cleanup unused functions
    
    * Elevate more constants into constexpr
    
    * Make more constants constexpr
    
    * Undo changing #define to constexpr to see if it caused numerical difference
    
    * revert sign change

--- vowpalwabbit/constant.h
+++ vowpalwabbit/constant.h
@@ -4,14 +4,14 @@ individual contributors. All rights reserved.  Released under a BSD
 license as described in the file LICENSE.
  */
 #pragma once
-const int quadratic_constant = 27942141;
-const int cubic_constant = 21791;
-const int cubic_constant2 = 37663;
-const int affix_constant = 13903957;
-const uint64_t constant = 11650396;
+constexpr int quadratic_constant = 27942141;
+constexpr int cubic_constant = 21791;
+constexpr int cubic_constant2 = 37663;
+constexpr int affix_constant = 13903957;
+constexpr uint64_t constant = 11650396;
 
-const float probability_tolerance = 1e-5f;
+constexpr float probability_tolerance = 1e-5f;
 
 // FNV-like hash constant for 32bit
 // http://www.isthe.com/chongo/tech/comp/fnv/#FNV-param
-const uint32_t FNV_prime = 16777619;
+constexpr uint32_t FNV_prime = 16777619;
