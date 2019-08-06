ec0f79c3eeb989177a9dfe94059a2f0751ff9ff3
Jacob Alber <jacob.alber@microsoft.com>
2019-08-06 15:01:20 -0400

    Remove deprecated projects and old scripts (#1992)
    * Remove deprecated projects and old scripts
    
    * Removes Azure projects which have not been updated/supported in some time.
    * Removes out-of-date README.Windows file (up-to-date content in Wiki)
    * Removes out of date deploy/publish scripts
    * Also adds missing projects to restore.cmd
    
    * Remove reference to deleted README.Windows.md

--- cs/azure_service/DownloadServiceConfiguration.ps1
+++
@@ -1,5 +0,0 @@
-$mcURL = Read-Host -Prompt "Management Center URL" 
-$storageAccountKey = Read-Host "Storage Account Key" 
-
-$storageAccountKey = [uri]::EscapeDataString($storageAccountKey)
-Invoke-WebRequest -Uri "$($mcURL)Deployment/GenerateTrainerConfig?key=$storageAccountKey" -OutFile ServiceConfiguration.Local.cscfg
