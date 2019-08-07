1820d53de2ab69b5f927d5f3ffdaacba6cbcf027
ataymano@microsoft.com <Alexey Taymanov>
2019-08-07 17:55:30 -0400

    unit tests memory leaks fixes

--- test/unit_test/ccb_parser_test.cc
+++ test/unit_test/ccb_parser_test.cc
@@ -9,89 +9,125 @@
 #include "conditional_contextual_bandit.h"
 #include "parser.h"
 
-CCB::label parse_label(parser* p, std::string label)
+void parse_label(label_parser& lp, parser* p, std::string label, CCB::label& l)
 {
-  auto lp = CCB::ccb_label_parser;
   tokenize(' ', { const_cast<char*>(label.c_str()), const_cast<char*>(label.c_str()) + strlen(label.c_str()) }, p->words);
-  CCB::label l;
   lp.default_label(&l);
   lp.parse_label(p, nullptr, &l, p->words);
-
-  return l;
 }
 
 BOOST_AUTO_TEST_CASE(ccb_parse_label)
 {
+  auto lp = CCB::ccb_label_parser;
   parser p{8 /*ring_size*/, false /*strict parse*/};
   p.words = v_init<substring>();
   p.parse_name = v_init<substring>();
 
-  auto label = parse_label(&p, "ccb shared");
-  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 0);
-  BOOST_CHECK(label.outcome == nullptr);
-  BOOST_CHECK_EQUAL(label.type, CCB::example_type::shared);
-
-  label = parse_label(&p, "ccb action");
-  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 0);
-  BOOST_CHECK(label.outcome == nullptr);
-  BOOST_CHECK_EQUAL(label.type, CCB::example_type::action);
-
-  label = parse_label(&p, "ccb slot");
-  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 0);
-  BOOST_CHECK(label.outcome == nullptr);
-  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
-
-  label = parse_label(&p, "ccb slot 1,3,4");
-  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 3);
-  BOOST_CHECK_EQUAL(label.explicit_included_actions[0], 1);
-  BOOST_CHECK_EQUAL(label.explicit_included_actions[1], 3);
-  BOOST_CHECK_EQUAL(label.explicit_included_actions[2], 4);
-  BOOST_CHECK(label.outcome == nullptr);
-  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
-
-  label = parse_label(&p, "ccb slot 1:1.0:0.5 3");
-  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 1);
-  BOOST_CHECK_EQUAL(label.explicit_included_actions[0], 3);
-  BOOST_CHECK_CLOSE(label.outcome->cost, 1.0f, FLOAT_TOL);
-  BOOST_CHECK_EQUAL(label.outcome->probabilities.size(), 1);
-  BOOST_CHECK_EQUAL(label.outcome->probabilities[0].action, 1);
-  BOOST_CHECK_CLOSE(label.outcome->probabilities[0].score, .5f, FLOAT_TOL);
-  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
-
-  label = parse_label(&p, "ccb slot 1:-2.0:0.5,2:0.25,3:0.25 3,4");
-  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 2);
-  BOOST_CHECK_EQUAL(label.explicit_included_actions[0], 3);
-  BOOST_CHECK_EQUAL(label.explicit_included_actions[1], 4);
-  BOOST_CHECK_CLOSE(label.outcome->cost, -2.0f, FLOAT_TOL);
-  BOOST_CHECK_EQUAL(label.outcome->probabilities.size(), 3);
-  BOOST_CHECK_EQUAL(label.outcome->probabilities[0].action, 1);
-  BOOST_CHECK_CLOSE(label.outcome->probabilities[0].score, .5f, FLOAT_TOL);
-  BOOST_CHECK_EQUAL(label.outcome->probabilities[1].action, 2);
-  BOOST_CHECK_CLOSE(label.outcome->probabilities[1].score, .25f, FLOAT_TOL);
-  BOOST_CHECK_EQUAL(label.outcome->probabilities[2].action, 3);
-  BOOST_CHECK_CLOSE(label.outcome->probabilities[2].score, .25f, FLOAT_TOL);
-  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
-
-  BOOST_REQUIRE_THROW(parse_label(&p, "shared"), VW::vw_exception);
-  BOOST_REQUIRE_THROW(parse_label(&p, "other shared"), VW::vw_exception);
-  BOOST_REQUIRE_THROW(parse_label(&p, "other"), VW::vw_exception);
-  BOOST_REQUIRE_THROW(parse_label(&p, "ccb unknown"), VW::vw_exception);
-  BOOST_REQUIRE_THROW(parse_label(&p, "ccb slot 1:1.0:0.5,4:0.7"), VW::vw_exception);
+  {
+	  CCB::label label;
+	  parse_label(lp, &p, "ccb shared", label);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 0);
+	  BOOST_CHECK(label.outcome == nullptr);
+	  BOOST_CHECK_EQUAL(label.type, CCB::example_type::shared);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  parse_label(lp, &p, "ccb action", label);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 0);
+	  BOOST_CHECK(label.outcome == nullptr);
+	  BOOST_CHECK_EQUAL(label.type, CCB::example_type::action);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  parse_label(lp, &p, "ccb slot", label);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 0);
+	  BOOST_CHECK(label.outcome == nullptr);
+	  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  parse_label(lp, &p, "ccb slot 1,3,4", label);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 3);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions[0], 1);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions[1], 3);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions[2], 4);
+	  BOOST_CHECK(label.outcome == nullptr);
+	  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  parse_label(lp, &p, "ccb slot 1:1.0:0.5 3", label);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 1);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions[0], 3);
+	  BOOST_CHECK_CLOSE(label.outcome->cost, 1.0f, FLOAT_TOL);
+	  BOOST_CHECK_EQUAL(label.outcome->probabilities.size(), 1);
+	  BOOST_CHECK_EQUAL(label.outcome->probabilities[0].action, 1);
+	  BOOST_CHECK_CLOSE(label.outcome->probabilities[0].score, .5f, FLOAT_TOL);
+	  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  parse_label(lp, &p, "ccb slot 1:-2.0:0.5,2:0.25,3:0.25 3,4", label);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions.size(), 2);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions[0], 3);
+	  BOOST_CHECK_EQUAL(label.explicit_included_actions[1], 4);
+	  BOOST_CHECK_CLOSE(label.outcome->cost, -2.0f, FLOAT_TOL);
+	  BOOST_CHECK_EQUAL(label.outcome->probabilities.size(), 3);
+	  BOOST_CHECK_EQUAL(label.outcome->probabilities[0].action, 1);
+	  BOOST_CHECK_CLOSE(label.outcome->probabilities[0].score, .5f, FLOAT_TOL);
+	  BOOST_CHECK_EQUAL(label.outcome->probabilities[1].action, 2);
+	  BOOST_CHECK_CLOSE(label.outcome->probabilities[1].score, .25f, FLOAT_TOL);
+	  BOOST_CHECK_EQUAL(label.outcome->probabilities[2].action, 3);
+	  BOOST_CHECK_CLOSE(label.outcome->probabilities[2].score, .25f, FLOAT_TOL);
+	  BOOST_CHECK_EQUAL(label.type, CCB::example_type::slot);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  BOOST_REQUIRE_THROW(parse_label(lp, &p, "shared", label), VW::vw_exception);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  BOOST_REQUIRE_THROW(parse_label(lp, &p, "other shared", label), VW::vw_exception);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  BOOST_REQUIRE_THROW(parse_label(lp, &p, "other", label), VW::vw_exception);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  BOOST_REQUIRE_THROW(parse_label(lp, &p, "ccb unknown", label), VW::vw_exception);
+	  lp.delete_label(&label);
+  }
+  {
+	  CCB::label label;
+	  BOOST_REQUIRE_THROW(parse_label(lp, &p, "ccb slot 1:1.0:0.5,4:0.7", label), VW::vw_exception);
+	  lp.delete_label(&label);
+  }
+  p.words.delete_v();
+  p.parse_name.delete_v();
 }
 
 BOOST_AUTO_TEST_CASE(ccb_cache_label)
 {
   io_buf io;
   io.init();
-  io.space.resize(1000);
-  io.space.end() = io.space.begin() + 1000;
 
   parser p{8 /*ring_size*/, false /*strict parse*/};
   p.words = v_init<substring>();
   p.parse_name = v_init<substring>();
 
   auto lp = CCB::ccb_label_parser;
-  auto label = parse_label(&p, "ccb slot 1:-2.0:0.5,2:0.25,3:0.25 3,4");
+  CCB::label label;
+  parse_label(lp, &p, "ccb slot 1:-2.0:0.5,2:0.25,3:0.25 3,4", label);
 
   lp.cache_label(&label, io);
   io.head = io.space.begin();
@@ -112,6 +148,10 @@ BOOST_AUTO_TEST_CASE(ccb_cache_label)
   BOOST_CHECK_EQUAL(uncached_label.outcome->probabilities[2].action, 3);
   BOOST_CHECK_CLOSE(uncached_label.outcome->probabilities[2].score, .25f, FLOAT_TOL);
   BOOST_CHECK_EQUAL(uncached_label.type, CCB::example_type::slot);
+  lp.delete_label(&label);
+  lp.delete_label(&uncached_label);
+  p.words.delete_v();
+  p.parse_name.delete_v();
 }
 
 BOOST_AUTO_TEST_CASE(ccb_copy_label)
@@ -120,8 +160,9 @@ BOOST_AUTO_TEST_CASE(ccb_copy_label)
   p.words = v_init<substring>();
   p.parse_name = v_init<substring>();
   auto lp = CCB::ccb_label_parser;
-
-  auto label = parse_label(&p, "ccb slot 1:-2.0:0.5,2:0.25,3:0.25 3,4");
+  
+  CCB::label label;
+  parse_label(lp, &p, "ccb slot 1:-2.0:0.5,2:0.25,3:0.25 3,4", label);
 
   CCB::label copied_to;
   lp.default_label(&copied_to);
@@ -140,4 +181,8 @@ BOOST_AUTO_TEST_CASE(ccb_copy_label)
   BOOST_CHECK_EQUAL(copied_to.outcome->probabilities[2].action, 3);
   BOOST_CHECK_CLOSE(copied_to.outcome->probabilities[2].score, .25f, FLOAT_TOL);
   BOOST_CHECK_EQUAL(copied_to.type, CCB::example_type::slot);
+  lp.delete_label(&label);
+  lp.delete_label(&copied_to);
+  p.words.delete_v();
+  p.parse_name.delete_v();
 }
