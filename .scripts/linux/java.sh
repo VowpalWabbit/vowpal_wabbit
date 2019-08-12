cdf9d66394518fabf243dc5bf2585dac5577e53d
Markus Cozowicz <marcozo@microsoft.com>
2019-08-12 18:00:28 +0200

    add maven publishing

--- .scripts/linux/java.sh
+++ .scripts/linux/java.sh
@@ -8,3 +8,17 @@ cd $REPO_DIR
 
 # Run Java build and test
 mvn clean test -f java/pom.xml
+
+if [ -z "$ossrh_username" ]
+then
+	# template for username/password for sonatype repository server
+	cp java/settings.xml ~/.m2/settings.xml
+
+	# import signing key
+	# gpg --export-secret-keys 'Markus Cozowicz <marcozo@microsoft.com>' | base64 -w 0
+	echo $ossrh_gpg | base64 -d | gpg --import -
+
+	MAVEN_OPTS="-Dossrh.username=$ossrh_username -Dossrh.password=$ossrh_password"
+	# mvn -f java/pom.xml release:prepare $MAVEN_OPTS
+	mvn -f java/pom.xml verify gpg:sign deploy:deploy -Dmaven.test.skip=true $MAVEN_OPTS
+fi
