2a13aae7b0964f7321baa4f4cb92a884a74a0cf4
Jacob Alber <jaalber@microsoft.com>
2019-08-15 09:03:08 -0400

    Upgrade travisCI build container version
    * Also removes superfluous upgrade of CMake.

--- build-linux.sh
+++ build-linux.sh
@@ -9,18 +9,6 @@ export PATH="$HOME/miniconda/bin:$PATH"
 cd /vw
 ./utl/clang-format check
 
-sudo apt remove --yes --force-yes cmake
-
-# Upgrade CMake
-version=3.5
-build=2
-mkdir ~/temp
-cd ~/temp
-wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh
-sudo mkdir /opt/cmake
-sudo sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
-sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
-
 cd /vw
 mkdir build
 cd build
