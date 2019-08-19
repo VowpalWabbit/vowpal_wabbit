af08a84cc4a1a15fa0987217bd8c3c4dbeba8f36
Markus Cozowicz <marcozo@microsoft.com>
2019-08-19 19:07:05 +0200

    added documentation

--- .scripts/linux/build-static-java.sh
+++ .scripts/linux/build-static-java.sh
@@ -9,6 +9,7 @@ cd $REPO_DIR
 mkdir -p build
 cd build
 # Boost unit tests don't like the static linking
+# /usr/local/bin/gcc + g++ is 9.2.0 version
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWARNINGS=Off -DBUILD_JAVA=On -DBUILD_DOCS=Off \
  -DBUILD_PYTHON=Off -DSTATIC_LINK_VW_JAVA=On -DCMAKE_C_COMPILER=/usr/local/bin/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ \
  -DBUILD_TESTS=Off
