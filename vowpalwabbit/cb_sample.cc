10dde624f60b8b74dab277014d57da8717ffb4a5
Jack Gerrits <jackgerrits95@gmail.com>
2019-09-16 21:53:57 -0600

    move cb sample to a class

--- vowpalwabbit/cb_sample.cc
+++ vowpalwabbit/cb_sample.cc
@@ -8,82 +8,91 @@ using namespace LEARNER;
 using namespace VW;
 using namespace VW::config;
 
+namespace VW
+{
 struct cb_sample_data
 {
-  uint64_t seed_state;
-};
+  explicit cb_sample_data(uint64_t seedState) : _seed_state(seedState) {}
 
-template <bool is_learn>
-void learn_or_predict(cb_sample_data& data, multi_learner& base, multi_ex& examples)
-{
-  multiline_learn_or_predict<is_learn>(base, examples, examples[0]->ft_offset);
-
-  auto action_scores = examples[0]->pred.a_s;
-  uint32_t chosen_action = -1;
-
-  int labelled_action = -1;
-  // Find that chosen action in the learning case, skip the shared example.
-  for(size_t i = 0; i < examples.size(); i++)
+  template <bool is_learn>
+  void learn_or_predict(multi_learner &base, multi_ex &examples)
   {
-    if(examples[i]->l.cb.costs.size() > 0)
+    multiline_learn_or_predict<is_learn>(base, examples, examples[0]->ft_offset);
+
+    auto action_scores = examples[0]->pred.a_s;
+    uint32_t chosen_action = -1;
+
+    int labelled_action = -1;
+    // Find that chosen action in the learning case, skip the shared example.
+    auto it = std::find_if(examples.begin(), examples.end(), [](example *item) { return !item->l.cb.costs.empty(); });
+    if (it != examples.end())
     {
-      // Must remove 1 because of shared example index.
-      labelled_action = static_cast<uint32_t>(i);
+      labelled_action = std::distance(examples.begin(), it);
     }
-  }
 
-  // If we are learning and have a label, then take that action as the chosen action. Otherwise sample the distribution.
-  if (is_learn && labelled_action != -1)
-  {
-    // Find where the labelled action is in the final prediction to determine if swapping needs to occur.
-    // This only matters if the prediction decided to explore, but the same output should happen for the learn case.
-    for (size_t i = 0; i < action_scores.size(); i++)
+    // If we are learning and have a label, then take that action as the chosen action. Otherwise sample the
+    // distribution.
+    if (is_learn && labelled_action != -1)
     {
-      auto& a_s = action_scores[i];
-      if (a_s.action == static_cast<uint32_t>(labelled_action))
+      // Find where the labelled action is in the final prediction to determine if swapping needs to occur.
+      // This only matters if the prediction decided to explore, but the same output should happen for the learn case.
+      for (size_t i = 0; i < action_scores.size(); i++)
       {
-        chosen_action = static_cast<uint32_t>(i);
-        break;
+        auto &a_s = action_scores[i];
+        if (a_s.action == static_cast<uint32_t>(labelled_action))
+        {
+          chosen_action = static_cast<uint32_t>(i);
+          break;
+        }
       }
     }
-  }
-  else
-  {
-    bool tag_provided_seed = false;
-    uint64_t seed = data.seed_state;
-    if (examples[0]->tag.size() > 0)
+    else
     {
-      const std::string SEED_IDENTIFIER = "seed=";
-      if (strncmp(examples[0]->tag.begin(), SEED_IDENTIFIER.c_str(), SEED_IDENTIFIER.size()) == 0 &&
-          examples[0]->tag.size() > SEED_IDENTIFIER.size())
+      bool tag_provided_seed = false;
+      uint64_t seed = _seed_state;
+      if (!examples[0]->tag.empty())
       {
-        substring tag_seed{examples[0]->tag.begin() + 5, examples[0]->tag.begin() + examples[0]->tag.size()};
-        seed = uniform_hash(tag_seed.begin, substring_len(tag_seed), 0);
-        tag_provided_seed = true;
+        const std::string SEED_IDENTIFIER = "seed=";
+        if (strncmp(examples[0]->tag.begin(), SEED_IDENTIFIER.c_str(), SEED_IDENTIFIER.size()) == 0 &&
+            examples[0]->tag.size() > SEED_IDENTIFIER.size())
+        {
+          substring tag_seed{examples[0]->tag.begin() + 5, examples[0]->tag.begin() + examples[0]->tag.size()};
+          seed = uniform_hash(tag_seed.begin, substring_len(tag_seed), 0);
+          tag_provided_seed = true;
+        }
+      }
+
+      // Sampling is done after the base learner has generated a pdf.
+      auto result = exploration::sample_after_normalizing(
+          seed, ACTION_SCORE::begin_scores(action_scores), ACTION_SCORE::end_scores(action_scores), chosen_action);
+      assert(result == S_EXPLORATION_OK);
+      _UNUSED(result);
+
+      // Update the seed state in place if it was used for this example.
+      if (!tag_provided_seed)
+      {
+        merand48(_seed_state);
       }
     }
 
-    // Sampling is done after the base learner has generated a pdf.
-    auto result = exploration::sample_after_normalizing(
-        seed, ACTION_SCORE::begin_scores(action_scores), ACTION_SCORE::end_scores(action_scores), chosen_action);
+    auto result = exploration::swap_chosen(action_scores.begin(), action_scores.end(), chosen_action);
     assert(result == S_EXPLORATION_OK);
     _UNUSED(result);
-
-    // Update the seed state in place if it was used for this example.
-    if (!tag_provided_seed)
-    {
-      merand48(data.seed_state);
-    }
   }
 
-  auto result = exploration::swap_chosen(action_scores.begin(), action_scores.end(), chosen_action);
-  assert(result == S_EXPLORATION_OK);
-  _UNUSED(result);
+ private:
+  uint64_t _seed_state;
+};
+}  // namespace VW
+
+template <bool is_learn>
+void learn_or_predict(cb_sample_data &data, multi_learner &base, multi_ex &examples)
+{
+  data.learn_or_predict<is_learn>(base, examples);
 }
 
-base_learner* cb_sample_setup(options_i& options, vw& all)
+base_learner *cb_sample_setup(options_i &options, vw &all)
 {
-  auto data = scoped_calloc_or_throw<cb_sample_data>();
   bool cb_sample_option = false;
 
   option_group_definition new_options("CB Sample");
@@ -93,8 +102,7 @@ base_learner* cb_sample_setup(options_i& options, vw& all)
   if (!cb_sample_option)
     return nullptr;
 
-  data->seed_state = all.random_seed;
-
+  auto data = scoped_calloc_or_throw<cb_sample_data>(all.random_seed);
   return make_base(init_learner(data, as_multiline(setup_base(options, all)), learn_or_predict<true>,
       learn_or_predict<false>, 1 /* weights */, prediction_type::action_probs));
 }
--- vowpalwabbit/cb_sample.h
+++ vowpalwabbit/cb_sample.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#include "reductions_fwd.h
+
 LEARNER::base_learner* cb_sample_setup(VW::config::options_i& options, vw& all);
