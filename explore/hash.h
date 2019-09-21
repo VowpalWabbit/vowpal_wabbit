813f5d514a6d786c6b90d684b4f19350e702aca0
Jack Gerrits <jackgerrits95@gmail.com>
2019-09-20 23:08:26 -0600

    Make hashing constexpr in C++14

--- explore/hash.h
+++ explore/hash.h
@@ -29,59 +29,48 @@ license as described in the file LICENSE.
 //----
 #pragma once
 
-#include <sys/types.h>  // defines size_t
+// #include <cstdlib>
+// #include <cstdint>
 
-// Platform-specific functions and macros
-#if defined(_MSC_VER)                       // Microsoft Visual Studio
-#   include <stdint.h>
+#include "future_compat.h"
 
-#   include <stdlib.h>
-#   define ROTL32(x,y)  _rotl(x,y)
-#   define BIG_CONSTANT(x) (x)
 
-#else                                       // Other compilers
-#   include <stdint.h>   // defines uint32_t etc
+#include <sys/types.h>
+#include <stdint.h>
 
+// All modern compilers will optimize this to the rotate intrinsic.
 constexpr inline uint32_t rotl32(uint32_t x, int8_t r) noexcept
-{ return (x << r) | (x >> (32 - r));
+{
+  return (x << r) | (x >> (32 - r));
 }
-
-#   define ROTL32(x,y)     rotl32(x,y)
-#   define BIG_CONSTANT(x) (x##LLU)
-
-#endif                                      // !defined(_MSC_VER)
 
 namespace MURMUR_HASH_3
 {
+  //-----------------------------------------------------------------------------
+  // Finalization mix - force all bits of a hash block to avalanche
+  STD14_CONSTEXPR static inline uint32_t fmix(uint32_t h) noexcept
+  {
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
 
-//-----------------------------------------------------------------------------
-// Finalization mix - force all bits of a hash block to avalanche
+    return h;
+  }
 
-static inline uint32_t fmix(uint32_t h) noexcept
-{ h ^= h >> 16;
-  h *= 0x85ebca6b;
-  h ^= h >> 13;
-  h *= 0xc2b2ae35;
-  h ^= h >> 16;
-
-  return h;
+  //-----------------------------------------------------------------------------
+  // Block read - if your platform needs to do endian-swapping or can only
+  // handle aligned reads, do the conversion here
+  constexpr static inline uint32_t getblock(const uint32_t * p, int i) noexcept
+  {
+    return p[i];
+  }
 }
 
-
-//-----------------------------------------------------------------------------
-// Block read - if your platform needs to do endian-swapping or can only
-// handle aligned reads, do the conversion here
-
-constexpr static inline uint32_t getblock(const uint32_t * p, int i) noexcept
+STD14_CONSTEXPR inline uint64_t uniform_hash(const void* key, size_t len, uint64_t seed)
 {
-  return p[i];
-}
-
-}
-
-inline uint64_t uniform_hash(const void * key, size_t len, uint64_t seed)
-{
-  const uint8_t * data = (const uint8_t*)key;
+  const uint8_t* data = (const uint8_t*)key;
   const int nblocks = (int)len / 4;
 
   uint32_t h1 = (uint32_t)seed;
@@ -97,11 +86,11 @@ inline uint64_t uniform_hash(const void * key, size_t len, uint64_t seed)
     uint32_t k1 = MURMUR_HASH_3::getblock(blocks, i);
 
     k1 *= c1;
-    k1 = ROTL32(k1, 15);
+    k1 = rotl32(k1, 15);
     k1 *= c2;
 
     h1 ^= k1;
-    h1 = ROTL32(h1, 13);
+    h1 = rotl32(h1, 13);
     h1 = h1 * 5 + 0xe6546b64;
   }
 
@@ -122,7 +111,7 @@ inline uint64_t uniform_hash(const void * key, size_t len, uint64_t seed)
     // fall through
   case 1: k1 ^= tail[0];
     k1 *= c1;
-    k1 = ROTL32(k1, 15);
+    k1 = rotl32(k1, 15);
     k1 *= c2; h1 ^= k1;
   }
 
---
+++ vowpalwabbit/future_compat.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#if __cplusplus < 201103L
+#pragma error "At least C++11 is required."
+#endif
+
+#if __cplusplus >= 201402L
+#define HAS_STD14
+#endif
+
+#if __cplusplus >= 201703L
+#define HAS_STD17
+#endif
+
+#ifdef HAS_STD17
+#define ATTR(name) [[ name ]]
+#else
+#define ATTR(name)
+#endif
+
+#ifdef HAS_STD14
+// #define STD14_CONSTEXPR constexpr
+#define STD14_CONSTEXPR
+#else
+#define STD14_CONSTEXPR
+#endif
