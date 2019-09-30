71e835f67fe38a702e2e243eff80425f309c36b7
peterychang <49209570+peterychang@users.noreply.github.com>
2019-09-30 11:33:14 -0400

    Cb explore adf atomization (#2069)
    * cb_explore_adf split into seperate reductions per explore types
    
    * turn random_state into an object
    
    * incorporation PR comments
    
    * Fix initilizer
    
    * fix spammy compiler warning
    
    * using random state object directly in all reductions
    
    * cleaning up header includes
    
    * make member variables private, add reduction constructors
    
    * service-ify random state
    
    * changing member variable naming conventions from m_ to _
    
    * moving class definitions to source files
    
    * Composition instead of inheritance for cb_explore_adf
    
    * clean up after refactor, add comment to existing code

--- vowpalwabbit/cb_explore_adf.h
+++
@@ -1,8 +0,0 @@
-/*
-Copyright (c) by respective owners including Yahoo!, Microsoft, and
-individual contributors. All rights reserved.  Released under a BSD
-license as described in the file LICENSE.
-*/
-#pragma once
-
-LEARNER::base_learner* cb_explore_adf_setup(VW::config::options_i& options, vw& all);
