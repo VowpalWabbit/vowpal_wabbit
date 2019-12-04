d5ce1fd0139d1b010e3e4c3a64198eec42af7507
Rajan Chari <ranaras@microsoft.com>
2019-12-04 06:37:23 -0500

    Fix CCB

--- vowpalwabbit/conditional_contextual_bandit.cc
+++ vowpalwabbit/conditional_contextual_bandit.cc
@@ -424,9 +424,18 @@ void learn_or_predict(ccb& data, multi_learner& base, multi_ex& examples)
     else
       inject_slot_id<false>(data, data.shared, slot_id);
 
+    // the cb example contains at least 1 action
     if (has_action(data.cb_ex))
     {
-      // the cb example contains at least 1 action
+
+      // Notes: This reduction does not seem to have progressive validation or holdoff
+      // validation.  When this work is complete, remove the following line.
+      // For now we emulate this by calling predict first on base reductions to cache
+      // downstream predictions.
+
+      if (is_learn)
+        multiline_learn_or_predict<false>(base, data.cb_ex, examples[0]->ft_offset);
+
       multiline_learn_or_predict<is_learn>(base, data.cb_ex, examples[0]->ft_offset);
       save_action_scores(data, decision_scores);
       clear_pred_and_label(data);
