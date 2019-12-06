293f3dc5e500b4de1872ded78267b1be7d1b7a86
Paul Mineiro <pmineiro@microsoft.com>
2019-12-06 15:21:16 -0800

    cb_dro set example weight

--- vowpalwabbit/cb_dro.cc
+++ vowpalwabbit/cb_dro.cc
@@ -11,7 +11,6 @@ using namespace VW::config;
 
 namespace VW
 {
-// cb_dro is used to automatically sample and swap from a cb explore pdf.
 struct cb_dro_data
 {
   explicit cb_dro_data(double alpha, double tau) : chisq(alpha, tau) {}
@@ -19,57 +18,59 @@ struct cb_dro_data
   template <bool is_learn>
   inline void learn_or_predict(multi_learner &base, multi_ex &examples)
   {
+    // Some explanation required.
+    //
+    // VW currently is either
+    //    in prediction mode with exploration (aka --cb_explore_adf)
+    //    or in (off-policy) learning without exploration (aka --cb_adf)
+    //
+    // In practice, nobody seems to do "truly online policy learning"
+    // aka "--cb_explore_adf while supplying data with labels."  Arguably
+    // distributional robustness is not important for on-policy learning.
+    //
+    // Ergo, the following always optimizes the bound on the derived
+    // deterministic argmax score policy.
+
+
     multiline_learn_or_predict<false>(base, examples, examples[0]->ft_offset);
 
     if (is_learn)
-    {
-      auto action_scores = examples[0]->pred.a_s;
-      // Find that chosen action in the learning case, skip the shared example.
-      auto it = std::find_if(examples.begin(), examples.end(), [](example *item) { return !item->l.cb.costs.empty(); });
-
-      if (it != examples.end())
       {
-        CB::cb_class logged = (*it)->l.cb.costs[0];
-        uint32_t labelled_action = std::distance(examples.begin(), it);
-        float chosen_probability = 0;
-        double total = 0.0;
+        auto it = std::find_if(examples.begin(), examples.end(), [](example *item) { return !item->l.cb.costs.empty(); });
 
-        // Find where the logged action is in the final prediction to determine if swapping needs to occur.
-        // This only matters if the prediction decided to explore, but the same output should happen for the learn
-        // case.
-        for (size_t i = 0; i < action_scores.size(); i++)
+        if (it != examples.end())
         {
-          auto &a_s = action_scores[i];
-          total += a_s.score;
+          CB::cb_class logged = (*it)->l.cb.costs[0];
+          uint32_t labelled_action = std::distance(examples.begin(), it);
 
-          if (a_s.action == static_cast<uint32_t>(labelled_action))
-          {
-            chosen_probability = a_s.score;
-          }
+          auto action_scores = examples[0]->pred.a_s;
+
+          auto maxit = std::max_element(action_scores.begin(),
+                                        action_scores.end(),
+                                        [](const ACTION_SCORE::action_score& a, const ACTION_SCORE::action_score& b) { return ACTION_SCORE::score_comp(&a, &b) < 0; });
+          uint32_t chosen_action = maxit->action;
+
+          float w = logged.probability > 0 && chosen_action == labelled_action ? 1 / logged.probability : 0;
+          // TODO: rmin, rmax (?)
+          float r = -logged.cost;
+
+          chisq.update(w, r);
+
+          float qlb = chisq.qlb(w, r);
+
+          // save the original weights and scale the example weights
+          std::vector<double> save_weight;
+          std::transform(examples.begin(), examples.end(), std::back_inserter(save_weight), [](example *item) { return item->weight; });
+          std::for_each(examples.begin(), examples.end(), [qlb](example *item) { item->weight *= qlb; });
+
+          // TODO: make sure descendants "do the right thing" with example->weight
+          multiline_learn_or_predict<true>(base, examples, examples[0]->ft_offset);
+
+          // restore the original weights
+          auto save_weight_it = save_weight.begin();
+          std::for_each(examples.begin(), examples.end(), [&save_weight_it](example *item) { item->weight = *save_weight_it++; });
         }
-
-        chosen_probability = total > 0 ? chosen_probability / total : 0;
-
-        float w = logged.probability > 0 ? chosen_probability / logged.probability : 0;
-        float r = logged.cost;
-
-        chisq.update(w, r);
-
-        float qlb = chisq.qlb(w, r);
-
-        // save the original weights and scale the example weights
-        std::vector<double> save_weight;
-        std::transform(examples.begin(), examples.end(), std::back_inserter(save_weight), [](example *item) { return item->weight; });
-        std::for_each(examples.begin(), examples.end(), [qlb](example *item) { item->weight *= qlb; });
-
-        // TODO: make sure descendants "do the right thing" with example->weight
-        multiline_learn_or_predict<true>(base, examples, examples[0]->ft_offset);
-
-        // restore the original weights
-        auto save_weight_it = save_weight.begin();
-        std::for_each(examples.begin(), examples.end(), [&save_weight_it](example *item) { item->weight = *save_weight_it++; });
       }
-    }
   }
 
  private:
@@ -104,9 +105,9 @@ base_learner *cb_dro_setup(options_i &options, vw &all)
     THROW("cb_dro cannot be used with no_predict");
   }
 
-  if (!options.was_supplied("cb_explore"))
+  if (!options.was_supplied("cb_adf"))
   {
-    THROW("cb_dro requires cb_explore");
+    THROW("cb_dro requires cb_adf");
   }
 
   if (alpha <= 0 || alpha >= 1)
