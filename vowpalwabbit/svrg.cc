95ea4cfa0881480a234943f788da14c8abc7cb31
Jack Gerrits <jackgerrits95@gmail.com>
2019-12-09 13:59:54 -0800

    add revised

--- vowpalwabbit/svrg.cc
+++ vowpalwabbit/svrg.cc
@@ -1,6 +1,6 @@
 
 // Copyright (c) by respective owners including Yahoo!, Microsoft, and
-// individual contributors. All rights reserved. Released under a BSD
+// individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
 
 #include <cassert>
