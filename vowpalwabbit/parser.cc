ffc19b6752aaeb4900df7cc89f0ef896ac080180
Jack Gerrits <jackgerrits95@gmail.com>
2019-12-13 10:16:00 -0500

    Ensure test_only is correctly set when calling predict on examples

--- vowpalwabbit/parser.cc
+++ vowpalwabbit/parser.cc
@@ -693,8 +693,10 @@ void setup_example(vw& all, example* ae)
   if (!all.p->emptylines_separate_examples)
     all.p->in_pass_counter++;
 
+  // Determine if this example is part of the holdout set.
   ae->test_only = is_test_only(all.p->in_pass_counter, all.holdout_period, all.holdout_after, all.holdout_set_off,
       all.p->emptylines_separate_examples ? (all.holdout_period - 1) : 0);
+  // If this example has a test only label then it is true regardless.
   ae->test_only |= all.p->lp.test_label(&ae->l);
 
   if (all.p->emptylines_separate_examples && example_is_newline(*ae))
