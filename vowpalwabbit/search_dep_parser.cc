3865e632359d2eaff97e34ac7f9097ec8e39663a
Rodrigo Kumpera <kumpera@gmail.com>
2020-01-22 17:37:33 -0500

    Remove triples usage from search_dep_parser.cc

--- vowpalwabbit/search_dep_parser.cc
+++ vowpalwabbit/search_dep_parser.cc
@@ -91,11 +91,10 @@ void initialize(Search::search &sch, size_t & /*num_actions*/, options_i &option
   std::vector<std::string> newpairs(pair, pair + 19);
   std::vector<std::string> newtriples(triple, triple + 12);
   all.pairs.swap(newpairs);
-  all.triples.swap(newtriples);
 
   all.interactions.clear();
   all.interactions.insert(std::end(all.interactions), std::begin(all.pairs), std::end(all.pairs));
-  all.interactions.insert(std::end(all.interactions), std::begin(all.triples), std::end(all.triples));
+  all.interactions.insert(std::end(all.interactions), std::begin(newtriples), std::end(newtriples));
   if (data->cost_to_go)
     sch.set_options(AUTO_CONDITION_FEATURES | NO_CACHING | ACTION_COSTS);
   else
