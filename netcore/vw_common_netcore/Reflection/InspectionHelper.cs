e42d46aa7eca01df4657033627b94a154de39b3e
Marat Turaev <marat.turaev@hotmail.com>
2020-02-06 16:53:10 -0800

    netcore31:   vw_common.csproj -> vw_common_netcore.csproj   vw_clr.vcxproj -> vw_netcore.vcxproj

---
+++ netcore/vw_common_netcore/Reflection/InspectionHelper.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InspectionHelper.cs">
+//   Copyright (c) by respective owners including Yahoo!, Microsoft, and
+//   individual contributors. All rights reserved.  Released under a BSD
+//   license as described in the file LICENSE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace VW.Reflection
+{
+    /// <summary>
+    /// Utilitiy class supporting feature type inspection.
+    /// </summary>
+    public static class InspectionHelper
+    {
+        /// <summary>
+        /// Determines if the <paramref name="elemType"/> is a supported numeric type.
+        /// </summary>
+        /// <param name="elemType">The type to be inspected.</param>
+        /// <returns>True if numeric, false otherwise.</returns>
+        public static bool IsNumericType(Type elemType)
+        {
+            return IsNumericTypeInternal(elemType) ||
+                   (elemType != null
+                    && elemType.IsGenericType
+                    &&  elemType.GetGenericTypeDefinition() == typeof(Nullable<>)
+                    && IsNumericTypeInternal(elemType.GetGenericArguments()[0]));
+        }
+
+        private static bool IsNumericTypeInternal(Type elemType)
+        {
+            return elemType == typeof(double)
+                    || elemType == typeof(float)
+                    || elemType == typeof(byte)
+                    || elemType == typeof(sbyte)
+                    || elemType == typeof(char)
+                    || elemType == typeof(decimal)
+                    || elemType == typeof(UInt16)
+                    || elemType == typeof(UInt32)
+                    || elemType == typeof(UInt64)
+                    || elemType == typeof(Int16)
+                    || elemType == typeof(Int32)
+                    || elemType == typeof(Int64);
+        }
+
+        /// <summary>
+        /// If <paramref name="type"/> is an enumerable type (such as array or <see cref="IEnumerable{T}"/>), this method will 
+        /// return the element type.
+        /// </summary>
+        /// <param name="type">The type to be inspected.</param>
+        /// <returns>If <paramref name="type"/> is an enumerable type the element type is returned, otherwise null.</returns>
+        public static Type GetEnumerableElementType(Type type)
+        {
+            Contract.Requires(type != null);
+
+            if (type.IsArray)
+                return type.GetElementType();
+
+            var enumerableType = type.GetInterfaces().Union(new[] { type })
+                    .FirstOrDefault(it => it.IsGenericType && it.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+
+            // let's get T of IEnumerable<T>
+            if (enumerableType != null)
+                return enumerableType.GetGenericArguments()[0];
+
+            return null;
+        }
+    }
+}
---
+++ netcore/vw_common_netcore/VowpalWabbitDecisionServiceInteractionHeader.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VowpalWabbitDecisionServiceInteraction.cs">
+//   Copyright (c) by respective owners including Yahoo!, Microsoft, and
+//   individual contributors. All rights reserved.  Released under a BSD
+//   license as described in the file LICENSE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace VW
+{
+    /// <summary>
+    /// Decision Service interaction header information.
+    /// </summary>
+    public sealed class VowpalWabbitDecisionServiceInteractionHeader
+    {
+        /// <summary>
+        /// EventId extracted from Decision Service Interaction JSON data.
+        /// </summary>
+        public string EventId { get; set; }
+
+        /// <summary>
+        /// Actions extracted from Decision Service Interaction JSON data.
+        /// </summary>
+        public int[] Actions { get; set; }
+
+        /// <summary>
+        /// Probabilities extracted from Decision Service Interaction JSON data.
+        /// </summary>
+        public float[] Probabilities { get; set; }
+
+        /// <summary>
+        /// Probability of drop extracted from Decision Service Interaction JSON data.
+        /// </summary>
+        public float ProbabilityOfDrop { get; set; }
+
+        /// <summary>
+        /// Flag if the action should not be used for learning
+        /// </summary>
+        public bool SkipLearn { get; set; }
+    }
+}
