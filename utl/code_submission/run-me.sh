9be708ea63a0912f18ee8c61304f35bfe871bce2
Rajan Chari <ranaras@microsoft.com>
2020-02-20 05:42:22 -0500

    curl in slilent mode with errors only

--- utl/code_submission/run-me.sh
+++ utl/code_submission/run-me.sh
@@ -17,11 +17,11 @@ make -j vw-bin
 cd ..
 mkdir test/train-sets
 mkdir test/train-sets/regression
-curl -o test/train-sets/regression/BNG_wisconsin.csv https://www.openml.org/data/get_csv/150677/BNG_wisconsin.arff &
-curl -o test/train-sets/regression/BNG_cpu_act.csv https://www.openml.org/data/get_csv/150680/BNG_cpu_act.arff &
-curl -o test/train-sets/regression/BNG_auto_price.csv https://www.openml.org/data/get_csv/150679/BNG_auto_price.arff &
-curl -o test/train-sets/regression/black_friday.csv https://www.openml.org/data/get_csv/21230845/file639340bd9ca9.arff &
-curl -o test/train-sets/regression/zurich.csv https://www.openml.org/data/get_csv/5698591/file62a9329beed2.arff &
+curl -Ss -o test/train-sets/regression/BNG_wisconsin.csv https://www.openml.org/data/get_csv/150677/BNG_wisconsin.arff &
+curl -Ss -o test/train-sets/regression/BNG_cpu_act.csv https://www.openml.org/data/get_csv/150680/BNG_cpu_act.arff &
+curl -Ss -o test/train-sets/regression/BNG_auto_price.csv https://www.openml.org/data/get_csv/150679/BNG_auto_price.arff &
+curl -Ss -o test/train-sets/regression/black_friday.csv https://www.openml.org/data/get_csv/21230845/file639340bd9ca9.arff &
+curl -Ss -o test/train-sets/regression/zurich.csv https://www.openml.org/data/get_csv/5698591/file62a9329beed2.arff &
 
 # Wait for all background jobs to finish
 wait
