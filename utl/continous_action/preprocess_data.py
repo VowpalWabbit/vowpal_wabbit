74ad54556fa166c1fb090fb23a01f405516b1651
Rajan Chari <ranaras@microsoft.com>
2020-02-19 23:32:20 -0500

    1) data transform 2)User friendly messages

--- utl/continous_action/preprocess_data.py
+++ utl/continous_action/preprocess_data.py
@@ -12,7 +12,8 @@ class PreProcessCSVData:
 
   def process(self, csv_file_name):
     print('csv_file_name = ', csv_file_name)
-    dat_file_name = self.get_file_root(csv_file_name)+'.dat'
+    file_name_root = self.get_file_root(csv_file_name)
+    dat_file_name = file_name_root +'.dat'
     print('dat_file_name = ', dat_file_name)
     with open(dat_file_name, mode='w') as dat_file:
       dat_writer = csv.writer(dat_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -37,17 +38,38 @@ class PreProcessCSVData:
           minv = min(minv, float(row[0]))
           maxv = max(maxv, float(row[0]))
           row_num += 1
-    print('row_num = ', row_num)
-    print('min_value = ', minv)
-    print('max_value = ', minv)
+          if(row_num % 10000 == 0):
+            print('.',end='',sep='',flush=True)
+
+    print('', flush=True)
+    print('row_num = ', row_num, flush=True)
+    print('min_value = ', minv, flush=True)
+    print('max_value = ', maxv, flush=True)
+
     percent = 0.80
+
+    print('Reading into dat file into pandas data frame: ', dat_file_name, flush=True)
     df = pd.read_csv(dat_file_name, header=None)
+
+    print('Sampling data frame', flush=True)
     ds = df.sample(frac=1)
+
+    print('Generating training data set', flush=True)
     ds_train = ds[0:math.floor(df.size*percent)]
+
+    print('Generating test data set', flush=True)
     ds_test = ds[math.floor(df.size*percent):]
+
+    print('Writing whole file = ', dat_file_name, flush=True)
     ds.to_csv(dat_file_name, header = False, index=False)
-    ds_train.to_csv(self.get_file_root(csv_file_name)+'_train.dat', header = False, index=False)
-    ds_test.to_csv(self.get_file_root(csv_file_name)+'_test.dat', header = False, index=False)
+
+    train_file_name = file_name_root + "_train.dat"
+    print('Writing train file = ', train_file_name, flush=True)
+    ds_train.to_csv(train_file_name, header = False, index=False)
+
+    test_file_name = file_name_root + "_test.dat"
+    print('Writing test file = ', test_file_name, flush=True)
+    ds_test.to_csv(test_file_name, header = False, index=False)
 
 if __name__ == "__main__":
   csv_file_name = "data.csv"
