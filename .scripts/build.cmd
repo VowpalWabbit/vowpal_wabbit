50d5c1be8b10283ae6010815bb4d76f087fda74b
Marat Turaev <marat.turaev@hotmail.com>
2020-02-27 16:57:45 -0800

    buidl /v:d

--- .scripts/build.cmd
+++ .scripts/build.cmd
@@ -20,7 +20,7 @@ PUSHD %~dp0
 
 REM TODO: Figure out how to parametrize this script?! (is there a standard, or do we actually need parse args?)
 ECHO Building "%vwRoot%\vowpalwabbit\vw.sln" for Release x64
-"%msbuildPath%" /v:diag %__MULTIBUILD_SWITCH% /nr:false /p:Configuration=Release;Platform=x64 "%vwRoot%\vowpalwabbit\vw.sln" %1
+"%msbuildPath%" /v:d %__MULTIBUILD_SWITCH% /nr:false /p:Configuration=Release;Platform=x64 "%vwRoot%\vowpalwabbit\vw.sln" %1
 
 POPD
 
