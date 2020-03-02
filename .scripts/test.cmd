1221efaefb92d8f9e89cf2abedf35fa2c828515d
Marat Turaev <marat.turaev@hotmail.com>
2020-03-02 11:05:09 -0800

    Build and test scripts

--- .scripts/test.cmd
+++ .scripts/test.cmd
@@ -26,8 +26,8 @@ IF "%ERRORLEVEL%" NEQ "0" (
 
 ECHO Running VW Unit Tests in C#
 REM TODO: Add explicit logging configuration so it can be uploaded to pipeline results.
-"%vstestPath%" /Platform:x64 /inIsolation /Framework:".NETFramework,Version=4.5.2" /Parallel "%vwRoot%\vowpalwabbit\out\target\Release\x64\net452\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO" --logger:trx "--ResultsDirectory:%vwRoot%\vowpalwabbit\out\test\Release\x64\net452"
-"%vstestPath%" /Platform:x64 /inIsolation /Framework:".NETCoreApp,Version=3.1" /Parallel "%vwRoot%\vowpalwabbit\out\target\Release\x64\netcoreapp3.1\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO" --logger:trx "--ResultsDirectory:%vwRoot%\vowpalwabbit\out\test\Release\x64\netcoreapp3.1"
+"%vstestPath%" /Platform:x64 /inIsolation /Framework:".NETFramework,Version=4.5.2" "%vwRoot%\vowpalwabbit\out\target\Release\x64\net452\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO" --logger:trx "--ResultsDirectory:%vwRoot%\vowpalwabbit\out\test\Release\x64\net452"
+"%vstestPath%" /Platform:x64 /inIsolation /Framework:".NETCoreApp,Version=3.1" "%vwRoot%\vowpalwabbit\out\target\Release\x64\netcoreapp3.1\cs_unittest.dll" /TestCaseFilter:"TestCategory!=NotOnVSO" --logger:trx "--ResultsDirectory:%vwRoot%\vowpalwabbit\out\test\Release\x64\netcoreapp3.1"
 
 IF "%ERRORLEVEL%" NEQ "0" (
     ENDLOCAL
