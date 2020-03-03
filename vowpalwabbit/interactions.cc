b4ab1df5d75b41b3df1cb0fecb9b19614c5b6bfd
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-03-04 05:05:02 +0530

    Add auto keyword

--- vowpalwabbit/interactions.cc
+++ vowpalwabbit/interactions.cc
@@ -268,7 +268,7 @@ void eval_count_of_generated_ft(vw& all, example& ec, size_t& new_features_cnt, 
     generate_interactions<eval_gen_data, uint64_t, ft_cnt>(all, ec, dat);
 #endif
 
-    for (std::vector<uint8_t>& inter : *ec.interactions)
+    for (auto &inter : *ec.interactions)
     {
       size_t num_features_in_inter = 1;
       float sum_feat_sq_in_inter = 1.;
