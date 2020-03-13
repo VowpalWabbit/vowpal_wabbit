cd937895dced461ab8f0b54cb23a9f4745454643
Ritu Raj Singh <RituRajSingh878@gmail.com>
2020-03-13 15:09:27 -0400

    used BaseEstimator repr

--- python/vowpalwabbit/sklearn_vw.py
+++ python/vowpalwabbit/sklearn_vw.py
@@ -12,7 +12,6 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model.base import LinearClassifierMixin, SparseCoefMixin
 from sklearn.datasets import dump_svmlight_file
 from sklearn.utils import shuffle
-from sklearn.utils._pprint import _EstimatorPrettyPrinter
 from vowpalwabbit import pyvw
 
 DEFAULT_NS = ''
@@ -381,51 +380,8 @@ class VW(BaseEstimator):
             return str(self.params)
 
     def __repr__(self,  N_CHAR_MAX=700):
-        # N_CHAR_MAX is the (approximate) maximum number of non-blank
-        # characters to render. We pass it as an optional parameter to ease
-        # the tests.
 
-        N_MAX_ELEMENTS_TO_SHOW = 30  # number of elements to show in sequences
-
-        # use ellipsis for sequences with a lot of elements
-        pp = _EstimatorPrettyPrinter(
-            compact=True, indent=1, indent_at_name=True,
-            n_max_elements_to_show=N_MAX_ELEMENTS_TO_SHOW)
-
-        repr_ = pp.pformat(self)
-
-        # Use bruteforce ellipsis when there are a lot of non-blank characters
-        n_nonblank = len(''.join(repr_.split()))
-        if n_nonblank > N_CHAR_MAX:
-            lim = N_CHAR_MAX // 2  # apprx number of chars to keep on both ends
-            regex = r'^(\s*\S){%d}' % lim
-            # The regex '^(\s*\S){%d}' % n
-            # matches from the start of the string until the nth non-blank
-            # character:
-            # - ^ matches the start of string
-            # - (pattern){n} matches n repetitions of pattern
-            # - \s*\S matches a non-blank char following zero or more blanks
-            left_lim = re.match(regex, repr_).end()
-            right_lim = re.match(regex, repr_[::-1]).end()
-
-            if '\n' in repr_[left_lim:-right_lim]:
-                # The left side and right side aren't on the same line.
-                # To avoid weird cuts, e.g.:
-                # categoric...ore',
-                # we need to start the right side with an appropriate newline
-                # character so that it renders properly as:
-                # categoric...
-                # handle_unknown='ignore',
-                # so we add [^\n]*\n which matches until the next \n
-                regex += r'[^\n]*\n'
-                right_lim = re.match(regex, repr_[::-1]).end()
-
-            ellipsis = '...'
-            if left_lim + len(ellipsis) < len(repr_) - right_lim:
-                # Only add ellipsis if it results in a shorter repr
-                repr_ = repr_[:left_lim] + '...' + repr_[-right_lim:]
-
-        return repr_
+        return BaseEstimator.__repr__(self)
 
     def __del__(self):
         if hasattr(self, 'vw_') and self.vw_ is not None:
