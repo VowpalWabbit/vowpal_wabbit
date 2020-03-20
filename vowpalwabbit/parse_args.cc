751eece558989c9467bc68347efa6cb22a1c5507
adityauser <adityauser225@gmail.com>
2020-03-20 19:09:33 +0530

    Temporary fix

--- vowpalwabbit/parse_args.cc
+++ vowpalwabbit/parse_args.cc
@@ -1324,17 +1324,21 @@ vw& parse_args(options_i& options, trace_message_t trace_listener, void* trace_c
     time(&all.init_time);
 
     bool strict_parse = false;
-    int ring_size;
+    size_t ring_size;
+    int tmp;
     option_group_definition vw_args("VW options");
-    vw_args.add(make_option("ring_size", ring_size).default_value(256).help("size of example ring"))
+    vw_args.add(make_option("ring_size", tmp).default_value(256).help("size of example ring"))
         .add(make_option("strict_parse", strict_parse).help("throw on malformed examples"));
     options.add_and_parse(vw_args);
     
-    if (ring_size <= 0)
+    if (tmp <= 0)
     {
       THROW("ring_size should be positive");
     }
-
+    else
+    {
+        ring_size = tmp;
+    }
 
     all.p = new parser{ring_size, strict_parse};
     all.p->_shared_data = all.sd;
