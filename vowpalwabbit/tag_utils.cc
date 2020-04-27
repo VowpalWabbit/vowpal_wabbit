b8a4f99265a81ebd7e0dc1ca3a9fe97fdebc3460
Alexey Taymanov <ataymano@microsoft.com>
2020-04-27 18:45:51 -0400

    seed extraction fix

--- vowpalwabbit/tag_utils.cc
+++ vowpalwabbit/tag_utils.cc
@@ -7,10 +7,10 @@ namespace VW
     if (!ex.tag.empty())
     {
       const std::string SEED_IDENTIFIER = "seed=";
-      if (strncmp(ex.tag.begin(), SEED_IDENTIFIER.c_str(), SEED_IDENTIFIER.size()) == 0 &&
-          ex.tag.size() > SEED_IDENTIFIER.size())
+      const size_t prefix_length = SEED_IDENTIFIER.size();
+      if (ex.tag.size() > prefix_length && strncmp(ex.tag.begin(), SEED_IDENTIFIER.c_str(), prefix_length) == 0)
       {
-        return new VW::string_view(ex.tag.begin() + 5, ex.tag.size());
+        return new VW::string_view(ex.tag.begin() + prefix_length, ex.tag.size() - prefix_length);
       }
     }
     return nullptr;
