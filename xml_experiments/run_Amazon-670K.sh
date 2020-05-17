9d4df58ab0cd7f498d4194a9897598b6da69cc3f
Marek Wydmuch <marek@wydmuch.poznan.pl>
2020-05-17 18:39:40 +0200

    add plt demo

--- xml_experiments/run_Amazon-670K.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DATASET_NAME="Amazon-670K"
-FILES_PREFIX="amazon"
-K=670091
-PARAMS="-l 0.0004 --power_t 0.2 --kary_tree 64 --passes 20 -b 35"
-
-bash run_xml.sh $DATASET_NAME $FILES_PREFIX $K "$PARAMS"
--- xml_experiments/run_Mediamill.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DATASET_NAME="Mediamill"
-FILES_PREFIX="mediamill"
-K=101
-PARAMS="-l 3.0 --power_t 0.4 --kary_tree 12 --passes 20 -b 30"
-
-bash run_xml.sh $DATASET_NAME $FILES_PREFIX $K "$PARAMS"
