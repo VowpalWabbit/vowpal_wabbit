0a293763adbb7bea24357a823ba417b64e06c6bd
Cassandra Marcussen <cem2242@columbia.edu>
2020-05-18 15:32:31 -0400

    fixing segfault

--- vowpalwabbit/io_to_queue.h
+++ vowpalwabbit/io_to_queue.h
@@ -40,11 +40,11 @@ class IO_Item {
         ~IO_Item() {}
 
         inline std::string getString(){
-            return message;
+            return std::string(message);
         }
 
         inline int getNumCharsInit(){
-        return numCharsInit;
+          return numCharsInit;
         }
 
         inline void setString(std::string newMsg){
@@ -58,6 +58,49 @@ class IO_Item {
     private:
         std::string message;
         int numCharsInit;
+
+    /*public:
+
+        IO_Item(){
+            message = new std::string("");;
+            numCharsInit = 0;
+        }
+
+        IO_Item(std::string myMsg, int myNumCharsInit){
+            message = new std::string(myMsg);
+            numCharsInit = myNumCharsInit;
+        }
+
+        //add const string?
+
+        IO_Item operator=(IO_Item toCopy){
+            message = new std::string(*toCopy.getString());
+            numCharsInit = toCopy.getNumCharsInit();
+            return *this;
+        }
+
+        ~IO_Item() {}
+
+        inline std::string * getString(){
+          std::string *copymsg = new std::string(*message);
+          return copymsg;
+        }
+
+        inline int getNumCharsInit(){
+          return numCharsInit;
+        }
+
+        inline void setString(std::string newMsg){
+            message = new std::string(newMsg);
+        }
+
+        inline void setNumCharsInit(int newNum){
+            numCharsInit = newNum;
+        }
+    
+    private:
+        std::string *message;
+        int numCharsInit;*/
 
 };
 //}
@@ -116,7 +159,9 @@ inline IO_Item pop_io_queue(bool should_pop){
     if(should_pop && input_lines_copy->size() > 0)
     {
 
+      //IO_Item *front = new IO_Item(input_lines_copy->front());
       IO_Item front = input_lines_copy->front();
+      //std::cout << "front string: " << front.getString();
 
       input_lines_copy->pop();
 
@@ -127,6 +172,34 @@ inline IO_Item pop_io_queue(bool should_pop){
     }
 
     return IO_Item(std::string("bad"), 0);
+
+}
+
+
+inline IO_Item * pop_io_queue2(bool should_pop){
+
+    std::lock_guard<std::mutex> lck(_mutex_io);
+
+    if(should_pop && input_lines_copy->size() > 0)
+    {
+
+      IO_Item *front = new IO_Item(input_lines_copy->front());
+      //IO_Item front = input_lines_copy->front();
+      //std::cout << "front string: " << front.getString();
+
+      input_lines_copy->pop();
+
+      return front;
+      
+    }else if(input_lines_copy->size() == 0 && have_added_io){
+      IO_Item *front = new IO_Item(std::string("empty"), 0);
+
+      return front; 
+    }
+
+    IO_Item *front = new IO_Item(std::string("bad"), 0);
+
+    return front; 
 
 }
 
