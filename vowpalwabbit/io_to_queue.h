9159433d0fe3828abfd2947ca15d0b8246b33762
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-05-23 12:16:05 -0400

    Updating io_to_queue with IO_State in parser (passes all tests)

--- vowpalwabbit/io_to_queue.h
+++ vowpalwabbit/io_to_queue.h
@@ -4,146 +4,91 @@
 #include <thread>
 #include <future>
 #include <mutex>  
-#include <chrono>     
+#include <chrono>   
+
+#include "io_item.h"
 
 static std::mutex _mutex_io;
 
-#ifndef _IO_ITEM_
-#define _IO_ITEM_
-class IO_Item {
+inline bool add_to_queue(vw& all, char *& line){
 
-  public:
+ // std::cout << "add_to_queue" << std::endl;
 
-      std::string message;
-      int numCharsInit;
+  std::lock_guard<std::mutex> lck(_mutex_io);
 
-      IO_Item(){
-          numCharsInit = 0;
-      }
+  bool finish = false;
 
-      IO_Item(std::string myMsg, int myNumCharsInit){
+  size_t num_chars_initial = readto(*(all.p->input), line, '\n');
 
-          message.assign(myMsg);
-          numCharsInit = myNumCharsInit;
-      }
+  //std::cout << "line: " << line << std::endl;
+  
+  if(num_chars_initial < 1 || strlen(line) < 1){
+      all.p->_io_state.have_added_io = true;
+      finish = true;
+  }
 
-      IO_Item operator=(const IO_Item &toCopy){
-          message.assign(toCopy.message);
-          numCharsInit = toCopy.numCharsInit;
-          return *this;
-      }
+  IO_Item line_item(std::string(line), num_chars_initial);
 
-      IO_Item(const IO_Item &toCopy){
-          message.assign(toCopy.message);
-          numCharsInit = toCopy.numCharsInit;
-      }
+  //std::cout << "push line to io queue" << std::endl;
 
-      ~IO_Item() {}
+  all.p->_io_state.io_lines->push(line_item);
 
-      inline std::string getString(){
-          return std::string(message);
-      }
+  all.p->_io_state.have_added_io = true;
 
-      inline int getNumCharsInit(){
-        return numCharsInit;
-      }
+  return finish;
 
-      inline void setString(std::string newMsg){
-          message.assign(newMsg);
-      }
+}
 
-      inline void setNumCharsInit(int newNum){
-          numCharsInit = newNum;
-      }
+//inline void io_lines_toqueue(vw& all, IO_State *iostate){
+inline void io_lines_toqueue(vw& all, std::queue<IO_Item> *io_to_set){
 
-};
+ // std::lock_guard<std::mutex> lck(_mutex_io);
 
-#endif
-
-static std::queue<IO_Item> *input_lines_copy = new std::queue<IO_Item>;
-static bool called_i_l_t = false;
-static bool have_added_io = false;
-static bool done_with_io = false;
-
-inline void io_lines_toqueue(vw& all, std::queue<IO_Item> *input_lines){
-
-  //std::lock_guard<std::mutex> lck(_mutex_io);
-
-  //parser *original_p = all->p;
+  all.p->_io_state.io_lines = io_to_set;
+  //std::cout << "io_lines_toqueue" << std::endl;
+  
   parser *original_p = all.p;
   
   char* line = nullptr;
 
-  while(true)
+  bool should_finish = false;
+
+  while(!should_finish)
   {
 
+    //std::this_thread::sleep_for (std::chrono::seconds(1));
     //std::cout << "readto" << std::endl;
     //size_t num_chars_initial = readto(*(all->p->input), line, '\n');
-    size_t num_chars_initial = readto(*(all.p->input), line, '\n');
-
-    //std::cout << "line: " << line << std::endl;
-   
-    if(num_chars_initial < 1 || strlen(line) < 1){
-        called_i_l_t = true;
-        have_added_io = true;
-        break;
-    }
-
-    IO_Item line_item(std::string(line), num_chars_initial);
-
-    //std::cout << "push line to io queue" << std::endl;
-
-    input_lines->push(line_item);
-    input_lines_copy->push(line_item);
-
-   // std::this_thread::sleep_for (std::chrono::seconds(1));
-
-    called_i_l_t = true;
-    have_added_io = true;
+    
+    should_finish = add_to_queue(all, line);
 
   }
 
-  //all->p = original_p;
   all.p = original_p;
 
-  done_with_io = true;
+  //std::cout << "done with io" << std::endl;
+  all.p->_io_state.done_with_io = true;
  /* std::cout << "input_lines_copy->size(): " << input_lines_copy->size() << std::endl;
   std::cout << "input_lines->size(): " << input_lines->size() << std::endl;*/
 
 }
 
-inline bool added_io(){
-  return have_added_io;
-}
-
-inline bool io_done(){
-  return done_with_io;
-}
-
-//will hopefully be called after io_lines_to_queue called.
-inline IO_Item pop_io_queue(bool should_pop){
-
-
+inline IO_Item pop_io_queue(vw *all, bool should_pop){
+  
   std::lock_guard<std::mutex> lck(_mutex_io);
 
-  if(should_pop && input_lines_copy->size() > 0)
+  IO_Item front;
+  
+  if(should_pop && (*all).p->_io_state.io_lines->size() > 0)
   {
-    IO_Item front = input_lines_copy->front();
 
-    //std::cout << "pop line from io queue (when item in)" << std::endl;
+    front = (*all).p->_io_state.io_lines->front();
 
-    input_lines_copy->pop();
-
-    return front;
-    
-  }else if(input_lines_copy->size() == 0 && have_added_io && done_with_io){
-    //std::cout << "pop line from io queue (before item in)" << std::endl;
-    return IO_Item(std::string("empty"), 0);
-  }else if(input_lines_copy->size() == 0 && have_added_io && !done_with_io){
-    return IO_Item(std::string("bad"), 0);
+    (*all).p->_io_state.io_lines->pop();
+   
   }
 
-  return IO_Item(std::string("bad"), 0);
+  return front;
 
 }
 
