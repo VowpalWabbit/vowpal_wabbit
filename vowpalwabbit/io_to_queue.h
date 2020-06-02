49898630c5c589b8bca9e23ecf0d404df3308e24
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-01 20:57:48 -0400

    Remove push/pop io queue functions from io_to_queue.h (moved to io_state)

--- vowpalwabbit/io_to_queue.h
+++ vowpalwabbit/io_to_queue.h
@@ -1,5 +1,8 @@
-#ifndef _IO_QUEUE_
-#define _IO_QUEUE_
+// Copyright (c) by respective owners including Yahoo!, Microsoft, and
+// individual contributors. All rights reserved. Released under a BSD (revised)
+// license as described in the file LICENSE.
+
+#pragma once
 
 #include <thread>
 #include <future>
@@ -7,28 +10,6 @@
 #include <chrono>   
 
 #include "io_item.h"
-
-//Adds a line of input to the input queue
-inline bool add_to_queue(vw& all, char *& line){
-
-  bool finish = false;
-
-  size_t num_chars_initial = readto(*(all.p->input), line, '\n');
-  
-  if(num_chars_initial < 1 || strlen(line) < 1){
-      finish = true;
-  }
-
-  IO_Item line_item(std::string(line), num_chars_initial);
-
-  {
-    std::lock_guard<std::mutex> lck(all.p->io_queue_lock);
-    all.p->_io_state.io_lines->push(line_item);
-  }
-
-  return finish;
-
-}
 
 //Adds all lines of input to the input queue
 inline void io_lines_toqueue(vw& all){
@@ -41,7 +22,7 @@ inline void io_lines_toqueue(vw& all){
 
   while(!should_finish)
   {    
-    should_finish = add_to_queue(all, line);
+    should_finish = all.p->_io_state.add_to_queue(line, all.p->input);
 
   }
 
@@ -51,26 +32,27 @@ inline void io_lines_toqueue(vw& all){
 
 }
 
-//Pops a line of input from the input queue
-inline IO_Item pop_io_queue(vw *all){
-  
-  std::lock_guard<std::mutex> lck((*all).p->io_queue_lock);
+//try to encapsulate start io thread and end io thread, so it can be moved out of parse dispatch loop.
+namespace VW {
 
-  IO_Item front;
-  
-  if((*all).p->_io_state.io_lines->size() > 0)
+inline void start_io_thread(vw& all){
+
+  /*io_state curr_io_state;
+
+  all.p->_io_state = curr_io_state ;*/
+
+  all.io_thread = std::thread([&all]() 
   {
-
-    front = (*all).p->_io_state.io_lines->front();
-
-    (*all).p->_io_state.io_lines->pop();
-   
-  }
-
-  return front;
+    io_lines_toqueue(all);
+  });
 
 }
 
-#endif
+inline void end_io_thread(vw& all){
 
+  all.io_thread.join();
+
+}
+
+}
 
