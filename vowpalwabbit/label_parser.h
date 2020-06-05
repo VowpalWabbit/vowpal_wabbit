9c5ff934fd53cb9f3505bbcf62f09f4721f0b606
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-05 16:18:29 -0400

    label_parser - add v_array parameter to parse_label function prototype

--- vowpalwabbit/label_parser.h
+++ vowpalwabbit/label_parser.h
@@ -16,7 +16,7 @@ struct shared_data;
 struct label_parser
 {
   void (*default_label)(void*);
-  void (*parse_label)(parser*, shared_data*, void*, v_array<VW::string_view>&);
+  void (*parse_label)(parser*, shared_data*, void*, v_array<VW::string_view>&, v_array<VW::string_view>&);
   void (*cache_label)(void*, io_buf& cache);
   size_t (*read_cached_label)(shared_data*, void*, io_buf& cache);
   void (*delete_label)(void*);
