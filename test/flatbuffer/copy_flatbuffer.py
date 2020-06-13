e1168430fedc24a1d1c15e613e9ef962086b66c4
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-06-14 03:36:41 +0530

    New flatbuffer test folder with data and ref files

---
+++ test/flatbuffer/copy_flatbuffer.py
@@ -0,0 +1,33 @@
+import os
+
+# files = os.listdir('../train-sets/')
+
+# flatbuffers = [i for i in files if '.fb' in i and '.fb.cache' not in i]
+
+# for i in flatbuffers:
+#     os.system('cp ../train-sets/{} train-sets/{}'.format(i, i[:-3]))
+
+with open('RunTests', 'r') as file:
+    lines = file.read()
+
+
+def extract_something(lines, something):
+    predsets = []
+    for i in lines:
+        if something in i:
+            predsets.append(i.strip())
+
+    return predsets
+
+predsets = extract_something(lines.split('\n'), 'pred-sets/ref')
+testsets = extract_something(lines.split('\n'), 'test-sets/ref')
+trainsets = extract_something(lines.split('\n'), 'train-sets/ref')
+
+print(len(predsets) + len(testsets) + len(trainsets))
+
+for i in predsets:
+    os.system('cp ../{} {}'.format(i, i))
+for i in trainsets:
+    os.system('cp ../{} {}'.format(i, i))
+for i in testsets:
+    os.system('cp ../{} {}'.format(i, i))
