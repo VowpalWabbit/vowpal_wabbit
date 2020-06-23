5efb3a6f7cdb2dc0e3cb2cf36d556c7d026c063f
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-23 16:22:18 -0400

    Best_constant.h - lock count_label to avoid race condition

--- vowpalwabbit/best_constant.h
+++ vowpalwabbit/best_constant.h
@@ -6,26 +6,31 @@
 #include <cfloat>
 #include "vw.h"
 
+static std::mutex count_label_mutex;
+
 inline void count_label(shared_data* sd, float l)
 {
-  if (sd->is_more_than_two_labels_observed || l == FLT_MAX)
+
+  std::lock_guard<std::mutex> lck(count_label_mutex);
+
+  if (sd->is_more_than_two_labels_observed.load() || l == FLT_MAX)
     return;
 
-  if (sd->first_observed_label != FLT_MAX)
+  if (sd->first_observed_label.load() != FLT_MAX)
   {
-    if (sd->first_observed_label != l)
+    if (sd->first_observed_label.load() != l)
     {
-      if (sd->second_observed_label != FLT_MAX)
+      if (sd->second_observed_label.load() != FLT_MAX)
       {
-        if (sd->second_observed_label != l)
-          sd->is_more_than_two_labels_observed = true;
+        if (sd->second_observed_label.load() != l)
+          sd->is_more_than_two_labels_observed.store(true);
       }
       else
-        sd->second_observed_label = l;
+        sd->second_observed_label.store(l);
     }
   }
   else
-    sd->first_observed_label = l;
+    sd->first_observed_label.store(l);
 }
 
 bool get_best_constant(vw& all, float& best_constant, float& best_constant_loss);
