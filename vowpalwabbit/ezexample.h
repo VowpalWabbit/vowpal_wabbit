c0144622c7a87c65d6dcac2f11ee324d17f5cdb8
Eduardo Salinas <edus@microsoft.com>
2020-06-29 13:44:39 -0400

    address ci logs, no local linux compile yet

--- vowpalwabbit/ezexample.h
+++ vowpalwabbit/ezexample.h
@@ -101,7 +101,7 @@ class ezexample
     ec = get_new_example();
     we_create_ec = true;
 
-    if (vw_ref->add_constant)
+    if (vw_ref->gs.add_constant)
       VW::add_constant_feature(*vw_ref, ec);
   }
 
@@ -136,7 +136,7 @@ class ezexample
 
   bool ensure_ns_exists(char c)  // returns TRUE iff we should ignore it :)
   {
-    if (vw_ref->ignore_some && vw_ref->ignore[(int)c])
+    if (vw_ref->gs.ignore_some && vw_ref->fc.ignore[(int)c])
       return true;
     if (ns_exists[(int)c])
       return false;
@@ -238,7 +238,7 @@ class ezexample
     quadratic_features_num = 0;
     quadratic_features_sqr = 0.;
 
-    for (auto const& interaction : vw_ref->interactions)
+    for (auto const& interaction : vw_ref->gs.interactions)
     {
       if(interaction.size() != 2)
         continue;
@@ -248,7 +248,7 @@ class ezexample
     }
     ec->num_features += quadratic_features_num;
     ec->total_sum_feat_sq += quadratic_features_sqr;
-    ec->interactions = &vw_ref->interactions;
+    ec->interactions = &vw_ref->gs.interactions;
   }
 
   size_t get_num_features() { return ec->num_features; }
@@ -287,7 +287,7 @@ class ezexample
     else  // is multiline
     {     // we need to make a copy
       example* copy = get_new_example();
-      VW::copy_example_data(vw_ref->audit, copy, ec, vw_par_ref->p->lp.label_size, vw_par_ref->p->lp.copy_label);
+      VW::copy_example_data(vw_ref->oc.audit, copy, ec, vw_par_ref->p->lp.label_size, vw_par_ref->p->lp.copy_label);
       vw_ref->learn(*copy);
       example_copies.push_back(copy);
     }
