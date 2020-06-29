5350536fd47773bd675379737cfbc2de7c99fee0
Eduardo Salinas <edus@microsoft.com>
2020-06-29 15:04:23 -0400

    remove added empty line

--- cs/unittest/TestMarshalNumeric.cs
+++ cs/unittest/TestMarshalNumeric.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections;
 using System.Collections.Generic;
