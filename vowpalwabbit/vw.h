b733d4e33f26a80b66dd3b69e8f8c2262770a2be
Eduardo Salinas <edus@microsoft.com>
2020-06-30 11:20:14 -0400

    fix after merge

--- vowpalwabbit/vw.h
+++ vowpalwabbit/vw.h
@@ -181,7 +181,7 @@ inline uint64_t hash_feature_cstr(vw& all, char* fstr, uint64_t u)
 inline uint64_t chain_hash(vw& all, const std::string& name, const std::string& value, uint64_t u)
 {
   // chain hash is hash(feature_value, hash(feature_name, namespace_hash)) & parse_mask
-  return all.p->hasher(value.data(), value.length(), all.p->hasher(name.data(), name.length(), u)) & all.parse_mask;
+  return all.p->hasher(value.data(), value.length(), all.p->hasher(name.data(), name.length(), u)) & all.gs.parse_mask;
 }
 
 inline float get_weight(vw& all, uint32_t index, uint32_t offset)
