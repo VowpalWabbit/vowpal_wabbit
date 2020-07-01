485dfb5e43d7451f2af2e78a8fcd7cc75caef1df
Jack Gerrits <jackgerrits95@gmail.com>
2020-07-01 16:23:52 -0400

    Trial MacOS implementation

--- vowpalwabbit/vw_exception.h
+++ vowpalwabbit/vw_exception.h
@@ -116,11 +116,25 @@ inline std::string strerror_to_string(int error_number)
 #ifdef _WIN32
   constexpr auto BUFFER_SIZE = 256;
   std::array<char, BUFFER_SIZE> error_message_buffer;
-  auto result = strerror_s(error_message_buffer.data(), error_message_buffer.size(), error_number);
+  auto result = strerror_s(error_message_buffer.data(), error_message_buffer.size() - 1, error_number);
   if (result != 0) { return "unknown"; }
 
   auto length = std::strlen(error_message_buffer.data());
   return std::string(error_message_buffer.data(), length);
+#elif __APPLE__
+  constexpr auto BUFFER_SIZE = 256;
+  std::array<char, BUFFER_SIZE> error_message_buffer;
+  #if defined(__GLIBC__) && defined(_GNU_SOURCE)
+    // You must use the returned bffer and not the passed in buffer the GNU version.
+    char* message_buffer = strerror_r(error_number, error_message_buffer.data(), error_message_buffer.size() - 1);
+    auto length = std::strlen(message_buffer);
+    return std::string(message_buffer, length);
+  #else
+    auto result = strerror_r(error_number, error_message_buffer.data(), error_message_buffer.size() - 1);
+    if (result != 0) { return "unknown"; }
+    auto length = std::strlen(error_message_buffer.data());
+    return std::string(error_message_buffer.data(), length);
+  #endif
 #else
   // Passing "" for the locale means use the default system locale
   locale_t locale = newlocale(LC_ALL_MASK, "", static_cast<locale_t>(0));
