e6073bf5d5fb3cca0ed68628b7da70fa7f0ddc32
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-07-13 21:49:01 -0400

    Add should_finish and add_to_queue logic to io file readers

--- vowpalwabbit/io_to_queue.h
+++ vowpalwabbit/io_to_queue.h
@@ -21,46 +21,46 @@ inline void io_lines_toqueue(vw& all){
   while(!should_finish)
   {    
     v_array<example*> example_arr;
-    size_t num_chars_initial = all.p->input_file_reader(&all, line, example_arr);
-    should_finish = all.p->_io_state.add_to_queue(line, num_chars_initial);
+
+    should_finish = all.p->input_file_reader(&all, line, example_arr);
   }
 
   all.p->_io_state.done_with_io.store(true);
 
 }
 
-inline size_t read_input_file_ascii(vw *all, char *&line, v_array<example*>&) {
+inline bool read_input_file_ascii(vw *all, char *&line, v_array<example*>&) {
   std::cout << "read input file ascii" << std::endl;
-  return readto(*(all->p->input), line, '\n');
+
+  size_t num_chars_initial = readto(*(all->p->input), line, '\n');
+
+  bool should_finish = true;
+  
+  if(num_chars_initial > 0) {
+    should_finish = all->p->_io_state.add_to_queue(line, num_chars_initial);
+  }
+
+  return should_finish;
 }
 
-inline size_t read_input_file_binary(vw *all, char *&line, v_array<example*>& examples) {
+inline bool read_input_file_binary(vw *all, char *&line, v_array<example*>& examples) {
 
   std::cout << "read input file binary" << std::endl;
 
-  //all->p->input is the cache file.
+  size_t total_num_input_bytes = 0;
 
-  size_t total_num_bytes = 0;
-  std::string cache_file_contents = "";
+  //read_all_data returns the number of bytes successfully read from the input files
+  total_num_input_bytes = all->p->input->read_all_data(line, total_num_input_bytes);
 
-  char *buffer = nullptr;
-    
-  size_t num_bytes = 8;
+  std::cout << "total_num_input_bytes: " << total_num_input_bytes << std::endl;
 
-  size_t num_bytes_successfully_read;
-  all->p->input->buf_read(buffer, sizeof(buffer));
+  bool should_finish = true;
+  
+  if(total_num_input_bytes > 0) {
+    all->p->_io_state.add_to_queue(line, total_num_input_bytes);
+  }
 
-  num_bytes_successfully_read = strlen(buffer);
-
-  std::cout << "num_bytes_successfully_read: " << num_bytes_successfully_read << std::endl;
-  std::cout << "buffer:[" << buffer << "]" << std::endl;
-
-  line = std::move(buffer);
-
-  std::cout << "line: " << line << std::endl;
-
- //return 0 to indicate that we should finish reading in input (i.e. we are at the end of the cache file)
-  return 0;
+  return should_finish;
  
 }
 
@@ -84,7 +84,9 @@ inline void start_io_thread(vw& all){
 
 inline void end_io_thread(vw& all){
 
+  std::cout << "end io thread - beginning" << std::endl;
   all.io_thread.join();
+  std::cout << "end io thread - end" << std::endl;
 
 }
 
