4496b115de31c5d18c66d11569ea53911a4f0e4f
Jack Gerrits <jackgerrits@users.noreply.github.com>
2020-07-15 16:10:42 -0400

    Update Java Spark bindings for CB support (#2512)
    * added cb label types
    
    * added multi-line support to Spark
    
    * fixed multi line initialization
    added VowpalWabbitExample::toString for debugging
    
    * return predictions for CB adf
    fixed typo
    
    * Merge branch 'master' into marcozo/sparkcb
    
    * Build fixes for spark_vw
    
    * Use reinterpret cast
    
    * address comments
    
    Co-authored-by: Markus Cozowicz <marcozo@microsoft.com>

--- java/src/main/java/org/vowpalwabbit/spark/VowpalWabbitExample.java
+++ java/src/main/java/org/vowpalwabbit/spark/VowpalWabbitExample.java
@@ -4,15 +4,17 @@ import java.io.Closeable;
 
 /**
  * A wrapper for the native example data structure.
- * 
+ *
  * @author Markus Cozowicz
  */
 public class VowpalWabbitExample implements Closeable {
     /**
      * Initializes the native VowpalWabbitExampleWrapper data structure.
+     *
      * @param vwNativePointer the associated VW instance.
-     * @param isEmpty true if this should be initialized as new empty (=new-line) example.
-     * @return pointer to the native VowpalWabbitExampleWrapper data structure. 
+     * @param isEmpty         true if this should be initialized as new empty
+     *                        (=new-line) example.
+     * @return pointer to the native VowpalWabbitExampleWrapper data structure.
      */
     private static native long initialize(long vwNativePointer, boolean isEmpty);
 
@@ -27,58 +29,85 @@ public class VowpalWabbitExample implements Closeable {
     public native void clear();
 
     /**
-     * Adds the dense features values to the supplied namespace. The {@code baseIndex} is expected to be pre-hashed (e.g. hash(namespace)).
-     * 
-     * @param ns the first character of the namespace.
+     * Adds the dense features values to the supplied namespace. The
+     * {@code baseIndex} is expected to be pre-hashed (e.g. hash(namespace)).
+     *
+     * @param ns        the first character of the namespace.
      * @param baseIndex the base index for each of the {@code values}.
-     * @param values the feature values.
+     * @param values    the feature values.
      */
     public native void addToNamespaceDense(char ns, int baseIndex, double[] values);
 
     /**
      * Adds the sparse features values to the supplied naemspace.
-     * @param ns the first character of the namespace.
+     *
+     * @param ns      the first character of the namespace.
      * @param indices the indices of each corresponding feature value.
-     * @param values the feature values.
+     * @param values  the feature values.
      */
     public native void addToNamespaceSparse(char ns, int[] indices, double[] values);
 
     /**
      * Set the simple label.
+     *
      * @param weight weight of this example.
-     * @param label value of the label (e.g. -1, 1 for binary). 
+     * @param label  value of the label (e.g. -1, 1 for binary).
      */
     public native void setLabel(float weight, float label);
 
-    // https://github.com/VowpalWabbit/vowpal_wabbit/blob/master/cs/cli/vw_label.h
-    // TODO: support others too (e.g. multiclass)
+    /**
+     * Set a contextual bandit (CB) label
+     *
+     * @param action      this is 1-based.
+     * @param cost        cost assigned to the action.
+     * @param probability probability with which the action was chosen.
+     */
+    public native void setContextualBanditLabel(int action, double cost, double probability);
 
     /**
-     * Updates the associated VW model using this example. 
+     * Mark this as a shared CB label.
+     */
+    public native void setSharedLabel();
+
+    /**
+     * Initializes the label.
+     */
+    public native void setDefaultLabel();
+
+    /**
+     * Updates the associated VW model using this example.
      */
     public native void learn();
-    
+
     /**
-     * Gets the prediction from the current example. Useful after learning to get the 1-step ahead prediction.
+     * Gets the prediction from the current example. Useful after learning to get
+     * the 1-step ahead prediction.
+     *
      * @return the prediction.
      */
     public native Object getPrediction();
 
     /**
      * Gets the prediction from the current example.
+     *
      * @return the prediction.
      */
     public native Object predict();
+
+    @Override
+    public native String toString();
 
     /**
      * Pointer to the native VowpalWabbitExampleWrapper data structure.
      */
     private long nativePointer;
-        
+
     /**
      * Initializes the native VowpalWabbitExampleWrapper data structure.
+     *
      * @param vwNativePointer the associated VW instance.
-     * @param isEmpty true if this should be initialized as new empty (=new-line) example.
+     * @param isEmpty         true if this should be initialized as new empty
+     *                        (=new-line) example.
      */
     VowpalWabbitExample(long vwNativePointer, boolean isEmpty) {
         this.nativePointer = initialize(vwNativePointer, isEmpty);
@@ -86,7 +115,8 @@ public class VowpalWabbitExample implements Closeable {
 
     /**
      * Set the simple label using a weight of 1.
-     * @param label value of the label (e.g. -1, 1 for binary). 
+     *
+     * @param label value of the label (e.g. -1, 1 for binary).
      */
     public void setLabel(float label) {
         setLabel(1f, label);
