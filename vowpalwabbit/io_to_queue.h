505239f18c7fab202fa79395b40a69c13ad28567
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-08-23 12:40:39 -0400

    Clean up io_lines_toqueue and binary and ascii readers for compatibility with new cache format and removal of io_tem.h

--- vowpalwabbit/io_to_queue.h
+++ vowpalwabbit/io_to_queue.h
@@ -9,9 +9,6 @@
 #include <mutex>  
 #include <chrono>   
 
-#include "io_item.h"
-
-//Adds all lines of input to the input queue
 inline void io_lines_toqueue(vw& all){
   
   char* line = nullptr;
@@ -20,42 +17,52 @@ inline void io_lines_toqueue(vw& all){
 
   while(!should_finish)
   {    
-    v_array<example*> example_arr;
-
-    should_finish = all.p->input_file_reader(&all, line, example_arr);
+    should_finish = all.p->input_file_reader(all, line);
   }
 
-  all.p->_io_state.done_with_io.store(true);
+  all.p->done_with_io.store(true);
 
 }
 
-inline bool read_input_file_ascii(vw *all, char *&line, v_array<example*>&) {
+inline bool read_input_file_ascii(vw& all, char *&line) {
 
-  size_t num_chars_initial = readto(*(all->p->input), line, '\n');
+  size_t num_chars_initial = readto(*(all.p->input), line, '\n');
 
-  bool should_finish = true;
-  
-  if(num_chars_initial > 0) {
-    should_finish = all->p->_io_state.add_to_queue(line, num_chars_initial);
+  bool finish = false;
+
+  std::vector<char> *byte_array = new std::vector<char>();
+  byte_array->resize(num_chars_initial); // Note: This byte_array is NOT null terminated!
+
+  memcpy(byte_array->data(), line, num_chars_initial);
+
+  all.p->io_lines.push(std::move(byte_array));
+
+  if(num_chars_initial <= 0){
+    finish = true;
+    all.p->io_lines.set_done();
   }
 
-  return should_finish;
+  return finish;
+
 }
 
-inline bool read_input_file_binary(vw *all, char *&line, v_array<example*>&) {
+inline bool read_input_file_binary(vw& all, char *&line) {
 
   size_t total_num_input_bytes = 0;
 
   //read_all_data returns the number of bytes successfully read from the input files
-  total_num_input_bytes = all->p->input->read_all_data(line, total_num_input_bytes);
+  total_num_input_bytes = all.p->input->read_all_data(line, total_num_input_bytes);
 
-  bool should_finish = true;
-  
-  if(total_num_input_bytes > 0) {
-    all->p->_io_state.add_to_queue(line, total_num_input_bytes);
-  }
+  bool finish = true;
 
-  return should_finish;
+  std::vector<char> *byte_array = new std::vector<char>();
+  byte_array->resize(total_num_input_bytes); // Note: This byte_array is NOT null terminated!
+
+  memcpy(byte_array->data(), line, total_num_input_bytes);
+
+  all.p->io_lines.push(std::move(byte_array));
+
+  return finish;
  
 }
 
@@ -64,14 +71,10 @@ namespace VW {
 
 inline void start_io_thread(vw& all){
 
-  io_state curr_io_state;
-
   if(!all.early_terminate) {
     all.io_thread = std::thread([&all]() 
     {
-
       io_lines_toqueue(all);
-
     });
   }
 
