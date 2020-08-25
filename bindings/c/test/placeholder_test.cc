601f416ae62806e09c5919ec052d975b721156a5
Jack Gerrits <jack@jackgerrits.com>
2020-08-25 12:37:29 -0400

    Implement buildsystem infra for C bindings

---
+++ bindings/c/test/placeholder_test.cc
@@ -0,0 +1,7 @@
+#include <boost/test/unit_test.hpp>
+
+#include "vw/experimental/placeholder.h"
+
+BOOST_AUTO_TEST_CASE(placeholder_test)
+{
+}
---
+++ bindings/c/test/test_main.cc
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE Main
+#include <boost/test/unit_test.hpp>
