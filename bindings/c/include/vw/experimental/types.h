bb83080fe6ce123318fc561fa458fee3a0df9f12
Jack Gerrits <jack@jackgerrits.com>
2020-08-26 11:43:43 -0400

    Update error code commetn

--- bindings/c/include/vw/experimental/types.h
+++ bindings/c/include/vw/experimental/types.h
@@ -12,6 +12,9 @@ extern "C"
 #endif
 
   // TODO: these status codes should match the codes used in the rest of the codebase.
+  // We will need to workout how to use the one central definition of error code
+  // descriptions and then project it both to be used internally and in the
+  // public API.
   typedef uint32_t VWStatus;
   static const VWStatus VW_SUCCESS = 0;
   static const VWStatus VW_FAIL = 1;
