8e98f0a47c14d2598c5996f5413141709cad13d6
Jack Gerrits <jackgerrits@users.noreply.github.com>
2020-08-26 11:20:38 -0400

    [CAPI] Implement buildsystem for C bindings (#2551)
    * Implement buildsystem infra for C bindings
    
    * Change how proj links to boost test

---
+++ bindings/c/include/vw/experimental/placeholder.h
@@ -0,0 +1,14 @@
+// Copyright (c) by respective owners including Yahoo!, Microsoft, and
+// individual contributors. All rights reserved. Released under a BSD (revised)
+// license as described in the file LICENSE.
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
---
+++ bindings/c/src/placeholder.cc
@@ -0,0 +1,5 @@
+// Copyright (c) by respective owners including Yahoo!, Microsoft, and
+// individual contributors. All rights reserved. Released under a BSD (revised)
+// license as described in the file LICENSE.
+
+#include "vw/experimental/placeholder.h"
