a95f1d2ee3725ed7720355bb5e2aada863d1edef
Rajan Chari <rajan.chari@yahoo.com>
2020-08-31 19:07:06 -0400

    Enable unit test

--- test/unit_test/cats_tree_tests.cc
+++ test/unit_test/cats_tree_tests.cc
@@ -495,7 +495,7 @@ BOOST_AUTO_TEST_CASE(build_min_depth_tree_cont_1)
   BOOST_CHECK_EQUAL_COLLECTIONS(tree.nodes.begin(), tree.nodes.end(), expected.begin(), expected.end());
 }
 
-BOOST_AUTO_TEST_CASE(build_min_depth_tree_cont_too_big, *boost::unit_test::disabled())
+BOOST_AUTO_TEST_CASE(build_min_depth_tree_cont_too_big)
 {
   //Valgrind cannot handle new throwing while allocating memory
   if(is_invoked_with_valgrind())
