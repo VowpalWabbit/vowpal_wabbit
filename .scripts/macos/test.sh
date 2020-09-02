aa933b57abf98a068d8e6e0c35e6e66644af06f7
Rajan Chari <ranaras@microsoft.com>
2020-09-02 05:39:49 -0400

    Debug MacCI

--- .scripts/macos/test.sh
+++ .scripts/macos/test.sh
@@ -6,5 +6,6 @@ cd $1
 mkdir -p build
 cd build
 
-NUM_PROCESSORS=$(cat nprocs.txt)
+# NUM_PROCESSORS=$(cat nprocs.txt)
 make test_with_output -j ${NUM_PROCESSORS}
+test/unit_test/vw-unit-test.out -l all
