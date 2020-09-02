f2ed9e1ce3d4d18e60f260567baf9501a31fae8a
Rajan Chari <ranaras@microsoft.com>
2020-09-02 10:59:01 -0400

    MacCI fixes

--- .scripts/macos/test.sh
+++ .scripts/macos/test.sh
@@ -8,6 +8,4 @@ cd build
 
 # NUM_PROCESSORS=$(cat nprocs.txt)
 make help
-make vw-unit-test.out
-test/unit_test/vw-unit-test.out -l all MacCI
-make test_with_output -j ${NUM_PROCESSORS} -- MacCI
+make test_with_output -j ${NUM_PROCESSORS}
