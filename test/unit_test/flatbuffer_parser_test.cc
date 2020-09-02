e38ebf85e4b6756953871b2c2cf32b425efb36ce
Olga Vrousgou <olgavrou@gmail.com>
2020-09-02 14:07:16 +0100

    cleanup fb unit test

--- test/unit_test/flatbuffer_parser_test.cc
+++ test/unit_test/flatbuffer_parser_test.cc
@@ -12,12 +12,6 @@
 #include "parser/flatbuffer/parse_example_flatbuffer.h"
 #include "constant.h"
 
-struct flatbuilder
-{
-  flatbuffers::FlatBufferBuilder _builder;
-  flatbuilder() : _builder(1024) {}
-};
-
 multi_ex parse_flatbuffer(vw& all)
 {
   auto examples = v_init<example*>();
@@ -31,31 +25,31 @@ multi_ex parse_flatbuffer(vw& all)
   return result;
 }
 
-flatbuffers::Offset<void> get_label(flatbuilder& build, VW::parsers::flatbuffer::Label label_type)
+flatbuffers::Offset<void> get_label(flatbuffers::FlatBufferBuilder& builder, VW::parsers::flatbuffer::Label label_type)
 {
   flatbuffers::Offset<void> label;
   if (label_type == VW::parsers::flatbuffer::Label_SimpleLabel)
-    label = VW::parsers::flatbuffer::CreateSimpleLabel(build._builder, 0.0, 1.0).Union();
+    label = VW::parsers::flatbuffer::CreateSimpleLabel(builder, 0.0, 1.0).Union();
 
   return label;
 }
 
 flatbuffers::Offset<VW::parsers::flatbuffer::ExampleCollection> sample_flatbuffer(
-    flatbuilder& build, VW::parsers::flatbuffer::Label label_type)
+    flatbuffers::FlatBufferBuilder& builder, VW::parsers::flatbuffer::Label label_type)
 {
   std::vector<flatbuffers::Offset<VW::parsers::flatbuffer::Example>> examplecollection;
   std::vector<flatbuffers::Offset<VW::parsers::flatbuffer::Namespace>> namespaces;
   std::vector<flatbuffers::Offset<VW::parsers::flatbuffer::Feature>> fts;
 
-  auto label = get_label(build, label_type);
+  auto label = get_label(builder, label_type);
 
-  fts.push_back(VW::parsers::flatbuffer::CreateFeatureDirect(build._builder, "hello", 2.23f, constant));
+  fts.push_back(VW::parsers::flatbuffer::CreateFeatureDirect(builder, "hello", 2.23f, constant));
   namespaces.push_back(
-      VW::parsers::flatbuffer::CreateNamespaceDirect(build._builder, nullptr, constant_namespace, &fts));
+      VW::parsers::flatbuffer::CreateNamespaceDirect(builder, nullptr, constant_namespace, &fts));
   examplecollection.push_back(
-      VW::parsers::flatbuffer::CreateExampleDirect(build._builder, &namespaces, label_type, label));
+      VW::parsers::flatbuffer::CreateExampleDirect(builder, &namespaces, label_type, label));
 
-  return VW::parsers::flatbuffer::CreateExampleCollectionDirect(build._builder, &examplecollection);
+  return VW::parsers::flatbuffer::CreateExampleCollectionDirect(builder, &examplecollection);
 }
 
 BOOST_AUTO_TEST_CASE(check_flatbuffer)
@@ -63,13 +57,13 @@ BOOST_AUTO_TEST_CASE(check_flatbuffer)
   // How to get rid of reading a file?
   auto all = VW::initialize("--no_stdin --quiet", nullptr, false, nullptr, nullptr);
 
-  flatbuilder build;
+  flatbuffers::FlatBufferBuilder builder;
 
-  auto egcollection = sample_flatbuffer(build, VW::parsers::flatbuffer::Label_SimpleLabel);
-  build._builder.Finish(egcollection);
+  auto egcollection = sample_flatbuffer(builder, VW::parsers::flatbuffer::Label_SimpleLabel);
+  builder.Finish(egcollection);
 
-  uint8_t* buf = build._builder.GetBufferPointer();
-  int size = build._builder.GetSize();
+  uint8_t* buf = builder.GetBufferPointer();
+  int size = builder.GetSize();
 
   all->flat_converter = VW::make_unique<VW::parsers::flatbuffer::parser>(buf);
 
@@ -86,18 +80,19 @@ BOOST_AUTO_TEST_CASE(check_flatbuffer)
       all->flat_converter->data()->examples()->Get(0)->namespaces()->Get(0)->features()->Get(0)->hash(), constant);
   BOOST_CHECK_CLOSE(all->flat_converter->data()->examples()->Get(0)->namespaces()->Get(0)->features()->Get(0)->value(),
       2.23, FLOAT_TOL);
+  VW::finish(*all);
 }
 
 BOOST_AUTO_TEST_CASE(check_parsed_flatbuffer_examples)
 {
   auto all = VW::initialize("--no_stdin --quiet", nullptr, false, nullptr, nullptr);
 
-  flatbuilder build;
-  auto egcollection = sample_flatbuffer(build, VW::parsers::flatbuffer::Label_SimpleLabel);
-  build._builder.Finish(egcollection);
+  flatbuffers::FlatBufferBuilder builder;
+  auto egcollection = sample_flatbuffer(builder, VW::parsers::flatbuffer::Label_SimpleLabel);
+  builder.Finish(egcollection);
 
-  uint8_t* buf = build._builder.GetBufferPointer();
-  int size = build._builder.GetSize();
+  uint8_t* buf = builder.GetBufferPointer();
+  int size = builder.GetSize();
 
   all->flat_converter = VW::make_unique<VW::parsers::flatbuffer::parser>(buf);
   auto examples = parse_flatbuffer(*all);
