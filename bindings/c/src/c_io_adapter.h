667eee12194342db6ef7615fea57a663908e9ae3
Jack Gerrits <jackgerrits@users.noreply.github.com>
2020-09-08 11:52:31 -0400

    Implement reader and writer to be used with raw c api (#2565)

---
+++ bindings/c/src/c_io_adapter.h
@@ -0,0 +1,29 @@
+// Copyright (c) by respective owners including Yahoo!, Microsoft, and
+// individual contributors. All rights reserved. Released under a BSD (revised)
+// license as described in the file LICENSE.
+
+#pragma once
+
+#include "vw/experimental/types.h"
+
+#include "io/io_adapter.h"
+
+struct c_reader : public VW::io::reader
+{
+  c_reader(void* context, VWReadFunc* read);
+  ssize_t read(char* buffer, size_t num_bytes) override;
+
+private:
+  void* _context = nullptr;
+  VWReadFunc* _read_func = nullptr;
+};
+
+struct c_writer : public VW::io::writer
+{
+  c_writer(void* context, VWWriteFunc* write);
+  ssize_t write(const char* buffer, size_t num_bytes) override;
+
+private:
+  void* _context = nullptr;
+  VWWriteFunc* _write_func = nullptr;
+};
