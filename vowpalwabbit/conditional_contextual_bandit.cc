a9e98092b05ec857a7f0977231ba770766ce3272
Jack Gerrits <jack@jackgerrits.com>
2020-10-16 13:12:02 -0400

    When only a single slot is used make ccb and cb equivalent

--- vowpalwabbit/conditional_contextual_bandit.cc
+++ vowpalwabbit/conditional_contextual_bandit.cc
@@ -14,6 +14,8 @@
 #include "constant.h"
 #include "v_array_pool.h"
 #include "decision_scores.h"
+#include "vw_versions.h"
+#include "version.h"
 
 #include <numeric>
 #include <algorithm>
@@ -56,6 +58,11 @@ struct ccb
 
   VW::v_array_pool<CB::cb_class> cb_label_pool;
   VW::v_array_pool<ACTION_SCORE::action_score> action_score_pool;
+
+  VW::version_struct model_file_version;
+  // If the reduction has not yet seen a multi slot example, it will behave the same as if it were CB.
+  // This means the interactions aren't added and the slot feature is not added.
+  bool has_seen_multi_slot_example = false;
 };
 
 namespace CCB
@@ -412,6 +419,9 @@ void learn_or_predict(ccb& data, multi_learner& base, multi_ex& examples)
   }
 #endif
 
+  data.has_seen_multi_slot_example = data.has_seen_multi_slot_example || data.slots.size() > 1;
+  const bool should_augment_with_slot_info = data.has_seen_multi_slot_example;
+
   // This will overwrite the labels with CB.
   create_cb_labels(data);
   auto restore_guard = VW::scope_exit(
@@ -439,27 +449,27 @@ void learn_or_predict(ccb& data, multi_learner& base, multi_ex& examples)
   size_t slot_id = 0;
   for (example* slot : data.slots)
   {
-    // Namespace crossing for slot features.
-    data.generated_interactions.clear();
-    std::copy(data.original_interactions->begin(), data.original_interactions->end(),
-        std::back_inserter(data.generated_interactions));
-    calculate_and_insert_interactions(data.shared, data.actions, data.generated_interactions);
-    data.shared->interactions = &data.generated_interactions;
-    for (auto* ex : data.actions)
+    if (should_augment_with_slot_info)
     {
-      ex->interactions = &data.generated_interactions;
+      // Namespace crossing for slot features.
+      data.generated_interactions.clear();
+      std::copy(data.original_interactions->begin(), data.original_interactions->end(),
+          std::back_inserter(data.generated_interactions));
+      calculate_and_insert_interactions(data.shared, data.actions, data.generated_interactions);
+      data.shared->interactions = &data.generated_interactions;
+      for (auto* ex : data.actions) { ex->interactions = &data.generated_interactions; }
     }
 
     data.include_list.clear();
     build_cb_example<is_learn>(data.cb_ex, slot, data);
 
-    if (data.all->audit)
+    if (should_augment_with_slot_info)
     {
-      inject_slot_id<true>(data, data.shared, slot_id);
-    }
-    else
-    {
-      inject_slot_id<false>(data, data.shared, slot_id);
+      if (data.all->audit) { inject_slot_id<true>(data, data.shared, slot_id); }
+      else
+      {
+        inject_slot_id<false>(data, data.shared, slot_id);
+      }
     }
 
     if (has_action(data.cb_ex))
@@ -475,20 +485,20 @@ void learn_or_predict(ccb& data, multi_learner& base, multi_ex& examples)
       decision_scores.push_back(data.action_score_pool.get_object());
     }
 
-    data.shared->interactions = data.original_interactions;
-    for (auto* ex : data.actions)
+    if (should_augment_with_slot_info)
     {
-      ex->interactions = data.original_interactions;
+      data.shared->interactions = data.original_interactions;
+      for (auto* ex : data.actions) { ex->interactions = data.original_interactions; }
     }
     remove_slot_features(data.shared, slot);
 
-    if (data.all->audit)
+    if (should_augment_with_slot_info)
     {
-      remove_slot_id<true>(data.shared);
-    }
-    else
-    {
-      remove_slot_id<false>(data.shared);
+      if (data.all->audit) { remove_slot_id<true>(data.shared); }
+      else
+      {
+        remove_slot_id<false>(data.shared);
+      }
     }
 
     // Put back the original shared example tag.
@@ -503,7 +513,6 @@ void learn_or_predict(ccb& data, multi_learner& base, multi_ex& examples)
     data.all->trace_message << "CCB got exception from base reductions: " << e.what() << std::endl;
     throw;
   }
-
 }
 
 std::string generate_ccb_label_printout(const std::vector<example*>& slots)
@@ -616,6 +625,21 @@ void finish_multiline_example(vw& all, ccb& data, multi_ex& ec_seq)
   VW::finish_example(all, ec_seq);
 }
 
+void save_load(ccb& sm, io_buf& io, bool read, bool text)
+{
+  if (io.num_files() == 0) { return; }
+
+  // We want to enter this block if either we are writing, or reading a model file after the version in which this was
+  // added.
+  // if (!read || sm.model_file_version >= VERSION_FILE_WITH_CCB_MULTI_SLOTS_SEEN_FLAG)
+  {
+    std::stringstream msg;
+    if (!read) { msg << "CCB: has_seen_multi_slot_example = " << sm.has_seen_multi_slot_example << "\n"; }
+    bin_text_read_write_fixed_validated(
+        io, (char*)&sm.has_seen_multi_slot_example, sizeof(sm.has_seen_multi_slot_example), "", read, msg, text);
+  }
+}
+
 base_learner* ccb_explore_adf_setup(options_i& options, vw& all)
 {
   auto data = scoped_calloc_or_throw<ccb>();
@@ -660,6 +684,7 @@ base_learner* ccb_explore_adf_setup(options_i& options, vw& all)
   data->shared = nullptr;
   data->original_interactions = &all.interactions;
   data->all = &all;
+  data->model_file_version = all.model_file_ver;
 
   data->id_namespace_str.push_back((char)ccb_id_namespace);
   data->id_namespace_str.append("_id");
@@ -671,6 +696,7 @@ base_learner* ccb_explore_adf_setup(options_i& options, vw& all)
   all.delete_prediction = ACTION_SCORE::delete_action_scores;
 
   l.set_finish_example(finish_multiline_example);
+  l.set_save_load(save_load);
   return make_base(l);
 }
 
