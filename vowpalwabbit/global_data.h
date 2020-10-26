a21ee013627ef8076a6c94f5eb9325ab08e5293c
Rajan Chari <rajan.chari@yahoo.com>
2020-10-26 18:44:58 -0400

    Make prediction_buffer & it's operations private

--- vowpalwabbit/global_data.h
+++ vowpalwabbit/global_data.h
@@ -499,9 +499,6 @@ struct vw
 
   label_type_t label_type;
 
-  polyprediction _predict_buffer;
-  void cleanup_prediction();
-  void copy_prediction(const polyprediction& from_pred);
 
   vw();
   ~vw();
@@ -514,6 +511,12 @@ struct vw
   // That pointer would be invalidated if it were to be moved.
   vw(const vw&&) = delete;
   vw& operator=(const vw&&) = delete;
+
+  private:
+    polyprediction _predict_buffer;
+
+    void cleanup_prediction();
+    void copy_prediction(const polyprediction& from_pred);
 };
 
 VW_DEPRECATED("Use print_result_by_ref instead")
