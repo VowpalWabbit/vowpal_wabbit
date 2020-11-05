b628d2b7645d0867ac706ea08db05959246d74a9
Olga Vrousgou <olgavrou@gmail.com>
2020-11-05 13:26:55 +0000

    cleanup

--- .scripts/linux/build-with-benchmarks.sh
+++ .scripts/linux/build-with-benchmarks.sh
@@ -6,7 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
-# Clear out build directory then build using GCov and run one set of tests again
 rm -rf build
 mkdir build
 cd build
--- .scripts/linux/install-benchmark.sh
+++ .scripts/linux/install-benchmark.sh
@@ -16,9 +16,5 @@ $ cd benchmark
 $ cmake -E make_directory "build"
 # Generate build system files with cmake.
 $ cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
-# or, starting with CMake 3.13, use a simpler form:
-# cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
-# Build the library.
-$ cmake --build "build" --config Release
 # Install globally
 sudo cmake --build "build" --config Release --target install
