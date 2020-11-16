25a0df803078b5d2fba175c50e049abf9c0e36ba
zhxiaog <zhxiaog@outlook.com>
2020-11-16 16:11:48 +0800

    fix code-format

--- java/src/main/c++/vowpalWabbit_learner_VWCCBLearner.h
+++ java/src/main/c++/vowpalWabbit_learner_VWCCBLearner.h
@@ -3,28 +3,27 @@
 /* Header for class vowpalWabbit_learner_VWActionScoresLearner */
 
 #ifndef _Included_vowpalWabbit_learner_VWCCBLearner
-#define _Included_vowpalWabbit_learner_VWCCBLearner
-#ifdef __cplusplus
+#  define _Included_vowpalWabbit_learner_VWCCBLearner
+#  ifdef __cplusplus
 extern "C"
 {
-#endif
-/*
- * Class:     vowpalWabbit_learner_VWCCBLearner
- * Method:    predict
- * Signature: (Ljava/lang/String;ZJ)LvowpalWabbit/responses/DecisionScores;
- */
-JNIEXPORT jobject JNICALL Java_vowpalWabbit_learner_VWCCBLearner_predict
-(JNIEnv *, jobject, jstring, jboolean, jlong);
+#  endif
+  /*
+   * Class:     vowpalWabbit_learner_VWCCBLearner
+   * Method:    predict
+   * Signature: (Ljava/lang/String;ZJ)LvowpalWabbit/responses/DecisionScores;
+   */
+  JNIEXPORT jobject JNICALL Java_vowpalWabbit_learner_VWCCBLearner_predict(JNIEnv *, jobject, jstring, jboolean, jlong);
 
-/*
- * Class:     vowpalWabbit_learner_VWCCBLearner
- * Method:    predictMultiline
- * Signature: ([Ljava/lang/String;ZJ)LvowpalWabbit/responses/DecisionScores;
- */
-JNIEXPORT jobject JNICALL Java_vowpalWabbit_learner_VWCCBLearner_predictMultiline
-(JNIEnv *, jobject, jobjectArray, jboolean, jlong);
+  /*
+   * Class:     vowpalWabbit_learner_VWCCBLearner
+   * Method:    predictMultiline
+   * Signature: ([Ljava/lang/String;ZJ)LvowpalWabbit/responses/DecisionScores;
+   */
+  JNIEXPORT jobject JNICALL Java_vowpalWabbit_learner_VWCCBLearner_predictMultiline(
+      JNIEnv *, jobject, jobjectArray, jboolean, jlong);
 
-#ifdef __cplusplus
+#  ifdef __cplusplus
 }
-#endif
+#  endif
 #endif
