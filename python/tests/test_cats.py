d48b4580554c9c49abd9b8979cb386f339d03dc6
Olga Vrousgou <olgavrou@gmail.com>
2020-11-30 13:17:12 +0000

    fix action

--- python/tests/test_cats.py
+++ python/tests/test_cats.py
@@ -7,7 +7,7 @@ def test_cats():
     max_value = 20
 
     vw = pyvw.vw("--cats 4 --min_value " + str(min_value) + " --max_value " + str(max_value) + " --bandwidth 1")
-    vw_example = vw.parse("ca 185.121:0.657567:6.20426e-05 | f1 f2 f3 f4", pyvw.vw.lContinuous)
+    vw_example = vw.parse("ca 15:0.657567:6.20426e-05 | f1 f2 f3 f4", pyvw.vw.lContinuous)
     vw.learn(vw_example)
     vw.finish_example(vw_example)
 
