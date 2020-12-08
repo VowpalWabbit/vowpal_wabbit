3dec16cbde417ac0865dd21bb8e90e260f345449
Jack Gerrits <jack@jackgerrits.com>
2020-12-08 10:06:29 -0500

    Allow port to be selected in spanning tree tool

--- cluster/spanning_tree_main.cc
+++ cluster/spanning_tree_main.cc
@@ -10,6 +10,8 @@ This creates a binary tree topology over a set of n nodes that connect.
 #include "spanning_tree.h"
 #include "vw_exception.h"
 
+#include "options_boost_po.h"
+
 #ifdef _WIN32
 int daemon(int a, int b) { return 0; }
 int getpid() { return (int)::GetCurrentProcessId(); }
@@ -23,27 +25,51 @@ int getpid() { return (int)::GetCurrentProcessId(); }
 
 using namespace VW;
 
+void usage(const VW::config::options_boost_po& options)
+{
+  std::cout << "usage: spanning_tree [--port number] [--nondaemon] [--help] [pid_file]" << std::endl;
+  std::cout << options.help() << std::endl;
+  exit(0);
+}
+
 int main(int argc, char* argv[])
 {
-  if (argc > 2)
+  int port;
+  bool nondaemon = false;
+  bool help = false;
+  VW::config::options_boost_po options(argc, argv);
+  VW::config::option_group_definition spanning_tree_options_group("Spanning Tree");
+  spanning_tree_options_group.add(
+      VW::config::make_option("port", port).default_value(26543).help("Port number for spanning tree to listen on"));
+  spanning_tree_options_group.add(
+      VW::config::make_option("nondaemon", nondaemon).help("Run spanning tree in foreground"));
+  spanning_tree_options_group.add(VW::config::make_option("help", help).help("Print help message"));
+  options.add_and_parse(spanning_tree_options_group);
+  options.check_unregistered();
+
+  if (help) { usage(options); }
+
+  auto positional_tokens = options.get_positional_tokens();
+  std::string pid_file_name = "";
+  if (positional_tokens.size() == 1) { pid_file_name = positional_tokens[0]; }
+  else if (positional_tokens.size() > 1)
   {
-    std::cout << "usage: spanning_tree [--nondaemon | pid_file]" << std::endl;
-    exit(0);
+    usage(options);
   }
 
   try
   {
-    if (argc == 2 && strcmp("--nondaemon", argv[1]) == 0)
-      ;
-    else if (daemon(1, 1))
-      THROWERRNO("daemon: ");
+    if (!nondaemon)
+    {
+      if (daemon(1, 1)) { THROWERRNO("daemon: "); }
+    }
 
-    SpanningTree spanningTree;
+    SpanningTree spanningTree(port);
 
-    if (argc == 2 && strcmp("--nondaemon", argv[1]) != 0)
+    if (!pid_file_name.empty())
     {
       std::ofstream pid_file;
-      pid_file.open(argv[1]);
+      pid_file.open(pid_file_name);
       if (!pid_file.is_open())
       {
         std::cerr << "error writing pid file" << std::endl;
