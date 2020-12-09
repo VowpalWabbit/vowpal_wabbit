fa67895d65100500720301227f376dba6e891a10
Eduardo Salinas <edus@microsoft.com>
2020-12-09 17:25:15 +0000

    small fixes

--- vowpalwabbit/cb_to_cb_adf.cc
+++ vowpalwabbit/cb_to_cb_adf.cc
@@ -89,7 +89,6 @@ VW::LEARNER::base_learner* cb_to_cb_adf_setup(options_i& options, vw& all)
   option_group_definition new_options("Contextual Bandit Options");
   new_options
       .add(make_option("cb_to_cbadf", num_actions)
-               .keep()
                .necessary()
                .help("Maps cb_adf to cb. Disable with cb_force_legacy."))
       .add(make_option("cb", num_actions).keep().help("Maps cb_adf to cb. Disable with cb_force_legacy."))
