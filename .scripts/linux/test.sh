0395447542878f6df5e473b93ddb2bf6afc319f1
Jack Gerrits <jack@jackgerrits.com>
2020-12-11 16:48:28 -0500

    Update test script

--- .scripts/linux/test.sh
+++ .scripts/linux/test.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
-# cd build
-# make test_with_output
+cd build
+make test_with_output
 
-cd test
+cd ../test
 python3 run_tests.py -f -j $(nproc)
