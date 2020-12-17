f92877a60d3bffd7a1aa346f929c6495c719792d
Jack Gerrits <jackgerrits@users.noreply.github.com>
2020-12-17 11:15:27 -0500

    Update run-benchmarks.sh

--- .scripts/linux/run-benchmarks.sh
+++ .scripts/linux/run-benchmarks.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
-./build/test/benchmarks/vw-benchmarks.out --benchmark_format=json --benchmark_repetitions=10 --benchmark_min_time=4 --benchmark_report_aggregates_only=true > "$1"
+./build/test/benchmarks/vw-benchmarks.out --benchmark_format=json --benchmark_repetitions=10 --benchmark_min_time=2 --benchmark_display_aggregates_only=false --benchmark_report_aggregates_only=true | tee "$1"
