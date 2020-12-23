1243eb17b198a6e11091c5c4adcc20aa12be2bcc
Olga Vrousgou <olgavrou@gmail.com>
2020-12-23 15:55:47 +0000

    add test file

---
+++ .scripts/linux/test-flatbuffers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -x
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+REPO_DIR=$SCRIPT_DIR/../../
+cd $REPO_DIR
+
+cd test
+
+python3 run_tests.py -f --for_flatbuffers -j $(nproc)
