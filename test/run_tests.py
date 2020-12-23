ed1ea9108dbf9692733a338f4b3cc5877bf31e7a
Olga Vrousgou <olgavrou@gmail.com>
2020-12-23 14:18:03 +0000

    tidy up

--- test/run_tests.py
+++ test/run_tests.py
@@ -512,6 +512,45 @@ def transform_tests_for_flatbuffers(tests, to_flatbuff, working_dir, color_enum)
     
     def change_input_file(input_file):
         return 'train-set' in input_file or 'test-set' in input_file
+    
+    def replace_filename_in_stderr(working_dir, test_id, stderr_file, input_files):
+        stderr_test_file = working_dir.joinpath('test_' + test_id).joinpath(os.path.basename(working_dir.joinpath(stderr_file)))
+        shutil.copyfile(stderr_file, str(stderr_test_file))        
+        temp = str(stderr_test_file) + '.bak'
+        with open(stderr_test_file, 'r') as f:
+            with open(temp, 'w') as tmp_f:
+                for line in f:
+                    for i, input_file in enumerate(input_files):
+                        if change_input_file(input_file):
+                            line = line.replace(str(input_file), str(fb_input_files_full_path[i]))
+                    tmp_f.write(line)
+        
+        # swap temp with file
+        shutil.move(temp, stderr_test_file)
+        return str(stderr_test_file)
+    
+    def should_transform(test, files_to_be_transformed):
+        if 'depends_on' in test: # assuming dependent tests use same input files, so might already be transformed
+            # check if the file exists in the dependant directories
+            for f in files_to_be_transformed:
+                search_paths = []
+                dependencies = test['depends_on']
+                search_paths.extend([Path(test_base_working_dir).joinpath(
+                    "test_{}".format(x), os.path.basename(f)) for x in dependencies]) # for input_files with a full path
+
+                for search_path in search_paths:
+                    if search_path.exists() and not search_path.is_dir():
+                        return False
+        return True
+        
+    def replace_test_input_files(test, input_files):
+        files_to_be_transformed = []
+        # replace the input_file to point to the generated flatbuffer file
+        for i, input_file in enumerate(input_files):
+            if change_input_file(input_file):
+                test['input_files'][i] = str(fb_input_files_full_path[i])
+                files_to_be_transformed.append(str(fb_input_files_full_path[i]))
+        return files_to_be_transformed
 
     test_base_working_dir = str(working_dir)
     if not Path(test_base_working_dir).exists():
@@ -559,7 +598,7 @@ def transform_tests_for_flatbuffers(tests, to_flatbuff, working_dir, color_enum)
         fb_input_files_full_path = []
         fb_input_files = []
 
-        for input_file in input_files:
+        for i, input_file in enumerate(input_files):
             if change_input_file(input_file):
                 file_basename = os.path.basename(input_file)
                 fb_file = ''.join([file_basename, '.fb'])
@@ -569,27 +608,10 @@ def transform_tests_for_flatbuffers(tests, to_flatbuff, working_dir, color_enum)
 
         # edit stderr to output the generated flatbuffer file(s) instead
         if 'stderr' in test['diff_files']:
-            stderr_test_file = working_dir.joinpath('test_' + test_id).joinpath(os.path.basename(working_dir.joinpath(test['diff_files']['stderr'])))
-            shutil.copyfile(test['diff_files']['stderr'], str(stderr_test_file))
-            test['diff_files']['stderr'] = str(stderr_test_file)
-            temp = str(stderr_test_file) + '.bak'
-            with open(stderr_test_file, 'r') as f:
-                with open(temp, 'w') as tmp_f:
-                    for line in f:
-                        for i, input_file in enumerate(input_files):
-                            if change_input_file(input_file):
-                                line = line.replace(str(input_file), str(fb_input_files_full_path[i]))
-                        tmp_f.write(line)
-            
-            # swap temp with file
-            shutil.move(temp, stderr_test_file)
+            new_file = replace_filename_in_stderr(working_dir, test_id, test['diff_files']['stderr'], input_files)
+            test['diff_files']['stderr'] = new_file
 
-        transformed_file_names = []
-        # replace the input_file to point to the generated flatbuffer file
-        for i, input_file in enumerate(input_files):
-            if change_input_file(input_file):
-                test['input_files'][i] = str(fb_input_files_full_path[i])
-                transformed_file_names.append(str(fb_input_files_full_path[i]))
+        files_to_be_transformed = replace_test_input_files(test, input_files)
 
         # arguments and flats not supported or needed in flatbuffer transformation
         flags_to_remove = ['--audit', '-c ','--bfgs', '--onethread', '-t ', '--search_span_bilou']
@@ -607,21 +629,8 @@ def transform_tests_for_flatbuffers(tests, to_flatbuff, working_dir, color_enum)
         test['vw_command'] = remove_arguments(test['vw_command'], arguments_to_remove)
         test['vw_command'] = remove_arguments(test['vw_command'], flags_to_remove, flags=True)
 
-        should_generate = True
-        if 'depends_on' in test: # assuming dependent tests use same input files, so might already be transformed
-            # check if the file exists in the dependant directories
-            for f in transformed_file_names:
-                search_paths = []
-                dependencies = test['depends_on']
-                search_paths.extend([Path(test_base_working_dir).joinpath(
-                    "test_{}".format(x), os.path.basename(f)) for x in dependencies]) # for input_files with a full path
-
-                for search_path in search_paths:
-                    if search_path.exists() and not search_path.is_dir():
-                        should_generate = False
-
-        if should_generate:
-            for f in transformed_file_names:
+        if should_transform(test, files_to_be_transformed): # transformation might already be done by depended_on test
+            for f in files_to_be_transformed:
                 cmd = "{} {} {} {}".format((to_flatbuff), (test['vw_command']), ('--fb_out'), (f))
                 print("{}COMMAND {} {}{}".format(color_enum.LIGHT_PURPLE, test['id'], cmd, color_enum.ENDC))
                 result = subprocess.run(
