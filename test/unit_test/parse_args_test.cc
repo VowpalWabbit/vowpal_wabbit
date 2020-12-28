a97440a378d21bdfba60016ac736a9496f97c1c7
Alexey Taymanov <ataymano@gmail.com>
2020-12-28 11:41:19 -0500

    more passing tests + cleanup

--- test/unit_test/parse_args_test.cc
+++ test/unit_test/parse_args_test.cc
@@ -13,14 +13,14 @@
 
 using namespace VW::config;
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_empty_options_dup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_no_opts_dup)
 {
   const std::vector<std::string> strings;
   auto opts = VW::make_unique<options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, true, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -33,17 +33,19 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_empty_options_dup)
   BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
 
   BOOST_CHECK_EQUAL(false, opts->was_supplied("int_opt"));
+  BOOST_CHECK_EQUAL(-1, int_opt);
   BOOST_CHECK_EQUAL(false, opts->was_supplied("bool_opt"));
+  BOOST_CHECK_EQUAL(false, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_empty_options_nodup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_no_opts_nodup)
 {
   const std::vector<std::string> strings;
   auto opts = VW::make_unique<options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, false, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -56,17 +58,19 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_empty_options_nodup)
   BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
 
   BOOST_CHECK_EQUAL(false, opts->was_supplied("int_opt"));
+  BOOST_CHECK_EQUAL(-1, int_opt);
   BOOST_CHECK_EQUAL(false, opts->was_supplied("bool_opt"));
+  BOOST_CHECK_EQUAL(false, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_only_dup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_opt_dup)
 {
   const std::vector<std::string> strings{"--bool_opt"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, true, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -79,18 +83,19 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_only_dup)
   BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
 
   BOOST_CHECK_EQUAL(false, opts->was_supplied("some_opt"));
+  BOOST_CHECK_EQUAL(-1, int_opt);
   BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt"));
   BOOST_CHECK_EQUAL(true, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_only_nodup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_opt_nodup)
 {
   const std::vector<std::string> strings{"--bool_opt"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, false, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -99,22 +104,21 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_only_nodup)
   arg_group.add(make_option("bool_opt", bool_opt));
 
   BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
-
-  BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
 
   BOOST_CHECK_EQUAL(false, opts->was_supplied("int_opt"));
+  BOOST_CHECK_EQUAL(-1, int_opt);
   BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt"));
   BOOST_CHECK_EQUAL(true, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_value_dup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_opt_dup)
 {
   const std::vector<std::string> strings{"--int_opt", "3"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, true, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -123,8 +127,6 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_value_dup)
   arg_group.add(make_option("bool_opt", bool_opt));
 
   BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
-
-  BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
 
   BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt"));
   BOOST_CHECK_EQUAL(3, int_opt);
@@ -132,14 +134,14 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_value_dup)
   BOOST_CHECK_EQUAL(false, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_value_nodup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_opt_nodup)
 {
   const std::vector<std::string> strings{"--int_opt", "3"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, false, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -148,8 +150,6 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_value_nodup)
   arg_group.add(make_option("bool_opt", bool_opt));
 
   BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
-
-  BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
 
   BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt"));
   BOOST_CHECK_EQUAL(3, int_opt);
@@ -157,14 +157,14 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_single_key_value_nodup)
   BOOST_CHECK_EQUAL(false, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_and_int_opt_dup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_int_opt_dup)
 {
   const std::vector<std::string> strings{"--bool_opt", "--int_opt", "3"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, true, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -174,22 +174,20 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_and_int_opt_dup)
 
   BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
 
-  BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
-
   BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt"));
   BOOST_CHECK_EQUAL(3, int_opt);
   BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt"));
   BOOST_CHECK_EQUAL(true, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_and_int_opt_nodup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_int_opt_nodup)
 {
   const std::vector<std::string> strings{"--bool_opt", "--int_opt", "3"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, false, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -199,22 +197,20 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_bool_and_int_opt_nodup)
 
   BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
 
-  BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
-
   BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt"));
   BOOST_CHECK_EQUAL(3, int_opt);
   BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt"));
   BOOST_CHECK_EQUAL(true, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_and_bool_opt_dup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_bool_opt_dup)
 {
   const std::vector<std::string> strings{"--int_opt", "3", "--bool_opt"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, true, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -223,8 +219,6 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_and_bool_opt_dup)
   arg_group.add(make_option("bool_opt", bool_opt));
 
   BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
-
-  BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
 
   BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt"));
   BOOST_CHECK_EQUAL(3, int_opt);
@@ -232,14 +226,14 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_and_bool_opt_dup)
   BOOST_CHECK_EQUAL(true, bool_opt);
 }
 
-BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_and_bool_opt_nodup)
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_bool_opt_nodup)
 {
   const std::vector<std::string> strings{"--int_opt", "3", "--bool_opt"};
   auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
 
   merge_options_from_header_strings(strings, false, *opts);
 
-  int int_opt;
+  int int_opt = -1;
   bool bool_opt;
 
   option_group_definition arg_group("");
@@ -249,10 +243,76 @@ BOOST_AUTO_TEST_CASE(merge_from_header_strings_int_and_bool_opt_nodup)
 
   BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
 
-  BOOST_CHECK_EQUAL(2, opts->get_all_options().size());
-
   BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt"));
   BOOST_CHECK_EQUAL(3, int_opt);
   BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt"));
   BOOST_CHECK_EQUAL(true, bool_opt);
+}
+
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_bools_ints_interactions_opt_dup)
+{
+  const std::vector<std::string> strings{"--bool_opt1", "--int_opt1", "3", "--interactions", "::", "--bool_opt2", "--int_opt2", "4"};
+  auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
+
+  merge_options_from_header_strings(strings, true, *opts);
+
+  int int_opt1 = -1;
+  bool bool_opt1;
+  std::string interactions = "NONE";
+  int int_opt2 = -1;
+  bool bool_opt2;
+
+  option_group_definition arg_group("");
+  arg_group.add(make_option("int_opt1", int_opt1));
+  arg_group.add(make_option("bool_opt1", bool_opt1));
+  arg_group.add(make_option("interactions", interactions));
+  arg_group.add(make_option("int_opt2", int_opt2));
+  arg_group.add(make_option("bool_opt2", bool_opt2));
+
+  BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
+
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt1"));
+  BOOST_CHECK_EQUAL(3, int_opt1);
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt1"));
+  BOOST_CHECK_EQUAL(true, bool_opt1);
+  BOOST_CHECK_EQUAL(false, opts->was_supplied("interactions"));
+  BOOST_CHECK_EQUAL("NONE", interactions);
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt2"));
+  BOOST_CHECK_EQUAL(4, int_opt2);
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt2"));
+  BOOST_CHECK_EQUAL(true, bool_opt2);
+}
+
+BOOST_AUTO_TEST_CASE(merge_from_header_strings_bools_ints_interactions_opt_nodup)
+{
+  const std::vector<std::string> strings{"--bool_opt1", "--int_opt1", "3", "--interactions", "::", "--bool_opt2", "--int_opt2", "4"};
+  auto opts = VW::make_unique<VW::config::options_boost_po>(std::vector<std::string>());
+
+  merge_options_from_header_strings(strings, false, *opts);
+
+  int int_opt1 = -1;
+  bool bool_opt1;
+  std::string interactions = "NONE";
+  int int_opt2 = -1;
+  bool bool_opt2;
+
+  option_group_definition arg_group("");
+  arg_group.add(make_option("int_opt1", int_opt1));
+  arg_group.add(make_option("bool_opt1", bool_opt1));
+  arg_group.add(make_option("interactions", interactions));
+  arg_group.add(make_option("int_opt2", int_opt2));
+  arg_group.add(make_option("bool_opt2", bool_opt2));
+
+  BOOST_CHECK_NO_THROW(opts->add_and_parse(arg_group));
+
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt1"));
+  BOOST_CHECK_EQUAL(3, int_opt1);
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt1"));
+  BOOST_CHECK_EQUAL(true, bool_opt1);
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("interactions"));
+  BOOST_CHECK_EQUAL("::", interactions);
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("int_opt2"));
+  BOOST_CHECK_EQUAL(4, int_opt2);
+  BOOST_CHECK_EQUAL(true, opts->was_supplied("bool_opt2"));
+  BOOST_CHECK_EQUAL(true, bool_opt2);
 }
