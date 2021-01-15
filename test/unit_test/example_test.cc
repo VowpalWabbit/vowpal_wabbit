7e6e6d0a94540e02869e3fa688d30dcec62c9000
Rodrigo Kumpera <kumpera@gmail.com>
2021-01-15 12:08:02 -0500

    try to fix linux.

--- test/unit_test/example_test.cc
+++ test/unit_test/example_test.cc
@@ -12,11 +12,10 @@ BOOST_AUTO_TEST_CASE(example_move_ctor_moves_pred)
   example ex;
   ex.pred.scalars.push_back(8);
   BOOST_CHECK_EQUAL(ex.pred.scalars.size(), 1);
-  BOOST_CHECK_NE(ex.pred.scalars.cbegin(), nullptr);
 
   example ex2(std::move(ex));
 
-  BOOST_CHECK_EQUAL(ex.pred.scalars.cbegin(), nullptr);
+  BOOST_CHECK_EQUAL(ex.pred.scalars.size(), 0);
   BOOST_CHECK_EQUAL(ex2.pred.scalars.size(), 1);
 }
 
@@ -25,12 +24,11 @@ BOOST_AUTO_TEST_CASE(example_move_assign_op_moves_pred)
   example ex;
   ex.pred.a_s.push_back({0, 0});
   BOOST_CHECK_EQUAL(ex.pred.a_s.size(), 1);
-  BOOST_CHECK_NE(ex.pred.a_s.cbegin(), nullptr);
 
   example ex2;
-  BOOST_CHECK_EQUAL(ex2.pred.a_s.cbegin(), nullptr);
+  BOOST_CHECK_EQUAL(ex2.pred.a_s.size(), 0);
   ex2 = std::move(ex);
 
-  BOOST_CHECK_EQUAL(ex.pred.a_s.cbegin(), nullptr);
+  BOOST_CHECK_EQUAL(ex.pred.a_s.size(), 0);
   BOOST_CHECK_EQUAL(ex2.pred.a_s.size(), 1);
 }
