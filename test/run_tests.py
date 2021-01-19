da0ff6e3ea5bee42324df21bd40ae1045931aa93
Olga Vrousgou <olgavrou@gmail.com>
2021-01-19 19:16:11 +0000

    remove print

--- test/run_tests.py
+++ test/run_tests.py
@@ -652,7 +652,6 @@ def main():
     if args.for_flatbuffers:
         to_flatbuff = find_to_flatbuf_binary(test_base_ref_dir, args.to_flatbuff_path)
         tests = convert_tests_for_flatbuffers(tests, to_flatbuff, args.working_dir, color_enum)
-        print(len(tests))
 
     tasks = []
     completed_tests = Completion()
