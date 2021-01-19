4ae740f3fd09ad493c84a3983f826287db007f5b
Olga Vrousgou <olgavrou@gmail.com>
2021-01-19 16:07:09 +0000

    fix cbify tests

--- vowpalwabbit/cbify.cc
+++ vowpalwabbit/cbify.cc
@@ -747,11 +747,13 @@ base_learner* cbify_setup(options_i& options, vw& all)
     {
       l = &init_cost_sensitive_learner(
           data, base, predict_or_learn_adf<true, true>, predict_or_learn_adf<false, true>, all.example_parser, 1);
+      all.label_type = label_type_t::cs;
     }
     else
     {
       l = &init_multiclass_learner(
           data, base, predict_or_learn_adf<true, false>, predict_or_learn_adf<false, false>, all.example_parser, 1);
+      all.label_type = label_type_t::mc;
     }
   }
   else
@@ -778,11 +780,13 @@ base_learner* cbify_setup(options_i& options, vw& all)
     {
       l = &init_cost_sensitive_learner(
           data, base, predict_or_learn<true, true>, predict_or_learn<false, true>, all.example_parser, 1);
+      all.label_type = label_type_t::cs;
     }
     else
     {
       l = &init_multiclass_learner(
           data, base, predict_or_learn<true, false>, predict_or_learn<false, false>, all.example_parser, 1);
+      all.label_type = label_type_t::mc;
     }
   }
   all.delete_prediction = nullptr;
