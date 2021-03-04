b2b69dd3d047505b762e381d3e8db55d7134729e
Rajan Chari <ranaras@microsoft.com>
2021-03-04 00:37:59 -0500

    more merge changes v_array changed to vector

--- vowpalwabbit/gen_cs_example.cc
+++ vowpalwabbit/gen_cs_example.cc
@@ -168,14 +168,13 @@ void gen_cs_example_sm(multi_ex&, uint32_t chosen_action, float sign_offset, ACT
   }
 }
 
-void cs_prep_labels(multi_ex& examples, v_array<CB::label>& cb_labels, COST_SENSITIVE::label& cs_labels,
-    v_array<COST_SENSITIVE::label>& prepped_cs_labels, uint64_t offset)
+void cs_prep_labels(multi_ex& examples, std::vector<CB::label>& cb_labels, COST_SENSITIVE::label& cs_labels,
+    std::vector<COST_SENSITIVE::label>& prepped_cs_labels, uint64_t offset)
 {
   cb_labels.clear();
   if (prepped_cs_labels.size() < cs_labels.costs.size() + 1)
   {
     prepped_cs_labels.resize(cs_labels.costs.size() + 1);
-    prepped_cs_labels.end() = prepped_cs_labels.end_array;
   }
 
   size_t index = 0;
