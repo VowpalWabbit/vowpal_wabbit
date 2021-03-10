838a8f2b4115470f4e716a61e18553ab0f1e59ce
Jack Gerrits <jack@jackgerrits.com>
2021-03-10 13:26:19 -0500

    formatting

--- vowpalwabbit/conditional_contextual_bandit.cc
+++ vowpalwabbit/conditional_contextual_bandit.cc
@@ -373,7 +373,9 @@ void learn_or_predict(ccb& data, multi_learner& base, multi_ex& examples)
   auto restore_labels_guard = VW::scope_exit([&data, &examples] {
     // Restore ccb labels to the example objects.
     for (size_t i = 0; i < examples.size(); i++)
-    { examples[i]->l.conditional_contextual_bandit = std::move(data.stored_labels[i]); }
+    {
+      examples[i]->l.conditional_contextual_bandit = std::move(data.stored_labels[i]);
+    }
   });
 
   if (data.slots.size() > data.actions.size())
@@ -410,9 +412,7 @@ void learn_or_predict(ccb& data, multi_learner& base, multi_ex& examples)
 
   // This will overwrite the labels with CB.
   create_cb_labels(data);
-  auto delete_cb_labels_guard = VW::scope_exit([&data, &examples] {
-    delete_cb_labels(data);
-  });
+  auto delete_cb_labels_guard = VW::scope_exit([&data, &examples] { delete_cb_labels(data); });
 
   // this is temporary only so we can get some logging of what's going on
   try
