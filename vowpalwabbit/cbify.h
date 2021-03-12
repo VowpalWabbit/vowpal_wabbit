8e06d76b3bc3476ca391b2be72a7d3f7d67836cc
Eduardo Salinas <edus@microsoft.com>
2021-03-12 15:24:09 +0000

    cbify_adf_data refactor

--- vowpalwabbit/cbify.h
+++ vowpalwabbit/cbify.h
@@ -4,6 +4,25 @@
 
 #pragma once
 #include "reductions_fwd.h"
+#include "example.h"
+
+template <typename T>
+void v_move(v_array<T>& dst, v_array<T>& src)
+{
+  dst = src;
+  src = v_init<T>();
+}
+
+struct cbify_adf_data
+{
+  multi_ex ecs;
+  size_t num_actions;
+
+  void init_adf_data(const std::size_t num_actions, namespace_interactions& interactions);
+  void copy_example_to_adf(parameters& weights, example& ec);
+
+  ~cbify_adf_data();
+};
 
 VW::LEARNER::base_learner* cbify_setup(VW::config::options_i& options, vw& all);
 VW::LEARNER::base_learner* cbifyldf_setup(VW::config::options_i& options, vw& all);
