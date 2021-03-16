4dca649c5d9e93f63c3c66ec3626551ccd5bd13c
Jack Gerrits <jack@jackgerrits.com>
2021-03-16 15:09:15 -0400

    Hide internal variables. Don't let begin or end be modified

--- vowpalwabbit/v_array.h
+++ vowpalwabbit/v_array.h
@@ -75,12 +75,10 @@ private:
     memmove(&_begin[idx + width], &_begin[idx], (size() - (idx + width)) * sizeof(T));
   }
 
-public:
-  // private:
+
+private:
   T* _begin;
   T* _end;
-
-public:
   T* end_array;
 
 private:
@@ -96,8 +94,8 @@ public:
   using const_iterator = const value_type*;
 
   // enable C++ 11 for loops
-  inline iterator& begin() { return _begin; }
-  inline iterator& end() { return _end; }
+  inline iterator begin() { return _begin; }
+  inline iterator end() { return _end; }
 
   inline const_iterator begin() const { return _begin; }
   inline const_iterator end() const { return _end; }
