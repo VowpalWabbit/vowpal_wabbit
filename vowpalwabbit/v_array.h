b04fcc547e88c8cf564b6a322abd994df347c410
Jack Gerrits <jack@jackgerrits.com>
2021-03-21 15:21:11 -0400

    v_array: rename end_array -> _end_array

--- vowpalwabbit/v_array.h
+++ vowpalwabbit/v_array.h
@@ -46,7 +46,7 @@ private:
     }
     _begin = nullptr;
     _end = nullptr;
-    end_array = nullptr;
+    _end_array = nullptr;
     _erase_count = 0;
   }
 
@@ -63,8 +63,8 @@ private:
     }
 
     _end = _begin + std::min(old_len, length);
-    end_array = _begin + length;
-    memset(_end, 0, (end_array - _end) * sizeof(T));
+    _end_array = _begin + length;
+    memset(_end, 0, (_end_array - _end) * sizeof(T));
   }
 
   // This will move all elements after idx by width positions and reallocate the underlying buffer if needed.
@@ -77,7 +77,7 @@ private:
 
   T* _begin;
   T* _end;
-  T* end_array;
+  T* _end_array;
   size_t _erase_count;
 
 public:
@@ -99,7 +99,7 @@ public:
   inline const_iterator cbegin() const { return _begin; }
   inline const_iterator cend() const { return _end; }
 
-  v_array() noexcept : _begin(nullptr), _end(nullptr), end_array(nullptr), _erase_count(0) {}
+  v_array() noexcept : _begin(nullptr), _end(nullptr), _end_array(nullptr), _erase_count(0) {}
   ~v_array() { delete_v_array(); }
 
   v_array(v_array<T>&& other) noexcept
@@ -107,11 +107,11 @@ public:
     _erase_count = 0;
     _begin = nullptr;
     _end = nullptr;
-    end_array = nullptr;
+    _end_array = nullptr;
 
     std::swap(_begin, other._begin);
     std::swap(_end, other._end);
-    std::swap(end_array, other.end_array);
+    std::swap(_end_array, other._end_array);
     std::swap(_erase_count, other._erase_count);
   }
 
@@ -119,7 +119,7 @@ public:
   {
     std::swap(_begin, other._begin);
     std::swap(_end, other._end);
-    std::swap(end_array, other.end_array);
+    std::swap(_end_array, other._end_array);
     std::swap(_erase_count, other._erase_count);
     return *this;
   }
@@ -128,7 +128,7 @@ public:
   {
     _begin = nullptr;
     _end = nullptr;
-    end_array = nullptr;
+    _end_array = nullptr;
     _erase_count = 0;
 
     // TODO this should use the other version when T is trivially copyable and this otherwise.
@@ -170,12 +170,12 @@ public:
   void decr() { _end--; }
   void incr()
   {
-    if (_end == end_array) resize(2 * capacity() + 3);
+    if (_end == _end_array) resize(2 * capacity() + 3);
     _end++;
   }
   T& operator[](size_t i) const { return _begin[i]; }
   inline size_t size() const { return _end - _begin; }
-  inline size_t capacity() const { return end_array - _begin; }
+  inline size_t capacity() const { return _end_array - _begin; }
 
   // maintain the original (deprecated) interface for compatibility. To be removed in VW 10
   //   VW_DEPRECATED(
@@ -321,7 +321,7 @@ public:
 
   void push_back(const T& new_ele)
   {
-    if (_end == end_array) resize(2 * capacity() + 3);
+    if (_end == _end_array) resize(2 * capacity() + 3);
     new (_end++) T(new_ele);
   }
 
@@ -330,7 +330,7 @@ public:
   template <class... Args>
   void emplace_back(Args&&... args)
   {
-    if (_end == end_array) resize(2 * capacity() + 3);
+    if (_end == _end_array) resize(2 * capacity() + 3);
     new (_end++) T(std::forward<Args>(args)...);
   }
 
@@ -367,7 +367,7 @@ public:
 
     if (!contain_sorted(new_ele, index))
     {
-      if (_end == end_array) resize(2 * capacity() + 3);
+      if (_end == _end_array) resize(2 * capacity() + 3);
 
       to_move = size - index;
 
