28fa67c39398584e87379c5b12b1c0e71c8d0dd5
Jack Gerrits <jack@jackgerrits.com>
2021-03-24 08:58:51 -0400

    Deprecate incr and decr

--- vowpalwabbit/v_array.h
+++ vowpalwabbit/v_array.h
@@ -168,7 +168,11 @@ public:
   T last() const { return *(_end - 1); }
 
   bool empty() const { return _begin == _end; }
+
+  VW_DEPRECATED("v_array::decr() is deprecated")
   void decr() { _end--; }
+
+  VW_DEPRECATED("v_array::incr() is deprecated")
   void incr()
   {
     if (_end == _end_array) reserve_nocheck(2 * capacity() + 3);
