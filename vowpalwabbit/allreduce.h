44fe27360847c4f8294b44f2d6d474a87503fafd
Jack Gerrits <jackgerrits95@gmail.com>
2021-04-01 21:10:01 -0400

    Try change without cli guard

--- vowpalwabbit/allreduce.h
+++ vowpalwabbit/allreduce.h
@@ -25,9 +25,6 @@ namespace std
 template <typename T>
 class promise;
 
-class condition_variable;
-
-class mutex;
 }  // namespace std
 #else
 #  include <sys/socket.h>
@@ -46,6 +43,22 @@ typedef int socket_t;
 #include "vw_exception.h"
 #include "vwvis.h"
 #include <cassert>
+
+// // mutex, condition_variable cannot be used in managed C++, tell the compiler that this is unmanaged even if included in a managed project.
+// #ifdef _M_CEE
+// #  pragma managed(push, off)
+// #  undef _M_CEE
+// #  include <mutex>
+// #  include <condition_variable>
+// #  define _M_CEE 001
+// #  pragma managed(pop)
+// #else
+// #  include <mutex>
+// #  include <condition_variable>
+// #endif
+
+#  include <mutex>
+#  include <condition_variable>
 
 constexpr size_t ar_buf_size = 1 << 16;
 
