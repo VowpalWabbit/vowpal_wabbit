2e4bb4d9a3c123e2580e024f7ea2799ca1e15ecc
olgavrou <olgavrou@gmail.com>
2021-04-06 13:46:37 +0100

    fix: [Version update to 8.10.1] make example_is_newline check depend on bool instead of internal example characteristics (#2890)

--- vowpalwabbit/parser.cc
+++ vowpalwabbit/parser.cc
@@ -859,6 +859,7 @@ void empty_example(vw& /*all*/, example& ec)
   ec.tag.clear();
   ec.sorted = false;
   ec.end_pass = false;
+  ec.is_newline = false;
   ec._reduction_features.clear();
   ec.initial = 0.f;
 }
