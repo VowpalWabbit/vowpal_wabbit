b34550cbcb46bca16e1b48dc885fa6d0f84b2d11
Jack Gerrits <jackgerrits95@gmail.com>
2021-04-07 09:47:57 -0400

    Bandaid fix for slimvw

--- vowpalwabbit/simple_label.h
+++ vowpalwabbit/simple_label.h
@@ -2,8 +2,6 @@
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
 #pragma once
-#include "label_parser.h"
-#include <cmath>
 
 struct example;
 struct vw;
@@ -32,9 +30,6 @@ struct simple_label_reduction_features
 };
 
 void return_simple_example(vw& all, void*, example& ec);
-
-extern label_parser simple_label_parser;
-
 bool summarize_holdout_set(vw& all, size_t& no_win_counter);
 void print_update(vw& all, example& ec);
 void output_and_account_example(vw& all, example& ec);
