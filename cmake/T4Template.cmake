eeed09c8f201dd0af5c64ad97dd26b8db8c76195
Jacob Alber <jacob.alber@microsoft.com>
2021-04-08 01:46:16 -0400

    Update T4Template utility to run T4 tool on configure
    * This takes the generated sources of out the sourcetree

--- cmake/T4Template.cmake
+++ cmake/T4Template.cmake
@@ -1,3 +1,5 @@
+find_program(netfx_t4_COMMAND TextTransform.exe)
+
 # A function to set up T4 Templates and their generated code files
 #
 # t4_templates         : A list of T4 Template Files to annotate in the project
@@ -9,23 +11,38 @@ function(configure_t4 t4_templates
   set(generated_files)
 
   foreach(t4_TEMPLATE IN ITEMS ${t4_templates})
-    string(REGEX REPLACE "^(.+)/(.+).tt$" "\\2.tt" t4_TEMPLATE_FILENAME ${t4_TEMPLATE})
-    string(REGEX REPLACE "^(.+)\\.tt$" "\\1.cs" t4_TARGET ${t4_TEMPLATE})
-    string(REGEX REPLACE "^(.+)/(.+).tt$" "\\2.cs" t4_TARGET_FILENAME ${t4_TEMPLATE})
-    message(VERBOSE "Processing ${t4_TEMPLATE} => ${t4_TARGET}")
+    if (${t4_TEMPLATE} MATCHES "^(.*/)?([^/]+).tt$")
+      if (${CMAKE_MATCH_COUNT} EQUAL 2)
+        set(t4_TARGET_DIR ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_MATCH_1})
+        set(t4_TARGET_FILENAME ${CMAKE_MATCH_2}.cs)
+        set(t4_TEMPLATE_FILENAME ${CMAKE_MATCH_2}.tt)
+      elseif (${CMAKE_MATCH_COUNT} EQUAL 1)
+        set(t4_TARGET_FILENAME ${CMAKE_MATCH_1}.cs)
+        set(t4_TEMPLATE_FILENAME ${CMAKE_MATCH_1}.tt)
+      endif()
+
+      set(t4_TARGET ${t4_TARGET_DIR}${t4_TARGET_FILENAME})
+    endif()
+        
+    message(STATUS "Processing ${t4_TEMPLATE} => ${t4_TARGET}")
+
+    file(MAKE_DIRECTORY ${t4_TARGET_DIR})
+
+    exec_program(${netfx_t4_COMMAND}
+      ARGS "-out ${t4_TARGET}" "${CMAKE_CURRENT_SOURCE_DIR}/${t4_TEMPLATE}")
 
     set(generated_files ${generated_files} ${t4_TARGET})
 
     set_source_files_properties(${t4_TARGET}
-        PROPERTIES
-        VS_CSHARP_AutoGen "True"
-        VS_CSHARP_DesignTime "True"
-        VS_CSHARP_DependentUpon ${t4_TEMPLATE_FILENAME})
+      PROPERTIES
+      VS_CSHARP_AutoGen "True"
+      VS_CSHARP_DesignTime "True"
+      VS_CSHARP_DependentUpon ${t4_TEMPLATE_FILENAME})
 
     set_source_files_properties(${t4_TEMPLATE}
-        PROPERTIES
-        VS_CSHARP_Generator "TextTemplatingFileGenerator"
-        VS_CSHARP_LastGenOutput ${t4_TARGET_FILENAME})
+      PROPERTIES
+      VS_CSHARP_Generator "TextTemplatingFileGenerator"
+      VS_CSHARP_LastGenOutput ${t4_TARGET_FILENAME})
   endforeach()
 
   set(${generated_files_var} ${${generated_files_var}} ${generated_files} PARENT_SCOPE)
