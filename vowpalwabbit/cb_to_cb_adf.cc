24f81b8b3962e71c16bd526c534228e63a8c49fd
Eduardo Salinas <edus@microsoft.com>
2021-04-16 14:45:16 +0000

    cwcb2cbadf returns pred when learning

--- vowpalwabbit/cb_to_cb_adf.cc
+++ vowpalwabbit/cb_to_cb_adf.cc
@@ -184,14 +184,14 @@ VW::LEARNER::base_learner* cb_to_cb_adf_setup(options_i& options, vw& all)
     data->adf_learner = as_multiline(base->get_learner_by_name_prefix("cb_explore_adf_"));
 
     l = &init_learner(data, base, predict_or_learn<true>, predict_or_learn<false>, 1, prediction_type_t::action_probs,
-        "cb_to_cb_adf", base->learn_returns_prediction);
+        "cb_to_cb_adf", true);
   }
   else
   {
     data->adf_learner = as_multiline(base->get_learner_by_name_prefix("cb_adf"));
 
     l = &init_learner(data, base, predict_or_learn<true>, predict_or_learn<false>, 1, prediction_type_t::multiclass,
-        "cb_to_cb_adf", base->learn_returns_prediction);
+        "cb_to_cb_adf", true);
   }
 
   l->set_finish_example(finish_example);
