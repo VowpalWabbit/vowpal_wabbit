2976ff15c08272e0df3469dffa4649de32f4a280
Jack Gerrits <jack@jackgerrits.com>
2021-05-12 14:42:04 -0400

    quadratic fast path

--- vowpalwabbit/generate_interactions.h
+++ vowpalwabbit/generate_interactions.h
@@ -22,10 +22,48 @@ inline bool contains_wildcard(const std::vector<namespace_index>& interaction)
   return std::find(interaction.begin(), interaction.end(), wildcard_namespace) != interaction.end();
 }
 
+inline bool sort_interactions_comparator(const std::vector<namespace_index>& a, const std::vector<namespace_index>& b)
+{
+  if (a.size() != b.size()) { return a.size() > b.size(); }
+  for (size_t i = 0; i < a.size(); i++)
+  {
+    if (a[i] < b[i])
+      return true;
+    else if (a[i] == b[i])
+      continue;
+    else
+      return false;
+  }
+  return false;
+}
+
+inline std::vector<std::vector<namespace_index>> expand_quadratics_wildcard_interactions(bool leave_duplicate_interactions, const std::set<namespace_index>& new_example_indices)
+{
+  std::vector<std::vector<namespace_index>> interactions;
+
+  for (auto it = new_example_indices.begin(); it != new_example_indices.end(); ++it)
+  {
+    auto idx1 = *it;
+    interactions.push_back({idx1, idx1});
+
+    for (auto jt = it; jt != new_example_indices.end(); ++jt)
+    {
+      auto idx2 = *jt;
+      interactions.push_back({idx1, idx2});
+      interactions.push_back({idx2, idx2});
+      if (leave_duplicate_interactions)
+      {
+        interactions.push_back({idx2, idx1});
+      }
+    }
+  }
+  return interactions;
+}
+
 using generate_func_t = std::vector<std::vector<namespace_index>>(
     const std::set<namespace_index>& namespaces, size_t num_to_pick);
 
-template <generate_func_t generate_func>
+template <generate_func_t generate_func, bool leave_duplicate_interactions>
 std::vector<std::vector<namespace_index>> compile_interaction(
     const std::vector<namespace_index>& interaction, const std::set<namespace_index>& indices)
 {
@@ -45,7 +83,8 @@ std::vector<std::vector<namespace_index>> compile_interaction(
     }
   }
 
-  auto result = generate_func(indices, num_wildcards);
+  // Quadratic fast path or generic generation function.
+  auto result = num_wildcards == 2 ? expand_quadratics_wildcard_interactions(leave_duplicate_interactions, indices) : generate_func(indices, num_wildcards);
   for (size_t i = 0; i < insertion_indices.size(); i++)
   {
     for (auto& res : result) { res.insert(res.begin() + insertion_indices[i], insertion_ns[i]); }
@@ -63,7 +102,7 @@ std::vector<std::vector<namespace_index>> compile_interactions(
   {
     if (contains_wildcard(inter))
     {
-      auto compiled = compile_interaction<generate_func>(inter, indices);
+      auto compiled = compile_interaction<generate_func, leave_duplicate_interactions>(inter, indices);
       std::copy(compiled.begin(), compiled.end(), std::back_inserter(final_interactions));
     }
     else
