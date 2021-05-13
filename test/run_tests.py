47fb512b9a058c6371bd0db2d8387eaa5159f546
Eduardo Salinas <edus@microsoft.com>
2021-05-13 15:23:36 +0000

    ignore test

--- test/run_tests.py
+++ test/run_tests.py
@@ -593,10 +593,10 @@ def convert_tests_for_flatbuffers(tests, to_flatbuff, working_dir, color_enum):
             print("{}Skipping test {} for flatbuffers, --help test{}".format(color_enum.LIGHT_CYAN, test_id, color_enum.ENDC))
             test['skip'] = True
             continue
-        #todo: understand why is it failing
-        # test 189, 312 and 316 depend on dsjson parser behaviour
+        #todo: 300 understand why is it failing
+        # test 189, 312, 316 and 317 depend on dsjson parser behaviour
         # they can be enabled if we ignore diffing the --extra_metrics
-        if str(test_id) in ('300', '189', '312', '316'):
+        if str(test_id) in ('300', '189', '312', '316', '317'):
             continue
 
         # test id is being used as an index here, not necessarily a contract
