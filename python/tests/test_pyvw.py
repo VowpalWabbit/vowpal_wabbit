bcab66d9f9924087494bbaa49ad0bd98547b3540
Eduardo Salinas <edus@microsoft.com>
2021-05-25 13:21:12 +0000

    check float type

--- python/tests/test_pyvw.py
+++ python/tests/test_pyvw.py
@@ -433,7 +433,7 @@ def test_dsjson():
 def test_dsjson_with_metrics():
     vw = pyvw.vw('--extra_metrics metrics.json --cb_explore_adf --epsilon 0.2 --dsjson')
 
-    ex_l_str='{"_label_cost":-1.0,"_label_probability":0.5,"_label_Action":1,"_labelIndex":0,"o":[{"v":1.0,"EventId":"38cbf24f-70b2-4c76-aa0c-970d0c8d388e","ActionTaken":false}],"Timestamp":"2020-11-15T17:09:31.8350000Z","Version":"1","EventId":"38cbf24f-70b2-4c76-aa0c-970d0c8d388e","a":[1,2],"c":{ "GUser":{"id":"person5","major":"engineering","hobby":"hiking","favorite_character":"spock"}, "_multi": [ { "TAction":{"topic":"SkiConditions-VT"} }, { "TAction":{"topic":"HerbGarden"} } ] },"p":[0.5,0.5],"VWState":{"m":"N/A"}}\n'
+    ex_l_str='{"_label_cost":-0.9,"_label_probability":0.5,"_label_Action":1,"_labelIndex":0,"o":[{"v":1.0,"EventId":"38cbf24f-70b2-4c76-aa0c-970d0c8d388e","ActionTaken":false}],"Timestamp":"2020-11-15T17:09:31.8350000Z","Version":"1","EventId":"38cbf24f-70b2-4c76-aa0c-970d0c8d388e","a":[1,2],"c":{ "GUser":{"id":"person5","major":"engineering","hobby":"hiking","favorite_character":"spock"}, "_multi": [ { "TAction":{"topic":"SkiConditions-VT"} }, { "TAction":{"topic":"HerbGarden"} } ] },"p":[0.5,0.5],"VWState":{"m":"N/A"}}\n'
     ex_l = vw.parse(ex_l_str)
     vw.learn(ex_l)
     pred = ex_l[0].get_action_scores()
@@ -457,8 +457,8 @@ def test_dsjson_with_metrics():
     assert(learner_metric_dict["cbea_predict_in_learn"] == 0)
     assert(learner_metric_dict["cbea_label_first_action"] == 1)
     assert(learner_metric_dict["cbea_label_not_first"] == 0)
-    assert(learner_metric_dict["cbea_sum_cost"] == -1.0)
-    assert(learner_metric_dict["cbea_sum_cost_baseline"] == -1.0)
+    assert(pytest.approx(learner_metric_dict["cbea_sum_cost"]) == -0.9)
+    assert(pytest.approx(learner_metric_dict["cbea_sum_cost_baseline"]) == -0.9)
     assert(len(vw.get_learner_metrics()) == 8)
 
 def test_constructor_exception_is_safe():
