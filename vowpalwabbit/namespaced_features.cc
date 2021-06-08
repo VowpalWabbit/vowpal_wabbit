9f37708fcd4bd1ae4d9b313c46e59d7188dbc869
Jack Gerrits <jackgerrits@users.noreply.github.com>
2021-06-08 14:17:11 -0400

    feat: add several functions to namespaced_features (#3040)
    * feat: add several functions to namespaced_features
    
    * Add files i forgot to add in the last commit
    
    * Change VW_NOEXCEPT behavior
    
    * Ifdef in header

--- vowpalwabbit/namespaced_features.cc
+++ vowpalwabbit/namespaced_features.cc
@@ -23,13 +23,20 @@ const features* namespaced_features::get_feature_group(uint64_t hash) const
   return &_feature_groups[it->second];
 }
 
-std::vector<namespace_index> namespaced_features::get_indices() const
+const std::set<namespace_index>& namespaced_features::get_indices() const { return _contained_indices; }
+
+namespace_index namespaced_features::get_index_for_hash(uint64_t hash) const
 {
-  auto indices_copy = _namespace_indices;
-  std::sort(indices_copy.begin(), indices_copy.end());
-  auto last = std::unique(indices_copy.begin(), indices_copy.end());
-  indices_copy.erase(last, indices_copy.end());
-  return indices_copy;
+  auto it = _hash_to_index_mapping.find(hash);
+  if (it == _hash_to_index_mapping.end())
+  {
+#ifdef VW_NOEXCEPT
+    return {};
+#else
+    THROW("No index found for hash: " << hash);
+#endif
+  }
+  return _namespace_indices[it->second];
 }
 
 std::pair<namespaced_features::indexed_iterator, namespaced_features::indexed_iterator>
@@ -55,28 +62,33 @@ features& namespaced_features::get_or_create_feature_group(uint64_t hash, namesp
     auto new_index = _feature_groups.size() - 1;
     _hash_to_index_mapping[hash] = new_index;
     _legacy_indices_to_index_mapping[ns_index].push_back(new_index);
+    // If size is 1, that means this is the first time the ns_index is added and we should add it to the set.
+    if (_legacy_indices_to_index_mapping[ns_index].size() == 1) { _contained_indices.insert(ns_index); }
     existing_group = &_feature_groups.back();
   }
 
   return *existing_group;
 }
 
+// This operation is only allowed in code that allows exceptions.
+// get_feature_group should be used instead for noexcept code
+#ifndef VW_NOEXCEPT
 const features& namespaced_features::operator[](uint64_t hash) const
 {
   auto* existing_group = get_feature_group(hash);
-#ifndef VW_NOEXCEPT
   if (existing_group == nullptr) { THROW("No group found for hash: " << hash); }
-#endif
   return *existing_group;
 }
+#endif
+
+#ifndef VW_NOEXCEPT
 features& namespaced_features::operator[](uint64_t hash)
 {
   auto* existing_group = get_feature_group(hash);
-#ifndef VW_NOEXCEPT
   if (existing_group == nullptr) { THROW("No group found for hash: " << hash); }
-#endif
   return *existing_group;
 }
+#endif
 
 void namespaced_features::remove_feature_group(uint64_t hash)
 {
@@ -106,7 +118,12 @@ void namespaced_features::remove_feature_group(uint64_t hash)
   // If any groups are left empty, remove them.
   for (auto it = _legacy_indices_to_index_mapping.begin(); it != _legacy_indices_to_index_mapping.end();)
   {
-    if (it->second.empty()) { it = _legacy_indices_to_index_mapping.erase(it); }
+    if (it->second.empty())
+    {
+      // There are no more feature groups which correspond to this index.
+      _contained_indices.erase(it->first);
+      it = _legacy_indices_to_index_mapping.erase(it);
+    }
     else
     {
       ++it;
@@ -117,6 +134,16 @@ void namespaced_features::remove_feature_group(uint64_t hash)
   {
     if (kv.second > existing_index) { kv.second -= 1; }
   }
+}
+
+void namespaced_features::clear()
+{
+  _feature_groups.clear();
+  _namespace_indices.clear();
+  _namespace_hashes.clear();
+  _legacy_indices_to_index_mapping.clear();
+  _hash_to_index_mapping.clear();
+  _contained_indices.clear();
 }
 
 generic_range<namespaced_features::indexed_iterator> namespaced_features::namespace_index_range(
