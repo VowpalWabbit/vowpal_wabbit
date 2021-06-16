171da54a8e0554b2065a8a5be5c269fd5668e803
Jack Gerrits <jackgerrits95@gmail.com>
2021-06-16 12:29:11 -0400

    refactor remove old complexity

--- vowpalwabbit/interactions_predict.h
+++ vowpalwabbit/interactions_predict.h
@@ -47,21 +47,28 @@ inline void call_FuncT(DataT& dat, WeightsT& /*weights*/, const float ft_value, 
 // contains N feature_gen_data records (where N is length of interaction)
 struct feature_gen_data
 {
-  size_t loop_idx;  // current feature id in namespace
-  uint64_t hash;    // hash of feature interactions of previous namespaces in the list
-  float x;          // value of feature interactions of previous namespaces in the list
-  size_t loop_end;  // last feature id. May be less than number of features if namespace involved in interaction more
-                    // than once calculated at preprocessing together with same_ns
-  size_t self_interaction;  // namespace interacting with itself
-  features* ft_arr;
-  //    feature_gen_data(): loop_idx(0), x(1.), loop_end(0), self_interaction(false) {}
+  // current feature id in namespace
+  size_t loop_idx = 0;
+
+  // hash of feature interactions of previous namespaces in the list
+  uint64_t hash;
+  // value of feature interactions of previous namespaces in the list
+  float x = 1.f;
+
+  // last feature id. May be less than number of features if namespace involved in interaction more
+  // than once calculated at preprocessing together with same_ns
+  size_t loop_end = 0;
+
+  // namespace interacting with itself
+  bool self_interaction = false;
+  features* ft_arr = nullptr;
 };
 
 // The inline function below may be adjusted to change the way
 // synthetic (interaction) features' values are calculated, e.g.,
 // fabs(value1-value2) or even value1>value2?1.0:-1.0
 // Beware - its result must be non-zero.
-inline float INTERACTION_VALUE(float value1, float value2) { return value1 * value2; }
+constexpr inline float interaction_value(float value1, float value2) { return value1 * value2; }
 
 // uncomment line below to disable usage of inner 'for' loops for pair and triple interactions
 // end switch to usage of non-recursive feature generation algorithm for interactions of any length
@@ -79,7 +86,7 @@ inline void inner_kernel(DataT& dat, features::const_audit_iterator& begin, feat
     {
       audit_func(dat, begin.audit() == nullptr ? &EMPTY_AUDIT_STRINGS : begin.audit()->get());
       call_FuncT<DataT, FuncT>(
-          dat, weights, INTERACTION_VALUE(ft_value, begin.value()), (begin.index() ^ halfhash) + offset);
+          dat, weights, interaction_value(ft_value, begin.value()), (begin.index() ^ halfhash) + offset);
       audit_func(dat, nullptr);
     }
   }
@@ -87,7 +94,7 @@ inline void inner_kernel(DataT& dat, features::const_audit_iterator& begin, feat
   {
     for (; begin != end; ++begin)
       call_FuncT<DataT, FuncT>(
-          dat, weights, INTERACTION_VALUE(ft_value, begin.value()), (begin.index() ^ halfhash) + offset);
+          dat, weights, interaction_value(ft_value, begin.value()), (begin.index() ^ halfhash) + offset);
   }
 }
 
@@ -110,12 +117,6 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
 
   // statedata for generic non-recursive iteration
   v_array<feature_gen_data> state_data;
-
-  feature_gen_data empty_ns_data;  // micro-optimization. don't want to call its constructor each time in loop.
-  empty_ns_data.loop_idx = 0;
-  empty_ns_data.x = 1.;
-  empty_ns_data.loop_end = 0;
-  empty_ns_data.self_interaction = false;
 
   for (const auto& ns : interactions)
   {  // current list of namespaces to interact.
@@ -145,7 +146,7 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
             // next index differs for permutations and simple combinations
             feature_value ft_value = first.values[i];
             auto begin = second.audit_cbegin();
-            if (same_namespace) { begin += (PROCESS_SELF_INTERACTIONS(ft_value)) ? i : i + 1; }
+            if (same_namespace) { begin += i; }
             auto end = second.audit_cend();
             num_features += std::distance(begin, end);
             inner_kernel<DataT, WeightOrIndexT, FuncT, audit, audit_func>(
@@ -177,8 +178,10 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
               const uint64_t halfhash1 = FNV_prime * static_cast<uint64_t>(first.indicies[i]);
               float first_ft_value = first.values[i];
               size_t j = 0;
-              if (same_namespace1)  // next index differs for permutations and simple combinations
-                j = (PROCESS_SELF_INTERACTIONS(first_ft_value)) ? i : i + 1;
+              if (same_namespace1)
+              {
+                j = i;
+              }
 
               for (; j < second.indicies.size(); ++j)
               {  // f3 x k*(f2 x k*f1)
@@ -187,11 +190,11 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
                   audit_func(dat, j < second.space_names.size() ? second.space_names[j].get() : &EMPTY_AUDIT_STRINGS);
                 }
                 feature_index halfhash = FNV_prime * (halfhash1 ^ static_cast<uint64_t>(second.indicies[j]));
-                feature_value ft_value = INTERACTION_VALUE(first_ft_value, second.values[j]);
+                feature_value ft_value = interaction_value(first_ft_value, second.values[j]);
 
                 auto begin = third.audit_cbegin();
                 // next index differs for permutations and simple combinations
-                if (same_namespace2) { begin += (PROCESS_SELF_INTERACTIONS(ft_value)) ? j : j + 1; }
+                if (same_namespace2) { begin += j; }
                 auto end = third.audit_cend();
                 num_features += std::distance(begin, end);
                 inner_kernel<DataT, WeightOrIndexT, FuncT, audit, audit_func>(
@@ -226,7 +229,7 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
 
         if (fgd == state_data.end())
         {
-          state_data.push_back(empty_ns_data);
+          state_data.push_back(feature_gen_data{});
           fgd = state_data.end() - 1;  // reassign as memory could be realloced
         }
 
@@ -238,42 +241,17 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
       // if any of interacting namespace has 0 features - whole interaction is skipped
       if (must_skip_interaction) continue;  // no_data_to_interact
 
-      if (!permutations)  // adjust state_data for simple combinations
-      {                   // if permutations mode is disabeled then namespaces in ns are already sorted and thus grouped
+      if (!permutations)
+      {
+        // adjust state_data for simple combinations
+        // if permutations mode is disabeled then namespaces in ns are already sorted and thus grouped
         // (in fact, currently they are sorted even for enabled permutations mode)
-        // let's go throw the list and calculate number of features to skip in namespaces which
-        // repeated more than once to generate only simple combinations of features
-
-        size_t margin = 0;  // number of features to ignore if namespace has been seen before
-
-        // iterate list backward as margin grows in this order
-
         for (fgd = state_data.end() - 1; fgd > state_data.begin(); --fgd)
         {
           fgd2 = fgd - 1;
           fgd->self_interaction = (fgd->ft_arr == fgd2->ft_arr);  // state_data.begin().self_interaction is always false
-          if (fgd->self_interaction)
-          {
-            size_t& loop_end = fgd2->loop_end;
-
-            if (!PROCESS_SELF_INTERACTIONS((*fgd2->ft_arr).values[loop_end - margin]))
-            {
-              ++margin;  // otherwise margin can't be increased
-              must_skip_interaction = loop_end < margin;
-              if (must_skip_interaction) break;
-            }
-
-            if (margin != 0) loop_end -= margin;  // skip some features and increase margin
-          }
-          else if (margin != 0)
-            margin = 0;
         }
-
-        // if impossible_without_permutations == true then we faced with case like interaction 'aaaa'
-        // where namespace 'a' contains less than 4 unique features. It's impossible to make simple
-        // combination of length 4 without repetitions from 3 or less elements.
-        if (must_skip_interaction) continue;  // impossible_without_permutations
-      }                                       // end of state_data adjustment
+      }
 
       fgd = state_data.begin();     // always equal to first ns
       fgd2 = state_data.end() - 1;  // always equal to last ns
@@ -298,12 +276,9 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
           features& fs = *(cur_data->ft_arr);
 
           if (next_data->self_interaction)
-          {  // if next namespace is same, we should start with loop_idx + 1 to avoid feature interaction with itself
-            // unless feature has value x and x != x*x. E.g. x != 0 and x != 1. Features with x == 0 are already
-            // filtered out in parce_args.cc::maybeFeature().
-
-            next_data->loop_idx =
-                (PROCESS_SELF_INTERACTIONS(fs.values[feature])) ? cur_data->loop_idx : cur_data->loop_idx + 1;
+          {
+            // if next namespace is same, we should start with loop_idx to avoid feature interaction with features already interacted.
+            next_data->loop_idx = cur_data->loop_idx;
           }
           else
             next_data->loop_idx = 0;
@@ -318,7 +293,7 @@ inline void generate_interactions(const std::vector<std::vector<namespace_index>
           else
           {  // feature2 xor (16777619*feature1)
             next_data->hash = FNV_prime * (cur_data->hash ^ static_cast<uint64_t>(fs.indicies[feature]));
-            next_data->x = INTERACTION_VALUE(fs.values[feature], cur_data->x);
+            next_data->x = interaction_value(fs.values[feature], cur_data->x);
           }
 
           ++cur_data;
