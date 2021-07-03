3e14c62e98a7de4795aa879ec9f14765a8f2b587
Jack Gerrits <jack@jackgerrits.com>
2021-07-03 09:13:24 -0400

    Formatting

--- vowpalwabbit/lda_core.cc
+++ vowpalwabbit/lda_core.cc
@@ -162,9 +162,9 @@ namespace
 {
 inline bool is_aligned16(void *ptr)
 {
-#if BOOST_VERSION >= 105600
+#    if BOOST_VERSION >= 105600
   return boost::alignment::is_aligned(16, ptr);
-#else
+#    else
   return ((reinterpret_cast<uintptr_t>(ptr) & 0x0f) == 0);
 #endif
 }
