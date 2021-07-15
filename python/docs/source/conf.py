624daedb68b4546d16dd6902255cc80e16626a69
Jack Gerrits <jack@jackgerrits.com>
2021-07-14 20:32:47 -0400

    fix headers, move assets, copy assets to build

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -100,6 +100,9 @@ html_context = {
     "doc_path": "python/docs/source",
 }
 
+html_extra_path = ["tutorials/assets/"]
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
