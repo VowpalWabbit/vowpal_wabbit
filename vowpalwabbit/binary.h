8573a1eeb9dfc7e1962a8913dd620309b900b6c2
Eduardo Salinas <edus@microsoft.com>
2021-07-16 18:36:27 -0400

    Update vowpalwabbit/binary.h
    Co-authored-by: Rodrigo Kumpera <kumpera@users.noreply.github.com>

--- vowpalwabbit/binary.h
+++ vowpalwabbit/binary.h
@@ -9,6 +9,6 @@ namespace VW
 {
 namespace binary
 {
-VW::LEARNER::base_learner* binary_setup(setup_base_fn& setup_base_fn, VW::config::options_i& options, vw& all);
+VW::LEARNER::base_learner* binary_setup(setup_base_fn& setup_base, VW::config::options_i& options, vw& all);
 }
 }  // namespace VW
