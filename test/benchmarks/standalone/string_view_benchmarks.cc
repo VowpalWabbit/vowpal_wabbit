f3e792d4c80ef9b7646fec620153954fe1d9a3d2
Rajan Chari <rajan.chari@yahoo.com>
2021-07-27 00:06:27 -0400

    Formatting fixes

--- test/benchmarks/standalone/string_view_benchmarks.cc
+++ test/benchmarks/standalone/string_view_benchmarks.cc
@@ -3,30 +3,32 @@
 
 struct by_ref
 {
-    size_t save(VW::string_view& ref) {
-      _save = ref; 
-      return _save.size();
-    }
+  size_t save(VW::string_view& ref)
+  {
+    _save = ref;
+    return _save.size();
+  }
 
-    VW::string_view& get() {return _save;}
-    VW::string_view _save;
+  VW::string_view& get() { return _save; }
+  VW::string_view _save;
 };
 
 struct by_val
 {
-    size_t save(VW::string_view val) {
-      _save = val; 
-      return _save.size();
-    }
+  size_t save(VW::string_view val)
+  {
+    _save = val;
+    return _save.size();
+  }
 
-    VW::string_view get() {return _save;}
-    VW::string_view _save;
+  VW::string_view get() { return _save; }
+  VW::string_view _save;
 };
 
-template<typename T>
+template <typename T>
 static void string_view(benchmark::State& state)
 {
-  T r;  
+  T r;
   VW::string_view sv("some data");
 
   for (auto _ : state)
@@ -37,15 +39,9 @@ static void string_view(benchmark::State& state)
   }
 }
 
-static void string_view_by_ref(benchmark::State& state)
-{
-  string_view<by_ref>(state);
-}
+static void string_view_by_ref(benchmark::State& state) { string_view<by_ref>(state); }
 
-static void string_view_by_val(benchmark::State& state)
-{
-  string_view<by_val>(state);
-}
+static void string_view_by_val(benchmark::State& state) { string_view<by_val>(state); }
 
 BENCHMARK(string_view_by_val);
 BENCHMARK(string_view_by_ref);
