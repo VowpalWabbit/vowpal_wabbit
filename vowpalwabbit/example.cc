1b10a67da34459e924c105c3a751cdd4fbfd05b9
manavsinghal157 <manav157.os@gmail.com>
2021-08-12 14:53:44 +0530

    Cleaning

--- vowpalwabbit/example.cc
+++ vowpalwabbit/example.cc
@@ -77,7 +77,6 @@ void copy_example_metadata(example* dst, const example* src)
   dst->example_counter = src->example_counter;
 
   dst->ft_offset = src->ft_offset;
-  dst->tag_hash=src->tag_hash;
   dst->partial_prediction = src->partial_prediction;
   if (src->passthrough == nullptr)
     dst->passthrough = nullptr;
@@ -93,6 +92,7 @@ void copy_example_metadata(example* dst, const example* src)
   dst->end_pass = src->end_pass;
   dst->is_newline = src->is_newline;
   dst->sorted = src->sorted;
+  dst->tag_hash=src->tag_hash;
 }
 
 void copy_example_data(example* dst, const example* src)
