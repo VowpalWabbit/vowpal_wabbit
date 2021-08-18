a0d675ab1ac718d5c8645313249e975c03f552e7
Jack Gerrits <jackgerrits@users.noreply.github.com>
2021-08-18 13:33:37 -0400

    fix: fix comparator for interactions sorting

--- vowpalwabbit/interactions.cc
+++ vowpalwabbit/interactions.cc
@@ -156,7 +156,7 @@ void eval_count_of_generated_ft(bool permutations, const std::vector<std::vector
 
 bool sort_interactions_comparator(const std::vector<namespace_index>& a, const std::vector<namespace_index>& b)
 {
-  if (a.size() != b.size()) { return a.size() > b.size(); }
+  if (a.size() != b.size()) { return a.size() < b.size(); }
   for (size_t i = 0; i < a.size(); i++)
   {
     if (a[i] < b[i])
