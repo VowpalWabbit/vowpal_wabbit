519d43dfcd455bd92e17572052ec6acf5d472082
Jack Gerrits <jackgerrits@users.noreply.github.com>
2021-09-01 16:27:30 -0400

    ci: Change valgrind log path (#3276)

--- test/run_tests.py
+++ test/run_tests.py
@@ -260,7 +260,7 @@ def run_command_line_test(test_id,
             valgrind_log_file_name = "test-{}.valgrind-err".format(test_id)
             valgrind_log_file_path = os.path.join(
                     working_dir, valgrind_log_file_name)
-            command_line = "valgrind --quiet --error-exitcode=100 --track-origins=yes --leak-check=full --log-file='{}' {}".format(valgrind_log_file_name, command_line)
+            command_line = "valgrind --quiet --error-exitcode=100 --track-origins=yes --leak-check=full --log-file='{}' {}".format(valgrind_log_file_path, command_line)
 
         if is_shell:
             cmd = command_line
