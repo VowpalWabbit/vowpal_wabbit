e2bc80b99dee37f1df780fbf4b2effb1168dfdbc
Jack Gerrits <jack@jackgerrits.com>
2021-09-01 12:31:27 -0400

    ci: turn on new valgrind script in parallel

---
+++ .scripts/linux/test-valgrind-run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+set -x
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+REPO_DIR=$SCRIPT_DIR/../../
+cd $REPO_DIR
+
+cd test
+source activate test-python36
+python3 run_tests.py -f -j $(nproc) --include_flatbuffers --valgrind
+source deactivate
