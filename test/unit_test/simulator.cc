a88b94ff7146e99d1fca9a1608e28d973a501cee
Eduardo Salinas <edus@microsoft.com>
2021-09-02 17:41:09 -0400

    test: unit tests improvements (#3281)
    1) remove print from explore_test.cc
    2) shift things around from tutorial_test.cc to simulator.h
    3) add callback hook to simulator

--- test/unit_test/simulator.cc
+++ test/unit_test/simulator.cc
@@ -1,8 +1,19 @@
+#include "vw.h"
 #include "simulator.h"
+
+#include <numeric>
+#include <fmt/format.h>
 
 namespace simulator
 {
-cb_sim::cb_sim(int seed) : seed(seed) { srand(seed); }
+cb_sim::cb_sim(int seed)
+    : seed(seed)
+    , users({"Tom", "Anna"})
+    , times_of_day({"morning", "afternoon"})
+    , actions({"politics", "sports", "music", "food", "finance", "health", "camping"})
+{
+  srand(seed);
+}
 
 float cb_sim::get_cost(const std::map<std::string, std::string>& context, const std::string& action)
 {
@@ -58,7 +69,7 @@ std::pair<int, float> cb_sim::sample_custom_pmf(std::vector<float>& pmf)
 
 std::pair<std::string, float> cb_sim::get_action(vw* vw, const std::map<std::string, std::string>& context)
 {
-  std::vector<std::string> multi_ex_str = to_vw_example_format(context);
+  std::vector<std::string> multi_ex_str = cb_sim::to_vw_example_format(context, "");
   multi_ex examples;
   for (const std::string& ex : multi_ex_str) { examples.push_back(VW::read_example(*vw, ex)); }
   vw->predict(examples);
@@ -123,6 +134,59 @@ std::vector<float> cb_sim::run_simulation(vw* vw, int num_iterations, bool do_le
     // We negate this so that on the plot instead of minimizing cost, we are maximizing reward
     ctr.push_back(-1 * cost_sum / static_cast<float>(i));
   }
+  return ctr;
+}
+std::vector<float> cb_sim::run_simulation_hook(
+    vw* vw, int num_iterations, callback_map& callbacks, bool do_learn, int shift)
+{
+  auto res = cb_sim::run_simulation(vw, num_iterations, do_learn, shift);
+
+  if (!callbacks.empty())
+  {
+    BOOST_TEST_MESSAGE("executing callbacks...");
+    for (auto it = callbacks.begin(); it != callbacks.end(); it++) { it->second(vw); }
+  }
+
+  return res;
+}
+
+std::vector<float> _test_helper(const std::string& vw_arg, int num_iterations, int seed)
+{
+  auto vw = VW::initialize(vw_arg);
+  simulator::cb_sim sim(seed);
+  auto ctr = sim.run_simulation(vw, num_iterations);
+  VW::finish(*vw);
+  return ctr;
+}
+
+std::vector<float> _test_helper_save_load(const std::string& vw_arg, int num_iterations, int seed)
+{
+  int split = 1500;
+  int before_save = num_iterations - split;
+
+  auto first_vw = VW::initialize(vw_arg);
+  simulator::cb_sim sim(seed);
+  // first chunk
+  auto ctr = sim.run_simulation(first_vw, before_save);
+  // save
+  std::string model_file = "test_save_load.vw";
+  VW::save_predictor(*first_vw, model_file);
+  VW::finish(*first_vw);
+  // reload in another instance
+  auto other_vw = VW::initialize("--quiet -i " + model_file);
+  // continue
+  ctr = sim.run_simulation(other_vw, split, true, before_save + 1);
+  VW::finish(*other_vw);
+  return ctr;
+}
+
+std::vector<float> _test_helper_hook(const std::string& vw_arg, callback_map& hooks, int num_iterations, int seed)
+{
+  BOOST_CHECK(true);
+  auto* vw = VW::initialize(vw_arg);
+  simulator::cb_sim sim(seed);
+  auto ctr = sim.run_simulation_hook(vw, num_iterations, hooks);
+  VW::finish(*vw);
   return ctr;
 }
 }  // namespace simulator
--- test/unit_test/simulator.h
+++ test/unit_test/simulator.h
@@ -1,17 +1,34 @@
-#include "vw.h"
+#pragma once
+
+#ifndef STATIC_LINK_VW
+#  define BOOST_TEST_DYN_LINK
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+
 #include "test_common.h"
-#include <numeric>
-#include <fmt/format.h>
+
+#include <functional>
+#include <map>
+#include <vector>
+#include <string>
+
+struct vw;
 
 namespace simulator
 {
+// maps an int: # learned examples
+// with a function to 'test' at that point in time in the simulator
+using callback_map = typename std::map<int, std::function<bool(vw*)>>;
+
 class cb_sim
 {
-  float USER_LIKED_ARTICLE = -1.f;
-  float USER_DISLIKED_ARTICLE = 0.f;
-  std::vector<std::string> users{"Tom", "Anna"};
-  std::vector<std::string> times_of_day{"morning", "afternoon"};
-  std::vector<std::string> actions{"politics", "sports", "music", "food", "finance", "health", "camping"};
+  const float USER_LIKED_ARTICLE = -1.f;
+  const float USER_DISLIKED_ARTICLE = 0.f;
+  const std::vector<std::string> users;
+  const std::vector<std::string> times_of_day;
+  const std::vector<std::string> actions;
   int seed;
   float cost_sum = 0.f;
   std::vector<float> ctr;
@@ -20,11 +37,16 @@ public:
   cb_sim(int = 0);
   float get_cost(const std::map<std::string, std::string>&, const std::string&);
   std::vector<std::string> to_vw_example_format(
-      const std::map<std::string, std::string>&, const std::string& = "", float = 0.f, float = 0.f);
+      const std::map<std::string, std::string>&, const std::string&, float = 0.f, float = 0.f);
   std::pair<int, float> sample_custom_pmf(std::vector<float>& pmf);
   std::pair<std::string, float> get_action(vw* vw, const std::map<std::string, std::string>&);
   const std::string& choose_user();
   const std::string& choose_time_of_day();
   std::vector<float> run_simulation(vw*, int, bool = true, int = 1);
+  std::vector<float> run_simulation_hook(vw*, int, callback_map&, bool = true, int = 1);
 };
+
+std::vector<float> _test_helper(const std::string&, int = 3000, int = 10);
+std::vector<float> _test_helper_save_load(const std::string&, int = 3000, int = 10);
+std::vector<float> _test_helper_hook(const std::string&, callback_map&, int = 3000, int = 10);
 }  // namespace simulator
