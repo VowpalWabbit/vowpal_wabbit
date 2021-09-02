d32ade407124713d59094f3999d3bf77ebefb86f
Eduardo Salinas <edus@microsoft.com>
2021-09-02 20:57:23 +0000

    missing boost/test_common.h

--- test/unit_test/simulator.h
+++ test/unit_test/simulator.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#ifndef STATIC_LINK_VW
+#  define BOOST_TEST_DYN_LINK
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+
+#include "test_common.h"
+
 #include <functional>
 #include <map>
 #include <vector>
 #include <string>
-
-#include "test_common.h"
 
 struct vw;
 
