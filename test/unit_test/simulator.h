12c9ef6ea2608d3ceca7004cef6fa3b2a1298fb0
Eduardo Salinas <edus@microsoft.com>
2021-09-03 12:51:30 -0400

    test: simulator with per example callback (#3291)

--- test/unit_test/simulator.h
+++ test/unit_test/simulator.h
@@ -1,3 +1,7 @@
+// Copyright (c) by respective owners including Yahoo!, Microsoft, and
+// individual contributors. All rights reserved. Released under a BSD (revised)
+// license as described in the file LICENSE.
+
 #pragma once
 
 #ifndef STATIC_LINK_VW
@@ -20,7 +24,7 @@ namespace simulator
 {
 // maps an int: # learned examples
 // with a function to 'test' at that point in time in the simulator
-using callback_map = typename std::map<int, std::function<bool(vw*)>>;
+using callback_map = typename std::map<size_t, std::function<bool(vw&, multi_ex&)>>;
 
 class cb_sim
 {
@@ -32,6 +36,7 @@ class cb_sim
   int seed;
   float cost_sum = 0.f;
   std::vector<float> ctr;
+  size_t callback_count;
 
 public:
   cb_sim(int = 0);
@@ -42,11 +47,14 @@ public:
   std::pair<std::string, float> get_action(vw* vw, const std::map<std::string, std::string>&);
   const std::string& choose_user();
   const std::string& choose_time_of_day();
-  std::vector<float> run_simulation(vw*, int, bool = true, int = 1);
-  std::vector<float> run_simulation_hook(vw*, int, callback_map&, bool = true, int = 1);
+  std::vector<float> run_simulation(vw*, size_t, bool = true, size_t = 1);
+  std::vector<float> run_simulation_hook(vw*, size_t, callback_map&, bool = true, size_t = 1);
+
+private:
+  void call_if_exists(vw&, multi_ex&, const callback_map&, const size_t);
 };
 
-std::vector<float> _test_helper(const std::string&, int = 3000, int = 10);
-std::vector<float> _test_helper_save_load(const std::string&, int = 3000, int = 10);
-std::vector<float> _test_helper_hook(const std::string&, callback_map&, int = 3000, int = 10);
+std::vector<float> _test_helper(const std::string&, size_t = 3000, int = 10);
+std::vector<float> _test_helper_save_load(const std::string&, size_t = 3000, int = 10);
+std::vector<float> _test_helper_hook(const std::string&, callback_map&, size_t = 3000, int = 10);
 }  // namespace simulator
