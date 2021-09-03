f68b7eee39ff1c74e27877e61e3baec02b4786ab
Eduardo Salinas <edus@microsoft.com>
2021-09-03 13:38:48 +0000

    index of test hook working

--- test/unit_test/automl_test.cc
+++ test/unit_test/automl_test.cc
@@ -1,3 +1,7 @@
+// Copyright (c) by respective owners including Yahoo!, Microsoft, and
+// individual contributors. All rights reserved. Released under a BSD (revised)
+// license as described in the file LICENSE.
+
 #ifndef STATIC_LINK_VW
 #  define BOOST_TEST_DYN_LINK
 #endif
@@ -7,6 +11,7 @@
 
 #include "test_common.h"
 #include "simulator.h"
+#include "reductions_fwd.h"
 #include "test_red.cc"
 #include "constant.h"
 
@@ -43,20 +48,20 @@ size_t get_index_for_feature(vw& all, const std::string& ns, const std::string& 
 }
 
 // see gd.cc:audit_feature(..)
-size_t hash_to_index(vw& all, size_t hash)
+size_t hash_to_index(parameters& weights, size_t hash)
 {
-  hash = hash & all.weights.mask();
-  hash = hash >> all.weights.stride_shift();
+  hash = hash & weights.mask();
+  hash = hash >> weights.stride_shift();
 
   return hash;
 }
 
 // craft feature index for interaction
 // see: interactions_predict.h:process_quadratic_interaction(..)
-size_t interaction_to_index(vw& all, size_t one, size_t two)
+size_t interaction_to_index(parameters& weights, size_t one, size_t two)
 {
   // FNV_prime from constant.h
-  return hash_to_index(all, (FNV_prime * one) ^ two);
+  return hash_to_index(weights, (FNV_prime * one) ^ two);
 }
 }  // namespace helper
 
@@ -68,19 +73,19 @@ using namespace helper;
 // n) asserts weights before/after every operation
 // NOTE: interactions are currently 0 for offset 0 since
 // config 0 is hard-coded to be empty interactions for now.
-bool weights_offset_test(vw* all)
+bool weights_offset_test(vw& all, multi_ex&)
 {
   const size_t offset_to_clear = 1;
-  auto& weights = all->weights.dense_weights;
+  auto& weights = all.weights.dense_weights;
 
   std::vector<std::uint64_t> feature_indexes;
   // hardcoded features that correspond to simulator.h
-  feature_indexes.emplace_back(hash_to_index(*all, get_index_for_feature(*all, "Action", "article=music")));
-  feature_indexes.emplace_back(hash_to_index(*all, get_index_for_feature(*all, "Action", "article=politics")));
-  feature_indexes.emplace_back(hash_to_index(*all, get_index_for_feature(*all, "User", "user=Anna")));
+  feature_indexes.emplace_back(hash_to_index(all.weights, get_index_for_feature(all, "Action", "article=music")));
+  feature_indexes.emplace_back(hash_to_index(all.weights, get_index_for_feature(all, "Action", "article=politics")));
+  feature_indexes.emplace_back(hash_to_index(all.weights, get_index_for_feature(all, "User", "user=Anna")));
 
-  const size_t interaction_index = interaction_to_index(
-      *all, get_index_for_feature(*all, "Action", "article=sports"), get_index_for_feature(*all, "User", "user=Tom"));
+  const size_t interaction_index = interaction_to_index(all.weights,
+      get_index_for_feature(all, "Action", "article=sports"), get_index_for_feature(all, "User", "user=Tom"));
 
   const float expected_w1 = 0.584335148f;
   const float expected_w2 = 0.584628344f;
@@ -100,7 +105,7 @@ bool weights_offset_test(vw* all)
   BOOST_CHECK_CLOSE(expected_w2, weights.strided_index(interaction_index + offset_to_clear + 1), FLOAT_TOL);
 
   // all weights of offset 1 will be set to zero
-  weights.clear_offset(offset_to_clear, all->wpp);
+  weights.clear_offset(offset_to_clear, all.wpp);
 
   for (auto index : feature_indexes)
   {
@@ -109,14 +114,14 @@ bool weights_offset_test(vw* all)
     float w2 = weights.strided_index(index + offset_to_clear + 1);
     BOOST_CHECK_EQUAL(ZERO, w1);
     BOOST_CHECK_NE(ZERO, w2);
-    BOOST_CHECK_NE(ZERO, w2);
+    BOOST_CHECK_NE(w1, w2);
   }
   BOOST_CHECK_EQUAL(ZERO, weights.strided_index(interaction_index));
   BOOST_CHECK_EQUAL(ZERO, weights.strided_index(interaction_index + offset_to_clear));
   BOOST_CHECK_CLOSE(expected_w2, weights.strided_index(interaction_index + offset_to_clear + 1), FLOAT_TOL);
 
   // copy from offset 2 to offset 1
-  weights.copy_offsets(offset_to_clear + 1, offset_to_clear, all->wpp);
+  weights.copy_offsets(offset_to_clear + 1, offset_to_clear, all.wpp);
 
   for (auto index : feature_indexes)
   {
@@ -143,23 +148,23 @@ using namespace automl_test;
 
 BOOST_AUTO_TEST_CASE(automl_weight_operations)
 {
+  const size_t num_iterations = 1331;
   callback_map test_hooks;
-  size_t num_iterations = 1331;
 
   // lambda test/assert
-  test_hooks.emplace(0, [&num_iterations](vw* all) {
-    VW::test_red::tr_data* tr = helper::get_automl_data(*all);
-    BOOST_CHECK_EQUAL(tr->cm.county, num_iterations);
+  test_hooks.emplace(num_iterations - 1, [&num_iterations](vw& all, multi_ex&) {
+    VW::test_red::tr_data* tr = helper::get_automl_data(all);
+    BOOST_CHECK_EQUAL(tr->cm.county, num_iterations - 1);
     BOOST_CHECK_GT(tr->cm.max_live_configs, 2);
     BOOST_CHECK_EQUAL(tr->cm.current_state, VW::test_red::config_state::Experimenting);
     return true;
   });
 
   // fn test/assert
-  test_hooks.emplace(1, weights_offset_test);
+  test_hooks.emplace(num_iterations, weights_offset_test);
 
   auto ctr = simulator::_test_helper_hook(
-      "--test_red 0 --cb_explore_adf --quiet --epsilon 0.2 --random_seed 5", test_hooks, (int)num_iterations);
+      "--test_red 0 --cb_explore_adf --quiet --epsilon 0.2 --random_seed 5", test_hooks, num_iterations);
 
   BOOST_CHECK_GT(ctr.back(), 0.6f);
 }
@@ -178,4 +183,32 @@ BOOST_AUTO_TEST_CASE(automl_save_load)
   BOOST_CHECK_GT(with_save, 0.7f);
 
   BOOST_CHECK_CLOSE(without_save, with_save, FLOAT_TOL);
+}
+
+BOOST_AUTO_TEST_CASE(assert_0th_event)
+{
+  const size_t zero = 0;
+  const size_t num_iterations = 10;
+  callback_map test_hooks;
+
+  // technically runs after the 0th example is learned
+  test_hooks.emplace(zero, [&zero](vw& all, multi_ex&) {
+    VW::test_red::tr_data* tr = helper::get_automl_data(all);
+    BOOST_CHECK_EQUAL(tr->cm.county, zero);
+    BOOST_CHECK_EQUAL(tr->cm.current_state, VW::test_red::config_state::Idle);
+    return true;
+  });
+
+  // test executes right after learn call of the 10th example
+  test_hooks.emplace(num_iterations, [&num_iterations](vw& all, multi_ex&) {
+    VW::test_red::tr_data* tr = helper::get_automl_data(all);
+    BOOST_CHECK_EQUAL(tr->cm.county, num_iterations);
+    BOOST_CHECK_EQUAL(tr->cm.current_state, VW::test_red::config_state::Experimenting);
+    return true;
+  });
+
+  auto ctr = simulator::_test_helper_hook(
+      "--test_red 0 --cb_explore_adf --quiet --epsilon 0.2 --random_seed 5", test_hooks, num_iterations);
+
+  BOOST_CHECK_GT(ctr.back(), 0.1f);
 }
