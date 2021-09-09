9aa50b9b62984b683986fbb7eb6e316077032e67
Jack Gerrits <jack@jackgerrits.com>
2021-09-09 17:03:27 -0400

    fix flag

--- .scripts/linux/build-slim.sh
+++ .scripts/linux/build-slim.sh
@@ -7,5 +7,5 @@ REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
 rm -rf build
-cmake -S . -B build -g Ninja -DCMAKE_BUILD_TYPE=Debug -DWARNINGS=OFF -DBUILD_SLIM_VW=On -DBUILD_JAVA=Off -DBUILD_PYTHON=Off -DBUILD_TESTS=On -DBUILD_FLATBUFFERS=Off
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DWARNINGS=OFF -DBUILD_SLIM_VW=On -DBUILD_JAVA=Off -DBUILD_PYTHON=Off -DBUILD_TESTS=On -DBUILD_FLATBUFFERS=Off
 cmake --build build --target vwslim vw-slim-test
