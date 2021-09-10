6c3263322cc610fc8c9f03b7e76d761838b10e2c
Jack Gerrits <jackgerrits@users.noreply.github.com>
2021-09-10 12:09:17 -0400

    ci: move to Ubuntu18.04 for CI jobs (#3308)
    * migrate valgrind to 1804
    
    * update slim CI
    
    * update all pipelines, remove some scripts that were merged into pipeline
    
    * fix upload coverage
    
    * build to_flatbuff
    
    * Update build_linux.yml
    
    * remove unecessary cmake args
    
    * Update valgrind.yml
    
    * Use correct python from image
    
    * Change coverage script
    
    * Update upload_coverage.yml
    
    * roll back most of the changes
    
    * undo valgrind
    
    * fix flag
    
    * use ctest directly
    
    * fix cmake invocation

--- .scripts/linux/build-with-coverage.sh
+++ .scripts/linux/build-with-coverage.sh
@@ -6,10 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
-# Clear out build directory then build using GCov and run one set of tests again
-rm -rf build
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DGCOV=ON -DWARNINGS=OFF -DBUILD_JAVA=Off -DBUILD_PYTHON=Off -DBUILD_TESTS=On -DBUILD_FLATBUFFERS=On
-NUM_PROCESSORS=$(cat nprocs.txt)
-make vw-bin spanning_tree vw-unit-test.out -j ${NUM_PROCESSORS}
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DGCOV=ON -DWARNINGS=OFF -DBUILD_JAVA=Off -DBUILD_PYTHON=Off -DBUILD_TESTS=On -DBUILD_FLATBUFFERS=On
+cmake --build build --target vw-bin spanning_tree vw-unit-test.out
