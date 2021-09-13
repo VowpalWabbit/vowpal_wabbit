4c029c3219639a66e074ee5922c054b693533207
Jack Gerrits <jack@jackgerrits.com>
2021-09-13 09:15:45 -0400

    Formatting

--- test/benchmarks/benchmarks_common.h
+++ test/benchmarks/benchmarks_common.h
@@ -2,7 +2,8 @@
 #include <sstream>
 #include <string>
 
-inline std::string get_x_numerical_fts(int feature_size) {
+inline std::string get_x_numerical_fts(int feature_size)
+{
   std::stringstream ss;
   ss << "1:1:0.5 |";
   for (size_t i = 0; i < feature_size; i++) { ss << " " << std::to_string(i) + ":4.36352"; }
@@ -10,7 +11,8 @@ inline std::string get_x_numerical_fts(int feature_size) {
   return s;
 };
 
-inline std::string get_x_string_fts(int feature_size) {
+inline std::string get_x_string_fts(int feature_size)
+{
   std::stringstream ss;
   ss << "1:1:0.5 | ";
   for (size_t i = 0; i < feature_size; i++) { ss << "bigfeaturename" + std::to_string(i) + ":10 "; }
@@ -18,7 +20,8 @@ inline std::string get_x_string_fts(int feature_size) {
   return s;
 };
 
-inline std::string get_x_string_fts_no_label(int feature_size, size_t action_index = 0) {
+inline std::string get_x_string_fts_no_label(int feature_size, size_t action_index = 0)
+{
   std::stringstream ss;
   ss << " | ";
   for (size_t j = 0; j < feature_size; j++) { ss << std::to_string(action_index) + "_" + std::to_string(j) << +" "; }
@@ -27,7 +30,9 @@ inline std::string get_x_string_fts_no_label(int feature_size, size_t action_ind
   return ss.str();
 };
 
-inline std::string get_x_string_fts_multi_ex(int feature_size, size_t actions, bool shared, bool label, size_t start_index = 0) {
+inline std::string get_x_string_fts_multi_ex(
+    int feature_size, size_t actions, bool shared, bool label, size_t start_index = 0)
+{
   size_t action_start = 0;
   std::stringstream ss;
   if (shared) { ss << "shared | s_1 s_2 s_3 s_4" << std::endl; }
