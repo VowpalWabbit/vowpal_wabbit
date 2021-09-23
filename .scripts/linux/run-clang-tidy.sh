f98d66b8453ba3b1749f7a9ffff74912b876dfbd
Jack Gerrits <jack@jackgerrits.com>
2021-09-23 12:16:06 -0400

    Fix issues

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$(realpath $SCRIPT_DIR/../../)
 cd $REPO_DIR
 
-CHECKS=-"*,performance-unnecessary-copy-initialization,performance-for-range-copy,performance-move-const-arg"
+CHECKS="-*,performance-unnecessary-copy-initialization,performance-for-range-copy,performance-move-const-arg"
 cmake -S . -B build -G Ninja "-DCMAKE_CXX_CLANG_TIDY=clang-tidy;-checks=$CHECKS;-warnings-as-errors=$CHECKS;-header-filter=$REPO_DIR/vowpalwabbit/*"
 cmake --build build --target vw-bin
