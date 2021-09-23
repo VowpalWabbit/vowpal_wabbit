032beb0015920e63b4174bee956cd18c0d9d3ec5
Jack Gerrits <jack@jackgerrits.com>
2021-09-23 11:21:47 -0400

    Use CMake to drive clang tidy

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -6,8 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$(realpath $SCRIPT_DIR/../../)
 cd $REPO_DIR
 
-# Assumes cmake has been configured to build directory
-
-cat $REPO_DIR/build/compile_commands.json | \
-    jq -r ".[] | select(.file | startswith(\"$REPO_DIR/vowpalwabbit\")) | .file" | \
-    xargs -n 1 -I {} clang-tidy --config="$(cat $REPO_DIR/.clang-tidy-ci)" -header-filter=$REPO_DIR/vowpalwabbit/* -p=build {}
+CHECKS=-"*,performance-unnecessary-copy-initialization,performance-for-range-copy,performance-move-const-arg"
+cmake -S . -B build -G Ninja "-DCMAKE_CXX_CLANG_TIDY=clang-tidy;-checks=$CHECKS;-warnings-as-errors=$CHECKS;-header-filter=$REPO_DIR/vowpalwabbit/*"
+cmake --build build --target vw-bin
