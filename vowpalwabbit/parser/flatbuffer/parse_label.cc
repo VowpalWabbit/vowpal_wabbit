eb9e50bb0fcb18579621d0b0fa4f3fea933279e5
Jack Gerrits <jack@jackgerrits.com>
2021-10-12 14:18:35 -0400

    warning

--- vowpalwabbit/parser/flatbuffer/parse_label.cc
+++ vowpalwabbit/parser/flatbuffer/parse_label.cc
@@ -21,7 +21,7 @@ namespace parsers
 namespace flatbuffer
 {
 void parser::parse_simple_label(
-    shared_data* sd, polylabel* l, reduction_features* red_features, const SimpleLabel* label)
+    shared_data* /*sd*/, polylabel* l, reduction_features* red_features, const SimpleLabel* label)
 {
   auto& simple_red_features = red_features->template get<simple_label_reduction_features>();
   l->simple.label = label->label();
