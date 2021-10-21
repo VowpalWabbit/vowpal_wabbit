a34df3fbaea6d1423224bfc4e4409cda0f0ec217
Jack Gerrits <jack@jackgerrits.com>
2021-10-21 09:56:23 -0400

    remove header guard from cc file

--- vowpalwabbit/label_parser.cc
+++ vowpalwabbit/label_parser.cc
@@ -2,8 +2,6 @@
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
 
-#pragma once
-
 #include "label_parser.h"
 
 #include "simple_label_parser.h"
