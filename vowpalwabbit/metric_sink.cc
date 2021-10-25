f1aeb7d9d1f064621a34f95243144c4b82648908
Jack Gerrits <jack@jackgerrits.com>
2021-10-25 16:59:43 -0400

    remove pragma once from impl file

--- vowpalwabbit/metric_sink.cc
+++ vowpalwabbit/metric_sink.cc
@@ -1,4 +1,3 @@
-#pragma once
 // Copyright (c) by respective owners including Yahoo!, Microsoft, and
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
