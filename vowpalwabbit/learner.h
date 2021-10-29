806f7933ef3788d1521e606960d3a07aeefcc44a
Griffin Bassman <griffinbassman@gmail.com>
2021-10-29 14:55:56 -0400

    set defaul ouput label input pred

--- vowpalwabbit/learner.h
+++ vowpalwabbit/learner.h
@@ -666,6 +666,8 @@ struct reduction_learner_builder
 
     // Default here is passthrough.
     super::set_output_prediction_type(base->get_output_prediction_type());
+    super::set_input_prediction_type(base->get_input_prediction_type());
+    super::set_output_label_type(base->get_output_label_type());
     super::set_input_label_type(base->get_input_label_type());
   }
 
@@ -703,6 +705,8 @@ struct reduction_no_data_learner_builder
     set_params_per_weight(1);
     // Default here is passthrough.
     super::set_output_prediction_type(base->get_output_prediction_type());
+    super::set_input_prediction_type(base->get_input_prediction_type());
+    super::set_output_label_type(base->get_output_label_type());
     super::set_input_label_type(base->get_input_label_type());
   }
 
@@ -743,6 +747,8 @@ struct base_learner_builder
     this->_learner->learn_fd.data = this->_learner->learner_data.get();
 
     super::set_output_prediction_type(out_pred_type);
+    super::set_output_prediction_type(prediction_type_t::nopred);
+    super::set_input_label_type(label_type_t::nolabel);
     super::set_input_label_type(in_label_type);
 
     set_params_per_weight(1);
