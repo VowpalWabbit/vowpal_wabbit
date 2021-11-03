f2b2a4b3105cdbc4628926ba064b14ddf32f8d96
Jack Gerrits <jack@jackgerrits.com>
2021-11-03 08:30:39 -0400

    Update skips and add list of known limitations

--- python/vowpalwabbit/__main__.py
+++ python/vowpalwabbit/__main__.py
@@ -2,6 +2,10 @@ import sys
 
 from vowpalwabbit import pyvw
 
+# Known limitations:
+# - Stdin cannot be used
+# - vw.exe specifc args cannot be used: --args, --onethread
+
 
 def main():
     opts = sys.argv[1:]
