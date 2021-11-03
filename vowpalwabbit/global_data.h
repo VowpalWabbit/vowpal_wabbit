2787934d8cc55e18e9035b01830aa14b2e21b524
Jack Gerrits <jack@jackgerrits.com>
2021-11-03 14:00:54 -0400

    bring back changes

--- vowpalwabbit/global_data.h
+++ vowpalwabbit/global_data.h
@@ -257,6 +257,8 @@ public:
 
   vw_logger logger;
   bool audit;     // should I print lots of debugging information?
+  std::shared_ptr<std::vector<char>> _audit_buffer;
+  std::unique_ptr<VW::io::writer> _audit_writer;
   bool training;  // Should I train if lable data is available?
   bool active;
   bool invariant_updates;  // Should we use importance aware/safe updates
