820d4d93ecb98a80bb0eb29e04ec51c8ed0a724f
Olga Vrousgou <olgavrou@gmail.com>
2021-11-22 09:57:41 -0800

    fix tests

--- test/unit_test/weights_test.cc
+++ test/unit_test/weights_test.cc
@@ -2,10 +2,6 @@
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
 
-#ifndef STATIC_LINK_VW
-#  define BOOST_TEST_DYN_LINK
-#endif
-
 #include <boost/test/unit_test.hpp>
 #include <boost/mpl/vector.hpp>
 
