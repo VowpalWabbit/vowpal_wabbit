fe81b8c0f71ca11d4c4e77be702ca75f0e999152
manavsinghal157 <manav157.os@gmail.com>
2021-11-25 00:38:14 +0530

    Missed benchmark addition

--- test/benchmarks/standalone/benchmark_text_input.cc
+++ test/benchmarks/standalone/benchmark_text_input.cc
@@ -51,7 +51,7 @@ static void benchmark_cb_adf_learn(benchmark::State& state, int feature_count)
 {
   auto vw = VW::initialize("--cb_explore_adf --epsilon 0.1 --quiet -q ::", nullptr, false, nullptr, nullptr);
   multi_ex examples;
-  examples.push_back(VW::read_example(*vw, std::string("shared | s_1 s_2")));
+  examples.push_back(VW::read_example(*vw, std::string("shared tag1| s_1 s_2")));
   examples.push_back(VW::read_example(*vw, get_x_string_fts(feature_count)));
   examples.push_back(VW::read_example(*vw, get_x_string_fts_no_label(feature_count)));
   examples.push_back(VW::read_example(*vw, get_x_string_fts_no_label(feature_count)));
