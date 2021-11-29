1be9fb1a6ba7c1741f6654210d96d57a10b84b82
Eduardo Salinas <edus@microsoft.com>
2021-11-29 18:25:48 +0000

    use header files

--- vowpalwabbit/parse_args.cc
+++ vowpalwabbit/parse_args.cc
@@ -23,6 +23,8 @@
 
 #include "rand48.h"
 #include "learner.h"
+#include "prediction_type.h"
+#include "label_type.h"
 #include "parse_example.h"
 #include "best_constant.h"
 #include "vw_exception.h"
@@ -1613,8 +1615,8 @@ VW::workspace* initialize_with_builder(std::unique_ptr<options_i, options_delete
 
     if (!all.logger.quiet)
     {
-      *(all.trace_message) << "Input label = " << to_string(all.example_parser->lbl_parser.label_type) << std::endl;
-      *(all.trace_message) << "Output pred = " << to_string(all.l->pred_type) << std::endl;
+      *(all.trace_message) << "Input label = " << VW::to_string(all.l->get_input_label_type()).substr(14) << std::endl;
+      *(all.trace_message) << "Output pred = " << VW::to_string(all.l->get_output_prediction_type()).substr(19) << std::endl;
     }
 
     if (!all.options->get_typed_option<bool>("dry_run").value())
