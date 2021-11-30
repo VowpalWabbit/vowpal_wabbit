f0e16add28ece1b8d0ca42dc8e73c24ecedd02c2
Manav Singhal <manav157.os@gmail.com>
2021-12-01 02:59:19 +0530

    feat: Privacy Preserving Learning (#3334)

--- test/benchmarks/standalone/benchmark_text_input.cc
+++ test/benchmarks/standalone/benchmark_text_input.cc
@@ -51,7 +51,7 @@ static void benchmark_cb_adf_learn(benchmark::State& state, int feature_count)
 {
   auto vw = VW::initialize("--cb_explore_adf --epsilon 0.1 --quiet -q ::", nullptr, false, nullptr, nullptr);
   multi_ex examples;
-  examples.push_back(VW::read_example(*vw, std::string("shared | s_1 s_2")));
+  examples.push_back(VW::read_example(*vw, std::string("shared tag1| s_1 s_2")));
   examples.push_back(VW::read_example(*vw, get_x_string_fts(feature_count)));
   examples.push_back(VW::read_example(*vw, get_x_string_fts_no_label(feature_count)));
   examples.push_back(VW::read_example(*vw, get_x_string_fts_no_label(feature_count)));
@@ -64,6 +64,27 @@ static void benchmark_cb_adf_learn(benchmark::State& state, int feature_count)
   vw->finish_example(examples);
   VW::finish(*vw);
 }
+
+#ifdef PRIVACY_ACTIVATION
+static void benchmark_cb_adf_learn_privacy_preserving(benchmark::State& state, int feature_count)
+{
+  auto vw = VW::initialize(
+      "--privacy_activation --cb_explore_adf --epsilon 0.1 --quiet -q ::", nullptr, false, nullptr, nullptr);
+  multi_ex examples;
+  examples.push_back(VW::read_example(*vw, std::string("shared tag1| s_1 s_2")));
+  examples.push_back(VW::read_example(*vw, get_x_string_fts(feature_count)));
+  examples.push_back(VW::read_example(*vw, get_x_string_fts_no_label(feature_count)));
+  examples.push_back(VW::read_example(*vw, get_x_string_fts_no_label(feature_count)));
+
+  for (auto _ : state)
+  {
+    vw->learn(examples);
+    benchmark::ClobberMemory();
+  }
+  vw->finish_example(examples);
+  VW::finish(*vw);
+}
+#endif
 
 static void benchmark_ccb_adf_learn(benchmark::State& state, std::string feature_string)
 {
@@ -224,6 +245,11 @@ BENCHMARK_CAPTURE(benchmark_ccb_adf_learn, many_features, "a b c d e f g h i j k
 
 BENCHMARK_CAPTURE(benchmark_cb_adf_learn, few_features, 2);
 BENCHMARK_CAPTURE(benchmark_cb_adf_learn, many_features, 120);
+
+#ifdef PRIVACY_ACTIVATION
+BENCHMARK_CAPTURE(benchmark_cb_adf_learn_privacy_preserving, few_features, 2);
+BENCHMARK_CAPTURE(benchmark_cb_adf_learn_privacy_preserving, many_features, 120);
+#endif
 
 BENCHMARK_CAPTURE(benchmark_multi, cb_adf_no_namespaces, gen_cb_examples(100, 7, 3, 6, 1, 4, 14, 2, false),
     "--cb_explore_adf --quiet");
