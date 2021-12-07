ec020c38b4ec2202beb3dfc3a6acd75945d02497
Jack Gerrits <jackgerrits95@gmail.com>
2021-12-07 09:38:12 -0500

    update working dir and test

--- .scripts/linux/test-slim.sh
+++ .scripts/linux/test-slim.sh
@@ -6,6 +6,4 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
-cd ./build/vowpalwabbit/slim/test/
-# Needs to be run from this directory as tests load files relative to current dir.
-./vw-slim-test
+cmake --build build --target test
