a7d00140b2f007a116f23bc883acf31ad4178917
Griffin Bassman <griffinbassman@gmail.com>
2022-01-04 13:53:24 -0500

    keep model in same location

--- library/train.sh
+++ library/train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -f train.cache train.w
-../build/vowpalwabbit/vw -c -d train -f ../test/train.w -q st --passes 100 --hash all --noconstant --csoaa_ldf m --holdout_off
-../build/vowpalwabbit/vw -t -d train -i ../test/train.w -p train.pred --noconstant
+../build/vowpalwabbit/vw -c -d train -f train.w -q st --passes 100 --hash all --noconstant --csoaa_ldf m --holdout_off
+../build/vowpalwabbit/vw -t -d train -i train.w -p train.pred --noconstant
 
