cdfd140be54ab944f9e74e3436a6ee4e209067ef
Griffin Bassman <griffinbassman@gmail.com>
2022-01-05 17:09:34 -0500

    feat: Add epsilon reduction features for decay of exploration

---
+++ vowpalwabbit/epsilon_reduction_features.h
@@ -0,0 +1,29 @@
+// Copyright (c) by respective owners including Yahoo!, Microsoft, and
+// individual contributors. All rights reserved. Released under a BSD (revised)
+// license as described in the file LICENSE.
+
+#pragma once
+
+namespace VW
+{
+namespace cb_explore_adf
+{
+namespace greedy
+{
+struct reduction_features
+{
+  float epsilon;
+
+  reduction_features()
+  {
+    epsilon = -1.f;
+  }
+
+  void clear()
+  {
+    epsilon = -1.f;
+  }
+};
+}  // namespace greedy
+}  // namespace cb_explore_adf
+}  // namespace VW
