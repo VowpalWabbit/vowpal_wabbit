9b2c33ebef29f9fc50cccdfb1eea255fb138bf12
Griffin Bassman <griffinbassman@gmail.com>
2022-01-24 12:17:07 -0500

    merge and small updates

--- test/unit_test/ae_test.cc
+++ test/unit_test/ae_test.cc
@@ -17,11 +17,9 @@ using simulator::callback_map;
 using simulator::cb_sim;
 using namespace VW::ae;
 
-
 BOOST_AUTO_TEST_CASE(ae_test)
 {
   // we initialize the reduction pointing to position 0 as champ, that config is hard-coded to empty
   auto ctr = simulator::_test_helper("--agedexp 3 --cb_explore_adf --quiet --epsilon 0.2 --random_seed 5");
   float with_save = ctr.back();
 }
-
--- vowpalwabbit/ae.cc
+++ vowpalwabbit/ae.cc
@@ -25,14 +25,13 @@ namespace VW
 {
 namespace ae
 {
-
 struct ae_score : scored_config
 {
   ae_score() : _lower_bound(0), _model_idx(0) {}
   ae_score(uint64_t model_idx) : _lower_bound(0), _model_idx(model_idx) {}
   float get_upper_bound() const { return this->current_ips(); }
-  float get_lower_bound() const { return _lower_bound;  }
-  uint64_t get_model_idx() const { return _model_idx; } 
+  float get_lower_bound() const { return _lower_bound; }
+  uint64_t get_model_idx() const { return _model_idx; }
   void update_bounds(float w, float r);
 
   float _lower_bound;
@@ -41,17 +40,15 @@ struct ae_score : scored_config
 
 struct ae_data
 {
-  ae_data(float _epsilon, uint64_t num_configs) : scored_configs(num_configs)
+  ae_data(float _epsilon, uint64_t num_configs, parameters& weights) : scored_configs(num_configs), weights(weights)
   {
     initial_epsilon = _epsilon;
-    for (uint64_t i = 0; i < num_configs; ++i) {
-      scored_configs[i]._model_idx = i;
-    }
+    for (uint64_t i = 0; i < num_configs; ++i) { scored_configs[i]._model_idx = i; }
   }
   float initial_epsilon;
-  VW::workspace* all = nullptr;  // statistics, loss
   std::vector<uint64_t> update_counts;
   std::vector<ae_score> scored_configs;
+  parameters& weights;
 };
 
 void ae_score::update_bounds(float w, float r)
@@ -72,11 +69,12 @@ ForwardIt swap_models(ForwardIt first, ForwardIt n_first, ForwardIt end)
 }
 
 template <class ForwardIt>
-void reset_models(ForwardIt first, ForwardIt end, VW::workspace* workspace, uint64_t model_count)
+void reset_models(ForwardIt first, ForwardIt end, parameters& weights, uint64_t model_count)
 {
-  for (; first != end; ++first) {
+  for (; first != end; ++first)
+  {
     first->reset_stats();
-    workspace->weights.dense_weights.clear_offset(first->get_model_idx(), model_count);
+    weights.dense_weights.clear_offset(first->get_model_idx(), model_count);
   }
 }
 
@@ -91,7 +89,7 @@ void predict(ae_data& data, VW::LEARNER::multi_learner& base, multi_ex& examples
 {
   auto& ep_fts = examples[0]->_reduction_features.template get<VW::cb_explore_adf::greedy::reduction_features>();
   auto active_iter = data.scored_configs.end() - 1;
-  ep_fts.epsilon = decayed_epsilon(data.initial_epsilon, (*active_iter).update_count);
+  ep_fts.epsilon = decayed_epsilon(data.initial_epsilon, active_iter->update_count);
   base.predict(examples, active_iter->get_model_idx());
 }
 
@@ -109,15 +107,15 @@ void learn(ae_data& data, VW::LEARNER::multi_learner& base, multi_ex& ec)
   // Process each model, then update the upper/lower bounds for each model
   for (auto config_iter = data.scored_configs.begin(); config_iter != data.scored_configs.end(); ++config_iter)
   {
-      // Update the scoring of all configs
-      // Only call if learn calls predict is set
-      if (!base.learn_returns_prediction) { base.predict(ec, config_iter->get_model_idx()); }
-      base.learn(ec, config_iter->get_model_idx());
+    // Update the scoring of all configs
+    // Only call if learn calls predict is set
+    if (!base.learn_returns_prediction) { base.predict(ec, config_iter->get_model_idx()); }
+    base.learn(ec, config_iter->get_model_idx());
 
-      const uint32_t chosen_action = ec[0]->pred.a_s[0].action;
-      const float w = logged.probability > 0 ? 1 / logged.probability : 0;
-      const float r = -logged.cost;
-      config_iter->update_bounds((chosen_action == labelled_action) ? 0 : w, r);
+    const uint32_t chosen_action = ec[0]->pred.a_s[0].action;
+    const float w = logged.probability > 0 ? 1 / logged.probability : 0;
+    const float r = -logged.cost;
+    config_iter->update_bounds((chosen_action == labelled_action) ? 0 : w, r);
   }
 
   // If the lower bound of a model exceeds the upperbound of the champion, migrate the new model as
@@ -130,7 +128,7 @@ void learn(ae_data& data, VW::LEARNER::multi_learner& base, multi_ex& ec)
     if (candidate_iter->get_lower_bound() > champion_iter->get_upper_bound())
     {
       auto n_iter = swap_models(candidate_iter, champion_iter, end_iter);
-      reset_models(n_iter, end_iter, data.all, model_count);
+      reset_models(n_iter, end_iter, data.weights, model_count);
       break;
     }
   }
@@ -138,15 +136,16 @@ void learn(ae_data& data, VW::LEARNER::multi_learner& base, multi_ex& ec)
   // check if any model counts are higher than the champion. If so, shift the model
   // back to the beginning of the list and reset its counts
   auto model_idx = model_count - 1;
-  for (auto candidate_iter = champion_iter + 1; candidate_iter != end_iter; ++candidate_iter, --model_idx){
-    if (candidate_iter->update_count > (pow(champion_iter->update_count, model_idx / model_count))) {
-      auto n_iter = swap_models(candidate_iter+1, candidate_iter, end_iter);
-      reset_models(n_iter, end_iter, data.all, model_count);
+  for (auto candidate_iter = champion_iter + 1; candidate_iter != end_iter; ++candidate_iter, --model_idx)
+  {
+    if (candidate_iter->update_count > (pow(champion_iter->update_count, model_idx / model_count)))
+    {
+      auto n_iter = swap_models(candidate_iter + 1, candidate_iter, end_iter);
+      reset_models(n_iter, end_iter, data.weights, model_count);
       break;
     }
   }
 }
-
 
 void finish_example(VW::workspace& all, ae_data& data, multi_ex& ec)
 {
@@ -154,30 +153,30 @@ void finish_example(VW::workspace& all, ae_data& data, multi_ex& ec)
   _UNUSED(data);
 }
 
-
 VW::LEARNER::base_learner* ae_setup(VW::setup_base_i& stack_builder)
 {
   VW::config::options_i& options = *stack_builder.get_options();
+  VW::workspace& all = *stack_builder.get_all_pointer();
+
   std::string arg;
   uint64_t model_count;
   float epsilon;
 
   option_group_definition new_options("Aged Exploration");
   new_options
-    .add(make_option("agedexp", model_count)
-      .necessary()
-      .keep()
-      .default_value(3)
-      .help("Set number of exploration models"))
-    .add(make_option("epsilon", epsilon)
-      .keep()
-      .allow_override()
-      .default_value(0.05f)
-      .help("Epsilon-greedy exploration initial value"));
+      .add(make_option("agedexp", model_count)
+               .necessary()
+               .keep()
+               .default_value(3)
+               .help("Set number of exploration models"))
+      .add(make_option("epsilon", epsilon)
+               .keep()
+               .allow_override()
+               .default_value(0.05f)
+               .help("Epsilon-greedy exploration initial value"));
 
   if (!options.add_parse_and_check_necessary(new_options)) { return nullptr; }
-  auto data = VW::make_unique<ae_data>(epsilon, model_count);
-  data->all = stack_builder.get_all_pointer();
+  auto data = VW::make_unique<ae_data>(epsilon, model_count, all.weights);
 
   // make sure we setup the rest of the stack with cleared interactions
   // to make sure there are not subtle bugs
@@ -198,7 +197,6 @@ VW::LEARNER::base_learner* ae_setup(VW::setup_base_i& stack_builder)
     // not implemented yet
     THROW("fatal: aged learner not supported for single line learners yet");
   }
-
 }
 
 }  // namespace ae
