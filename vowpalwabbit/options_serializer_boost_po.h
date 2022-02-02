15b09e5dae4b5082c81b464fb466e602c41a4a08
Jack Gerrits <jack@jackgerrits.com>
2022-02-02 09:44:05 -0500

    feat: Support visitor pattern for handling typed options

--- vowpalwabbit/options_serializer_boost_po.h
+++ vowpalwabbit/options_serializer_boost_po.h
@@ -6,17 +6,16 @@
 
 #include "options.h"
 
-#include "options_types.h"
 
-#include "vw_exception.h"
-
+#include <string>
 #include <sstream>
 
 namespace VW
 {
 namespace config
 {
-struct options_serializer_boost_po : options_serializer_i
+
+struct options_serializer_boost_po : options_serializer_i, typed_option_visitor
 {
   options_serializer_boost_po() { m_output_stream.precision(15); }
 
@@ -24,54 +23,18 @@ struct options_serializer_boost_po : options_serializer_i
   std::string str() const override;
   size_t size() const override;
 
+  void visit(typed_option<uint32_t>& option) override;
+  void visit(typed_option<uint64_t>& option) override;
+  void visit(typed_option<int32_t>& option) override;
+  void visit(typed_option<int64_t>& option) override;
+  void visit(typed_option<float>& option) override;
+  void visit(typed_option<std::string>& option) override;
+  void visit(typed_option<bool>& option) override;
+  void visit(typed_option<std::vector<std::string>>& option) override;
+
 private:
-  template <typename T>
-  bool serialize_if_t(base_option& base_option)
-  {
-    if (base_option.m_type_hash == typeid(T).hash_code())
-    {
-      auto typed = dynamic_cast<typed_option<T>&>(base_option);
-      serialize(typed);
-      return true;
-    }
-
-    return false;
-  }
-
-  template <typename T>
-  void serialize(typed_option<T>& typed_option)
-  {
-    m_output_stream << " --" << typed_option.m_name << " " << typed_option.value();
-  }
-
-  template <typename T>
-  void serialize(typed_option<std::vector<T>>& typed_option)
-  {
-    auto vec = typed_option.value();
-    if (vec.size() > 0)
-    {
-      for (auto const& value : vec)
-      {
-        m_output_stream << " --" << typed_option.m_name;
-        m_output_stream << " " << value;
-      }
-    }
-  }
-
-  template <typename TTypes>
-  void add_impl(base_option& options)
-  {
-    if (serialize_if_t<typename TTypes::head>(options)) { return; }
-    add_impl<typename TTypes::tail>(options);
-  }
-
   std::stringstream m_output_stream;
 };
 
-template <>
-void options_serializer_boost_po::serialize<bool>(typed_option<bool>& typed_argument);
-
-template <>
-void options_serializer_boost_po::add_impl<typelist<>>(base_option& options);
 }  // namespace config
 }  // namespace VW
