fe8a767e02f48ed09e20a1cbf2ab1fd9f35899a2
Jack Gerrits <jack@jackgerrits.com>
2022-02-04 10:43:11 -0500

    build: use standard BUILD_TESTING flag in build and steer users away from installing vendored deps

--- .scripts/linux/build-slim.sh
+++ .scripts/linux/build-slim.sh
@@ -7,5 +7,5 @@ REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
 rm -rf build
-cmake -S ./vowpalwabbit/slim -B build -G Ninja -DBUILD_TESTS=On
+cmake -S ./vowpalwabbit/slim -B build -G Ninja -DBUILD_TESTING=On
 cmake --build build --target vwslim vw-slim-test
