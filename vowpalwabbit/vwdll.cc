9d05b90aca6d61df9755a3d82dd948f35652f587
Jack Gerrits <jack@jackgerrits.com>
2022-02-15 12:30:09 -0500

    fix dll

--- vowpalwabbit/vwdll.cc
+++ vowpalwabbit/vwdll.cc
@@ -54,7 +54,8 @@ extern "C"
     return VW_InitializeA(utf16_to_utf8(pstrArgs).c_str());
   }
 
-  // Note: this not functions the same as VW_InitializeA as the command line is interpreted with knowledge of quotes and escapes
+  // Note: this not functions the same as VW_InitializeA as the command line is interpreted with knowledge of quotes and
+  // escapes
   VW_DLL_PUBLIC VW_HANDLE VW_CALLING_CONV VW_InitializeEscaped(const char16_t* pstrArgs)
   {
     return VW_InitializeEscapedA(utf16_to_utf8(pstrArgs).c_str());
@@ -68,7 +69,8 @@ extern "C"
     return static_cast<VW_HANDLE>(all);
   }
 
-  // Note: this not functions the same as VW_InitializeA as the command line is interpreted with knowledge of quotes and escapes
+  // Note: this not functions the same as VW_InitializeA as the command line is interpreted with knowledge of quotes and
+  // escapes
   VW_DLL_PUBLIC VW_HANDLE VW_CALLING_CONV VW_InitializeEscapedA(const char* pstrArgs)
   {
     std::string s(pstrArgs);
