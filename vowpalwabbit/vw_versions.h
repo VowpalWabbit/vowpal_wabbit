1379b45e2d2c2eccf6247365db30d1876350400c
Jack Gerrits <jack@jackgerrits.com>
2022-02-17 16:13:36 -0500

    Update version

--- vowpalwabbit/vw_versions.h
+++ vowpalwabbit/vw_versions.h
@@ -63,7 +63,7 @@ constexpr VW::version_struct VERSION_FILE_WITH_ACTIVE_SEEN_LABELS{9, 0, 0};
 /// Moved option values from command line to model data
 constexpr VW::version_struct VERSION_FILE_WITH_L1_AND_L2_STATE_IN_MODEL_DATA{9, 0, 0};
 
-constexpr VW::version_struct VERSION_FILE_WITH_ESCAPED_COMMAND_LINE{9, 0, 0};
+constexpr VW::version_struct VERSION_FILE_WITH_ESCAPED_COMMAND_LINE{9, 1, 0};
 
 }  // namespace version_definitions
 }  // namespace VW
