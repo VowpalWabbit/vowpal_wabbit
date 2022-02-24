c9c25dea2e52e2d94a857b5d741b43a90ac73ddc
Griffin Bassman <griffinbassman@gmail.com>
2022-02-24 13:27:11 -0500

    dont throw for slim

--- vowpalwabbit/epsilon_reduction_features.h
+++ vowpalwabbit/epsilon_reduction_features.h
@@ -16,15 +16,7 @@ struct reduction_features
 
   reduction_features() { epsilon = -1.f; }
 
-  bool is_epsilon_supplied()
-  {
-    if (epsilon >= 0.f && epsilon <= 1.f) { return true; }
-    else if (epsilon == -1.f)
-    {
-      return false;
-    }
-    THROW("Invalid epsilon value: " << epsilon << " supplied. Epsilon must be between 0 and 1 inclusive.");
-  }
+  bool valid_epsilon_supplied() { return epsilon >= 0.f && epsilon <= 1.f; }
   void reset_to_default() { epsilon = -1.f; }
 };
 }  // namespace greedy
