41cc930470932df017147a09f8a1d199afaab6fe
Alexey Taymanov <ataymano@gmail.com>
2022-03-03 10:14:12 -0500

    condition removal fix

--- vowpalwabbit/reductions/csoaa_ldf.cc
+++ vowpalwabbit/reductions/csoaa_ldf.cc
@@ -50,7 +50,7 @@ struct ldf
 bool ec_is_label_definition(const example& ec)  // label defs look like "0:___" or just "label:___"
 {
   if (ec.indices.empty()) return false;
-//  if (ec.indices[0] != 'l') return false;
+  return false;
   const auto& costs = ec.l.cs.costs;
   for (auto const& cost : costs)
     if ((cost.class_index != 0) || (cost.x <= 0.)) return false;
