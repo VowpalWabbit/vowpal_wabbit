9dd7d868bfdce743651ac5417750da3cfabc0723
Jack Gerrits <jack@jackgerrits.com>
2022-03-08 17:18:17 -0500

    build: remove c_test as it is already covered in unit_tests

--- .scripts/test.cmd
+++ .scripts/test.cmd
@@ -7,15 +7,6 @@ SETLOCAL
 
 CALL %~dp0init.cmd
 
-REM TODO: Ensure all errors caught by the smoke test will result in non-zero exit codes
-ECHO Running VW C Smoke test
-"%vwRoot%\vowpalwabbit\out\target\Release\x64\c_test.exe"
-
-IF "%ERRORLEVEL%" NEQ "0" (
-    ENDLOCAL
-    EXIT /B %ERRORLEVEL%
-)
-
 ECHO Running VW Unit Tests in C++
 "%vwRoot%\vowpalwabbit\out\target\Release\x64\vw_unit_test.exe"
 
