0aefa531f75bb7b309dc61958ee53e6a9b3beea5
Jack Gerrits <jack@jackgerrits.com>
2022-03-10 22:50:49 -0500

    format

--- utl/flatbuffer/vw_to_flat.h
+++ utl/flatbuffer/vw_to_flat.h
@@ -85,6 +85,6 @@ private:
   void write_to_file(bool collection, bool is_multiline, MultiExampleBuilder& multi_ex_builder,
       ExampleBuilder& ex_builder, std::ofstream& outfile);
 
-  flatbuffers::Offset<VW::parsers::flatbuffer::Namespace> create_namespace(
-      features::audit_iterator begin, features::audit_iterator end, VW::namespace_index index, uint64_t hash, bool audit);
+  flatbuffers::Offset<VW::parsers::flatbuffer::Namespace> create_namespace(features::audit_iterator begin,
+      features::audit_iterator end, VW::namespace_index index, uint64_t hash, bool audit);
 };
