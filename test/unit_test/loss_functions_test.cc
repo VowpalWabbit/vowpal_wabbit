ff8d3e32053a745fa306323427291b213a298e0f
Farsang Mónika <monika.farsang@hotmail.com>
2022-03-17 21:52:46 +0100

    Added a unit test for expectile loss

--- test/unit_test/loss_functions_test.cc
+++ test/unit_test/loss_functions_test.cc
@@ -38,3 +38,35 @@ BOOST_AUTO_TEST_CASE(squared_loss_test)
 
   VW::finish(vw);
 }
+
+BOOST_AUTO_TEST_CASE(expectile_loss_test)
+{
+  auto& vw = *VW::initialize("--quiet");
+  const std::string loss_type("expectile");
+  const float parameter(0.4f);
+
+  auto loss = getLossFunction(vw, loss_type, parameter);
+  shared_data sd;
+  sd.min_label = 0.0f;
+  sd.max_label = 1.0f;
+  constexpr float eta = 0.1f;     // learning rate
+  constexpr float weight = 1.0f;  // example weight
+
+  constexpr float label = 0.5f;
+  constexpr float prediction = 0.4f;
+  constexpr float update_scale = eta * weight;
+  constexpr float pred_per_update = 1.0f;  // Use dummy value here, see gd.cc for details.
+
+  BOOST_CHECK_EQUAL(loss_type, loss->getType());
+  BOOST_CHECK_CLOSE(parameter, loss->getParameter(), FLOAT_TOL);
+
+  BOOST_CHECK_CLOSE(0.004f, loss->getLoss(&sd, prediction, label), FLOAT_TOL);
+  BOOST_CHECK_CLOSE(0.007688365f, loss->getUpdate(prediction, label, update_scale, pred_per_update), FLOAT_TOL);
+  BOOST_CHECK_CLOSE(0.008f, loss->getUnsafeUpdate(prediction, label, update_scale), FLOAT_TOL);
+
+  BOOST_CHECK_CLOSE(0.0064f, loss->getSquareGrad(prediction, label), FLOAT_TOL);
+  BOOST_CHECK_CLOSE(-0.08f, loss->first_derivative(&sd, prediction, label), FLOAT_TOL);
+  BOOST_CHECK_CLOSE(0.8f, loss->second_derivative(&sd, prediction, label), FLOAT_TOL);
+
+  VW::finish(vw);
+}
