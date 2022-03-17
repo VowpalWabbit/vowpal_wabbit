c66594b26aef2e4ec7eeb150097a596824bd87a2
Jack Gerrits <jack@jackgerrits.com>
2022-03-17 12:23:54 -0400

    change path handling for vwbin

--- test/run_tests.py
+++ test/run_tests.py
@@ -1064,6 +1064,9 @@ def main():
     if vw_bin is None:
         print("Can't find vw binary. Did you build the 'vw-bin' target?")
         sys.exit(1)
+    # test if vw_bin is a Path object
+    elif isinstance(vw_bin, Path):
+        vw_bin = str(vw_bin.resolve())
     print(f"Using VW binary: {vw_bin}")
 
     spanning_tree_bin: Optional[Path] = None
