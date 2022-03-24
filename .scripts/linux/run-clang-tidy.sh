e2db329d40b5d7efbdb51661567ab1c6f2972b1f
Jack Gerrits <jack@jackgerrits.com>
2022-03-24 10:03:56 -0400

    ci: consolidate .clang-tidy config file

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$(realpath $SCRIPT_DIR/../../)
 cd $REPO_DIR
 
-CONFIG=$(cat $REPO_DIR/.scripts/linux/clang-tidy-ci-config.yml | tr -d "\n")
+CONFIG=$(cat $REPO_DIR/.clang-tidy | tr -d "\n")
 cmake -S . -B build -G Ninja -D "CMAKE_CXX_CLANG_TIDY=clang-tidy;-config=$CONFIG;-header-filter=$REPO_DIR/vowpalwabbit/*"
 cmake --build build --target vw-bin
