79eca8b9bdbce18360673b5f2e56d66475fa3275
Jack Gerrits <jack@jackgerrits.com>
2022-04-08 12:19:41 -0400

    use older syntax

--- .scripts/linux/test-slim.sh
+++ .scripts/linux/test-slim.sh
@@ -6,4 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
-ctest --test-dir build -R vw_slim_test --extra-verbose  --output-on-failure
+cd build
+ctest -R vw_slim_test --extra-verbose  --output-on-failure
