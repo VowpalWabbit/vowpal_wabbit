d3c5c8e14ddd7f449903b50b56836ba1af2658e6
Jack Gerrits <jack@jackgerrits.com>
2022-04-15 12:52:21 -0400

    check versions

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -6,5 +6,9 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
+cmake --version
+
+clang --version
+
 cmake -S . -B build -G Ninja -DFMT_SYS_DEP=On -DSPDLOG_SYS_DEP=On -DVW_BOOST_MATH_SYS_DEP=On -DBUILD_TESTING=Off -DDO_NOT_BUILD_VW_C_WRAPPER=On
 run-clang-tidy -p build -header-filter=vw/*
