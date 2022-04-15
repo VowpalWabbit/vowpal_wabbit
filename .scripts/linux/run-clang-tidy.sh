56f26fe079dd585d7e60a09854bee8aeb8971458
Jack Gerrits <jack@jackgerrits.com>
2022-04-15 13:01:17 -0400

    fix compile commands not existing

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -6,9 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
-cmake --version
-
-clang --version
-
-cmake -S . -B build -G Ninja -DFMT_SYS_DEP=On -DSPDLOG_SYS_DEP=On -DVW_BOOST_MATH_SYS_DEP=On -DBUILD_TESTING=Off -DDO_NOT_BUILD_VW_C_WRAPPER=On
+# -DCMAKE_EXPORT_COMPILE_COMMANDS=On is manually set because in CMake 3.16, just setting it in the CMakeLists.txt does not work.
+cmake -S . -B build -DFMT_SYS_DEP=On -DSPDLOG_SYS_DEP=On -DVW_BOOST_MATH_SYS_DEP=On -DBUILD_TESTING=Off -DDO_NOT_BUILD_VW_C_WRAPPER=On -DCMAKE_EXPORT_COMPILE_COMMANDS=On
 run-clang-tidy -p build -header-filter=vw/*
