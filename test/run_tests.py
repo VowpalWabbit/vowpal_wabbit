8fdc68506e5959bec2f5fdc08c002f52711c7766
Eduardo Salinas <edus@microsoft.com>
2022-04-21 16:27:19 -0400

    ignore 400 in flatbuffer

--- test/run_tests.py
+++ test/run_tests.py
@@ -785,6 +785,7 @@ def convert_tests_for_flatbuffers(
             "337",
             "338",
             "351",
+            "400",
         ):
             test.skip = True
             test.skip_reason = "test skipped for automatic converted flatbuffer tests for unknown reason"
