ad85cc8c276f04db2d109a59255e2dd6f07aac42
Olga Vrousgou <olgavrou@gmail.com>
2022-04-21 13:50:35 -0400

    organise test a bit

--- test/unit_test/cb_large_actions_test.cc
+++ test/unit_test/cb_large_actions_test.cc
@@ -11,10 +11,58 @@
 
 #include <boost/test/unit_test.hpp>
 
+using internal_action_space =
+    VW::cb_explore_adf::cb_explore_adf_base<VW::cb_explore_adf::cb_explore_adf_large_action_space>;
+
+void check_matrix_multiplication(
+    VW::workspace& vw, internal_action_space* action_space, const VW::multi_ex& examples, uint64_t d)
+{
+  // assuming no shared features or interactions at this point since doing manual calculations by re-using the raw
+  // unprocessed example
+
+  auto num_actions = examples.size();
+  // gather the feature indexes
+
+  for (size_t action_index = 0; action_index < num_actions; action_index++)
+  {
+    auto* ex = examples[action_index];
+
+    std::vector<feature_index> ft_indexes;
+    // test sanity - test assumes no shared features
+    BOOST_CHECK_EQUAL(!CB::ec_is_example_header(*ex), true);
+    for (auto ns : ex->indices)
+    {
+      for (auto ft_index : ex->feature_space[ns].indices) { ft_indexes.push_back(ft_index); }
+    }
+
+    std::vector<weight> ft_weights;
+    for (auto ft_index : ft_indexes) { ft_weights.push_back(vw.weights.dense_weights[ft_index]); }
+
+    uint64_t seed = vw.get_random_state()->get_current_state();
+
+    // checking the multiplication of matrix A of vw action weights dim(#actions x #features) which is a sparse
+    // matrix, with the random matrix with Gaussian entries of dim(#features x d)
+    for (uint64_t gaussian_col = 0; gaussian_col < d; gaussian_col++)
+    {
+      float dot_product = 0;
+      for (auto ft_index : ft_indexes)
+      {
+        auto gaussian_row = ft_index;
+        auto combined_index = gaussian_row + gaussian_col + seed;
+        auto lazyg_cell = merand48_boxmuller(combined_index);
+        auto Action_matrix_cell_weight = vw.weights.dense_weights[ft_index];
+        dot_product += Action_matrix_cell_weight * lazyg_cell;
+      }
+      BOOST_CHECK_EQUAL(dot_product, action_space->explore.Q(action_index, gaussian_col));
+    }
+  }
+}
+
 BOOST_AUTO_TEST_CASE(creation_of_Q_with_lazy_gaussian)
 {
-  auto& vw = *VW::initialize(
-      "--cb_explore_adf --large_action_space --max_actions 2 --quiet", nullptr, false, nullptr, nullptr);
+  auto d = 2;
+  auto& vw = *VW::initialize("--cb_explore_adf --large_action_space --max_actions " + std::to_string(d) + " --quiet",
+      nullptr, false, nullptr, nullptr);
 
   {
     VW::multi_ex examples;
@@ -23,6 +71,7 @@ BOOST_AUTO_TEST_CASE(creation_of_Q_with_lazy_gaussian)
 
     vw.learn(examples);
   }
+
   std::vector<std::string> e_r;
   vw.l->get_enabled_reductions(e_r);
   if (std::find(e_r.begin(), e_r.end(), "cb_explore_adf_large_action_space") == e_r.end())
@@ -31,8 +80,7 @@ BOOST_AUTO_TEST_CASE(creation_of_Q_with_lazy_gaussian)
   VW::LEARNER::multi_learner* learner =
       as_multiline(vw.l->get_learner_by_name_prefix("cb_explore_adf_large_action_space"));
 
-  auto action_space = (VW::cb_explore_adf::cb_explore_adf_base<VW::cb_explore_adf::cb_explore_adf_large_action_space>*)
-                          learner->get_internal_type_erased_data_pointer_test_use_only();
+  auto action_space = (internal_action_space*)learner->get_internal_type_erased_data_pointer_test_use_only();
 
   BOOST_CHECK_EQUAL(action_space != nullptr, true);
 
@@ -46,69 +94,12 @@ BOOST_AUTO_TEST_CASE(creation_of_Q_with_lazy_gaussian)
     action_space->explore.generate_Q(examples);
     std::cout << action_space->explore.Q << std::endl;
 
-    // feature 1 -> stride shifted
-    // feature 2 -> stride shifted
-    // feature 3 -> stride shifted
-    auto f1_i = 1 << 2;
-    auto f2_i = 2 << 2;
-    auto f3_i = 3 << 2;
-    auto constant_i = examples[0]->feature_space[constant_namespace].indices[0];
+    check_matrix_multiplication(vw, action_space, examples, d);
 
-    auto fw1 = vw.weights.dense_weights[f1_i];
-    auto fw2 = vw.weights.dense_weights[f2_i];
-    auto fw3 = vw.weights.dense_weights[f3_i];
-
-    auto fw_constant = vw.weights.dense_weights[constant_i];
-
-    uint64_t seed = vw.get_random_state()->get_current_state();
-    // checking the multiplication of matrix A of vw action weights dim(#actions x #features) which is a sparse
-    // matrix, with the random matrix with Gaussian entries of dim(#features x d)
-
-    {
-      uint64_t gauss_col = 0;
-
-      uint64_t A_row = f1_i;
-      auto combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_0_row_f1 = merand48_boxmuller(combined_index);
-
-      A_row = f2_i;
-      combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_0_row_f2 = merand48_boxmuller(combined_index);
-
-      A_row = f3_i;
-      combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_0_row_f3 = merand48_boxmuller(combined_index);
-
-      A_row = constant_i;
-      combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_0_row_const = merand48_boxmuller(combined_index);
-
-      float dot_product = fw1 * lazyg_col_0_row_f1 + fw2 * lazyg_col_0_row_f2 + fw3 * lazyg_col_0_row_f3 +
-          fw_constant * lazyg_col_0_row_const;
-      BOOST_CHECK_EQUAL(dot_product, action_space->explore.Q(0, gauss_col));
-    }
-    {
-      uint64_t gauss_col = 1;
-
-      uint64_t A_row = f1_i;
-      auto combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_1_row_f1 = merand48_boxmuller(combined_index);
-
-      A_row = f2_i;
-      combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_1_row_f2 = merand48_boxmuller(combined_index);
-
-      A_row = f3_i;
-      combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_1_row_f3 = merand48_boxmuller(combined_index);
-
-      A_row = constant_i;
-      combined_index = A_row + gauss_col + seed;
-      auto lazyg_col_1_row_const = merand48_boxmuller(combined_index);
-
-      float dot_product = fw1 * lazyg_col_1_row_f1 + fw2 * lazyg_col_1_row_f2 + fw3 * lazyg_col_1_row_f3 +
-          fw_constant * lazyg_col_1_row_const;
-      BOOST_CHECK_EQUAL(dot_product, action_space->explore.Q(0, gauss_col));
-    }
+    // check that feature values do not affect the matrix manipulation as they are not part of it
+    // push new example but since we did not run predict, Q should be the same
+    examples.clear();
+    examples.push_back(VW::read_example(vw, "0:1.0:0.5 | 1:100 2:100 3:100"));
+    check_matrix_multiplication(vw, action_space, examples, d);
   }
 }
