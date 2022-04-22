20f2be589365c10e5549e940eaa3ae6a8cb808ae
Eduardo Salinas <edus@microsoft.com>
2022-04-22 12:24:52 -0400

    Update run_tests.py

--- test/run_tests.py
+++ test/run_tests.py
@@ -788,6 +788,7 @@ def convert_tests_for_flatbuffers(
             "399",
             "400",
             "404",
+            "405",
         ):
             test.skip = True
             test.skip_reason = "test skipped for automatic converted flatbuffer tests for unknown reason"
