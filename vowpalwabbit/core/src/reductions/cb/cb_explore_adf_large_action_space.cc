c1e48a39e6956e256425a91773bb60192ff8480a
olgavrou <olgavrou@gmail.com>
2022-05-04 11:09:09 -0400

    Update vowpalwabbit/core/src/reductions/cb/cb_explore_adf_large_action_space.cc
    Co-authored-by: Jack Gerrits <jackgerrits@users.noreply.github.com>

--- vowpalwabbit/core/src/reductions/cb/cb_explore_adf_large_action_space.cc
+++ vowpalwabbit/core/src/reductions/cb/cb_explore_adf_large_action_space.cc
@@ -212,7 +212,7 @@ void cb_explore_adf_large_action_space::predict_or_learn_impl(VW::LEARNER::multi
       if (!generate_A(examples)) { return; }
 
       float min_ck = std::min_element(preds.begin(), preds.end(),
-          [](ACTION_SCORE::action_score& a, ACTION_SCORE::action_score& b) { return a.score < b.score; })
+          [](const ACTION_SCORE::action_score& a, const ACTION_SCORE::action_score& b) { return a.score < b.score; })
                          ->score;
 
       calculate_shrink_factor(preds, min_ck);
