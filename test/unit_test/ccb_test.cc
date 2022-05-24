063207a14d5d9163c3dcdaeae0695ad4ed46e652
Eduardo Salinas <edus@microsoft.com>
2022-05-24 14:19:17 -0400

    Update ccb_test.cc

--- test/unit_test/ccb_test.cc
+++ test/unit_test/ccb_test.cc
@@ -125,7 +125,7 @@ std::string ns_to_str(unsigned char ns)
     return std::string(1, ns);
 }
 
-std::set<std::string> interaction_vec_t_to_set(const std::vector<std::vector<namespace_index>>& interactions)
+std::set<std::string> interaction_vec_t_to_set(const std::vector<std::vector<VW::namespace_index>>& interactions)
 {
   std::set<std::string> result;
   std::stringstream ss;
