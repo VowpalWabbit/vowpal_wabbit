2b8f66394e2ffc34f34c56ef7e114fa4626082e9
Griffin Bassman <griffinbassman@gmail.com>
2022-06-09 09:57:09 -0400

    remove java static build

--- .scripts/linux/build-static-java.sh
+++ .scripts/linux/build-static-java.sh
@@ -11,7 +11,7 @@ cd build
 # Boost unit tests don't like the static linking
 # /usr/local/bin/gcc + g++ is 9.2.0 version
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWARNINGS=Off -DBUILD_JAVA=On -DBUILD_DOCS=Off -DBUILD_FLATBUFFERS=On\
- -DBUILD_PYTHON=Off -DSTATIC_LINK_VW_JAVA=On -DCMAKE_C_COMPILER=/usr/local/bin/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ \
+ -DBUILD_PYTHON=Off -DCMAKE_C_COMPILER=/usr/local/bin/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ \
  -DBUILD_TESTING=Off
 NUM_PROCESSORS=$(nproc)
 make all -j ${NUM_PROCESSORS}
