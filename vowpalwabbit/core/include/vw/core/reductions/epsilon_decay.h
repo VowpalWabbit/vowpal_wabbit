79adbfdb6ced2b9e218a863fd3f016b6421f963f
Griffin Bassman <griffinbassman@gmail.com>
2022-06-15 15:29:51 -0400

    fix: [Epsilon Decay] Keep weights consistent across models (#3920)
    * fix: keep weights consistant across models
    
    * clang
    
    * clang
    
    * add constant_epsilon
    
    * lint
    
    * address comment
    
    * add constant epsilon to learn
    
    * reset lower bound with reset_stats
    
    * change model_idx to score_idx
    
    * separate sections of learn
    
    * clang
    
    * refactor and add unit tets
    
    * clang
    
    * modularize functions
    
    * clang
    
    * address comments
    
    * clang
    
    * commented changes
    
    * small optimizations
    
    * clang
    
    * emplace
    
    * revert
    
    * revert iota
    
    * resize
    
    * remove score count
    
    * comments
    
    * clang
    
    Co-authored-by: Jack Gerrits <jackgerrits@users.noreply.github.com>

--- vowpalwabbit/core/include/vw/core/reductions/epsilon_decay.h
+++ vowpalwabbit/core/include/vw/core/reductions/epsilon_decay.h
@@ -10,6 +10,7 @@
 #include "vw/io/logger.h"
 
 #include <algorithm>
+#include <numeric>
 
 namespace VW
 {
@@ -22,47 +23,50 @@ namespace epsilon_decay
 struct epsilon_decay_score : scored_config
 {
   epsilon_decay_score() = default;
-  epsilon_decay_score(double alpha, double tau, uint64_t model_idx)
-      : VW::scored_config(alpha, tau), _model_idx(model_idx)
-  {
-  }
+  epsilon_decay_score(double alpha, double tau) : VW::scored_config(alpha, tau) {}
   float decayed_epsilon(uint64_t update_count);
-  uint64_t get_model_idx() const { return _model_idx; }
-
-  uint64_t _model_idx;
 };
 
 struct epsilon_decay_data
 {
-  epsilon_decay_data(uint64_t num_configs, uint64_t min_scope, double epsilon_decay_alpha, double epsilon_decay_tau,
-      parameters& weights, VW::io::logger logger, bool log_champ_changes)
+  epsilon_decay_data(uint64_t model_count, uint64_t min_scope, double epsilon_decay_alpha, double epsilon_decay_tau,
+      dense_parameters& weights, VW::io::logger logger, bool log_champ_changes, bool constant_epsilon)
       : _min_scope(min_scope)
       , _epsilon_decay_alpha(epsilon_decay_alpha)
       , _epsilon_decay_tau(epsilon_decay_tau)
       , _weights(weights)
       , _logger(std::move(logger))
       , _log_champ_changes(log_champ_changes)
+      , _constant_epsilon(constant_epsilon)
   {
-    uint64_t model_idx = 0;
-    for (uint64_t i = 0; i < num_configs; ++i)
+    _scored_configs.reserve(model_count);
+    _weight_indices.resize(model_count);  // resize required for iota
+    std::iota(_weight_indices.begin(), _weight_indices.end(), 0);
+    for (uint64_t i = 0; i < model_count; ++i)
     {
-      std::vector<epsilon_decay_score> score_vec;
+      _scored_configs.emplace_back(i + 1);  // Model i (0-indexed) will have (i + 1) scores
       for (uint64_t j = 0; j < i + 1; ++j)
-      {
-        epsilon_decay_score s(epsilon_decay_alpha, epsilon_decay_tau, model_idx);
-        score_vec.push_back(s);
-        ++model_idx;
-      }
-      _scored_configs.push_back(score_vec);
+      { _scored_configs.back().emplace_back(epsilon_decay_alpha, epsilon_decay_tau); }
     }
   }
+
+  void update_weights(VW::LEARNER::multi_learner& base, VW::multi_ex& examples);
+  void promote_model(int64_t model_ind, int64_t swap_dist);
+  void rebalance_greater_models(int64_t model_ind, int64_t swap_dist, int64_t model_count);
+  void clear_weights_and_scores(int64_t swap_dist, int64_t model_count);
+  void shift_model(int64_t model_ind, int64_t swap_dist, int64_t model_count);
+  void check_score_bounds();
+  void check_horizon_bounds();
+
   std::vector<std::vector<epsilon_decay_score>> _scored_configs;
+  std::vector<uint64_t> _weight_indices;
   uint64_t _min_scope;
   double _epsilon_decay_alpha;  // Confidence interval
   double _epsilon_decay_tau;    // Count decay time constant
-  parameters& _weights;
+  dense_parameters& _weights;
   VW::io::logger _logger;
   bool _log_champ_changes;
+  bool _constant_epsilon;
 };
 
 }  // namespace epsilon_decay
