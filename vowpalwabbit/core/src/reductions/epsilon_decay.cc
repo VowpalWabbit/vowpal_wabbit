5df5c4d7f4645547127a5cf91656ca68d6566ade
Griffin Bassman <griffinbassman@gmail.com>
2022-06-28 18:23:44 -0400

    feat: [automl/ep_decay] lb_trick option to use 1-lower_bound for upper_bound (#4002)
    * clang
    
    * clang
    
    * clang
    
    * clang
    
    Co-authored-by: Eduardo Salinas <edus@microsoft.com>

--- vowpalwabbit/core/src/reductions/epsilon_decay.cc
+++ vowpalwabbit/core/src/reductions/epsilon_decay.cc
@@ -30,7 +30,7 @@ float decayed_epsilon(uint64_t update_count) { return static_cast<float>(std::po
 
 epsilon_decay_data::epsilon_decay_data(uint64_t model_count, uint64_t min_scope,
     double epsilon_decay_significance_level, double epsilon_decay_estimator_decay, dense_parameters& weights,
-    VW::io::logger logger, bool log_champ_changes, bool constant_epsilon, uint32_t& wpp)
+    VW::io::logger logger, bool log_champ_changes, bool constant_epsilon, uint32_t& wpp, bool lb_trick)
     : _min_scope(min_scope)
     , _epsilon_decay_significance_level(epsilon_decay_significance_level)
     , _epsilon_decay_estimator_decay(epsilon_decay_estimator_decay)
@@ -39,6 +39,7 @@ epsilon_decay_data::epsilon_decay_data(uint64_t model_count, uint64_t min_scope,
     , _log_champ_changes(log_champ_changes)
     , _constant_epsilon(constant_epsilon)
     , _wpp(wpp)
+    , _lb_trick(lb_trick)
 {
   _weight_indices.resize(model_count);
   _scored_configs.reserve(model_count);
@@ -142,7 +143,10 @@ void epsilon_decay_data::check_score_bounds()
   auto final_model_idx = model_count - 1;
   for (int64_t i = 0; i < final_model_idx; ++i)
   {
-    if (_scored_configs[i][i].lower_bound() > _scored_configs[final_model_idx][i].upper_bound())
+    bool better = _lb_trick
+        ? _scored_configs[i][i].lower_bound() > (1.f - _scored_configs[final_model_idx][i].lower_bound())
+        : _scored_configs[i][i].lower_bound() > _scored_configs[final_model_idx][i].upper_bound();
+    if (better)
     {
       if (_log_champ_changes)
       {
@@ -260,6 +264,7 @@ VW::LEARNER::base_learner* VW::reductions::epsilon_decay_setup(VW::setup_base_i&
   bool _log_champ_changes = false;
   bool _constant_epsilon = false;
   bool _bonferroni = false;
+  bool _lb_trick = false;
 
   option_group_definition new_options("[Reduction] Epsilon-Decaying Exploration");
   new_options
@@ -296,6 +301,10 @@ VW::LEARNER::base_learner* VW::reductions::epsilon_decay_setup(VW::setup_base_i&
       .add(make_option("bonferroni", _bonferroni)
                .keep()
                .help("Use bonferroni correction (divide confidence interval by model count)")
+               .experimental())
+      .add(make_option("lb_trick", _lb_trick)
+               .default_value(false)
+               .help("Use 1-lower_bound as upper_bound for estimator")
                .experimental());
 
   if (!options.add_parse_and_check_necessary(new_options)) { return nullptr; }
@@ -308,7 +317,7 @@ VW::LEARNER::base_learner* VW::reductions::epsilon_decay_setup(VW::setup_base_i&
 
   auto data = VW::make_unique<VW::reductions::epsilon_decay::epsilon_decay_data>(model_count, _min_scope, scaled_alpha,
       _epsilon_decay_estimator_decay, all.weights.dense_weights, all.logger, _log_champ_changes, _constant_epsilon,
-      all.wpp);
+      all.wpp, _lb_trick);
 
   // make sure we setup the rest of the stack with cleared interactions
   // to make sure there are not subtle bugs
