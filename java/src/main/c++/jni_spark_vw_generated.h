4f99d14abf3642154496965879f4003699f8faee
Markus Cozowicz <marcozo@microsoft.com>
2022-07-05 21:48:32 +0200

    reverse messed merge

--- java/src/main/c++/jni_spark_vw_generated.h
+++ java/src/main/c++/jni_spark_vw_generated.h
@@ -162,28 +162,10 @@ extern "C"
 
   JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setMultiLabels(JNIEnv*, jobject, jintArray);
 
-  JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setMulticlassLabel(
-      JNIEnv*, jobject, jfloat, jint);
-
-  JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setCostSensitiveLabels(
-      JNIEnv*, jobject, jfloatArray, jintArray);
-
-  JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setContextualBanditContinuousLabel(
-      JNIEnv*, jobject, jfloatArray, jfloatArray, jfloatArray);
-
-  JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setMultiLabels(JNIEnv*, jobject, jintArray);
-
   JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setContextualBanditLabel(
       JNIEnv*, jobject, jint, jdouble, jdouble);
 
   JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setSharedLabel(JNIEnv*, jobject);
-
-  JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setSlatesSharedLabel(JNIEnv*, jobject, jfloat);
-
-  JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setSlatesActionLabel(JNIEnv*, jobject, jint);
-
-  JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setSlatesSlotLabel(
-      JNIEnv*, jobject, jintArray, jfloatArray);
 
   JNIEXPORT void JNICALL Java_org_vowpalwabbit_spark_VowpalWabbitExample_setSlatesSharedLabel(JNIEnv*, jobject, jfloat);
 
