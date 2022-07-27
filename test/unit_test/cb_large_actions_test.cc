ec3a9aa924c0806333e67340f447e26e0b373ddb
olgavrou <olgavrou@gmail.com>
2022-07-27 12:31:07 -0400

    remove test that is missing the svd step

--- test/unit_test/cb_large_actions_test.cc
+++ test/unit_test/cb_large_actions_test.cc
@@ -613,13 +613,6 @@ BOOST_AUTO_TEST_CASE(check_final_truncated_SVD_validity)
 
   vws.push_back({vw_model_weight_w_interactions_sq, true});
 
-  auto* vw_aatop_w_interactions_sq =
-      VW::initialize("--cb_explore_adf --squarecb --large_action_space --full_predictions --max_actions " +
-              std::to_string(d) + " --quiet --random_seed 5 -q :: --aatop",
-          nullptr, false, nullptr, nullptr);
-
-  vws.push_back({vw_aatop_w_interactions_sq, true});
-
   auto* vw_w_interactions_sq_sparse_weights = VW::initialize(
       "--cb_explore_adf --squarecb --sparse_weights --large_action_space --full_predictions --max_actions " +
           std::to_string(d) + " --quiet --random_seed 5 -q ::",
