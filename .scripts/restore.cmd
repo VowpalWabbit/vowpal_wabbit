03a0f2a6b67a8ce1584d30e3a1ca2e482713ffd7
Jack Gerrits <jackgerrits95@gmail.com>
2022-07-29 11:18:33 -0400

    dont run in repo dir to avoid triggering manifest mode usage

--- .scripts/restore.cmd
+++ .scripts/restore.cmd
@@ -6,11 +6,8 @@ IF DEFINED DebugBuildScripts (
 SETLOCAL
 
 CALL %~dp0init.cmd
-PUSHD %~dp0
 
 REM TODO: This really should be out-of-source
 %VCPKG_INSTALLATION_ROOT%\vcpkg install flatbuffers:x64-windows boost-test:x64-windows
-
-POPD
 
 ENDLOCAL
