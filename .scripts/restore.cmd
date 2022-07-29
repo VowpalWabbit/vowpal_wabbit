a8d687807a1a7c18ac62becc43017209305cc4af
Jack Gerrits <jackgerrits95@gmail.com>
2022-07-29 11:31:03 -0400

    try fix vcpkg win

--- .scripts/restore.cmd
+++ .scripts/restore.cmd
@@ -7,6 +7,9 @@ SETLOCAL
 
 CALL %~dp0init.cmd
 
+REM CD out of the repo dir as we need to avoid vcpkg recognizing the manifest
+cd ..
+
 REM TODO: This really should be out-of-source
 %VCPKG_INSTALLATION_ROOT%\vcpkg install flatbuffers:x64-windows boost-test:x64-windows
 
