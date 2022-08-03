6fae1b9f3b3de836169f7079f2d558d91b883530
Jack Gerrits <jackgerrits95@gmail.com>
2022-08-03 13:48:19 -0400

    format

--- vowpalwabbit/core/include/vw/core/merge.h
+++ vowpalwabbit/core/include/vw/core/merge.h
@@ -8,16 +8,16 @@
 namespace VW
 {
 /**
-* Merge several workspace objects into a single workspace. This merges weights
-* and all training state. All given workspaces must be compatible, meaning they
-* should have the same reduction stack and same training based options.
-*
-* Note: This is an experimental API.
-*
-* @param workspaces_to_merge List of workspaces to merge.
-* @param logger Optional logger to be used for logging during function and is given to the resulting workspace
-* @return std::unique_ptr<VW::workspace> Pointer to the resulting workspace.
-*/
+ * Merge several workspace objects into a single workspace. This merges weights
+ * and all training state. All given workspaces must be compatible, meaning they
+ * should have the same reduction stack and same training based options.
+ *
+ * Note: This is an experimental API.
+ *
+ * @param workspaces_to_merge List of workspaces to merge.
+ * @param logger Optional logger to be used for logging during function and is given to the resulting workspace
+ * @return std::unique_ptr<VW::workspace> Pointer to the resulting workspace.
+ */
 std::unique_ptr<VW::workspace> merge_models(
     const std::vector<const VW::workspace*>& workspaces_to_merge, VW::io::logger* logger = nullptr);
-}
+}  // namespace VW
