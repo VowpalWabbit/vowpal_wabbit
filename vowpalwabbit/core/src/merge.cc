5600ca0253e5918bf2154fe8bffb4e303f7d88fd
Jack Gerrits <jackgerrits95@gmail.com>
2022-08-03 11:29:57 -0400

    fix nullptr deref

--- vowpalwabbit/core/src/merge.cc
+++ vowpalwabbit/core/src/merge.cc
@@ -74,8 +74,8 @@ void validate_compatability(const std::vector<const VW::workspace*>& workspaces_
     auto src_command_line = get_keep_command_line(*model);
     if (destination_command_line != src_command_line)
     {
-      THROW("Command lines are not identical between models. One: " << destination_command_line
-                                                                    << "\n Other: " << src_command_line);
+      THROW("Command lines are not identical between models. One: '" << destination_command_line
+                                                                    << "', Other: '" << src_command_line << "'");
     }
   }
 }
@@ -133,7 +133,10 @@ std::unique_ptr<VW::workspace> merge_models(
     }
     else if (!target_learner->has_merge() && target_learner->learner_defines_own_save_load())
     {
-      logger->warn("Reduction '{}' supports save/load but does not have a merge function defined. Merging will still run but this reduction will not be merged and may result in incorrect results.", target_learner->get_name());
+      if (logger != nullptr)
+      {
+        logger->warn("Reduction '{}' supports save/load but does not have a merge function defined. Merging will still run but this reduction will not be merged and may result in incorrect results.", target_learner->get_name());
+      }
     }
     target_learner = target_learner->get_learn_base();
   }
