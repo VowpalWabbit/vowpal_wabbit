b7badd165dde4ed6da5d714d0b008d96ae1ec141
olgavrou <olgavrou@gmail.com>
2022-08-05 12:56:56 -0400

    correct path

--- python/model_version_tests/test/test_models.py
+++ python/model_version_tests/test/test_models.py
@@ -1,7 +1,7 @@
 from vowpalwabbit import pyvw
 
 def test_cb_model_with_regularization():
-    vw = pyvw.vw(quiet=True, i="cb_explore_adf_model_with_regularization.vw")
+    vw = pyvw.vw(quiet=True, i="vw_generated_models/cb_explore_adf_model_with_regularization.vw")
     assert vw.get_weight_from_name("b", "User") != 0
     assert vw.get_weight_from_name("d", "Action") != 0
     assert vw.get_weight_from_name("e", "Action") != 0
