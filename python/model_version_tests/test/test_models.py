b4f035682d532c30b755a4db5918260bffa37f87
olgavrou <olgavrou@gmail.com>
2022-08-05 13:40:50 -0400

    make sure it fails

--- python/model_version_tests/test/test_models.py
+++ python/model_version_tests/test/test_models.py
@@ -1,8 +1,8 @@
-import vowpalwabbit
+from vowpalwabbit import pyvw
 
 
 def test_cb_model_with_regularization():
-    vw = vowpalwabbit.pyvw.Workspace(
+    vw = pyvw.vw(
         quiet=True, i="vw_generated_models/cb_explore_adf_model_with_regularization.vw"
     )
     assert vw.get_weight_from_name("b", "User") != 0
