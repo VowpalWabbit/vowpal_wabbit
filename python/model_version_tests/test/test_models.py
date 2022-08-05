ee745c5e041b30dd1b60bfbb9fac7e617fe20781
olgavrou <olgavrou@gmail.com>
2022-08-05 13:59:58 -0400

    revert breaking demonstration

--- python/model_version_tests/test/test_models.py
+++ python/model_version_tests/test/test_models.py
@@ -1,8 +1,8 @@
-from vowpalwabbit import pyvw
+import vowpalwabbit
 
 
 def test_cb_model_with_regularization():
-    vw = pyvw.vw(
+    vw = vowpalwabbit.pyvw.Workspace(
         quiet=True, i="vw_generated_models/cb_explore_adf_model_with_regularization.vw"
     )
     assert vw.get_weight_from_name("b", "User") != 0
