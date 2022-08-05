02a8b0dcb37730eb4903b8dc0602cc82986d7a58
olgavrou <olgavrou@gmail.com>
2022-08-05 12:51:55 -0400

    should fail

--- python/model_version_tests/test/test_models.py
+++ python/model_version_tests/test/test_models.py
@@ -1,7 +1,7 @@
 import vowpalwabbit
 
 def test_cb_model_with_regularization():
-    vw = vowpalwabbit.Workspace(quiet=True, model="vw_generated_models/cb_explore_adf_model_with_regularization.vw")
+    vw = vowpalwabbit.pyvw.vw(quiet=True, model="vw_generated_models/cb_explore_adf_model_with_regularization.vw")
     assert vw.get_weight_from_name("b", "User") != 0
     assert vw.get_weight_from_name("d", "Action") != 0
     assert vw.get_weight_from_name("e", "Action") != 0
