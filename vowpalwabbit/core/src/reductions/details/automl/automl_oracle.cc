fd7a26db217b2702723e39127f0cf9858763df0b
Eduardo Salinas <edus@microsoft.com>
2022-08-17 14:42:52 -0400

    refactor: [automl] prepare aml_estimator for other estimator impl (#4114)
    smaller refactors included:
    - remove aml_estimator_cress
    - template 'using' statement for vector of estimators pairs
    - shuffle code around
    - one better() func impl
    - move gen_interactions to config_oracle
    - move choose to config_oracle
    - remove ref inside oracle to ns_counter
    - remove estimator_config.h from automl_impl.h

--- vowpalwabbit/core/src/reductions/details/automl/automl_oracle.cc
+++ vowpalwabbit/core/src/reductions/details/automl/automl_oracle.cc
@@ -3,6 +3,7 @@
 // license as described in the file LICENSE.
 
 #include "../automl_impl.h"
+#include "vw/core/reductions/conditional_contextual_bandit.h"
 
 namespace VW
 {
@@ -12,11 +13,9 @@ namespace automl
 {
 template <>
 config_oracle<oracle_rand_impl>::config_oracle(uint64_t global_lease, priority_func* calc_priority,
-    std::map<namespace_index, uint64_t>& ns_counter, const std::string& interaction_type,
-    const std::string& oracle_type, std::shared_ptr<VW::rand_state>& rand_state)
+    const std::string& interaction_type, const std::string& oracle_type, std::shared_ptr<VW::rand_state>& rand_state)
     : _interaction_type(interaction_type)
     , _oracle_type(oracle_type)
-    , ns_counter(ns_counter)
     , calc_priority(calc_priority)
     , global_lease(global_lease)
     , _impl(oracle_rand_impl(std::move(rand_state)))
@@ -24,11 +23,9 @@ config_oracle<oracle_rand_impl>::config_oracle(uint64_t global_lease, priority_f
 }
 template <typename oracle_impl>
 config_oracle<oracle_impl>::config_oracle(uint64_t global_lease, priority_func* calc_priority,
-    std::map<namespace_index, uint64_t>& ns_counter, const std::string& interaction_type,
-    const std::string& oracle_type, std::shared_ptr<VW::rand_state>&)
+    const std::string& interaction_type, const std::string& oracle_type, std::shared_ptr<VW::rand_state>&)
     : _interaction_type(interaction_type)
     , _oracle_type(oracle_type)
-    , ns_counter(ns_counter)
     , calc_priority(calc_priority)
     , global_lease(global_lease)
     , _impl(oracle_impl())
@@ -42,10 +39,73 @@ void config_oracle<oracle_impl>::insert_qcolcol()
   ++valid_config_size;
 }
 
+template <typename oracle_impl>
+uint64_t config_oracle<oracle_impl>::choose(std::priority_queue<std::pair<float, uint64_t>>& index_queue)
+{
+  uint64_t ret = index_queue.top().second;
+  index_queue.pop();
+  return ret;
+}
+
+// This code is primarily borrowed from expand_quadratics_wildcard_interactions in
+// interactions.cc. It will generate interactions with -q :: and exclude namespaces
+// from the corresponding live_slot. This function can be swapped out depending on
+// preference of how to generate interactions from a given set of exclusions.
+// Transforms exclusions -> interactions expected by VW.
+template <typename oracle_impl>
+void config_oracle<oracle_impl>::gen_interactions_from_exclusions(const bool ccb_on,
+    const std::map<namespace_index, uint64_t>& ns_counter, const std::string& interaction_type,
+    const std::set<std::vector<namespace_index>>& exclusions, interaction_vec_t& interactions)
+{
+  if (interaction_type == "quadratic")
+  {
+    if (!interactions.empty()) { interactions.clear(); }
+    for (auto it = ns_counter.begin(); it != ns_counter.end(); ++it)
+    {
+      auto idx1 = (*it).first;
+      for (auto jt = it; jt != ns_counter.end(); ++jt)
+      {
+        auto idx2 = (*jt).first;
+        std::vector<namespace_index> idx{idx1, idx2};
+        if (exclusions.find(idx) == exclusions.end()) { interactions.push_back({idx1, idx2}); }
+      }
+    }
+  }
+  else if (interaction_type == "cubic")
+  {
+    if (!interactions.empty()) { interactions.clear(); }
+    for (auto it = ns_counter.begin(); it != ns_counter.end(); ++it)
+    {
+      auto idx1 = (*it).first;
+      for (auto jt = it; jt != ns_counter.end(); ++jt)
+      {
+        auto idx2 = (*jt).first;
+        for (auto kt = jt; kt != ns_counter.end(); ++kt)
+        {
+          auto idx3 = (*kt).first;
+          std::vector<namespace_index> idx{idx1, idx2, idx3};
+          if (exclusions.find(idx) == exclusions.end()) { interactions.push_back({idx1, idx2, idx3}); }
+        }
+      }
+    }
+  }
+  else
+  {
+    THROW("Unknown interaction type.");
+  }
+
+  if (ccb_on)
+  {
+    std::vector<std::vector<extent_term>> empty;
+    ccb::insert_ccb_interactions(interactions, empty);
+  }
+}
+
 // Helper function to insert new configs from oracle into map of configs as well as index_queue.
 // Handles creating new config with exclusions or overwriting stale configs to avoid reallocation.
 template <typename oracle_impl>
-void config_oracle<oracle_impl>::insert_config(std::set<std::vector<namespace_index>>&& new_exclusions, bool allow_dups)
+void config_oracle<oracle_impl>::insert_config(std::set<std::vector<namespace_index>>&& new_exclusions,
+    const std::map<namespace_index, uint64_t>& ns_counter, bool allow_dups)
 {
   // First check if config already exists
   if (!allow_dups)
@@ -85,7 +145,8 @@ void config_oracle<oracle_impl>::insert_config(std::set<std::vector<namespace_in
 }
 
 void oracle_rand_impl::gen_exclusion_configs(config_oracle<oracle_rand_impl>* co,
-    const interaction_vec_t& champ_interactions, std::vector<exclusion_config>& configs)
+    const interaction_vec_t& champ_interactions, std::vector<exclusion_config>& configs,
+    const std::map<namespace_index, uint64_t>& ns_counter)
 {
   const uint64_t champ_index = 0;
   for (uint64_t i = 0; i < CONFIGS_PER_CHAMP_CHANGE; ++i)
@@ -111,11 +172,11 @@ void oracle_rand_impl::gen_exclusion_configs(config_oracle<oracle_rand_impl>* co
     {
       THROW("Unknown interaction type.");
     }
-    co->insert_config(std::move(new_exclusions));
+    co->insert_config(std::move(new_exclusions), ns_counter);
   }
 }
 void one_diff_impl::gen_exclusion_configs(config_oracle<one_diff_impl>* co, const interaction_vec_t& champ_interactions,
-    std::vector<exclusion_config>& configs)
+    std::vector<exclusion_config>& configs, const std::map<namespace_index, uint64_t>& ns_counter)
 {
   const uint64_t champ_index = 0;
   // Add one exclusion (for each interaction)
@@ -144,22 +205,22 @@ void one_diff_impl::gen_exclusion_configs(config_oracle<one_diff_impl>* co, cons
     {
       THROW("Unknown interaction type.");
     }
-    co->insert_config(std::move(new_exclusions));
+    co->insert_config(std::move(new_exclusions), ns_counter);
   }
   // Remove one exclusion (for each exclusion)
   for (auto& ns_pair : configs[champ_index].exclusions)
   {
     auto new_exclusions = configs[champ_index].exclusions;
     new_exclusions.erase(ns_pair);
-    co->insert_config(std::move(new_exclusions));
+    co->insert_config(std::move(new_exclusions), ns_counter);
   }
 }
-void champdupe_impl::gen_exclusion_configs(
-    config_oracle<champdupe_impl>* co, const interaction_vec_t&, std::vector<exclusion_config>& configs)
+void champdupe_impl::gen_exclusion_configs(config_oracle<champdupe_impl>* co, const interaction_vec_t&,
+    std::vector<exclusion_config>& configs, const std::map<namespace_index, uint64_t>& ns_counter)
 {
   const uint64_t champ_index = 0;
   for (uint64_t i = 0; co->configs.size() <= 2; ++i)
-  { co->insert_config(std::set<std::vector<namespace_index>>(configs[champ_index].exclusions), true); }
+  { co->insert_config(std::set<std::vector<namespace_index>>(configs[champ_index].exclusions), ns_counter, true); }
 }
 
 // This will generate configs based on the current champ. These configs will be
@@ -168,9 +229,10 @@ void champdupe_impl::gen_exclusion_configs(
 // of configs to generate per champ is hard-coded to 5 at the moment.
 // TODO: Add logic to avoid duplicate configs (could be very costly)
 template <typename oracle_impl>
-void config_oracle<oracle_impl>::gen_exclusion_configs(const interaction_vec_t& champ_interactions)
+void config_oracle<oracle_impl>::gen_exclusion_configs(
+    const interaction_vec_t& champ_interactions, const std::map<namespace_index, uint64_t>& ns_counter)
 {
-  _impl.gen_exclusion_configs(this, champ_interactions, configs);
+  _impl.gen_exclusion_configs(this, champ_interactions, configs, ns_counter);
 }
 
 // This function is triggered when all sets of interactions generated by the oracle have been tried and
@@ -179,7 +241,7 @@ void config_oracle<oracle_impl>::gen_exclusion_configs(const interaction_vec_t& 
 // may be better within the oracle, which could generate better priorities for different configs based
 // on ns_counter (which is updated as each example is processed)
 template <typename oracle_impl>
-bool config_oracle<oracle_impl>::repopulate_index_queue()
+bool config_oracle<oracle_impl>::repopulate_index_queue(const std::map<namespace_index, uint64_t>& ns_counter)
 {
   for (size_t i = 0; i < valid_config_size; ++i)
   {
