3f77701c6806b77a9a25c3b193f5f2113e7b55a5
Byron Xu <byronxu@microsoft.com>
2022-08-24 12:43:31 -0400

    Add benchmarks and reorganize nuget directory

---
+++ nuget/dotnet/benchmarks/BenchmarkLearnSimple.cs
@@ -0,0 +1,39 @@
+﻿using VW;
+using BenchmarkDotNet.Attributes;
+
+[Config(typeof(VWBenchmarkConfig))]
+public class BenchmarkLearnSimple
+{
+    [Benchmark]
+    [ArgumentsSource(nameof(ArgsGenerator))]
+    public void Benchmark(Args args)
+    {
+        args.vw.Learn(args.example);
+    }
+
+    public IEnumerable<Args> ArgsGenerator()
+    {
+        yield return new Args("8_features", "1 zebra|MetricFeatures:3.28 height:1.5 length:2.0 |Says black with white stripes |OtherFeatures NumberOfLegs:4.0 HasStripes");
+        yield return new Args("1_feature", "1 | a");
+    }
+
+    public class Args
+    {
+        public string testCaseName;
+        public VowpalWabbit vw;
+        public VowpalWabbitExample example;
+
+        public Args(string name, string example_str)
+        {
+            testCaseName = name;
+            vw = new VowpalWabbit("--quiet");
+            example = vw.ParseLine(example_str);
+        }
+
+        public override string ToString()
+        {
+            return testCaseName;
+        }
+    }
+}
+
---
+++ nuget/dotnet/benchmarks/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+
+
+public class Program
+{
+    static void Main(string[] args)
+        => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
+
+public class VWBenchmarkConfig : ManualConfig
+{
+    public VWBenchmarkConfig()
+    {
+        this.WithOptions(ConfigOptions.DisableOptimizationsValidator);
+    }
+}
+
