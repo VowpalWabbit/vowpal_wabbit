5e725a61554a8d77371e1714b275a43747d99d3b
Eduardo Salinas <edus@microsoft.com>
2022-08-25 13:00:49 -0400

    Update run-clang-tidy.sh

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -9,14 +9,14 @@ cd $REPO_DIR
 # -DCMAKE_EXPORT_COMPILE_COMMANDS=On is manually set because in CMake 3.16, just setting it in the CMakeLists.txt does not work.
 cmake -S . -B build -DFMT_SYS_DEP=On -DSPDLOG_SYS_DEP=On -DVW_BOOST_MATH_SYS_DEP=On -DBUILD_TESTING=Off -DVW_BUILD_VW_C_WRAPPER=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=On
 # remove grep to view warnings and not only errors
-run-clang-tidy -p build -quiet -header-filter=vw/* 1>out.txt 2>error.txt || true
-cat out.txt | grep -A 3 ": error" > onlyerrors.txt
+run-clang-tidy -p build -quiet -header-filter=vw/* 1>tidy_out.txt 2>tidy_error.txt || true
+grep -A 3 ": error" -f tidy_out.txt > tidy_onlyerrors.txt
 
-if [ -s onlyerrors.txt ]; then
+if [ -s tidy_onlyerrors.txt ]; then
         # The file is not-empty.
-        cat onlyerrors.txt
+        cat tidy_onlyerrors.txt
         exit 11
 else
         # The file is empty.
-        cat error.txt
+        cat tidy_error.txt
 fi
