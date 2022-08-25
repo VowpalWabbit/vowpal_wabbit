39bbd8cd3007f6c91d542af70d1c193f30e9081a
Eduardo Salinas <edus@microsoft.com>
2022-08-25 13:12:13 -0400

    Update run-clang-tidy.sh

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -10,7 +10,7 @@ cd $REPO_DIR
 cmake -S . -B build -DFMT_SYS_DEP=On -DSPDLOG_SYS_DEP=On -DVW_BOOST_MATH_SYS_DEP=On -DBUILD_TESTING=Off -DVW_BUILD_VW_C_WRAPPER=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=On
 # remove grep to view warnings and not only errors
 run-clang-tidy -p build -quiet -header-filter=vw/* 1>tidy_out.txt 2>tidy_error.txt || true
-grep -A 3 ": error" -f tidy_out.txt > tidy_onlyerrors.txt
+grep -A 3 ": error" tidy_out.txt > tidy_onlyerrors.txt
 
 if [ -s tidy_onlyerrors.txt ]; then
         # The file is not-empty.
