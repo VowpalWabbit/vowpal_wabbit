c06d33137545c4934c0603490ea2e6a99e436c4b
Griffin Bassman <griffinbassman@gmail.com>
2022-10-17 16:45:16 -0400

    fix audit string

--- vowpalwabbit/core/src/reductions/epsilon_decay.cc
+++ vowpalwabbit/core/src/reductions/epsilon_decay.cc
@@ -118,15 +118,15 @@ void epsilon_decay_data::update_weights(float init_ep, VW::LEARNER::multi_learne
           }
           if (_epsilon_decay_audit_str != "")
           {
-            if (model_ind == model_count - 1) { _audit_msg << "champ "; }
-            else
+            if (model_ind != model_count - 1)
             {
               _audit_msg << "challenger[" << (model_ind + 1) << "] ";
+            
+              _audit_msg << "update_count: " << conf_seq_estimators[model_ind][model_ind].update_count
+                        << " lb: " << conf_seq_estimators[model_ind][model_ind].lower_bound()
+                        << " champ_ub: " << conf_seq_estimators[model_count - 1][model_ind].upper_bound() << " p_pred: " << a_s.score
+                        << "\n";
             }
-            _audit_msg << "update_count: " << conf_seq_estimators[model_ind][model_ind].update_count
-                       << " lb: " << conf_seq_estimators[model_ind][model_ind].lower_bound()
-                       << " ub: " << conf_seq_estimators[model_ind][model_ind].upper_bound() << " p_pred: " << a_s.score
-                       << "\n";
           }
           break;
         }
