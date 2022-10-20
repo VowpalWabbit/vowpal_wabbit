a206c79e1f56560595e15fdeffa2ebdbf6df0807
Griffin Bassman <griffinbassman@gmail.com>
2022-10-20 11:14:50 -0400

    fix break

--- vowpalwabbit/core/src/label_type.cc
+++ vowpalwabbit/core/src/label_type.cc
@@ -15,25 +15,25 @@ VW::string_view VW::to_string(VW::label_type_t label_type)
   switch (label_type)
   {
     case label_type_t::SIMPLE:
-      return "label_type_t::SIMPLE";
+      return "label_type_t::simple";
     case label_type_t::CB:
-      return "label_type_t::CB";
+      return "label_type_t::cb";
     case label_type_t::CB_EVAL:
-      return "label_type_t::CB_EVAL";
+      return "label_type_t::cb_eval";
     case label_type_t::CS:
-      return "label_type_t::CS";
+      return "label_type_t::cs";
     case label_type_t::MULTILABEL:
-      return "label_type_t::MULTILABEL";
+      return "label_type_t::multilabel";
     case label_type_t::MULTICLASS:
-      return "label_type_t::MULTICLASS";
+      return "label_type_t::multiclass";
     case label_type_t::CCB:
-      return "label_type_t::CCB";
+      return "label_type_t::ccb";
     case label_type_t::SLATES:
-      return "label_type_t::SLATES";
+      return "label_type_t::slates";
     case label_type_t::NOLABEL:
-      return "label_type_t::NOLABEL";
+      return "label_type_t::nolabel";
     case label_type_t::CONTINUOUS:
-      return "label_type_t::CONTINUOUS";
+      return "label_type_t::continuous";
   }
 
   // The above enum is exhaustive and will warn on a new label type being added due to the lack of `default`
