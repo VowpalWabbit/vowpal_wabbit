bdf32ab14ec512682459bcb9e9942e26dbf7aa1b
Griffin Bassman <griffinbassman@gmail.com>
2022-10-21 12:26:55 -0400

    add csoaa

--- vowpalwabbit/core/src/reductions/csoaa.cc
+++ vowpalwabbit/core/src/reductions/csoaa.cc
@@ -185,29 +185,49 @@ void predict_or_learn(csoaa& c, single_learner& base, VW::example& ec)
 void finish_example(VW::workspace& all, csoaa&, VW::example& ec) { VW::details::finish_cs_example(all, ec); }
 }  // namespace
 
-base_learner* VW::reductions::csoaa_setup(VW::setup_base_i& stack_builder)
+struct options_csoaa_v1
 {
-  options_i& options = *stack_builder.get_options();
-  VW::workspace& all = *stack_builder.get_all_pointer();
-  auto c = VW::make_unique<csoaa>(all.logger, all.indexing);
+  uint32_t num_classes = 0;
+  uint32_t indexing = 2;
+  bool search = false;
+};
+
+std::unique_ptr<options_csoaa_v1> get_csoaa_options_instance(
+    const VW::workspace&, VW::io::logger& logger, options_i& options)
+{
+  auto csoaa_opts = VW::make_unique<options_csoaa_v1>();
   option_group_definition new_options("[Reduction] Cost Sensitive One Against All");
   new_options
-      .add(make_option("csoaa", c->num_classes).keep().necessary().help("One-against-all multiclass with <k> costs"))
-      .add(make_option("indexing", all.indexing).one_of({0, 1}).keep().help("Choose between 0 or 1-indexing"));
+      .add(make_option("csoaa", csoaa_opts->num_classes)
+               .keep()
+               .necessary()
+               .help("One-against-all multiclass with <k> costs"))
+      .add(make_option("indexing", csoaa_opts->indexing).one_of({0, 1}).keep().help("Choose between 0 or 1-indexing"));
 
   if (!options.add_parse_and_check_necessary(new_options)) { return nullptr; }
 
   if (options.was_supplied("probabilities"))
   { THROW("csoaa does not support probabilities flag, please use oaa or multilabel_oaa"); }
-  c->search = options.was_supplied("search");
+  csoaa_opts->search = options.was_supplied("search");
+  return csoaa_opts;
+}
 
-  c->pred = calloc_or_throw<VW::polyprediction>(c->num_classes);
-  size_t ws = c->num_classes;
-  auto* l = make_reduction_learner(std::move(c), as_singleline(stack_builder.setup_base_learner()),
+base_learner* VW::reductions::csoaa_setup(VW::setup_base_i& stack_builder)
+{
+  options_i& options = *stack_builder.get_options();
+  VW::workspace& all = *stack_builder.get_all_pointer();
+  auto csoaa_opts = get_csoaa_options_instance(all, all.logger, options);
+  if (csoaa_opts == nullptr) { return nullptr; }
+  all.indexing = csoaa_opts->indexing;
+  auto csoaa_data = VW::make_unique<csoaa>(all.logger, all.indexing);
+  csoaa_data->num_classes = csoaa_opts->num_classes;
+  csoaa_data->search = csoaa_opts->search;
+  csoaa_data->pred = calloc_or_throw<VW::polyprediction>(csoaa_data->num_classes);
+  auto* l = make_reduction_learner(std::move(csoaa_data), as_singleline(stack_builder.setup_base_learner()),
       predict_or_learn<true>, predict_or_learn<false>, stack_builder.get_setupfn_name(csoaa_setup))
                 .set_learn_returns_prediction(
                     true) /* csoaa.learn calls gd.learn. nothing to be gained by calling csoaa.predict first */
-                .set_params_per_weight(ws)
+                .set_params_per_weight(csoaa_opts->num_classes)
                 .set_output_prediction_type(VW::prediction_type_t::MULTICLASS)
                 .set_input_label_type(VW::label_type_t::CS)
                 .set_finish_example(::finish_example)
