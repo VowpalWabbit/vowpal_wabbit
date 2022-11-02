e01a4ed6c9d37fd7da0ecc569aec1dabe3f8941f
olgavrou <olgavrou@gmail.com>
2022-11-02 09:29:11 -0400

    Update vowpalwabbit/core/src/reductions/explore_eval.cc
    Co-authored-by: Jack Gerrits <jackgerrits@users.noreply.github.com>

--- vowpalwabbit/core/src/reductions/explore_eval.cc
+++ vowpalwabbit/core/src/reductions/explore_eval.cc
@@ -165,7 +165,7 @@ void do_actual_learning(explore_eval& data, multi_learner& base, VW::multi_ex& e
       if (data.known_cost.action == a_s[i].action) { action_probability = a_s[i].score; }
     }
 
-    if (action_probability == 0) { return; }
+    if (action_probability == 0.f) { return; }
 
     float threshold = action_probability / data.known_cost.probability;
 
