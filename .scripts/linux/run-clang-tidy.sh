2983bb59011a5a065e3914ad05facfd947600297
Jack Gerrits <jackgerrits95@gmail.com>
2022-11-04 18:35:42 -0400

    install more packages

--- .scripts/linux/run-clang-tidy.sh
+++ .scripts/linux/run-clang-tidy.sh
@@ -7,7 +7,7 @@ REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
 
 # -DCMAKE_EXPORT_COMPILE_COMMANDS=On is manually set because in CMake 3.16, just setting it in the CMakeLists.txt does not work.
-cmake -S . -B build -DFMT_SYS_DEP=On -DSPDLOG_SYS_DEP=On -DVW_BOOST_MATH_SYS_DEP=On -DBUILD_TESTING=Off -DVW_BUILD_VW_C_WRAPPER=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=On
+cmake -S . -B build -DBUILD_TESTING=Off -DVW_BUILD_VW_C_WRAPPER=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=On
 run-clang-tidy -p build -quiet -header-filter=vw/* 1>tidy_out.txt 2>tidy_error.txt || true
 grep -A 3 "error: " tidy_out.txt > tidy_onlyerrors.txt || true
 
