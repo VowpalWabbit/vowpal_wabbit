bff4024695a6c32058815f37fe729d7ae2655a90
Eduardo Salinas <edus@microsoft.com>
2022-11-17 20:23:54 +0000

    add test

--- vowpalwabbit/core/src/reductions/gd.cc
+++ vowpalwabbit/core/src/reductions/gd.cc
@@ -1023,7 +1023,6 @@ void save_load_online_state_weights(VW::workspace& all, io_buf& model_file, bool
   else
   {  // write binary or text
     bool hexfloat_print = all.options->was_supplied("hexfloat_weights");
-    hexfloat_print = true;
     if (hexfloat_print && (text || all.print_invert)) { msg << std::hexfloat; }
 
     for (typename T::iterator v = weights.begin(); v != weights.end(); ++v)
