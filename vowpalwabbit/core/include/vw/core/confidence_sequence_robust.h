8f0918533eaceb4e0a7cbaef45d3ec9cb151f0e4
Griffin Bassman <griffinbassman@gmail.com>
2022-12-01 12:41:16 -0500

    clang

--- vowpalwabbit/core/include/vw/core/confidence_sequence_robust.h
+++ vowpalwabbit/core/include/vw/core/confidence_sequence_robust.h
@@ -17,80 +17,80 @@ namespace VW
 class lower_cs_base
 {
 public:
-    void add_obs(double x);
-    double get_ci(double alpha);
+  void add_obs(double x);
+  double get_ci(double alpha);
 };
 
 class countable_discrete_base : public lower_cs_base
 {
 public:
-    countable_discrete_base(double lambda_max = 0.5, double xi = 1.6);
-    double lambertw(double x);
-    double get_ci(double alpha);
-    double get_lam_sqrt_tp1(double j);
-    double get_log_weight(double j);
-    double get_remaining_log_weight(double j);
-    double get_v_impl(std::vector<double>& memo, double j);
-    void log_wealth_mix(double mu, double s, double thres, std::vector<double>& memo);
-    double lb_log_wealth(double alpha);
-    double get_s();
-    double get_v();
+  countable_discrete_base(double lambda_max = 0.5, double xi = 1.6);
+  double lambertw(double x);
+  double get_ci(double alpha);
+  double get_lam_sqrt_tp1(double j);
+  double get_log_weight(double j);
+  double get_remaining_log_weight(double j);
+  double get_v_impl(std::vector<double>& memo, double j);
+  void log_wealth_mix(double mu, double s, double thres, std::vector<double>& memo);
+  double lb_log_wealth(double alpha);
+  double get_s();
+  double get_v();
 
 private:
-    double lambda_max;
-    double xi;
-    double log_xi;
-    uint64_t t;
+  double lambda_max;
+  double xi;
+  double log_xi;
+  uint64_t t;
 };
 
 class g_tilde
 {
 public:
-    g_tilde(double k);
-    double histo_variance(std::vector<double>& hist, double lam_sqrt_tp1);
-    void histo_insert(std::vector<double>& hist, double x);
-    void add_obs(double x);
-    double get_s();
-    double get_v(double lam_sqrt_tp1);
+  g_tilde(double k);
+  double histo_variance(std::vector<double>& hist, double lam_sqrt_tp1);
+  void histo_insert(std::vector<double>& hist, double x);
+  void add_obs(double x);
+  double get_s();
+  double get_v(double lam_sqrt_tp1);
 
 private:
-    double k;
-    double log_k;
-    double sum_low_v;
-    double sum_mid_v;
-    std::vector<double> sum_v_histo;
-    uint64_t t;
+  double k;
+  double log_k;
+  double sum_low_v;
+  double sum_mid_v;
+  std::vector<double> sum_v_histo;
+  uint64_t t;
 };
 
 class robust_mixture : public countable_discrete_base
 {
 public:
-    robust_mixture(double eta = 0.95f, double r = 2, double k = 1.5);
-    double get_log_weight(double j);
-    double get_log_remaining_weight(double j);
-    double get_s();
-    double get_v();
-    double add_obs(double x);
+  robust_mixture(double eta = 0.95f, double r = 2, double k = 1.5);
+  double get_log_weight(double j);
+  double get_log_remaining_weight(double j);
+  double get_s();
+  double get_v();
+  double add_obs(double x);
 
 private:
-    double eta;
-    double r;
-    g_tilde gtilde;
-    double scalefac;
-    double logscalefac;
-    double logxim1;
+  double eta;
+  double r;
+  g_tilde gtilde;
+  double scalefac;
+  double logscalefac;
+  double logxim1;
 };
 
 class off_policy_cs
 {
 public:
-    off_policy_cs() = default;
-    void add_obs(double w, double r);
-    std::pair<double, double> get_ci(double alpha);
+  off_policy_cs() = default;
+  void add_obs(double w, double r);
+  std::pair<double, double> get_ci(double alpha);
 
 private:
-    robust_mixture lower;
-    robust_mixture upper;
+  robust_mixture lower;
+  robust_mixture upper;
 };
 
 namespace model_utils
--- vowpalwabbit/core/src/confidence_sequence_robust.cc
+++ vowpalwabbit/core/src/confidence_sequence_robust.cc
@@ -11,61 +11,48 @@
 
 namespace VW
 {
-void lower_cs_base::add_obs(double x)
-{
-    THROW("Unimplemented");
-}
+void lower_cs_base::add_obs(double x) { THROW("Unimplemented"); }
 
-double lower_cs_base::get_ci(double alpha)
-{
-    THROW("Unimplemented");
-}
+double lower_cs_base::get_ci(double alpha) { THROW("Unimplemented"); }
 
 double countable_discrete_base::lambertw(double)
 {
-    // TODO: Find cpp equivalent
-    return 0.0;
+  // TODO: Find cpp equivalent
+  return 0.0;
 }
 
 countable_discrete_base::countable_discrete_base(double lambda_max, double xi)
 {
-    assert(0.0 < lambda_max && lambda_max <= 1.0 + lambertw(-std::exp(-2.0)));
-    assert(1.0 < xi);
+  assert(0.0 < lambda_max && lambda_max <= 1.0 + lambertw(-std::exp(-2.0)));
+  assert(1.0 < xi);
 
-    this->lambda_max = lambda_max;
-    this->xi = xi;
-    this->log_xi = std::log1p(xi - 1);
+  this->lambda_max = lambda_max;
+  this->xi = xi;
+  this->log_xi = std::log1p(xi - 1);
 }
 
-double countable_discrete_base::get_ci(double alpha)
-{
-    return lb_log_wealth(alpha);
-}
+double countable_discrete_base::get_ci(double alpha) { return lb_log_wealth(alpha); }
 
 double countable_discrete_base::get_lam_sqrt_tp1(double j)
 {
-    double log_den = (j + 0.5) * log_xi - 0.5 * std::log(t + 1);
-    return lambda_max * std::exp(-log_den);
+  double log_den = (j + 0.5) * log_xi - 0.5 * std::log(t + 1);
+  return lambda_max * std::exp(-log_den);
 }
-
-
-
 
 void off_policy_cs::add_obs(double w, double r)
 {
-    assert(w >= 0.0);
-    assert(0.0 <= r && r <= 1.0);
-    lower.add_obs(w * r);
-    upper.add_obs(w * (1.0 - r));
+  assert(w >= 0.0);
+  assert(0.0 <= r && r <= 1.0);
+  lower.add_obs(w * r);
+  upper.add_obs(w * (1.0 - r));
 }
 
 std::pair<double, double> off_policy_cs::get_ci(double alpha)
 {
-    return std::make_pair(lower.get_ci(alpha / 2.0), upper.get_ci(alpha / 2.0));
+  return std::make_pair(lower.get_ci(alpha / 2.0), upper.get_ci(alpha / 2.0));
 }
 
 namespace model_utils
 {
-
 }  // namespace model_utils
 }  // namespace VW
