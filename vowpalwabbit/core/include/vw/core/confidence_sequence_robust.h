323f04b57e1a01be6f4c351976c25833c957f20d
Griffin Bassman <griffinbassman@gmail.com>
2022-12-01 15:22:40 -0500

    clang

--- vowpalwabbit/core/include/vw/core/confidence_sequence_robust.h
+++ vowpalwabbit/core/include/vw/core/confidence_sequence_robust.h
@@ -29,26 +29,29 @@ public:
   double get_ci(double alpha);
   double get_lam_sqrt_tp1(double j);
   double get_log_weight(double j);
-  double get_remaining_log_weight(double j);
-  double get_v_impl(std::vector<double>& memo, double j);
-  void log_wealth_mix(double mu, double s, double thres, std::vector<double>& memo);
+  double get_log_remaining_weight(double j);
+  double get_v_impl(std::map<uint64_t, double>& memo, uint64_t j);
+  double log_sum_exp(std::vector<double> a);
+  double log_wealth_mix(double mu, double s, double thres, std::map<uint64_t, double>& memo);
+  double root_brentq(double s, double thres, std::map<uint64_t, double>& memo, double min_mu, double max_mu);
   double lb_log_wealth(double alpha);
   double get_s();
-  double get_v();
+  double get_v(double lam_sqrt_tp1);
+
+  double xi;
+  uint64_t t;
+  double log_xi;
 
 private:
   double lambda_max;
-  double xi;
-  double log_xi;
-  uint64_t t;
 };
 
 class g_tilde
 {
 public:
   g_tilde(double k);
-  double histo_variance(std::vector<double>& hist, double lam_sqrt_tp1);
-  void histo_insert(std::vector<double>& hist, double x);
+  double histo_variance(std::map<std::pair<uint64_t, bool>, double>& hist, double lam_sqrt_tp1);
+  void histo_insert(std::map<std::pair<uint64_t, bool>, double>& hist, double x);
   void add_obs(double x);
   double get_s();
   double get_v(double lam_sqrt_tp1);
@@ -56,9 +59,10 @@ public:
 private:
   double k;
   double log_k;
+  double sum_x;
   double sum_low_v;
   double sum_mid_v;
-  std::vector<double> sum_v_histo;
+  std::map<std::pair<uint64_t, bool>, double> sum_v_histo;
   uint64_t t;
 };
 
@@ -69,16 +73,16 @@ public:
   double get_log_weight(double j);
   double get_log_remaining_weight(double j);
   double get_s();
-  double get_v();
+  double get_v(double lam_sqrt_tp1);
   double add_obs(double x);
 
 private:
   double eta;
   double r;
-  g_tilde gtilde;
-  double scalefac;
-  double logscalefac;
-  double logxim1;
+  g_tilde gt;
+  double scale_fac;
+  double log_scale_fac;
+  double log_xi_m1;
 };
 
 class off_policy_cs
