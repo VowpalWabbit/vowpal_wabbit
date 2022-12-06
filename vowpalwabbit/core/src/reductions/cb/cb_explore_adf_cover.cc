49ad32a6dfe6cb8223bea56208291474ab8d1ab0
Jack Gerrits <jackgerrits@users.noreply.github.com>
2022-12-06 16:10:33 -0500

    refactor: fix conversion warnings in v_array and removed deprecated usages (#4310)
    * refactor: fix conversion warnings in v_array and removed deprecated usages
    
    * formatting
    
    * Update mwt.h
    
    * Fix assert the wrong way around

--- vowpalwabbit/core/src/reductions/cb/cb_explore_adf_cover.cc
+++ vowpalwabbit/core/src/reductions/cb/cb_explore_adf_cover.cc
@@ -62,7 +62,7 @@ private:
   VW::version_struct _model_file_version;
   VW::io::logger _logger;
 
-  v_array<VW::action_score> _action_probs;
+  VW::v_array<VW::action_score> _action_probs;
   std::vector<float> _scores;
   VW::cs_label _cs_labels;
   VW::cs_label _cs_labels_2;
@@ -125,7 +125,7 @@ void cb_explore_adf_cover::predict_or_learn_impl(VW::LEARNER::multi_learner& bas
     GEN_CS::gen_cs_example_ips(examples, _cs_labels, _logger);
     VW::LEARNER::multiline_learn_or_predict<false>(base, examples, examples[0]->ft_offset);
   }
-  v_array<VW::action_score>& preds = examples[0]->pred.a_s;
+  VW::v_array<VW::action_score>& preds = examples[0]->pred.a_s;
   const uint32_t num_actions = static_cast<uint32_t>(preds.size());
 
   float additive_probability = 1.f / static_cast<float>(_cover_size);
