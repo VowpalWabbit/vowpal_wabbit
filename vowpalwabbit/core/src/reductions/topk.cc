3ff9b2cecf2af5c92c276974dcf49f1d0fd2789c
Jack Gerrits <jackgerrits95@gmail.com>
2022-12-12 11:43:38 -0500

    change topk to produce scalars and change reporting format

--- vowpalwabbit/core/src/reductions/topk.cc
+++ vowpalwabbit/core/src/reductions/topk.cc
@@ -7,11 +7,15 @@
 #include "vw/core/learner.h"
 #include "vw/core/setup_base.h"
 #include "vw/core/shared_data.h"
+#include "vw/core/text_utils.h"
 #include "vw/core/vw.h"
 #include "vw/io/errno_handling.h"
 #include "vw/io/logger.h"
 
+#include <fmt/core.h>
+
 #include <cfloat>
+#include <numeric>
 #include <queue>
 #include <sstream>
 #include <utility>
@@ -23,7 +27,8 @@ namespace
 class topk
 {
 public:
-  using container_t = std::multimap<float, VW::v_array<char>>;
+  // Prediction to index map
+  using container_t = std::multimap<float, size_t>;
   using const_iterator_t = container_t::const_iterator;
   topk(uint32_t k_num);
 
@@ -33,7 +38,7 @@ public:
 
 private:
   void clear_container();
-  void update_priority_queue(float pred, const VW::v_array<char>& tag);
+  void update_priority_queue(float pred, size_t index);
 
   const uint32_t _k_num;
   container_t _pr_queue;
@@ -44,30 +49,42 @@ topk::topk(uint32_t k_num) : _k_num(k_num) {}
 void topk::predict(VW::LEARNER::single_learner& base, VW::multi_ex& ec_seq)
 {
   clear_container();
+  ec_seq[0]->pred.scalars.clear();
+
+  size_t current_index = 0;
   for (auto* ec : ec_seq)
   {
     base.predict(*ec);
-    update_priority_queue(ec->pred.scalar, ec->tag);
+    const auto prediction = ec->pred.scalar;
+    update_priority_queue(prediction, current_index);
+    ec_seq[0]->pred.scalars.push_back(prediction);
+    current_index++;
   }
 }
 
 void topk::learn(VW::LEARNER::single_learner& base, VW::multi_ex& ec_seq)
 {
   clear_container();
+  ec_seq[0]->pred.scalars.clear();
+
+  size_t current_index = 0;
   for (auto* ec : ec_seq)
   {
     base.learn(*ec);
-    update_priority_queue(ec->pred.scalar, ec->tag);
+    const auto prediction = ec->pred.scalar;
+    update_priority_queue(prediction, current_index);
+    ec_seq[0]->pred.scalars.push_back(prediction);
+    current_index++;
   }
 }
 
-void topk::update_priority_queue(float pred, const VW::v_array<char>& tag)
+void topk::update_priority_queue(float pred, size_t index)
 {
-  if (_pr_queue.size() < _k_num) { _pr_queue.insert({pred, tag}); }
+  if (_pr_queue.size() < _k_num) { _pr_queue.insert({pred, index}); }
   else if (_pr_queue.begin()->first < pred)
   {
     _pr_queue.erase(_pr_queue.begin());
-    _pr_queue.insert({pred, tag});
+    _pr_queue.insert({pred, index});
   }
 }
 
@@ -79,15 +96,19 @@ std::pair<topk::const_iterator_t, topk::const_iterator_t> topk::get_container_vi
 void topk::clear_container() { _pr_queue.clear(); }
 
 void print_result(VW::io::writer* file_descriptor,
-    std::pair<topk::const_iterator_t, topk::const_iterator_t> const& view, VW::io::logger& logger)
+    std::pair<topk::const_iterator_t, topk::const_iterator_t> const& view, const VW::multi_ex& ec_seq,
+    VW::io::logger& logger)
 {
   if (file_descriptor != nullptr)
   {
     std::stringstream ss;
     for (auto it = view.first; it != view.second; it++)
     {
-      ss << std::fixed << it->first << " ";
-      if (!it->second.empty()) { ss << " " << VW::string_view{it->second.begin(), it->second.size()}; }
+      const auto prediction = it->first;
+      const auto index = it->second;
+      const auto& tag = ec_seq[index]->tag;
+      ss << std::fixed << prediction << " ";
+      if (!tag.empty()) { ss << " " << VW::string_view{tag.begin(), tag.size()}; }
       ss << " \n";
     }
     ss << '\n';
@@ -109,15 +130,41 @@ void update_stats_topk(const VW::workspace& /* all */, shared_data& sd, const to
 }
 
 void output_example_prediction_topk(
-    VW::workspace& all, const topk& data, const VW::multi_ex& /* ec_seq */, VW::io::logger& logger)
+    VW::workspace& all, const topk& data, const VW::multi_ex& ec_seq, VW::io::logger& logger)
 {
-  for (auto& sink : all.final_prediction_sink) { print_result(sink.get(), data.get_container_view(), logger); }
+  for (auto& sink : all.final_prediction_sink) { print_result(sink.get(), data.get_container_view(), ec_seq, logger); }
 }
 
 void print_update_topk(VW::workspace& all, shared_data& sd, const topk& /* data */, const VW::multi_ex& ec_seq,
     VW::io::logger& /* unused */)
 {
-  for (auto* ec : ec_seq) { VW::details::print_update_simple_label(all, sd, *ec, all.logger); }
+  const bool should_print_driver_update =
+      all.sd->weighted_examples() >= all.sd->dump_interval && !all.quiet && !all.bfgs;
+
+  if (should_print_driver_update)
+  {
+    size_t num_features = std::accumulate(ec_seq.begin(), ec_seq.end(), 0,
+        [](size_t features_so_far, VW::example* ex) { return features_so_far + ex->get_num_features(); });
+
+    std::ostringstream label_ss;
+    std::string sep;
+    for (const auto* ec : ec_seq)
+    {
+      label_ss << sep << VW::fmt_float(ec->l.simple.label, VW::details::DEFAULT_FLOAT_FORMATTING_DECIMAL_PRECISION);
+      sep = ",";
+    }
+
+    std::ostringstream pred_ss;
+    sep = "";
+    for (auto pred : ec_seq[0]->pred.scalars)
+    {
+      pred_ss << sep << VW::fmt_float(pred, VW::details::DEFAULT_FLOAT_FORMATTING_DECIMAL_PRECISION);
+      sep = ",";
+    }
+
+    sd.print_update(*all.trace_message, all.holdout_set_off, all.current_pass, label_ss.str(), pred_ss.str(),
+        num_features, all.progress_add, all.progress_arg);
+  }
 }
 
 template <bool is_learn>
@@ -142,7 +189,9 @@ VW::LEARNER::base_learner* VW::reductions::topk_setup(VW::setup_base_i& stack_bu
       predict_or_learn<true>, predict_or_learn<false>, stack_builder.get_setupfn_name(topk_setup))
                 .set_learn_returns_prediction(true)
                 .set_input_prediction_type(VW::prediction_type_t::SCALAR)
-                .set_output_prediction_type(VW::prediction_type_t::SCALAR)
+                .set_output_prediction_type(VW::prediction_type_t::SCALARS)
+                .set_input_label_type(VW::label_type_t::SIMPLE)
+                .set_output_label_type(VW::label_type_t::SIMPLE)
                 .set_output_example_prediction(output_example_prediction_topk)
                 .set_print_update(print_update_topk)
                 .set_update_stats(update_stats_topk)
