377a80174c1f1f9874a9f135a6cefbc57c34c666
Griffin Bassman <griffinbassman@gmail.com>
2022-12-19 13:57:06 -0500

    black lint

--- python/tests/test_confidence_sequence_robust.py
+++ python/tests/test_confidence_sequence_robust.py
@@ -1,6 +1,7 @@
 from confidence_sequence_robust import DDRM as confidence_sequence_robust
 import numpy as np
 
+
 def test_confidence_sequence_robust():
     csr = confidence_sequence_robust()
     for _ in range(200):
@@ -8,7 +9,7 @@ def test_confidence_sequence_robust():
         csr.addobs((1.1, 0))
     alpha = 0.05
     lb, ub = csr.getci(alpha)
-    
+
     # Compare bounds to confidence_sequence_robust_test.cc
     np.testing.assert_almost_equal(lb, 0.4574146652500113, 6)
     np.testing.assert_almost_equal(ub, 0.5423597665906932, 6)
