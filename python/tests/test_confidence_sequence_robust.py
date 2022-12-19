03a95bf68eb007e4506af0f03a4d40432f4a4226
Griffin Bassman <griffinbassman@gmail.com>
2022-12-19 13:55:00 -0500

    add testing

---
+++ python/tests/test_confidence_sequence_robust.py
@@ -0,0 +1,14 @@
+from confidence_sequence_robust import DDRM as confidence_sequence_robust
+import numpy as np
+
+def test_confidence_sequence_robust():
+    csr = confidence_sequence_robust()
+    for _ in range(200):
+        csr.addobs((1.1, 1))
+        csr.addobs((1.1, 0))
+    alpha = 0.05
+    lb, ub = csr.getci(alpha)
+    
+    # Compare bounds to confidence_sequence_robust_test.cc
+    np.testing.assert_almost_equal(lb, 0.4574146652500113, 6)
+    np.testing.assert_almost_equal(ub, 0.5423597665906932, 6)
--- test/unit_test/confidence_sequence_robust_test.cc
+++ test/unit_test/confidence_sequence_robust_test.cc
@@ -6,17 +6,25 @@
 
 #include "test_common.h"
 
-#include <boost/math/tools/minima.hpp>
 #include <boost/test/test_tools.hpp>
 #include <boost/test/unit_test.hpp>
+
+constexpr double CSR_DOUBLE_TOL = 0.000001;
 
 BOOST_AUTO_TEST_CASE(confidence_sequence_robust_test)
 {
   VW::confidence_sequence_robust csr;
-  for (int i = 0; i < 200; ++i)
+
+  for (size_t i = 0; i < 200; ++i)
   {
     csr.update(1.1, 1);
     csr.update(1.1, 0);
   }
-  std::cout << csr.lower_bound() << " " << csr.upper_bound() << "\n";
+
+  double lb = csr.lower_bound();
+  double ub = csr.upper_bound();
+
+  // Compare to confidence_sequence_robust_test.py
+  BOOST_CHECK_CLOSE(lb, 0.4574146652500113, CSR_DOUBLE_TOL);
+  BOOST_CHECK_CLOSE(ub, 0.5423597665906932, CSR_DOUBLE_TOL);
 }
