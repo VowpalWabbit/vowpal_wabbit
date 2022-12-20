90b4f2c80f4645672454ab686b612ad022bb8ef5
Jack Gerrits <jackgerrits95@gmail.com>
2022-12-20 10:28:41 -0500

    refactor: move confidence_seq code to impl

--- vowpalwabbit/core/include/vw/core/confidence_sequence.h
+++ vowpalwabbit/core/include/vw/core/confidence_sequence.h
@@ -4,15 +4,11 @@
 
 #pragma once
 
-#if !defined(__APPLE__) && !defined(_WIN32)
-#  define __STDCPP_MATH_SPEC_FUNCS__ 201003L
-#  define __STDCPP_WANT_MATH_SPEC_FUNCS__ 1
-#endif
+#include "vw/core/vw_fwd.h"
 
-#include "vw/core/io_buf.h"
-#include "vw/core/metric_sink.h"
-
-#include <algorithm>
+#include <cstdint>
+#include <string>
+#include <vector>
 
 constexpr float CS_DEFAULT_ALPHA = 0.05f;
 
@@ -22,41 +18,9 @@ class incremental_f_sum
 {
 public:
   std::vector<double> partials;
-
-  incremental_f_sum operator+=(double x)
-  {
-    int i = 0;
-    for (double y : this->partials)
-    {
-      if (std::abs(x) < std::abs(y)) { std::swap(x, y); }
-      double hi = x + y;
-      double lo = y - (hi - x);
-      if (lo != 0.0)
-      {
-        this->partials[i] = lo;
-        ++i;
-      }
-      x = hi;
-    }
-    this->partials.resize(i + 1);
-    this->partials[i] = x;
-    return *this;
-  }
-
-  incremental_f_sum operator+(incremental_f_sum const& other)
-  {
-    incremental_f_sum result;
-    result.partials = this->partials;
-    for (double y : other.partials) { result += y; }
-    return result;
-  }
-
-  operator double() const
-  {
-    double result = 0.0;
-    for (double x : this->partials) { result += x; }
-    return result;
-  }
+  incremental_f_sum operator+=(double x);
+  incremental_f_sum operator+(incremental_f_sum const& other) const;
+  operator double() const;
 };
 
 class confidence_sequence
@@ -92,7 +56,7 @@ public:
   confidence_sequence(
       double alpha = CS_DEFAULT_ALPHA, double rmin_init = 0.0, double rmax_init = 1.0, bool adjust = true);
   void update(double w, double r, double p_drop = 0.0, double n_drop = -1.0);
-  void persist(metric_sink&, const std::string&);
+  void persist(metric_sink&, const std::string&) const;
   void reset_stats();
   float lower_bound() const;
   float upper_bound() const;
