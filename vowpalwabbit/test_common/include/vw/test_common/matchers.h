a8847da6d894bfea84e5901872e5809b678a3e1b
Jack Gerrits <jackgerrits95@gmail.com>
2022-12-21 10:09:13 -0500

    format

--- vowpalwabbit/test_common/include/vw/test_common/matchers.h
+++ vowpalwabbit/test_common/include/vw/test_common/matchers.h
@@ -7,7 +7,8 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-MATCHER(ActionScoreEqual, "") {
+MATCHER(ActionScoreEqual, "")
+{
   auto lhs_action = std::get<0>(arg).action;
   auto rhs_action = std::get<1>(arg).action;
   testing::Matcher<uint32_t> action_matcher = testing::Eq(lhs_action);
@@ -15,5 +16,6 @@ MATCHER(ActionScoreEqual, "") {
   auto lhs_score = std::get<0>(arg).score;
   auto rhs_score = std::get<1>(arg).score;
   testing::Matcher<float> score_matcher = testing::FloatEq(lhs_score);
-  return action_matcher.MatchAndExplain(rhs_action, result_listener) && score_matcher.MatchAndExplain(rhs_score, result_listener);
+  return action_matcher.MatchAndExplain(rhs_action, result_listener) &&
+      score_matcher.MatchAndExplain(rhs_score, result_listener);
 }
