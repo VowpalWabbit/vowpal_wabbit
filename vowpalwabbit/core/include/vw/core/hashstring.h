cf26afe8bf8a169ce0f35f0153b74bd4b509fcde
Jack Gerrits <jackgerrits95@gmail.com>
2023-01-03 16:04:34 -0500

    mark constexpr

--- vowpalwabbit/core/include/vw/core/hashstring.h
+++ vowpalwabbit/core/include/vw/core/hashstring.h
@@ -14,9 +14,9 @@ namespace VW
 {
 namespace details
 {
-inline uint32_t hashall(const char* s, size_t len, uint32_t h) { return VW::uniform_hash(s, len, h); }
+VW_STD14_CONSTEXPR inline uint32_t hashall(const char* s, size_t len, uint32_t h) { return VW::uniform_hash(s, len, h); }
 
-inline uint32_t hashstring(const char* s, size_t len, uint32_t h)
+VW_STD14_CONSTEXPR inline uint32_t hashstring(const char* s, size_t len, uint32_t h)
 {
   const char* front = s;
   while (len > 0 && front[0] <= 0x20 && static_cast<int>(front[0]) >= 0)
@@ -36,14 +36,14 @@ inline uint32_t hashstring(const char* s, size_t len, uint32_t h)
 
   return ret + h;
 }
-  }
+}  // namespace details
 
-  using hash_func_t = uint32_t (*)(const char*, size_t, uint32_t);
+using hash_func_t = uint32_t (*)(const char*, size_t, uint32_t);
 
-  hash_func_t get_hasher(const std::string& s);
+hash_func_t get_hasher(const std::string& s);
 
-  }  // namespace VW
-  using hash_func_t VW_DEPRECATED("Moved into VW namespace") = uint32_t (*)(const char*, size_t, uint32_t);
+}  // namespace VW
+using hash_func_t VW_DEPRECATED("Moved into VW namespace") = uint32_t (*)(const char*, size_t, uint32_t);
 
-  VW_DEPRECATED("Moved into VW namespace")
-  VW::hash_func_t get_hasher(const std::string& s);
+VW_DEPRECATED("Moved into VW namespace")
+VW::hash_func_t get_hasher(const std::string& s);
