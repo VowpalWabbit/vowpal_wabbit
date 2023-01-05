bccf36f4450df81e24037eaf62e7f27285cc835e
Jack Gerrits <jackgerrits95@gmail.com>
2023-01-04 19:52:46 -0500

    deprecated usage

--- vowpalwabbit/core/include/vw/core/cb.h
+++ vowpalwabbit/core/include/vw/core/cb.h
@@ -99,7 +99,7 @@ VW_DEPRECATED("Renamed to VW::ec_is_example_header_cb")
 inline bool ec_is_example_header(VW::example const& ec) { return VW::ec_is_example_header_cb(ec); }
 
 VW_DEPRECATED("Renamed to VW::get_observed_cost_cb")
-inline std::pair<bool, cb_class> get_observed_cost_cb(const label& ld) { return VW::get_observed_cost_cb(ld); }
+inline std::pair<bool, VW::cb_class> get_observed_cost_cb(const VW::cb_label& ld) { return VW::get_observed_cost_cb(ld); }
 }  // namespace CB
 
 namespace CB_EVAL
