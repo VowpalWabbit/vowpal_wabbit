aaaf41f6ea1af85b77bdd016257192f4a8835b6d
olgavrou <olgavrou@gmail.com>
2023-01-07 19:16:27 -0500

    fix test

--- vowpalwabbit/core/tests/cb_graph_feedback_label_text_parse_test.cc
+++ vowpalwabbit/core/tests/cb_graph_feedback_label_text_parse_test.cc
@@ -18,23 +18,24 @@
 
 using namespace testing;
 
-void parse_cb_label(VW::string_view label, CB::label& l, VW::reduction_features& red_features)
+void parse_cb_label(VW::label_parser& lp, VW::string_view label, VW::polylabel& l, VW::reduction_features& red_features)
 {
   std::vector<VW::string_view> words;
   VW::tokenize(' ', label, words);
-  l.reset_to_default();
+  lp.default_label(l);
   VW::label_parser_reuse_mem mem;
   auto null_logger = VW::io::create_null_logger();
-  CB::parse_label(l, red_features, mem, words, null_logger);
+  lp.parse_label(l, red_features, mem, nullptr, words, null_logger);
 }
 
 TEST(cb_gf_label_tests, cb_graph_parse_label)
 {
-  CB::label label;
+  VW::polylabel label;
+  auto lp = VW::cb_label_parser_global;
   VW::reduction_features red_features;
-  parse_cb_label("shared graph 0,1,2 1,2,3 5,5,5 | s_1 s_2", label, red_features);
+  parse_cb_label(lp, "shared graph 0,1,2 1,2,3 5,5,5 | s_1 s_2", label, red_features);
   // it is still a shared example
-  EXPECT_FLOAT_EQ(label.costs[0].probability, -1.f);
+  EXPECT_FLOAT_EQ(label.cb.costs[0].probability, -1.f);
   auto& graph_reduction_features = red_features.template get<VW::cb_graph_feedback::reduction_features>();
   EXPECT_EQ(graph_reduction_features.triplets.size(), 3);
 
@@ -53,11 +54,12 @@ TEST(cb_gf_label_tests, cb_graph_parse_label)
 
 TEST(cb_gf_label_tests, cb_graph_parse_label_no_fts)
 {
-  CB::label label;
+  VW::polylabel label;
+  auto lp = VW::cb_label_parser_global;
   VW::reduction_features red_features;
-  parse_cb_label("shared graph 0,1,2 1,2,3 5,5,5 |", label, red_features);
+  parse_cb_label(lp, "shared graph 0,1,2 1,2,3 5,5,5 |", label, red_features);
   // it is still a shared example
-  EXPECT_FLOAT_EQ(label.costs[0].probability, -1.f);
+  EXPECT_FLOAT_EQ(label.cb.costs[0].probability, -1.f);
   auto& graph_reduction_features = red_features.template get<VW::cb_graph_feedback::reduction_features>();
   EXPECT_EQ(graph_reduction_features.triplets.size(), 3);
 
@@ -76,22 +78,25 @@ TEST(cb_gf_label_tests, cb_graph_parse_label_no_fts)
 
 TEST(cb_gf_label_tests, throws_w_malformed_triplet)
 {
-  CB::label label;
+  VW::polylabel label;
+  auto lp = VW::cb_label_parser_global;
   VW::reduction_features red_features;
-  EXPECT_THROW(parse_cb_label("shared graph 0,1,2 0,1,2 5 | s_1 s_2", label, red_features), VW::vw_exception);
+  EXPECT_THROW(parse_cb_label(lp, "shared graph 0,1,2 0,1,2 5 | s_1 s_2", label, red_features), VW::vw_exception);
 }
 
 TEST(cb_gf_label_tests, throws_w_standalone_graph_example)
 {
-  CB::label label;
+  VW::polylabel label;
+  auto lp = VW::cb_label_parser_global;
   VW::reduction_features red_features;
-  EXPECT_THROW(parse_cb_label("graph 0,1,2 0,1,2 5,5,5 | s_1 s_2", label, red_features), VW::vw_exception);
+  EXPECT_THROW(parse_cb_label(lp, "graph 0,1,2 0,1,2 5,5,5 | s_1 s_2", label, red_features), VW::vw_exception);
 }
 
 TEST(cb_gf_label_tests, throws_w_graph_in_label)
 {
-  CB::label label;
+  VW::polylabel label;
+  auto lp = VW::cb_label_parser_global;
   VW::reduction_features red_features;
   EXPECT_THROW(
-      parse_cb_label("0:1.0:0.5 graph 0,1,2 0,1,2 5,5,5  | a_1 b_1 c_1", label, red_features), VW::vw_exception);
+      parse_cb_label(lp, "0:1.0:0.5 graph 0,1,2 0,1,2 5,5,5  | a_1 b_1 c_1", label, red_features), VW::vw_exception);
 }
