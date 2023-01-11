94bcd1f6e0bc9bb515d88dd897615d08f6f7f44a
Jack Gerrits <jackgerrits95@gmail.com>
2023-01-11 16:29:17 -0500

    refactor: consolidate random into common

--- vowpalwabbit/common/include/vw/common/random_details.h
+++ vowpalwabbit/common/include/vw/common/random_details.h
@@ -1,24 +1,26 @@
 // Copyright (c) by respective owners including Yahoo!, Microsoft, and
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
+#pragma once
 
-// A quick implementation similar to drand48 for cross-platform compatibility
 #include <cmath>
 #include <cstdint>
+
+namespace VW
+{
+namespace details
+{
 //
 // NB: the 'ULL' suffix is not part of the constant it is there to
 // prevent truncation of constant to (32-bit long) when compiling
 // in a 32-bit env: warning: integer constant is too large for "long" type
 //
-#ifdef __clang__
-#  pragma clang diagnostic ignored "-Wc++11-long-long"
-#endif
 constexpr uint64_t CONSTANT_A = 0xeece66d5deece66dULL;
 constexpr uint64_t CONSTANT_C = 2147483647;
 
 constexpr int BIAS = 127 << 23;
 
-float merand48(uint64_t& initial)
+inline float merand48(uint64_t& initial)
 {
   static_assert(
       sizeof(int32_t) == sizeof(float), "Floats and int32_ts are converted between, they must be the same size.");
@@ -27,9 +29,9 @@ float merand48(uint64_t& initial)
   return reinterpret_cast<float&>(temp) - 1;
 }
 
-float merand48_noadvance(uint64_t v) { return merand48(v); }
+inline float merand48_noadvance(uint64_t v) { return merand48(v); }
 
-float merand48_boxmuller(uint64_t& index)
+inline float merand48_boxmuller(uint64_t& index)
 {
   float x1 = 0.0;
   float x2 = 0.0;
@@ -42,3 +44,5 @@ float merand48_boxmuller(uint64_t& index)
   temp = std::sqrt((-2.0f * logf(temp)) / temp);
   return x1 * temp;
 }
+}  // namespace details
+}  // namespace VW
