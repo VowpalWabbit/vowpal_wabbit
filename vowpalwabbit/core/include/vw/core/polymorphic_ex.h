cd4320dac6bea2560c70ade3b1d8edf58786e8ec
Byron Xu <byronxu@microsoft.com>
2023-01-18 11:49:01 -0500

    change polymorphic_ex to use more const

--- vowpalwabbit/core/include/vw/core/polymorphic_ex.h
+++ vowpalwabbit/core/include/vw/core/polymorphic_ex.h
@@ -7,33 +7,29 @@ namespace VW
 class polymorphic_ex
 {
 public:
-  polymorphic_ex(VW::example* ex) : _example(ex), _is_multiline(false) {}
-  polymorphic_ex(VW::example& ex) : _example(&ex), _is_multiline(false) {}
-  polymorphic_ex(VW::multi_ex* ex) : _multi_ex(ex), _is_multiline(true) {}
-  polymorphic_ex(VW::multi_ex& ex) : _multi_ex(&ex), _is_multiline(true) {}
+  // This will implicitly convert both `example` and `const example` pointers and references into polymorphic_ex
+  polymorphic_ex(const VW::example* ex) : _example(ex), _is_multiline(false) {}
+  polymorphic_ex(const VW::example& ex) : _example(&ex), _is_multiline(false) {}
+  polymorphic_ex(const VW::multi_ex* ex) : _multi_ex(ex), _is_multiline(true) {}
+  polymorphic_ex(const VW::multi_ex& ex) : _multi_ex(&ex), _is_multiline(true) {}
 
-  // We need to be able to convert const example/multi_ex into polymorphic_ex
-  polymorphic_ex(const VW::example* ex) : _example(const_cast<VW::example*>(ex)), _is_multiline(false) {}
-  polymorphic_ex(const VW::example& ex) : _example(const_cast<VW::example*>(&ex)), _is_multiline(false) {}
-  polymorphic_ex(const VW::multi_ex* ex) : _multi_ex(const_cast<VW::multi_ex*>(ex)), _is_multiline(true) {}
-  polymorphic_ex(const VW::multi_ex& ex) : _multi_ex(const_cast<VW::multi_ex*>(&ex)), _is_multiline(true) {}
-
-  operator VW::example*() { if(_is_multiline) { THROW("Cannot convert example to multi_ex"); } return _example; }
-  operator VW::example&() { if(_is_multiline) { THROW("Cannot convert example to multi_ex"); } return *_example; }
-  operator VW::multi_ex*() { if(!_is_multiline) { THROW("Cannot convert multi_ex to example"); } return _multi_ex; }
-  operator VW::multi_ex&() { if(!_is_multiline) { THROW("Cannot convert multi_ex to example"); } return *_multi_ex; }
-
-  // Can convert back to const example/multi_ex only if the polymorphic_ex class itself is const
+  // Can always implicitly convert back to `const example`
   operator const VW::example*() const { if(_is_multiline) { THROW("Cannot convert example to multi_ex"); } return _example; }
   operator const VW::example&() const { if(_is_multiline) { THROW("Cannot convert example to multi_ex"); } return *_example; }
   operator const VW::multi_ex*() const { if(!_is_multiline) { THROW("Cannot convert multi_ex to example"); } return _multi_ex; }
   operator const VW::multi_ex&() const { if(!_is_multiline) { THROW("Cannot convert multi_ex to example"); } return *_multi_ex; }
 
+  // Can implicitly convert back to mutable `example` only if the polymorphic_ex object itself is not const
+  operator VW::example*() { if(_is_multiline) { THROW("Cannot convert example to multi_ex"); } return const_cast<VW::example*>(_example); }
+  operator VW::example&() { if(_is_multiline) { THROW("Cannot convert example to multi_ex"); } return const_cast<VW::example&>(*_example); }
+  operator VW::multi_ex*() { if(!_is_multiline) { THROW("Cannot convert multi_ex to example"); } return const_cast<VW::multi_ex*>(_multi_ex); }
+  operator VW::multi_ex&() { if(!_is_multiline) { THROW("Cannot convert multi_ex to example"); } return const_cast<VW::multi_ex&>(*_multi_ex); }
+
   bool is_multiline() const { return _is_multiline; }
 
 private:
-  VW::example* _example;
-  VW::multi_ex* _multi_ex;
-  bool _is_multiline = false;
+  const VW::example* _example;
+  const VW::multi_ex* _multi_ex;
+  const bool _is_multiline = false;
 };
 }
