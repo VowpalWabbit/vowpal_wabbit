d28f39fd302e65bcb4c113b7b220eae37e99273f
Griffin Bassman <griffinbassman@gmail.com>
2023-02-13 11:36:02 -0500

    comment

--- test/benchmarks/benchmark_epsilon_decay.cc
+++ test/benchmarks/benchmark_epsilon_decay.cc
@@ -14,13 +14,12 @@
 #include <benchmark/benchmark.h>
 
 template <class... ExtraArgs>
-static void bench_epsilon_decay(benchmark::State& state, ExtraArgs&&... extra_args)
+static void bench_epsilon_decay(benchmark::State& state, bool use_decay, ExtraArgs&&... extra_args)
 {
   std::array<std::string, sizeof...(extra_args)> res = {extra_args...};
-  std::string use_decay = res[0];
-  std::string model_count = res[1];
-  std::string bit_size = res[2];
-  std::string tolerance = res[3];
+  std::string model_count = res[0];
+  std::string bit_size = res[1];
+  std::string tolerance = res[2];
 
   using callback_map =
       typename std::map<size_t, std::function<bool(simulator::cb_sim&, VW::workspace&, VW::multi_ex&)>>;
@@ -31,7 +30,7 @@ static void bench_epsilon_decay(benchmark::State& state, ExtraArgs&&... extra_ar
     const size_t num_iterations = 1000;
     const size_t seed = 99;
     const std::vector<uint64_t> swap_after = {500};
-    if (use_decay == "decay")
+    if (use_decay)
     {
       simulator::_test_helper_hook(
           std::vector<std::string>{"-l", "1e-3", "--power_t", "0", "-q::", "--cb_explore_adf", "--epsilon_decay",
@@ -48,10 +47,10 @@ static void bench_epsilon_decay(benchmark::State& state, ExtraArgs&&... extra_ar
   }
 }
 
-BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_1_model_big_tol, "decay", "1", "18", "1e-2");
-BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_2_model_big_tol, "decay", "2", "19", "1e-2");
-BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_4_model_big_tol, "decay", "4", "20", "1e-2");
-BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_1_model_small_tol, "decay", "1", "18", "1e-6");
-BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_2_model_small_tol, "decay", "2", "19", "1e-6");
-BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_4_model_small_tol, "decay", "4", "20", "1e-6");
-BENCHMARK_CAPTURE(bench_epsilon_decay, without_epsilon_decay, "nondecay", "", "", "");
+BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_1_model_big_tol, true, "1", "18", "1e-2");
+BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_2_model_big_tol, true, "2", "19", "1e-2");
+BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_4_model_big_tol, true, "4", "20", "1e-2");
+BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_1_model_small_tol, true, "1", "18", "1e-6");
+BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_2_model_small_tol, true, "2", "19", "1e-6");
+BENCHMARK_CAPTURE(bench_epsilon_decay, epsilon_decay_4_model_small_tol, true, "4", "20", "1e-6");
+BENCHMARK_CAPTURE(bench_epsilon_decay, without_epsilon_decay, false, "", "", "");
