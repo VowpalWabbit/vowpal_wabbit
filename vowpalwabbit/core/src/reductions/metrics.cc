c7e5e3f4278c34d93d292887140c58c0e0063cb8
Eduardo Salinas <edus@microsoft.com>
2023-02-14 17:53:03 +0000

    metrics file

--- vowpalwabbit/core/src/reductions/metrics.cc
+++ vowpalwabbit/core/src/reductions/metrics.cc
@@ -137,7 +137,7 @@ void predict_or_learn(metrics_data& data, T& base, E& ec)
 
 void VW::reductions::additional_metrics(VW::workspace& all, VW::metric_sink& sink)
 {
-  sink.set_float("loss", all.sd->sum_loss);
+  sink.set_float("sum_loss", all.sd->sum_loss);
   sink.set_uint("total_log_calls", all.logger.get_log_count());
 
   std::vector<std::string> enabled_learners;
