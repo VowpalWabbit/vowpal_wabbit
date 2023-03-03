8ff840e8c9ca3d0342ddd2eb6a366fd72b1433c1
Eduardo Salinas <edus@microsoft.com>
2023-03-03 17:04:38 +0000

    fix: [epsdecay] return champ prediction always

--- vowpalwabbit/core/src/reductions/epsilon_decay.cc
+++ vowpalwabbit/core/src/reductions/epsilon_decay.cc
@@ -88,6 +88,9 @@ void epsilon_decay_data::update_weights(float init_ep, VW::LEARNER::learner& bas
       ++_global_counter;
     }
     auto& ep_fts = examples[0]->ex_reduction_features.template get<VW::cb_explore_adf::greedy::reduction_features>();
+
+    VW::action_scores champ_a_s;
+
     // Process each model, then update the upper/lower bounds for each model
     for (int64_t model_ind = model_count - 1; model_ind >= 0; --model_ind)
     {
@@ -120,11 +123,13 @@ void epsilon_decay_data::update_weights(float init_ep, VW::LEARNER::learner& bas
                        << " lb: " << conf_seq_estimators[model_ind][model_ind].lower_bound()
                        << " champ_ub: " << conf_seq_estimators[model_count - 1][model_ind].upper_bound()
                        << " p_pred: " << a_s.score << "\n";
+            if (model_ind == model_count - 1) { champ_a_s = examples[0]->pred.a_s; }
           }
           break;
         }
       }
     }
+    examples[0]->pred.a_s = champ_a_s;
   }
 }
 
