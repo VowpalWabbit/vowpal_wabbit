57926204254f314d67e0f9ebd8de800a54c00954
Marek Wydmuch <marek@wydmuch.poznan.pl>
2023-03-17 17:24:24 +0100

    apply black formating to plt_demo.py

--- demo/plt/plt_demo.py
+++ demo/plt/plt_demo.py
@@ -6,7 +6,7 @@ import time
 # This is demo example that demonstrates usage of PLT reduction on few popular multilabel datasets.
 
 # Select dataset
-dataset = "mediamill_exp1"  # should be in ["mediamill_exp1", "eurlex", "rcv1x", "wiki10", "amazonCat"]
+dataset = "eurlex"  # should be in ["mediamill_exp1", "eurlex", "rcv1x", "wiki10", "amazonCat"]
 
 # Select reduction
 reduction = "plt"  # should be in ["plt", "multilabel_oaa"]
@@ -17,13 +17,17 @@ train_data = f"{dataset}_train.vw"
 test_data = f"{dataset}_test.vw"
 output_model = f"{dataset}_{reduction}_model"
 
-# Parameters (for some datasets you might want to change number of passes)
-kary_tree = 16  # arity of the tree, 
-                # higher values usually give better results, but increase training time
-passes = 5  # number of passes over the dataset
+# Parameters
+kary_tree = 16  # arity of the tree,
+# higher values usually give better results, but increase training time
+
+passes = 5  # number of passes over the dataset,
+# for some datasets you might want to change number of passes
+
 l = 0.5  # learning rate
-other_training_params = "--holdout_off"  # because these dataset have many rare labels,
-                                         # disabling holdout set improves final performance
+
+other_training_params = "--holdout_off"  # because these datasets have many rare labels,
+# disabling the holdout set improves the final performance
 
 # dict with params for different datasets (k and b)
 params_dict = {
@@ -39,11 +43,11 @@ else:
     print(f"Dataset {dataset} is not supported by this demo.")
 
 # Download dataset (source: http://manikvarma.org/downloads/XC/XMLRepository.html)
-# Datasets were transformed to VW's format, 
+# Datasets were transformed to VW's format,
 # and features values were normalized (this helps with performance).
 if not os.path.exists(train_data):
     print("Downloading train dataset:")
-    os.system("wget http://www.cs.put.poznan.pl/mwydmuch/data/{}".format(train_data))    
+    os.system("wget http://www.cs.put.poznan.pl/mwydmuch/data/{}".format(train_data))
 
 if not os.path.exists(test_data):
     print("Downloading test dataset:")
@@ -75,9 +79,7 @@ print(f"threshold test time (s) = {thr_test_time:.3f}")
 if reduction == "plt":
     print("\nTesting with top-5 prediction\n")
     start = time.time()
-    test_topk_cmd = (
-        f"../../build/vowpalwabbit/cli/vw {test_data} -i {output_model} --loss_function logistic --top_k 5 -t"
-    )
+    test_topk_cmd = f"../../build/vowpalwabbit/cli/vw {test_data} -i {output_model} --loss_function logistic --top_k 5 -t"
     print(test_topk_cmd)
     os.system(test_topk_cmd)
     topk_test_time = time.time() - start
