6089104448141fd2608aaee9462486078468ec08
olgavrou <olgavrou@gmail.com>
2023-05-02 09:38:56 -0400

    feat: WASM VW JS wrapper file/classes (#4575)

---
+++ wasm/vw.js
@@ -0,0 +1,134 @@
+const { log } = require('console');
+const fs = require('fs');
+const VWWasmModule = require('./out/vw-wasm.js');
+
+
+// internals
+
+const ProblemType =
+{
+    All: 'All',
+    CB: 'Cb',
+};
+
+// exported
+
+class WorkspaceBase {
+    constructor(type, { args_str, model_file } = {}) {
+        if (args_str == undefined) {
+            console.error("Can not initialize vw object without args_str");
+        }
+
+        if (model_file !== undefined) {
+            let modelBuffer = fs.readFileSync(model_file);
+            let ptr = VWWasmModule._malloc(modelBuffer.byteLength);
+            let heapBytes = new Uint8Array(VWWasmModule.HEAPU8.buffer, ptr, modelBuffer.byteLength);
+            heapBytes.set(new Uint8Array(modelBuffer));
+            if (type == ProblemType.All) {
+                this._instance = new VWWasmModule.VWModel(args_str, ptr, modelBuffer.byteLength);
+            } else if (type == ProblemType.CB) {
+                this._instance = new VWWasmModule.VWCBModel(args_str, ptr, modelBuffer.byteLength);
+            }
+            else {
+                console.error("Unknown model type");
+            }
+            VWWasmModule._free(ptr);
+        }
+        else {
+            if (type == ProblemType.All) {
+                this._instance = new VWWasmModule.VWModel(args_str);
+            } else if (type == ProblemType.CB) {
+                this._instance = new VWWasmModule.VWCBModel(args_str);
+            }
+            else {
+                console.error("Unknown model type");
+            }
+        }
+
+
+        return new Proxy(this, {
+            get(target, propertyName, receiver) {
+                if (typeof target._instance[propertyName] == 'function') {
+                    return target._instance[propertyName].bind(target._instance);
+                }
+
+                return Reflect.get(target, propertyName, receiver);
+            }
+        });
+    }
+
+    saveModel(model_file) {
+        let char_vector = this._instance.getModel();
+        const size = char_vector.size();
+        const uint8Array = new Uint8Array(size);
+
+        for (let i = 0; i < size; ++i) {
+            uint8Array[i] = char_vector.get(i);
+        }
+
+        fs.writeFileSync(model_file, Buffer.from(uint8Array));
+
+        char_vector.delete();
+    }
+
+    loadModel(model_file) {
+        let modelBuffer = fs.readFileSync(model_file);
+        let ptr = VWWasmModule._malloc(modelBuffer.byteLength);
+        let heapBytes = new Uint8Array(VWWasmModule.HEAPU8.buffer, ptr, modelBuffer.byteLength);
+        heapBytes.set(new Uint8Array(modelBuffer));
+        this._instance.loadModelFromBuffer(ptr, modelBuffer.byteLength);
+        VWWasmModule._free(ptr);
+    }
+
+    delete() {
+        this._instance.delete();
+    }
+};
+
+class Workspace extends WorkspaceBase {
+    constructor({ args_str, model_file } = {}) {
+        super(ProblemType.All, { args_str, model_file });
+    }
+};
+
+class CbWorkspace extends WorkspaceBase {
+    constructor({ args_str, model_file } = {}) {
+        super(ProblemType.CB, { args_str, model_file });
+    }
+};
+
+function getExceptionMessage(exception) {
+    VWWasmModule.getExceptionMessage(exception)
+};
+
+module.exports = new Promise((resolve) => {
+    VWWasmModule.onRuntimeInitialized = () => {
+        class Prediction {
+            static Type = {
+                Scalar: VWWasmModule.PredictionType.scalar,
+                Scalars: VWWasmModule.PredictionType.scalars,
+                ActionScores: VWWasmModule.PredictionType.action_scores,
+                Pdf: VWWasmModule.PredictionType.pdf,
+                ActionProbs: VWWasmModule.PredictionType.action_probs,
+                MultiClass: VWWasmModule.PredictionType.multiclass,
+                MultiLabels: VWWasmModule.PredictionType.multilabels,
+                Prob: VWWasmModule.PredictionType.prob,
+                MultiClassProb: VWWasmModule.PredictionType.multiclassprob,
+                DecisionProbs: VWWasmModule.PredictionType.decision_probs,
+                ActionPdfValue: VWWasmModule.PredictionType.ActionPdfValue,
+                ActiveMultiClass: VWWasmModule.PredictionType.activeMultiClass,
+            };
+        };
+
+        resolve(
+            {
+                Workspace: Workspace,
+                CbWorkspace: CbWorkspace,
+                Prediction: Prediction,
+                getExceptionMessage: getExceptionMessage,
+                // todo hide wasm module?
+                VWWasmModule : VWWasmModule,
+            }
+        )
+    }
+})
