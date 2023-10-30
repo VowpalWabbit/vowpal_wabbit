3a7230f42e4ec00e56065ca782b22298d6b9efa4
Griffin Bassman <griffinbassman@gmail.com>
2023-10-30 16:16:18 -0400

    Update VowpalWabbitUtils.cmake

--- cmake/VowpalWabbitUtils.cmake
+++ cmake/VowpalWabbitUtils.cmake
@@ -22,8 +22,7 @@ if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
       include(FetchContent)
       FetchContent_Declare(
         googletest
-        URL https://github.com/google/googletest/archive/refs/tags/release-1.12.0.zip
-        DOWNLOAD_EXTRACT_TIMESTAMP true
+        URL https://github.com/google/googletest/archive/refs/tags/release-1.11.0.zip
       )
       # For Windows: Prevent overriding the parent project's compiler/linker settings
       set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
