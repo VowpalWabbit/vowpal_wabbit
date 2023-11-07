91644beb248f09d51aeea6ddb92d238157bd0d52
Griffin Bassman <griffinbassman@gmail.com>
2023-11-07 17:58:37 -0500

    1 job

--- test/run_tests.py
+++ test/run_tests.py
@@ -967,6 +967,13 @@ def main():
 
     default_test_spec_file: Path = test_ref_dir / "core.vwtest.json"
 
+    parser.add_argument(
+        "-j",
+        "--jobs",
+        type=int,
+        default=1,
+        help="Number of tests to run in parallel. Set to 1 to run tests sequentially.",
+    )
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
