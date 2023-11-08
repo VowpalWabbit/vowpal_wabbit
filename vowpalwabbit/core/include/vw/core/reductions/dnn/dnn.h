ba2e7f929def7860aa3f8bdde54320f9211d83ec
Rajan <rajan.chari@yahoo.com>
2023-11-08 17:12:39 -0500

    Implement dnn base reduction

--- vowpalwabbit/core/include/vw/core/reductions/dnn/dnn.h
+++ vowpalwabbit/core/include/vw/core/reductions/dnn/dnn.h
@@ -6,18 +6,56 @@
 
 #include "vw/core/vw_fwd.h"
 #include <memory>
+#include <unordered_map>
+
+// Forward declarations
+namespace at {  class Tensor; }
+namespace torch { namespace optim { class SGD; } }
 
 namespace VW
 {
 namespace reductions
 {
+class hash_location_mapper
+{
+  public:
+    hash_location_mapper(float* tensor_buffer, uint32_t num_inputs);
+    at::Tensor tensor_from_example(const example& ec);
+  private:
+    uint64_t get_dense_location(uint64_t hash);
+    std::unordered_map<uint64_t,uint64_t> _map_hash_to_dense;
+    float* _tensor_buffer = nullptr;  // not owned
+    uint32_t _num_inputs = 0;
+};
+
+struct Net;
+
 class dnn_learner
 {
 public:
-  void init();
-  void predict(example& ec);
-  void learn(const example& ec);
+  void init(
+    uint16_t num_layers,
+    uint16_t hidden_layer_size,
+    uint32_t num_inputs,
+    float contraction /*How much to contract the prediction of dnn*/,
+    uint32_t mini_batch_size);
+  at::Tensor predict(example& ec);
+  void learn(example& ec);
+  void mini_batch_update();
+  ~dnn_learner();
+private:
+  std::shared_ptr<hash_location_mapper> _phash_location_mapper;
+  std::shared_ptr<Net> _pmodel;
+  std::shared_ptr<torch::optim::SGD> _poptimizer;
+  float* _tensor_buffer = nullptr;
+  float _contraction = 1.0;         // How much to contract the prediction
+  uint16_t _num_layers = 0;         // Number of layers in the network
+  uint16_t _hidden_layer_size = 0;  // Size of the hidden layer
+  uint32_t _num_initial_inputs = 0; // Number of inputs to the network
+  uint32_t _mini_batch_size = 1;    // Number of examples to process in a batch
+  uint32_t _accumulate_gradient_count = 0;
 };
+
 
 std::shared_ptr<VW::LEARNER::learner> dnn_setup(VW::setup_base_i& stack_builder);
 }  // namespace reductions
--- vowpalwabbit/core/src/reductions/dnn/dnn.cc
+++ vowpalwabbit/core/src/reductions/dnn/dnn.cc
@@ -8,25 +8,145 @@
 using namespace VW::LEARNER;
 using namespace VW::config;
 
-namespace VW
+namespace VW { namespace reductions
 {
-namespace reductions
-{
+struct Net : torch::nn::Module {
 
-void dnn_learner::init()
+  Net(int16_t num_initial_inputs, int16_t hidden_layer_size, int32_t num_layers) :
+    _num_initial_inputs(num_initial_inputs),
+    _hidden_layer_size(hidden_layer_size),
+    _num_layers(num_layers)
+  {
+    // Construct and register n Linear submodules.
+    // Save the module pointers in a vector.
+    auto layer = register_module("fc0", torch::nn::Linear(_num_initial_inputs, _hidden_layer_size));
+    _layers.push_back(layer);
+    for(int i = 1; i < _num_layers-1; ++i) {
+        auto layer = register_module("fc" + std::to_string(i), torch::nn::Linear(_hidden_layer_size, _hidden_layer_size));
+        _layers.push_back(layer);
+    }
+
+    // The last layer has only one output
+    layer = register_module("fc"+ std::to_string(_num_layers), torch::nn::Linear(_num_initial_inputs, 1));
+    _layers.push_back(layer);
+
+  }
+
+  // Implement the Net's algorithm.
+  torch::Tensor forward(torch::Tensor x) {
+    for(int i = 0; i < _layers.size() - 1; ++i ) {
+        x = torch::relu(_layers[i]->forward(x));
+    }
+    // no relu on last layer
+    x = _layers[_layers.size()-1]->forward(x);
+    return x;
+  }
+
+  int32_t get_input_size() {
+     return _num_initial_inputs;
+   }
+
+  private:
+    std::vector<std::shared_ptr<torch::nn::LinearImpl>> _layers;
+    int16_t _num_initial_inputs;
+    int16_t _hidden_layer_size;
+    int32_t _num_layers;
+};
+
+ void dnn_learner::init(
+    uint16_t num_layers,
+    uint16_t hidden_layer_size,
+    uint32_t num_inputs,
+    float contraction,
+    uint32_t mini_batch_size)
 {
-  torch::Tensor tensor = torch::rand({2,3});
-  std::cout << "Hello from pytorch!" << tensor << std::endl;
+   std::cout << "dnn_init(): num_layers: " << num_layers << " hidden_layer_size: " << hidden_layer_size << " num_inputs: " << num_inputs << " contraction: " << contraction << ", activation: relu, optimizer: SGD"<<  std::endl;
+  _contraction = contraction;
+  _num_layers = num_layers;
+  _hidden_layer_size = hidden_layer_size;
+  _num_initial_inputs = num_inputs;
+  _tensor_buffer = new float[_num_initial_inputs];
+  _pmodel = std::make_shared<Net>(_num_initial_inputs, _hidden_layer_size, _num_layers);
+  _poptimizer = std::shared_ptr<torch::optim::SGD>(new torch::optim::SGD(_pmodel->parameters(),0.01));
+  _phash_location_mapper = std::make_shared<hash_location_mapper>(_tensor_buffer,_num_initial_inputs);
 }
 
-void dnn_learner::predict(example& ec)
+at::Tensor dnn_learner::predict(example& ec)
 {
-  printf("dnn_predict()\n"); 
+  //printf("dnn_predict()\n"); 
+  at::Tensor input = _phash_location_mapper->tensor_from_example(ec);
+  //std::cout << "input: \n" << input;
+  at::Tensor output = _pmodel->forward(input);
+
+  //std::cout << "output: \n" << output;
+  ec.partial_prediction = output.item<float>();
+  //std::cout << "predicted: \n" << output;
+  ec.partial_prediction *= static_cast<float>(_contraction);
+  //std::cout << "contracted prediction: \n" << ec.partial_prediction;
+  ec.pred.scalar = ec.partial_prediction;
+
+  return output;
 }
 
-void dnn_learner::learn(const example& ec)
+uint64_t hash_location_mapper::get_dense_location(uint64_t hash)
 {
-  printf("dnn_learn()\n");   
+  auto it = _map_hash_to_dense.find(hash);
+  if (it == _map_hash_to_dense.end()) {
+    uint64_t loc = _map_hash_to_dense.size();
+    _map_hash_to_dense[hash] = loc;
+    return loc;
+  }
+  return it->second;
+}
+
+hash_location_mapper::hash_location_mapper(float* tensor_buffer, uint32_t num_inputs) :
+  _tensor_buffer(tensor_buffer),
+  _num_inputs(num_inputs)
+{}
+
+at::Tensor hash_location_mapper::tensor_from_example(const example& ec)
+{
+  example_predict& ep = (example_predict&)ec;
+  float* dense = _tensor_buffer;
+  for (VW::example_predict::iterator ns = ep.begin(); ns != ep.end(); ++ns) {
+    for ( auto feat = (*ns).begin(); feat != (*ns).end(); ++feat) {
+      uint64_t loc = get_dense_location((*feat).index());
+      dense[loc] = (*feat).value();
+    }
+  }
+
+  // Zero pad the rest of the tensor up to input_sz
+  for (uint32_t i = _map_hash_to_dense.size(); i < _num_inputs; ++i) {
+   dense[i] = 0.0;
+  }
+  return torch::from_blob(dense,_num_inputs);
+}
+
+void dnn_learner::learn(example& ec)
+{
+  //printf("dnn_learn()\n");
+  auto output = predict(ec);
+  auto y = torch::tensor({ec.l.simple.label});
+  //std::cout << "expected: \n" << y;
+  auto err = torch::mse_loss(output,y);
+  ec.loss = err.item<float>();
+  err.backward();
+  //std::cout << "error: \n" << err;
+  ++_accumulate_gradient_count;
+}
+
+dnn_learner::~dnn_learner() {
+  delete[] _tensor_buffer;
+}
+
+void dnn_learner::mini_batch_update()
+{
+  // accumulate gradients for mini_batch examples before updating parameters
+  if(_accumulate_gradient_count >= _mini_batch_size-1) {
+    _poptimizer->step();
+    _pmodel->zero_grad();
+    _accumulate_gradient_count = 0;
+  }
 }
 
 void predict(dnn_learner& dnn , VW::example& ec)
@@ -39,9 +159,15 @@ void learn(dnn_learner& dnn, VW::example& ec)
   dnn.learn(ec);
 }
 
-void cleanup_example(dnn_learner& dnn, VW::example& ec)
+void update_stats(const VW::workspace& all, shared_data& sd, const dnn_learner&, const VW::example& ex, VW::io::logger& logger)
 {
-  printf("dnn_cleanup_example()\n"); 
+  //printf("dnn_update_stats()\n");
+  sd.update(ex.test_only, ex.l.simple.is_labeled(), ex.loss, ex.weight, ex.get_num_features());
+}
+
+void cleanup_example(dnn_learner& dnn, VW::example&)
+{
+  dnn.mini_batch_update();
 }
 
 std::shared_ptr<learner> dnn_setup(VW::setup_base_i& stack_builder)
@@ -51,35 +177,38 @@ std::shared_ptr<learner> dnn_setup(VW::setup_base_i& stack_builder)
 
   config::option_group_definition new_options("[Reduction] DNN");
   bool use_dnn = false;
-  int num_inputs = 100;
-  new_options.add(make_option("dnn", use_dnn).keep().necessary().help("Deep Neural Network Base Learner"))
-             .add(make_option("num_inputs", num_inputs).help("Inputs to dnn"));
+  int num_inputs = 20;
+  int num_layers = 3;
+  int hidden_layer_size = 20;
+  int mini_batch_size = 10;
+  new_options.add(make_option("dnn", use_dnn).keep().necessary().help("Fully connected deep neural network base learner."))
+             .add(make_option("num_layers", num_layers).help("Number of Layers in the dnn"))
+             .add(make_option("hidden_layer_size", hidden_layer_size).help("Size of hidden layers"))
+             .add(make_option("mini_batch_size", mini_batch_size).help("Number of gradients to accumulate before updating parameters."))
+             .add(make_option("num_inputs", num_inputs).help("Size of the initial input vector to dnn"));
 
   if (!options.add_parse_and_check_necessary(new_options)) { return nullptr; }
 
   auto dnn = VW::make_unique<dnn_learner>();
-  dnn->init();
+  dnn->init(
+    num_layers,
+    hidden_layer_size,
+    num_inputs,
+    all.sd->contraction,
+    mini_batch_size
+  );
 
   auto l = make_bottom_learner(
     std::move(dnn),learn, predict,
     stack_builder.get_setupfn_name(dnn_setup),
     prediction_type_t::SCALAR,label_type_t::SIMPLE)
+     .set_cleanup_example(cleanup_example)
      .set_learn_returns_prediction(true)
-     .set_cleanup_example(nullptr)
+     .set_update_stats(update_stats)
+     .set_output_example_prediction(VW::details::output_example_prediction_simple_label<dnn_learner>)
+     .set_print_update(VW::details::print_update_simple_label<dnn_learner>)
      .build();
-     // TODO: how do deal with multiple learners?
-     // TODO: add the following functions
-     // .set_multipredict(nullptr)
-     // .set_update(nullptr)
-     // .set_save_load(nullptr)
-     // .set_end_pass(nullptr)
-     // .set_merge_with_all(nullptr)
-     // .set_add_with_all(nullptr)
-     // .set_subtract_with_all(nullptr)
-     // .set_output_example_prediction(nullptr)
-     // .set_update_stats(nullptr)
-     // .set_print_update(nullptr)
   return l;
 }
 }  // namespace reductions
-}  // namespace VWW::reductions::dnn_setup(VW::setup_base_i& stack_builder)
+}  // namespace VW
