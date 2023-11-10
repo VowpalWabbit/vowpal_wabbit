de58ab72c6b93c61ad3debb05f8c4297686cf031
Griffin Bassman <griffinbassman@gmail.com>
2023-11-10 14:36:28 -0500

    check sqinv

--- vowpalwabbit/core/src/reductions/gd.cc
+++ vowpalwabbit/core/src/reductions/gd.cc
@@ -155,7 +155,33 @@ VW_WARNING_STATE_POP
 
 static inline float inv_sqrt(float x)
 {
+#if !defined(VW_NO_INLINE_SIMD)
+#  if defined(__ARM_NEON__)
+  // Propagate into vector
+  float32x2_t v1 = vdup_n_f32(x);
+  // Estimate
+  float32x2_t e1 = vrsqrte_f32(v1);
+  // N-R iteration 1
+  float32x2_t e2 = vmul_f32(e1, vrsqrts_f32(v1, vmul_f32(e1, e1)));
+  // N-R iteration 2
+  float32x2_t e3 = vmul_f32(e2, vrsqrts_f32(v1, vmul_f32(e2, e2)));
+  // Extract result
+  std::cout << "__ARM_NEON__" << "\n";
+  return vget_lane_f32(e3, 0);
+#  elif defined(__SSE2__)
+  __m128 eta = _mm_load_ss(&x);
+  eta = _mm_rsqrt_ss(eta);
+  _mm_store_ss(&x, eta);
+  std::cout << "__SSE2__" << "\n";
+#  else
+  std::cout << "None" << "\n";
   x = quake_inv_sqrt(x);
+#  endif
+#else
+  std::cout << "VW_NO_INLINE_SIMD" << "\n";
+  x = quake_inv_sqrt(x);
+#endif
+
   return x;
 }
 
