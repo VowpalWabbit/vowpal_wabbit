acb36e3447efd666b2adaa9bc85cbfd19cac3148
Jack Gerrits <jackgerrits95@gmail.com>
2023-12-08 16:01:33 -0500

    ensure empty predictions do not affect num_labeled as well as loss

--- vowpalwabbit/core/src/reductions/conditional_contextual_bandit.cc
+++ vowpalwabbit/core/src/reductions/conditional_contextual_bandit.cc
@@ -549,13 +549,13 @@ void update_stats_ccb(const VW::workspace& /* all */, shared_data& sd, const ccb
       auto* outcome = data.slots[i]->l.conditional_contextual_bandit.outcome;
       if (outcome != nullptr)
       {
+        // It is possible for the prediction to be empty if there were no actions available at the time of taking the
+        // slot decision. In this case it does not contribute to loss.
+        if (preds[i].empty()) { continue; }
+
         num_labeled++;
         if (i == 0 || data.all_slots_loss_report)
         {
-          // It is possible for the prediction to be empty if there were no actions available at the time of taking the
-          // slot decision. In this case it does not contribute to loss.
-          if (preds[i].empty()) { continue; }
-
           const float l = VW::get_cost_estimate(outcome->probabilities[VW::details::TOP_ACTION_INDEX], outcome->cost,
               preds[i][VW::details::TOP_ACTION_INDEX].action);
           loss += l * preds[i][VW::details::TOP_ACTION_INDEX].score * ec_seq[VW::details::SHARED_EX_INDEX]->weight;
