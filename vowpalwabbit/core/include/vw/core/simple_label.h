92e959ae5a206056aed64d53f583238f5e69dd23
JohnLangford <jl@hunch.net>
2026-02-18 14:12:40 -0500

    fix: restore print_update used by output_and_account_example
    The function is called at simple_label.cc:42 inside
    output_and_account_example, which is used by return_simple_example
    and a coverage test. Replace the stale TODO with a clarifying comment.

--- vowpalwabbit/core/include/vw/core/simple_label.h
+++ vowpalwabbit/core/include/vw/core/simple_label.h
@@ -43,6 +43,7 @@ namespace details
 {
 void return_simple_example(VW::workspace& all, void*, VW::example& ec);
 bool summarize_holdout_set(VW::workspace& all, size_t& no_win_counter);
+void print_update(VW::workspace& all, const VW::example& ec);
 void output_and_account_example(VW::workspace& all, const VW::example& ec);
 void update_stats_simple_label(
     const VW::workspace& all, shared_data& sd, const VW::example& ec, VW::io::logger& logger);
